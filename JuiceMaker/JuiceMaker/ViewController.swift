a02ced51ba21b60f0d1de91eeb4e0678480dbb2e
yagom <yagom@yagom.net>
2020-11-14 00:16:21 +0900

    add Xcode project

---
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  JuiceMaker - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
