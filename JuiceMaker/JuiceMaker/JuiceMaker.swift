ddb7a92db0c6a7f5d3b78f198e782b48614d010a
taetae <taetae.dev@gmail.com>
2020-11-17 19:16:50 +0900

    Add: StockManager

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -6,4 +6,84 @@
 
 import Foundation
 
-/// 쥬스 메이커 타입 
+class StockManager {
+    
+    var strawberry:Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
+    
+    var strawberryStock: Int {
+        get {
+            return strawberry
+        }
+    }
+    
+    var bananaStock: Int {
+        get {
+            return banana
+        }
+    }
+    
+    var pineappleStock: Int {
+        get {
+            return pineapple
+        }
+    }
+    
+    var kiwiStock: Int {
+        get {
+            return kiwi
+        }
+    }
+    
+    var mangoStock: Int {
+        get {
+            return mango
+        }
+    }
+    func addStrawberryStock() {
+        strawberry += 1
+    }
+    
+    func substractStrawberryStock() {
+        strawberry -= 1
+    }
+    
+    func addBananaStock() {
+        banana += 1
+    }
+    
+    func substractBananaStock() {
+        banana -= 1
+    }
+    
+    func addPineappleStock() {
+        pineapple += 1
+    }
+    
+    func substractPineappleStock() {
+        pineapple -= 1
+    }
+    
+    func plusMangoStock() {
+        mango += 1
+    }
+    
+    func substractMangoStock() {
+        mango -= 1
+    }
+    
+    func addKiwiStock() {
+        kiwi += 1
+    }
+    
+    func substractkiwiStock() {
+        kiwi -= 1
+    }
+    
+    func showFruitStock() {
+        print("딸기:\(strawberry) 바나나:\(banana) 파인애플:\(pineapple) 망고:\(mango) 키위:\(kiwi)")
+    }
+}
