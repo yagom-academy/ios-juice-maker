e3e73a9947db22d7a962c6e0ac432adb57870be5
innieminnie <rkddlsl1001@gmail.com>
2020-11-17 15:57:58 +0900

    feat: JuiceMenu enum타입 생성

---
+++ JuiceMaker/JuiceMaker/JuiceMenu.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceMenu.swift
+//  JuiceMaker
+//
+//  Created by 강인희 on 2020/11/16.
+//
+
+import Foundation
+
+enum JuiceMenu {
+    case 딸바쥬스
+    case 망고쥬스
+    case 망고키위쥬스
+    case 키위쥬스
+    case 파인애플쥬스
+}
+
