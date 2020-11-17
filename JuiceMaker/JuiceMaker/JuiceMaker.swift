35e1537967b8f6b9f138a96c46e4192c2523077f
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-11-17 20:39:35 +0900

    JuiceMaker타입 정의 - 1

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -1,9 +1,108 @@
 //
 //  JuiceMaker - JuiceMaker.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
-/// 쥬스 메이커 타입 
+/// 쥬스 메이커 타입
+
+//과일 종류
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwii
+    case mango
+}
+
+//주스 종류
+enum Juice {
+    case kiwiiJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiiJuice
+}
+
+//과일 수량
+struct FruitStock {
+    fileprivate var strawberry: Int
+    fileprivate var banana: Int
+    fileprivate var pineapple: Int
+    fileprivate var kiwii: Int
+    fileprivate var mango: Int
+    
+    var strawberryStock: Int {
+        get {
+            return strawberry
+        }
+    }
+    var bananaStock: Int {
+        get {
+            return banana
+        }
+    }
+    var pineappleStock: Int {
+        get {
+            return pineapple
+        }
+    }
+    var kiwiiStock: Int {
+        get {
+            return kiwii
+        }
+    }
+    var mangoStock: Int {
+        get {
+            return mango
+        }
+    }
+    
+    //과일 재고 추가
+    mutating func stockChanged(fruit: Fruit, stock: Int) {
+        switch fruit {
+        case .strawberry:
+            strawberry = stock
+        case .banana:
+            banana = stock
+        case .kiwii:
+            kiwii = stock
+        case .mango:
+            mango = stock
+        case .pineapple:
+            pineapple = stock
+        }
+    }
+}
+
+//주스 만들기
+class JuiceMaker {
+    var fruitStock: FruitStock
+    
+    init() {
+        fruitStock = FruitStock(strawberry: 0, banana: 0, pineapple: 0, kiwii: 0, mango: 0)
+    }
+    
+    func makeJuice(juice: Juice) {
+        switch juice {
+        case .kiwiiJuice:
+            self.fruitStock.kiwii -= 3
+            
+        case .mangoJuice:
+            self.fruitStock.mango -= 3
+            
+        case .mangoKiwiiJuice:
+            self.fruitStock.mango -= 2
+            self.fruitStock.kiwii -= 1
+            
+        case .pineappleJuice:
+            self.fruitStock.pineapple -= 2
+            
+        case .strawberryBananaJuice:
+            self.fruitStock.strawberry -= 10
+            self.fruitStock.banana -= 1
+        }
+    }
+}
