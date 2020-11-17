20262fb4a8990978a8701dae34d9cf0787df92cf
innieminnie <rkddlsl1001@gmail.com>
2020-11-17 16:03:21 +0900

    feat: JuiceMaker의 초기화 함수 구현

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -7,3 +7,43 @@
 import Foundation
 
 /// 쥬스 메이커 타입 
+struct JuiceMaker {
+    private var fruitStockList: [Fruit] = []
+    private var menuList: [JuiceMenu] = []
+    
+    private var strawberry = Fruit(fruitName: "strawberry", currentStock: 10)
+    private var banana = Fruit(fruitName: "banana", currentStock: 10)
+    private var pineapple = Fruit(fruitName: "pineapple", currentStock: 10)
+    private var kiwi = Fruit(fruitName: "kiwi", currentStock: 10)
+    private var mango = Fruit(fruitName: "mango", currentStock: 10)
+    
+    init() {
+        self.fruitStockList = initializeFruitStockList()
+        self.menuList = initializeMenuList()
+    }
+    
+    private func initializeFruitStockList() -> [Fruit] {
+        var initialFruitStockList: [Fruit] = []
+        
+        initialFruitStockList.append(strawberry)
+        initialFruitStockList.append(banana)
+        initialFruitStockList.append(pineapple)
+        initialFruitStockList.append(kiwi)
+        initialFruitStockList.append(mango)
+        
+        return initialFruitStockList
+    }
+    
+    private func initializeMenuList() -> [JuiceMenu] {
+        var initialMenuList = [JuiceMenu]()
+
+        initialMenuList.append(.딸바쥬스)
+        initialMenuList.append(.망고쥬스)
+        initialMenuList.append(.망고키위쥬스)
+        initialMenuList.append(.키위쥬스)
+        initialMenuList.append(.파인애플쥬스)
+
+        return initialMenuList
+    }
+}
+
