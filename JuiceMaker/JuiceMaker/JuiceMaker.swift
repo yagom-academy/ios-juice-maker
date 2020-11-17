368f0017684dbf61d09972acf39b9eaa942284f5
SM <hcooch2ch3@gmail.com>
2020-11-18 03:11:52 +0900

    Step1 변수이용해서 구현

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -6,4 +6,36 @@
 
 import Foundation
 
-/// 쥬스 메이커 타입 
+struct JuiceMaker {
+    
+    private var strawberry: UInt = 0
+    private var banana: UInt = 0
+    private var pineapple: UInt = 0
+    private var kiwi: UInt = 0
+    private var mango: UInt = 0
+    
+    var strawberryCount: UInt {
+        get { return strawberry }
+    }
+    var bananaCount: UInt {
+        get { return banana }
+    }
+    var pineappleCount: UInt {
+        get { return pineapple }
+    }
+    var kiwiCount: UInt {
+        get { return kiwi }
+    }
+    var mangoCount: UInt {
+        get { return mango }
+    }
+    
+    init(strawberry: UInt = 10, banana: UInt = 10, pineapple: UInt = 10, kiwi: UInt = 10, mango: UInt = 10) {
+        self.strawberry = strawberry
+        self.banana = banana
+        self.pineapple = pineapple
+        self.kiwi = kiwi
+        self.mango = mango
+    }
+    
+}
--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -7,10 +7,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var juiceMaker = JuiceMaker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        juiceMaker.test()
     }
 
 
