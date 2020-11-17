387830b4bf6752a69b97f3c4c90ffe6d9d8ab435
odong-Tree <ss12357@naver.com>
2020-11-17 17:57:32 +0900

    1. add properties

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -7,3 +7,28 @@
 import Foundation
 
 /// 쥬스 메이커 타입 
+class JuiceMaker {
+    // 과일 재고
+    var strawberryStock: Int {
+        return strawberry
+    }
+    var bananaStock: Int {
+        return banana
+    }
+    var pineappleStock: Int {
+        return pineapple
+    }
+    var kiwiStock: Int {
+        return kiwi
+    }
+    var mangoStock: Int {
+        return mango
+    }
+    
+    var strawberry = 10
+    var banana = 10
+    var pineapple = 10
+    var kiwi = 10
+    var mango = 10
+    
+}
