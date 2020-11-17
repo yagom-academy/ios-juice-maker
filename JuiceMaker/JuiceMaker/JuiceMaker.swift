41288e2dd9a432b6500529eb9847eb683835bb7e
odong-Tree <ss12357@naver.com>
2020-11-17 21:53:08 +0900

    4. add FruitStock struct

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -8,47 +8,71 @@ import Foundation
 
 /// 쥬스 메이커 타입 
 class JuiceMaker {
+    var fruitStock = FruitStock()
+    
     // 과일 재고
     var strawberryStock: Int {
-        return strawberry
+        return fruitStock.strawberry
     }
     var bananaStock: Int {
-        return banana
+        return fruitStock.banana
     }
     var pineappleStock: Int {
-        return pineapple
+        return fruitStock.pineapple
     }
     var kiwiStock: Int {
-        return kiwi
+        return fruitStock.kiwi
     }
     var mangoStock: Int {
-        return mango
+        return fruitStock.mango
     }
-    
+}
+
+// 과일쥬스 제조
+extension JuiceMaker {
+    func makeKiwiJuice() {
+        fruitStock.kiwi -= 3
+    }
+    func makePineappleJuice() {
+        fruitStock.pineapple -= 2
+    }
+    func makeStrawberryAndBananaJuice() {
+        fruitStock.strawberry -= 10
+        fruitStock.banana -= 1
+    }
+    func makeMangoJuice() {
+        fruitStock.mango -= 3
+    }
+    func makeMangoAndKiwiJuice() {
+        fruitStock.mango -= 2
+        fruitStock.kiwi -= 1
+    }
+}
+
+
+//enum FruitStock {
+//    case strawberry
+//    case banana
+//    case pineapple
+//    case kiwi
+//    case mango
+//}
+//
+//enum MakeJuice {
+//    case kiwiJuice
+//    case strawberryJuice
+//    case mangoJuice
+//    case strawberryAndBananaJuice
+//    case mangoAndKiwiJuice
+//}
+
+struct FruitStock {
+    // 과일 재고
     var strawberry = 10
     var banana = 10
     var pineapple = 10
     var kiwi = 10
     var mango = 10
-    
-    // 과일쥬스 제조
-    func makeKiwiJuice() {
-        kiwi -= 3
-    }
-    func makePineappleJuice() {
-        pineapple -= 2
-    }
-    func makeStrawberryAndBananaJuice() {
-        strawberry -= 10
-        banana -= 1
-    }
-    func makeMangoJuice() {
-        mango -= 3
-    }
-    func makeMangoAndKiwiJuice() {
-        mango -= 2
-        kiwi -= 1
-    }
     
     // 과일 재고 추가/제거
     func addOneFruit(_ fruit: inout Int) {
