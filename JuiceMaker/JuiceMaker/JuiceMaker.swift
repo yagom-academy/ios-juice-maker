cbaf614a9bbe3c6d0c704e92b6627e7da98dc9b1
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-11-19 06:03:47 +0900

    Feat : 쥬스메이커 타입 정의
    1. struct로 쥬스메이커 타입 정의
    2. 과일 종류를 enum 타입으로, 과일 재고 변수를 dictionary 타입으로 정의
    	- 과일 종류 수정 대비
    	- 재고 관리 코드 간결하게
    3. 쥬스 종류를 enum 타입으로, 상세 레시피 변수를 dictionary 타입으로 정의
    	- 쥬스 레시피 수정 대비
    	- 쥬스 주문 관련 코드 간결하게
    4. 쥬스 제조
    	- 하나의 메서드에서 선택한 쥬스 종류에 따라 쥬스 제조
    	- 에러 핸들링 적용

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -6,4 +6,96 @@
 
 import Foundation
 
-/// 쥬스 메이커 타입 
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
+
+enum Juice {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
+}
+
+enum JuiceMakerError: Error {
+    case lowStock
+    case noRecipe
+    case noStockList
+}
+
+struct JuiceMaker {
+        
+    private var fruitStocks: [Fruit: Int] = [
+        .strawberry:0,
+        .banana:0,
+        .pineapple:0,
+        .kiwi:0,
+        .mango:0
+    ]
+    
+    private var juiceRecipes: [Juice:[Fruit: Int]] = [
+        .strawberryJuice:[.strawberry:16],
+        .bananaJuice:[.banana:2],
+        .kiwiJuice:[.kiwi:3],
+        .pineappleJuice:[.pineapple:2],
+        .strawberryBananaJuice:[.strawberry:10, .banana:1],
+        .mangoJuice:[.mango:3],
+        .mangoKiwiJuice:[.mango:2, .kiwi:1]
+    ]
+    
+    func countStock(of fruit: Fruit) -> Int? {
+        return fruitStocks[fruit]
+    }
+    
+    mutating func setStock(of fruit: Fruit, count: Int) {
+        fruitStocks[fruit] = count
+    }
+    
+    mutating func addStock(of fruit: Fruit, count: Int) {
+        guard let currentCount = countStock(of: fruit) else {
+            print("\(fruit)가 재고 목록에 없음")
+            return
+        }
+        
+        fruitStocks[fruit] = currentCount + count
+    }
+    
+    mutating func subtractStock(of fruit: Fruit, count: Int) {
+        guard let currentCount = countStock(of: fruit) else {
+            print("\(fruit)가 재고 목록에 없음")
+            return
+        }
+   
+        fruitStocks[fruit] = currentCount - count
+    }
+    
+    mutating func make(juice: Juice) throws {
+        // 레시피 가져오기
+        guard let recipe = juiceRecipes[juice] else {
+            throw JuiceMakerError.noRecipe
+        }
+        
+        // 레시피에 필요한 과일의 재고 있는지 확인
+        for (fruit, count) in recipe {
+            guard let currentCount = countStock(of: fruit) else {
+                throw JuiceMakerError.noStockList
+            }
+            
+            guard currentCount >= count else {
+                throw JuiceMakerError.lowStock
+            }
+        }
+        
+        // 필요한 과일의 재고가 있으면 재고 소모
+        for (fruit, count) in recipe {
+            subtractStock(of: fruit, count: count)
+        }
+    }
+}
