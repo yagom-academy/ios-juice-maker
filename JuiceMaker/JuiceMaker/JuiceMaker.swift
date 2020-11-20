71d99e32a49248007312d98690201d8c0918d1ad
SM <hcooch2ch3@gmail.com>
2020-11-20 21:20:39 +0900

    [#2] Step1 수정

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -6,196 +6,116 @@
 
 import Foundation
 
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
+
+enum Juice {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case strawberrybanana
+    case mango
+    case mangokiwi
+}
+
+enum FruitManagerError: Error {
+    case notFoundFruit
+    case impossibleToSubtract
+}
+
 enum JuiceMakerError: Error {
-    case invalidInput(message: String)
+    case notFoundJuiceRecipe
+    case wrongRecipe
+    case insufficientFruit
+}
+
+struct FruitManager {
+    private(set) var fruitStore: [Fruit: UInt]
+    
+    init(fruitsStore: [Fruit: UInt]) {
+        self.fruitStore = fruitsStore
+    }
+    
+    /**
+    과일의 개수를 증가시키는 함수
+    
+     - Parameters:
+         - fruit: 증가 시킬 과일 종류
+         - count: 증가 시킬 과일 개수
+     - Throws: 입력된 과일이 저장소에 존재하지 않을 경우 'FruitManagerError.notFoundFruit'
+    */
+    mutating func add(fruit: Fruit, count: UInt) throws {
+        guard let currentCount = self.fruitStore[fruit] else {
+            throw FruitManagerError.notFoundFruit
+        }
+        
+        self.fruitStore[fruit] = currentCount + count
+    }
+    
+    /**
+    과일의 개수를 감소시키는 함수
+    
+     - Parameters:
+         - fruit: 감소 시킬 과일 종류
+         - count: 감소 시킬 과일 개수
+     - Throws:
+        - 입력된 과일이 저장소에 존재하지 않을 경우 'FruitManagerError.notFoundFruit'
+        - 현재 과일 개수보다 많은 개수가 입력으로 들어올 경우 'FruitManagerError.impossibleToSubtract'
+    */
+    mutating func subtract(fruit: Fruit, count: UInt) throws {
+        guard let currentCount = self.fruitStore[fruit] else {
+            throw FruitManagerError.notFoundFruit
+        }
+        
+        guard count <= currentCount else {
+            throw FruitManagerError.impossibleToSubtract
+        }
+        
+        self.fruitStore[fruit] = currentCount - count
+    }
 }
 
 struct JuiceMaker {
-    private var strawberry: UInt = 0
-    private var banana: UInt = 0
-    private var pineapple: UInt = 0
-    private var kiwi: UInt = 0
-    private var mango: UInt = 0
+    var fruitManager = FruitManager(fruitsStore: [Fruit.strawberry: 10, Fruit.banana: 10, Fruit.pineapple: 10, Fruit.kiwi: 10, Fruit.mango: 10])
     
-    var strawberryCount: UInt {
-        get { return self.strawberry }
-    }
-    var bananaCount: UInt {
-        get { return self.banana }
-    }
-    var pineappleCount: UInt {
-        get { return self.pineapple }
-    }
-    var kiwiCount: UInt {
-        get { return self.kiwi }
-    }
-    var mangoCount: UInt {
-        get { return self.mango }
-    }
+    let recipes: [Juice: [Fruit: UInt]] = [Juice.strawberry: [Fruit.strawberry: 16], Juice.banana: [Fruit.banana: 2], Juice.kiwi: [Fruit.kiwi: 3], Juice.pineapple: [Fruit.pineapple: 2], Juice.strawberrybanana: [Fruit.strawberry: 10, Fruit.banana: 1], Juice.mangokiwi: [Fruit.mango: 2, Fruit.kiwi: 1]]
     
-    init(strawberry: UInt = 10, banana: UInt = 10, pineapple: UInt = 10, kiwi: UInt = 10, mango: UInt = 10) {
-        self.strawberry = strawberry
-        self.banana = banana
-        self.pineapple = pineapple
-        self.kiwi = kiwi
-        self.mango = mango
-    }
+    /**
+    과일 쥬스를 제조하는 함수 (입력된 쥬스의 레시피대로 재고를 감소시키는 함수)
     
-    /// 원하는 쥬스 이름을 입력하면 쥬스를 만드는 함수
-    ///
-    /// - Parameter name: 쥬스 이름
-    /// - Throws: 잘못된 쥬스 이름 전달 되면 'JuiceMakerError.invalidInput(message)'
-    /// - Returns:쥬스를 만들었으면 true 아니면 false 반환
-    mutating func makeJuice(name: String) throws -> Bool {
-        switch name {
-        case "딸기쥬스":
-            if self.strawberry >= 16 {
-                self.strawberry -= 16
-                return true
-            } else {
-                return false
-            }
-        case "바나나쥬스":
-            if self.banana >= 2 {
-                self.banana -= 2
-                return true
-            } else {
-                return false
-            }
-        case "키위쥬스":
-            if self.kiwi >= 3 {
-                self.kiwi -= 3
-                return true
-            } else {
-                return false
-            }
-        case "파인애플쥬스":
-            if self.pineapple >= 2 {
-                self.pineapple -= 2
-                return true
-            } else {
-                return false
-            }
-        case "딸바쥬스":
-            if self.strawberry >= 10 && self.banana >= 1 {
-                self.strawberry -= 10
-                self.banana -= 1
-                return true
-            } else {
-                return false
-            }
-        case "망고쥬스":
-            if self.mango >= 3 {
-                self.mango -= 3
-                return true
-            } else {
-                return false
-            }
-        case "망고키위쥬스":
-            if self.mango >= 2 && self.kiwi >= 1 {
-                self.mango -= 2
-                self.kiwi -= 1
-                return true
-            } else {
-                return false
-            }
-        default:
-            throw JuiceMakerError.invalidInput(message: "잘못된 입력입니다. 존재하지 않는 쥬스 입니다.")
-        }
-    }
-    
-    /// 딸기 재고 개수를 입력값만큼 더함.
-    ///
-    /// - Parameter count: 더할 딸기 재고 개수
-    mutating func addStrawberry(count: UInt) {
-        self.strawberry += count
-    }
-    
-    /// 딸기 재고 개수를 입력값만큼 뺌.
-    ///
-    /// - Parameter count: 뺄 딸기 재고 개수
-    /// - Throws: 잘못된 개수(현재 개수보다 많은 개수)가 전달되면  'JuiceMakerError.invalidInput(message)'
-    mutating func subtractStrawberry(count: UInt) throws {
-        guard self.strawberry < count else {
-            throw JuiceMakerError.invalidInput(message: "잘못된 입력입니다. 현재 개수보다 많은 개수는 뺄 수 없습니다.")
+     - Parameter juice: 제조할 쥬스 종류
+     - Throws:
+        - 입력된 쥬스의 레시피가 존재하지 않을 경우 'JuiceMakerError.notFoundJuiceRecipe'
+        - 쥬스의 레시피에 포함된 과일이 저장소에 존재하지 않을 경우(레시피에 잘못된 과일이 포함) 'JuiceMakerError.wrongRecipe'
+        - 과일 재고가 모자랄 경우 'JuiceMakerError.insufficientFruit'
+    */
+    mutating func make(juice: Juice) throws {
+        guard let recipe = recipes[juice] else {
+            throw JuiceMakerError.notFoundJuiceRecipe
         }
         
-        self.strawberry -= count
-    }
-    
-    /// 바나나 재고 개수를 입력값만큼 더함.
-    ///
-    /// - Parameter count: 더할 바나나 재고 개수
-    mutating func addBanana(count: UInt) {
-        self.banana += count
-    }
-    
-    /// 바나나 재고 개수를 입력값만큼 뺌.
-    ///
-    /// - Parameter count: 뺄 바나나 재고 개수
-    /// - Throws: 잘못된 개수(현재 개수보다 많은 개수)가 전달되면  'JuiceMakerError.invalidInput(message)'
-    mutating func subtractBanana(count: UInt) throws {
-        guard self.banana < count else {
-            throw JuiceMakerError.invalidInput(message: "잘못된 입력입니다. 현재 개수보다 많은 개수는 뺄 수 없습니다.")
+        for (fruit, requiredCount) in recipe {
+            guard let currentCount = fruitManager.fruitStore[fruit] else {
+                throw JuiceMakerError.wrongRecipe
+            }
+            
+            if currentCount < requiredCount {
+                throw JuiceMakerError.insufficientFruit
+            }
         }
         
-        self.banana -= count
-    }
-    
-    /// 파인애플 재고 개수를 입력값만큼 더함.
-    ///
-    /// - Parameter count: 더할 파인애플 재고 개수
-    mutating func addPineapple(count: UInt) {
-        self.pineapple += count
-    }
-    
-    /// 파인애플 재고 개수를 입력값만큼 뺌.
-    ///
-    /// - Parameter count: 뺄 파인애플 재고 개수
-    /// - Throws: 잘못된 개수(현재 개수보다 많은 개수)가 전달되면  'JuiceMakerError.invalidInput(message)'
-    mutating func subtractPineapple(count: UInt) throws {
-        guard self.pineapple < count else {
-            throw JuiceMakerError.invalidInput(message: "잘못된 입력입니다. 현재 개수보다 많은 개수는 뺄 수 없습니다.")
+        for (fruit, requiredCount) in recipe {
+            do {
+                try self.fruitManager.subtract(fruit: fruit, count: requiredCount)
+            } catch {
+                print("\(fruit) 재고를 빼는 과정에서 문제가 발생했습니다.")
+            }
         }
-        
-        self.pineapple -= count
-    }
-    
-    /// 키위 재고 개수를 입력값만큼 더함.
-    ///
-    /// - Parameter count: 더할 키위 재고 개수
-    mutating func addKiwi(count: UInt) {
-        self.kiwi += count
-    }
-    
-    /// 키위 재고 개수를 입력값만큼 뺌.
-    ///
-    /// - Parameter count: 뺄 키위 재고 개수
-    /// - Throws: 잘못된 개수(현재 개수보다 많은 개수)가 전달되면  'JuiceMakerError.invalidInput(message)'
-    mutating func subtractKiwi(count: UInt) throws {
-        guard self.kiwi < count else {
-            throw JuiceMakerError.invalidInput(message: "잘못된 입력입니다. 현재 개수보다 많은 개수는 뺄 수 없습니다.")
-        }
-        
-        self.kiwi -= count
-    }
-    
-    /// 망고 재고 개수를 입력값만큼 더함.
-    ///
-    /// - Parameter count: 더할 망고 재고 개수
-    mutating func addMange(count: UInt) {
-        self.mango += count
-    }
-    
-    /// 망고 재고 개수를 입력값만큼 뺌.
-    ///
-    /// - Parameter count: 뺄 망고 재고 개수
-    /// - Throws: 잘못된 개수(현재 개수보다 많은 개수)가 전달되면  'JuiceMakerError.invalidInput(message)'
-    mutating func subtractMango(count: UInt) throws {
-        guard self.mango < count else {
-            throw JuiceMakerError.invalidInput(message: "잘못된 입력입니다. 현재 개수보다 많은 개수는 뺄 수 없습니다.")
-        }
-        
-        self.mango -= count
     }
 }
