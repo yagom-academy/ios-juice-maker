1986eeaf899818466dae021acfce6304cd3c7bf0
wonhee <wonhee010@gmail.com>
2020-11-20 15:18:51 +0900

    add ManageStockVC

---
+++ JuiceMaker/JuiceMaker/View/ManageStockViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ManageStockViewController.swift
+//  JuiceMaker
+//
+//  Created by Wonhee on 2020/11/20.
+//
+
+import UIKit
+
+class ManageStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
