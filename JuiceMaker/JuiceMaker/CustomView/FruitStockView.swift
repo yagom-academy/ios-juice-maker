b90a74903f461fc25666c1d257f00da3b244a835
wonhee <wonhee010@gmail.com>
2020-11-20 15:30:55 +0900

    add FruitStockView

---
+++ JuiceMaker/JuiceMaker/CustomView/FruitStockView.swift
@@ -0,0 +1,27 @@
+//
+//  FruitStockView.swift
+//  JuiceMaker
+//
+//  Created by Wonhee on 2020/11/20.
+//
+
+import Foundation
+import UIKit
+
+class FruitStockView : UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        let view = Bundle.main.loadNibNamed("FruitStockView", owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+}
