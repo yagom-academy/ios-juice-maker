ce0a383aad172cb3bfcd2efaf8e6d2446674b6ed
yeon <jryoun1@naver.com>
2020-11-22 23:46:59 +0900

    fix(ViewController.swif 0.9 / jake) : 쥬스메이커 타입 수정으로 인한 변경 및 재고 수정으로의 화면 전환 구현

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -10,7 +10,7 @@ class ViewController: UIViewController {
     var juiceMaker: JuiceMaker = JuiceMaker()
     private let warning: String = "경고"
     private let complete: String = "완료"
-    @IBOutlet weak var changeStock: UIButton!
+    @IBOutlet weak var changeStockButton: UIButton!
     @IBOutlet weak var strawberryStockLabel: UILabel!
     @IBOutlet weak var bananaStockLabel: UILabel!
     @IBOutlet weak var pineappleStockLabel: UILabel!
@@ -19,16 +19,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //todo -> delegate pattern을 사용해서 재고를 알아서 추적하도록 해보자!
+        //todo -> 화면전환할때 값을 같이 보내는 방법을 찾아서 step3를 진행할것
         showFruitsStock()
     }
     
     func showFruitsStock() {
-        let strawberryStock: Int = juiceMaker.strawberry.stock
-        let bananaStock: Int = juiceMaker.banana.stock
-        let pineappleStock: Int = juiceMaker.pineapple.stock
-        let kiwiStock: Int = juiceMaker.kiwi.stock
-        let mangoStock: Int = juiceMaker.mango.stock
+        let strawberryStock: Int = juiceMaker.stockManager.strawberry.stock
+        let bananaStock: Int = juiceMaker.stockManager.banana.stock
+        let pineappleStock: Int = juiceMaker.stockManager.pineapple.stock
+        let kiwiStock: Int = juiceMaker.stockManager.kiwi.stock
+        let mangoStock: Int = juiceMaker.stockManager.mango.stock
         
         strawberryStockLabel.text = String(strawberryStock)
         bananaStockLabel.text = String(bananaStock)
@@ -36,7 +36,7 @@ class ViewController: UIViewController {
         kiwiStockLabel.text = String(kiwiStock)
         mangoStockLabel.text = String(mangoStock)
     }
-    
+        
     @IBAction func touchUpStrawberryBananaJuiceButton(_ sender: UIButton) {
         let strawberryBananaJuice: String = "딸기바나나쥬스"
         guard let menu = FruitsJuice(rawValue: strawberryBananaJuice) else { return }
@@ -128,12 +128,26 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func touchUpChangeStockButton(_ sender: UIButton) {
+        self.moveToStockManageVC()
+    }
+    
+    func moveToStockManageVC() {
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "StockManageVC") else {
+            return
+        }
+        vc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+        present(vc, animated: true, completion: nil)
+    }
+    
     func showAlert(type: String, juiceName: String? = nil) {
         switch type {
         case warning:
             let warningMessage: String = "재료가 모자라요! 재고를 수정할까요?"
             let alert = UIAlertController(title: nil, message: warningMessage, preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "예", style: .default)
+            let okAction = UIAlertAction(title: "예", style: .default) { ACTION in
+                self.moveToStockManageVC()
+            }
             let noAction = UIAlertAction(title: "아니오", style: .cancel)
             
             alert.addAction(okAction)
