7b757d47c1f5073003507d0e8f2809199463b605
iluxsm <be9.smile@gmail.com>
2020-11-23 16:04:27 +0900

    fix[#2]: Step1 - 코멘트 기반 수정

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -4,38 +4,49 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import UIKit
+import Foundation
 
-enum FruitsName {
+enum Fruits {
     case strawberry, banana, pineapple, kiwi, mango
 }
 
 struct FruitsLabel {
-    let fruitsArray: [String] = ["strawberry", "banana", "pineapple", "kiwi", "mango"]
+    let fruits: [String] = ["strawberry", "banana", "pineapple", "kiwi", "mango"]
 }
 
 class FruitStock: NSObject {
-    @objc fileprivate(set) var strawberry: Int = 10
-    @objc fileprivate(set) var banana: Int = 10
-    @objc fileprivate(set) var pineapple: Int = 10
-    @objc fileprivate(set) var kiwi: Int = 10
-    @objc fileprivate(set) var mango: Int = 10
+    @objc fileprivate(set) var strawberry: Int
+    @objc fileprivate(set) var banana: Int
+    @objc fileprivate(set) var pineapple: Int
+    @objc fileprivate(set) var kiwi: Int
+    @objc fileprivate(set) var mango: Int
     
-    func addStocks(fruitsName: FruitsName, amount: Int) {
-        for fruit in FruitsLabel().fruitsArray {
-            if fruit == "\(fruitsName)" {
-                if let addFruit = value(forKey: fruit) as? Int {
-                    setValue(addFruit + amount, forKey: fruit)
-                } else {
-                    print("존재하지 않는 과일입니다. 알림 구현")
-                }
+    let instanceForSuperInit = NSObject()
+    
+    init(strawberry: Int, banana: Int, pineapple: Int, kiwi: Int, mango: Int) {
+        self.strawberry = strawberry
+        self.banana = banana
+        self.pineapple = pineapple
+        self.kiwi = kiwi
+        self.mango = mango
+    }
+    
+    
+    func addStocks(fruitsName: Fruits, amount: Int) {
+        let fruit = "\(fruitsName)"
+        if FruitsLabel().fruits.contains(fruit) {
+            if let addFruit = value(forKey: fruit) as? Int {
+                setValue(addFruit + amount, forKey: fruit)
+            } else {
+                print("존재하지 않는 과일입니다. 알림 구현")
             }
         }
     }
 }
 
 class JuiceMaker: FruitStock {
-    func makeOriginalJuice(material fruit: FruitsName) {
+    
+    func makeOriginalJuice(material fruit: Fruits) {
         switch fruit {
         case .strawberry:
             strawberry -= 16
@@ -50,7 +61,7 @@ class JuiceMaker: FruitStock {
         }
     }
     
-    func makeMixedJuice(fruit1: FruitsName, fruit2: FruitsName) {
+    func makeMixedJuice(fruit1: Fruits, fruit2: Fruits) {
         switch (fruit1, fruit2) {
         case (.strawberry, .banana), (.banana, .strawberry):
             strawberry -= 10
