a587076b905aaf6f1053e070a0b51d41cc510644
zziro95 <opers7878@gmail.com>
2020-11-24 16:57:20 +0900

    fix(JuiceMaker.swift 0.4 / zziro) : 함수명, 변수명, init 수정

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -7,59 +7,64 @@
 import Foundation
 
 class JuiceMaker {
-    let initialStock: Int
-    var fruitDictionary: [Fruit: Int]
+    private let sameInitialStock: UInt?
+    private var fruitManagement: [Fruit: UInt]
     
-    init(initialStock: Int) {
-        self.initialStock = initialStock
-        self.fruitDictionary = [.strawberry: initialStock, .banana: initialStock, .pineapple: initialStock, .kiwi: initialStock, .mango: initialStock]
+    init(sameInitialStock: UInt?, strawberryStock: UInt?, bananaStock: UInt?, pineappleStock: UInt?, kiwiStock: UInt?, mangoStock: UInt?) {
+        guard sameInitialStock != nil else {
+            self.sameInitialStock = nil
+            self.fruitManagement = [.strawberry: strawberryStock!, .banana: bananaStock!, .pineapple: pineappleStock!, .kiwi: kiwiStock!, .mango: mangoStock!]
+            return
+        }
+        self.sameInitialStock = sameInitialStock!
+        self.fruitManagement = [.strawberry: sameInitialStock!, .banana: sameInitialStock!, .pineapple: sameInitialStock!, .kiwi: sameInitialStock!, .mango: sameInitialStock!]
     }
     
     func checkFruitStock() {
-        for fruit in fruitDictionary {
+        for fruit in fruitManagement {
             print("\(fruit.key)는 \(fruit.value)개 남아 있습니다.")
         }
     }
     
-    func addNewFruit(newFruit: Fruit, fruitStock: Int) {
-        fruitDictionary[newFruit] = fruitStock
+    func addNewKind(fruit: Fruit, fruitStock: UInt) {
+        fruitManagement[fruit] = fruitStock
     }
     
-    func makeJuiceUseOneFruit(fruit: Fruit, requiredNumber: Int) {
-        guard let stock = fruitDictionary[fruit], stock >= requiredNumber else {
-            print("재고가 부족합니다. 현재 재고는 \(fruitDictionary[fruit]!)개 입니다.")
+    func makeJuiceUseOneKind(fruit: Fruit, requiredNumber: UInt) {
+        guard let stock = fruitManagement[fruit], stock >= requiredNumber else {
+            print("재고가 부족합니다. 현재 재고는 \(fruitManagement[fruit]!)개 입니다.")
             return
         }
         let stockAfterMakingJuice = stock - requiredNumber
-        fruitDictionary.updateValue(stockAfterMakingJuice, forKey: fruit)
+        fruitManagement.updateValue(stockAfterMakingJuice, forKey: fruit)
     }
     
     func makeStrawberryAndBananaJuice() {
-        guard let strawberryStock = fruitDictionary[.strawberry], let bananaStock = fruitDictionary[.banana], strawberryStock >= 10, bananaStock >= 1 else {
-            print("재고가 부족합니다. 현재 딸기 재고는 \(fruitDictionary[.strawberry]!)개 이고, 바나나 재고는 \(fruitDictionary[.banana]!)개 입니다.")
+        guard let strawberryStock = fruitManagement[.strawberry], let bananaStock = fruitManagement[.banana], strawberryStock >= 10, bananaStock >= 1 else {
+            print("재고가 부족합니다. 현재 딸기 재고는 \(fruitManagement[.strawberry]!)개 이고, 바나나 재고는 \(fruitManagement[.banana]!)개 입니다.")
             return
         }
         let strawberryStockAfterMakingJuice = strawberryStock - 10
         let bananaStockAfterMakingJuice = bananaStock - 1
-        fruitDictionary.updateValue(strawberryStockAfterMakingJuice, forKey: .strawberry)
-        fruitDictionary.updateValue(bananaStockAfterMakingJuice, forKey: .banana)
+        fruitManagement.updateValue(strawberryStockAfterMakingJuice, forKey: .strawberry)
+        fruitManagement.updateValue(bananaStockAfterMakingJuice, forKey: .banana)
     }
     
     func makeMangoAndKiwiJuice() {
-        guard let mangoStock = fruitDictionary[.mango], let kiwiStock = fruitDictionary[.kiwi], mangoStock >= 2, kiwiStock >= 1 else {
-            print("재고가 부족합니다. 현재 망고 재고는 \(fruitDictionary[.mango]!)개 이고, 키위 재고는 \(fruitDictionary[.kiwi]!)개 입니다.")
+        guard let mangoStock = fruitManagement[.mango], let kiwiStock = fruitManagement[.kiwi], mangoStock >= 2, kiwiStock >= 1 else {
+            print("재고가 부족합니다. 현재 망고 재고는 \(fruitManagement[.mango]!)개 이고, 키위 재고는 \(fruitManagement[.kiwi]!)개 입니다.")
             return
         }
         let mangoStockAfterMakingJuice = mangoStock - 2
         let kiwiStockAfterMakingJuice = kiwiStock - 1
-        fruitDictionary.updateValue(mangoStockAfterMakingJuice, forKey: .mango)
-        fruitDictionary.updateValue(kiwiStockAfterMakingJuice, forKey: .kiwi)
+        fruitManagement.updateValue(mangoStockAfterMakingJuice, forKey: .mango)
+        fruitManagement.updateValue(kiwiStockAfterMakingJuice, forKey: .kiwi)
     }
     
-    func addFruitStock(fruit: Fruit) {
-        guard let fruitStock = fruitDictionary[fruit] else {
+    func addStock(fruit: Fruit) {
+        guard let fruitStock = fruitManagement[fruit] else {
             return
         }
-        fruitDictionary.updateValue(fruitStock + 1, forKey: fruit)
+        fruitManagement.updateValue(fruitStock + 1, forKey: fruit)
     }
 }
