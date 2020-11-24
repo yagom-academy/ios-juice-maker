ad1b82d821ac51f05cf7967e699fe8cd59f95096
odong-Tree <ss12357@naver.com>
2020-11-25 02:28:38 +0900

    feat: 재고부족 alert에서, 재고관리 버튼을 통해서 재고관리화면으로 전환

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -54,6 +54,7 @@ class ViewController: UIViewController {
         makeJuice(.mangoAndKiwiJuice)
     }
     @IBAction func manageStockButton(_ sender: UIButton) {
+        moveToManageStockVC()
     }
 }
 
@@ -69,7 +70,10 @@ extension ViewController {
     
     func showFailAlert(order: Juice) {
         let alert = UIAlertController(title: nil, message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "예", style: .default, handler: nil)
+        let OKAction = UIAlertAction(title: "예", style: .default) {
+            (UIAlertAction) in
+            self.moveToManageStockVC()
+        }
         let cancleAction = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
         
         alert.addAction(OKAction)
