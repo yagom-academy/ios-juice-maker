824ce9b0bf828181782947ab65b3b9db5b2d7f05
lina0322 <llim@student.42seoul.kr>
2020-11-24 15:46:04 +0900

    Feat : StockView에 과일 재고라벨과 실제 재고 연결

--- JuiceMaker/JuiceMaker/StockViewController.swift
+++ JuiceMaker/JuiceMaker/StockViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class StockViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeLabels()
     }
     
     @IBAction func touchUpDismissPageButton() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func changeLabels() {
+        guard let strawberry = JuiceMaker.shared.fruits[.strawberry],
+              let banana = JuiceMaker.shared.fruits[.banana],
+              let mango = JuiceMaker.shared.fruits[.mango],
+              let kiwi = JuiceMaker.shared.fruits[.kiwi],
+              let pineapple = JuiceMaker.shared.fruits[.pineapple] else {
+            return
+        }
+        strawberryStockLabel.text = "\(strawberry.amount)"
+        bananaStockLabel.text = "\(banana.amount)"
+        mangoStockLabel.text = "\(mango.amount)"
+        kiwiStockLabel.text = "\(kiwi.amount)"
+        pineappleStockLabel.text = "\(pineapple.amount)"
     }
 }
