c02008c43b203a2a34699bdff1c67b288d09ba78
odong-Tree <ss12357@naver.com>
2020-11-24 20:53:18 +0900

    feat: fruitImageView, fruitStockLabel @IBOutlet 추가

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -8,6 +8,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fruitImageView1: UIImageView!
+    @IBOutlet weak var fruitImageView2: UIImageView!
+    @IBOutlet weak var fruitImageView3: UIImageView!
+    @IBOutlet weak var fruitImageView4: UIImageView!
+    @IBOutlet weak var fruitImageView5: UIImageView!
+    
+    @IBOutlet weak var fruitStockLabel1: UILabel!
+    @IBOutlet weak var fruitStockLabel2: UILabel!
+    @IBOutlet weak var fruitStockLabel3: UILabel!
+    @IBOutlet weak var fruitStockLabel4: UILabel!
+    @IBOutlet weak var fruitStockLabel5: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
