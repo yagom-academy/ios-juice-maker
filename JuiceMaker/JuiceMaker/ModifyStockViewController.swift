97caa867cfbfcab9ffb0f7f3aba25f0db455f409
innieminnie <rkddlsl1001@gmail.com>
2020-11-25 23:49:55 +0900

    refactor: 과일 재고 [UILabel]의 인덱스 접근방식으로 변경

--- JuiceMaker/JuiceMaker/ModifyStockViewController.swift
+++ JuiceMaker/JuiceMaker/ModifyStockViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ModifyStockViewController: UIViewController {
-
+    private var juiceMaker = JuiceMaker()
+    private var fruitSymbol = ["🍓", "🍌", "🍍", "🥝", "🥭"]
+    @IBOutlet var fruitSymbolLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initializeFruitSymbols()
     }
     
     @IBAction func tappedCloseButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+extension ModifyStockViewController {
+    private func initializeFruitSymbols() {
+        for fruitIndex in 0..<fruitSymbolLabels.count {
+            fruitSymbolLabels[fruitIndex].text = fruitSymbol[fruitIndex]
+        }
     }
 }
