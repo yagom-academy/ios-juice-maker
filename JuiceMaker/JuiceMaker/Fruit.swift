e98b151fc9ca1cf5f5b9f57dcffde12a841a40c5
yeon <jryoun1@naver.com>
2020-11-25 19:56:20 +0900

    add(Fruit.swift, FruitJuice.swift 1.2 / jake) : 연관된 타입끼리 모아서 분리

---
+++ JuiceMaker/JuiceMaker/Fruit.swift
@@ -0,0 +1,40 @@
+//
+//  FruitJuice.swift
+//  JuiceMaker
+//
+//  Created by Yeon on 2020/11/25.
+//
+
+enum FruitType: String {
+    case strawberry = "딸기"
+    case banana = "바나나"
+    case pineapple = "파인애플"
+    case kiwi = "키위"
+    case mango = "망고"
+    case none
+}
+
+class Fruits {
+    let fruitType: FruitType
+    private(set) var stock: Int
+    
+    init(fruitType: FruitType, stock: Int = 10){
+        self.fruitType = fruitType
+        self.stock = stock
+    }
+
+    func add(stock amount: Int) {
+        self.stock += amount
+    }
+    
+    func use(stock amount: Int) {
+        self.stock -= amount
+    }
+    
+    func isStockUsable(needAmount: Int) -> Bool {
+        guard needAmount <= stock else {
+           return false
+        }
+        return true
+    }
+}
