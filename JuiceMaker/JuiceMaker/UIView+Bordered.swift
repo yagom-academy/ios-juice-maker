bc5433300fd83691b50f2b0b785d6f5ed6477e07
yeon <jryoun1@naver.com>
2020-11-25 20:03:48 +0900

    fix(UIView+Bordered.swift 1.6 / jake) : set - get 에서 get - set 순서로 변경

--- JuiceMaker/JuiceMaker/UIView+Bordered.swift
+++ JuiceMaker/JuiceMaker/UIView+Bordered.swift
@@ -9,22 +9,22 @@ import UIKit
 @IBDesignable extension UIView {
     
     @IBInspectable var boarderWidth: CGFloat {
-        set {
-            layer.borderWidth = newValue
-        }
         get {
             return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
         }
     }
     
     @IBInspectable var borderColor: UIColor? {
-        set {
-            guard let uiColor = newValue else { return }
-            layer.borderColor = uiColor.cgColor
-        }
         get {
             guard let color = layer.borderColor else { return nil }
             return UIColor(cgColor: color)
+        }
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
         }
     }
 }
