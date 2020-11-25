8e3f506a5f0e50ec857abccba833d146170ac85f
sole <subuchappyv2@gmail.com>
2020-11-26 00:51:28 +0900

    Add function(makeJuice) to struct JuiceMaker

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -73,4 +73,27 @@ class Stock {
 
 struct JuiceMaker {
     var stock = Stock()
+    
+    func makeJuice(juiceName: JuiceName) {
+        switch juiceName {
+        case .bananaJuice where stock.banana >= 2:
+            stock.use(fruitName: .banana, stock: 2)
+        case .strawberryJuice where stock.starwberry >= 16:
+            stock.use(fruitName: .strawberry, stock: 16)
+        case .pineappleJuice where stock.pineapple >= 2:
+            stock.use(fruitName: .pineapple, stock: 2)
+        case .kiwiJuice where stock.kiwi >= 3:
+            stock.use(fruitName: .kiwi, stock: 3)
+        case .mangoJuice where stock.mango >= 3:
+            stock.use(fruitName: .mango, stock: 3)
+        case .strawberryBananaJuice where stock.starwberry >= 10 && stock.banana >= 1:
+            stock.use(fruitName: .strawberry, stock: 10)
+            stock.use(fruitName: .banana, stock: 1)
+        case .mangoKiwiJuice where stock.mango >= 2 && stock.kiwi >= 1:
+            stock.use(fruitName: .mango, stock: 2)
+            stock.use(fruitName: .kiwi, stock: 1)
+        default:
+            print("재고를 확인하세요")
+        }
+    }
 }
