859ec5c1e32bdb956b08876e6775d3b7784bdc26
sole <subuchappyv2@gmail.com>
2020-11-26 00:43:25 +0900

    Implement class Stock

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -24,3 +24,19 @@ enum FruitName: String {
     case mango
 }
 
+class Stock {
+    private(set) var starwberry: UInt
+    private(set) var banana: UInt
+    private(set) var kiwi: UInt
+    private(set) var mango: UInt
+    private(set) var pineapple: UInt
+    let initialStock: UInt = 10
+    
+    init() {
+        self.starwberry = initialStock
+        self.banana = initialStock
+        self.kiwi = initialStock
+        self.mango = initialStock
+        self.pineapple = initialStock
+    }
+}
