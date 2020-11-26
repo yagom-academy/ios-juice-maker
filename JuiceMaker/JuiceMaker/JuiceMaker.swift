8a8ddb5e35845a861d637106f1cdc22109747432
taetae <taetae.dev@gmail.com>
2020-11-26 23:10:27 +0900

    refactor: make[Fruit]Juice for error handle
    thorw 구문 추가

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -10,6 +10,7 @@ enum AlertMessage: String {
     case outOfStock = "재고가 부족합니다. 재고를 수정할까요?"
     case wrongRequest = "잘못된 요청입니다. 재고 수량은 0미만으로 지정할 수 없습니다."
     case successMade = " 나왔습니다. 맛있게 드세요."
+    case machineBreakdown = "관리자에게 문의하세요."
 }
 
 enum AlertCase: Error {
@@ -197,6 +198,8 @@ class StockManager {
 }
 
 class JuiceMaker {
+    static let common = JuiceMaker()
+    private init() {}
     let stockManager = StockManager()
     let messenger = Messenger()
     
---
+++ JuiceMaker/JuiceMaker/StockViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StockViewController.swift
+//  JuiceMaker
+//
+//  Created by 김태형 on 2020/11/26.
+//
+
+import UIKit
+
+class StockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
