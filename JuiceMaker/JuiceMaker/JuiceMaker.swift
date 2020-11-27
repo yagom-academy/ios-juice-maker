db118ea61576d6a8cb67066de335dd4ec6f7651f
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-11-28 03:05:54 +0900

    FruitStock타입 에러 해결 - 17
    ViewController에서 StockLabel을 업데이트 할 때 발생하는 에러로 인해 FruitStock타입을 class로 변경.
    Simultaneous accesses to 0x6000012a21d0, but modification requires exclusive access.

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -28,12 +28,20 @@ class StockError: Error {
 
 //과일 수량
 //FruitStock을 구조체로 선언한 이유는 값타입으로만 사용될 것 이라 생각하여 구조체를 사용했습니다.
-struct FruitStock {
+class FruitStock {
     private(set) var strawberry: Int
     private(set) var banana: Int
     private(set) var pineapple: Int
     private(set) var kiwii: Int
     private(set) var mango: Int
+    
+    init(strawberry: Int, banana: Int, pineapple: Int, kiwii: Int, mango: Int) {
+        self.strawberry = strawberry
+        self.banana = banana
+        self.pineapple = pineapple
+        self.kiwii = kiwii
+        self.mango = mango
+    }
     
     //현재 보유한 과일 재고로 가능한가?
     private func canMakeJuice(with recipe: Recipe) -> StockCheckResult {
@@ -54,7 +62,7 @@ struct FruitStock {
         return .available
     }
     
-    mutating func useFruit(recipe: Recipe, completionHandler: (Result<Any?, Error>) -> Void) {
+    fileprivate func useFruit(recipe: Recipe, completionHandler: (Result<Any?, Error>) -> Void) {
         switch canMakeJuice(with: recipe) {
         case .available:
             for (fruit, fruitUsed) in recipe {
@@ -78,7 +86,7 @@ struct FruitStock {
     }
     
     //과일 재고 추가
-    mutating func changeStock(fruit: Fruit, stock: Int) {
+    fileprivate func changeStock(fruit: Fruit, stock: Int) {
         switch fruit {
         case .strawberry:
             strawberry = stock
@@ -102,12 +110,7 @@ class JuiceMaker {
     
     //JuiceMaker 인스턴스 생성 시 초기 과일 재고 입력.
     init(stock: FruitStock) {
-        fruitStock = FruitStock(strawberry: stock.strawberry,
-                                banana: stock.banana,
-                                pineapple: stock.pineapple,
-                                kiwii: stock.kiwii,
-                                mango: stock.mango)
-        
+        fruitStock = stock
     }
     
     //어떤 과일을 몇개 써서 쥬스를 만들었나?
