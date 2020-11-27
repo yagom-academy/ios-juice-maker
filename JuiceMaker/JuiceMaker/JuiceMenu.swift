361259c6fe0784ee9664775ce6abb466d72a8850
innieminnie <rkddlsl1001@gmail.com>
2020-11-28 03:40:05 +0900

    feat: JuiceMenu 열거형의 연관값으로 재료요구사항 추가

--- JuiceMaker/JuiceMaker/JuiceMenu.swift
+++ JuiceMaker/JuiceMaker/JuiceMenu.swift
@@ -7,38 +7,13 @@
 
 import Foundation
 
-enum JuiceMenu: String {
-    case strawberryJuice = "딸기쥬스"
-    case bananaJuice = "바나나쥬스"
-    case ddalbaJuice = "딸바쥬스"
-    case mangoJuice = "망고쥬스"
-    case mangoKiwiJuice = "망고키위쥬스"
-    case kiwiJuice = "키위쥬스"
-    case pineappleJuice = "파인애플쥬스"
-    
-    func recipe() -> [String : Int] {
-        
-        var requiredFruits = [String : Int]()
-        
-        switch(self) {
-        case .strawberryJuice:
-            requiredFruits["strawberry"] = 16
-        case .bananaJuice:
-            requiredFruits["banana"] = 2
-        case .ddalbaJuice:
-            requiredFruits["strawberry"] = 10
-            requiredFruits["banana"] = 1
-        case .mangoJuice:
-            requiredFruits["mango"] = 3
-        case .mangoKiwiJuice:
-            requiredFruits["mango"] = 2
-            requiredFruits["kiwi"] = 1
-        case .kiwiJuice:
-            requiredFruits["kiwi"] = 3
-        case .pineappleJuice:
-            requiredFruits["pineapple"] = 2
-        }
-        return requiredFruits
-    }
+enum JuiceMenu{
+    case strawberryJuice(name: String,requiredStrawberries: Int)
+    case bananaJuice(name: String, requiredBananas: Int)
+    case ddalbaJuice(name: String, requiredStrawberries: Int, requiredBananas: Int)
+    case mangoJuice(name: String, requiredMangos: Int)
+    case mangoKiwiJuice(name: String, requiredMangos: Int, requiredKiwis: Int)
+    case kiwiJuice(name: String, requiredKiwis: Int)
+    case pineappleJuice(name: String, requiredPineapples: Int)
 }
 
