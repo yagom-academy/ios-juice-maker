e61efd02b6287bfe390721b71be4697f9bfe07fb
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-11-28 02:30:18 +0900

    IBAction메서드 구현 및 orderJuice, configureStockLabel메소드 정의 - 16
    각각의 버튼에 Action에서 실행될 코드 구현.
    orderJuice는 각각 Action을 통해 주문을 받을 때 실행 될 공통적인 코드들을 모아 정의.
    configureStock 메소드는 과일 재고들을 업데이트 하는 메소드로 정의.

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var kiwiiStock: UILabel!
     @IBOutlet weak var mangoStock: UILabel!
     
-    private var juiceMaker: JuiceMaker = JuiceMaker(stock: FruitStock(strawberry: 10, banana: 10, pineapple: 10, kiwii: 10, mango: 10))
+    let juiceMaker = JuiceMaker(stock: FruitStock(strawberry: 10, banana: 10, pineapple: 10, kiwii: 10, mango: 10))
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -26,29 +26,48 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    //MARK: - IBActions
+    @IBAction func orderStrawberryBanana(_ sender: Any) {
+        orderJuice(with: [.strawberry: 10, .banana: 1], juiceName: "딸바")
+    }
+    @IBAction func orderMangoKiwii(_ sender: UIButton) {
+        orderJuice(with: [.mango: 2, .kiwii: 1], juiceName: "망고키위")
+    }
+    @IBAction func orderStrawberry(_ sender: UIButton) {
+        orderJuice(with: [.strawberry: 16], juiceName: "딸기")
+    }
+    @IBAction func orderBanana(_ sender: UIButton) {
+        orderJuice(with: [.banana: 2], juiceName: "바나나")
+    }
+    @IBAction func orderPineApple(_ sender: UIButton) {
+        orderJuice(with: [.pineapple: 2], juiceName: "파인애플")
+    }
+    @IBAction func orderKiwii(_ sender: UIButton) {
+        orderJuice(with: [.kiwii: 3], juiceName: "키위")
+    }
+    @IBAction func orderMango(_ sender: UIButton) {
+        orderJuice(with: [.mango: 3], juiceName: "망고")
+    }
+    
+    private func orderJuice(with recipe: Recipe, juiceName: String) {
+        juiceMaker.makeJuice(with: recipe) { (result) in
+            switch result {
+            case .success(let message):
+                let alert = UIAlertController(title: "성공!", message: "\(juiceName)\(message)", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
     private func configureStockLabel() {
         strawberryStock.text = "\(juiceMaker.fruitStock.strawberry)"
         bananaStock.text = "\(juiceMaker.fruitStock.banana)"
         pineappleStock.text = "\(juiceMaker.fruitStock.pineapple)"
         kiwiiStock.text = "\(juiceMaker.fruitStock.kiwii)"
         mangoStock.text = "\(juiceMaker.fruitStock.mango)"
-    }
-
-    
-    //MARK: - IBActions
-    @IBAction func orderStrawberryBanana(_ sender: UIButton) {
-    }
-    @IBAction func orderMangoKiwii(_ sender: UIButton) {
-    }
-    @IBAction func orderStrawberry(_ sender: UIButton) {
-    }
-    @IBAction func orderBanana(_ sender: UIButton) {
-    }
-    @IBAction func orderPineApple(_ sender: UIButton) {
-    }
-    @IBAction func orderKiwii(_ sender: UIButton) {
-    }
-    @IBAction func orderMango(_ sender: UIButton) {
     }
 }
 
