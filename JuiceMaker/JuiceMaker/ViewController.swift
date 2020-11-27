93270e622f14e5fb72e43e9fab98e2e7da49fca8
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-11-28 00:52:00 +0900

    ViewController와 스토리보드 연결 - 14
    ViewController에 IBOutlet과 IBAction을 연결.

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -8,11 +8,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    @IBOutlet weak var strawberryStock: UILabel!
+    @IBOutlet weak var bananaStock: UILabel!
+    @IBOutlet weak var pineappleStock: UILabel!
+    @IBOutlet weak var kiwiiStock: UILabel!
+    @IBOutlet weak var mangoStock: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    //MARK: - IBActions
+    @IBAction func orderStrawberryBanana(_ sender: UIButton) {
+    }
+    @IBAction func orderMangoKiwii(_ sender: UIButton) {
+    }
+    @IBAction func orderStrawberry(_ sender: UIButton) {
+    }
+    @IBAction func orderBanana(_ sender: UIButton) {
+    }
+    @IBAction func orderPineApple(_ sender: UIButton) {
+    }
+    @IBAction func orderKiwii(_ sender: UIButton) {
+    }
+    @IBAction func orderMango(_ sender: UIButton) {
+    }
 }
 
