8aac622247992835305c4a5662e347c043eede92
inwoodev <dlsdn1207@gmail.com>
2021-03-08 16:15:20 +0900

    Style : 쥬스메이커 청사진 작성 - 다양한 과일과 과일 재고를 딕셔너리로 구현

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -7,3 +7,49 @@
 import Foundation
 
 /// 쥬스 메이커 타입 
+
+class Fruits {
+    
+    var fruitStocks: [String: Int] = [ "딸기": 16, "바나나": 23, "키위": 12, "파인애플": 3, "망고": 4]
+    
+}
+
+class JuiceMaker {
+
+    
+    func confirmFruitsInStock() {
+        
+    }
+    
+    func makeJuice() {
+        
+    }
+    
+    func makeStrawberryJuice() {
+        
+    }
+    
+    func makeBananaJuice() {
+        
+    }
+    
+    func makeKiwiJuice() {
+        
+    }
+    
+    func makeStrawberryBananaJuice() {
+        
+    }
+    
+    func makeMangoJuice() {
+        
+    }
+    
+    func makeMangoKiwiJuice() {
+        
+    }
+    
+    func addMoreFruits() {
+        
+    }
+}
