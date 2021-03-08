ac8f9dcfb2de5cae7bef5d7f6b9b65e838a9755b
CJMIN <jmintfree@gmail.com>
2021-03-09 07:02:44 +0900

    Chore: 연산프로퍼티명 변경

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -16,49 +16,49 @@ struct FruitInfo {
 
 /// 쥬스 메이커 타입 
 class JuiceMaker {
-    var fruitState : FruitInfo
+    var fruitStock : FruitInfo
     
     init(strawberry: Int, banana: Int, pineapple: Int, kiwi: Int, mango: Int) {
-        fruitState = FruitInfo(strawberry: strawberry, banana: banana, pineapple: pineapple, kiwi: kiwi, mango: mango)
+        fruitStock = FruitInfo(strawberry: strawberry, banana: banana, pineapple: pineapple, kiwi: kiwi, mango: mango)
     }
     
-    var checkFruitInventory: FruitInfo {
+    var fruitInventory: FruitInfo {
         get {
-            return fruitState
+            return fruitStock
         }
     }
     
     func makeFruitJuice(juiceName: String){
         switch juiceName {
         case "딸기 쥬스":
-            if fruitState.strawberry >= 16 {
-                fruitState.strawberry -= 16
+            if fruitStock.strawberry >= 16 {
+                fruitStock.strawberry -= 16
             }
         case "바나나 쥬스":
-            if fruitState.banana >= 2 {
-                fruitState.banana -= 2
+            if fruitStock.banana >= 2 {
+                fruitStock.banana -= 2
             }
         case "키위 쥬스":
-            if fruitState.kiwi >= 3 {
-                fruitState.kiwi -= 3
+            if fruitStock.kiwi >= 3 {
+                fruitStock.kiwi -= 3
             }
         case "파인애플 쥬스":
-            if fruitState.pineapple >= 2 {
-                fruitState.pineapple -= 2
+            if fruitStock.pineapple >= 2 {
+                fruitStock.pineapple -= 2
             }
         case "딸바 쥬스":
-            if fruitState.strawberry >= 10 && fruitState.banana >= 1 {
-                fruitState.strawberry -= 10
-                fruitState.banana -= 1
+            if fruitStock.strawberry >= 10 && fruitStock.banana >= 1 {
+                fruitStock.strawberry -= 10
+                fruitStock.banana -= 1
             }
         case "망고 쥬스":
-            if fruitState.mango >= 3 {
-                fruitState.mango -= 3
+            if fruitStock.mango >= 3 {
+                fruitStock.mango -= 3
             }
         case "망고키위 쥬스":
-            if fruitState.mango >= 2 && fruitState.kiwi >= 1 {
-                fruitState.mango -= 2
-                fruitState.kiwi -= 1
+            if fruitStock.mango >= 2 && fruitStock.kiwi >= 1 {
+                fruitStock.mango -= 2
+                fruitStock.kiwi -= 1
             }
         default:
             print("잘못된 입력입니다.")
@@ -68,15 +68,15 @@ class JuiceMaker {
     func addFruit(fruitName: String , numberOfFruit: Int){
         switch fruitName {
         case "strawberry":
-            fruitState.strawberry += numberOfFruit
+            fruitStock.strawberry += numberOfFruit
         case "banana":
-            fruitState.banana += numberOfFruit
+            fruitStock.banana += numberOfFruit
         case "pineapple":
-            fruitState.pineapple += numberOfFruit
+            fruitStock.pineapple += numberOfFruit
         case "kiwi":
-            fruitState.kiwi += numberOfFruit
+            fruitStock.kiwi += numberOfFruit
         case "mango":
-            fruitState.mango += numberOfFruit
+            fruitStock.mango += numberOfFruit
         default:
             print("잘못된 입력입니다.")
         }
