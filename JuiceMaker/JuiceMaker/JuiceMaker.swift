7aa71a5c6587815f70cadabbcc382eae602f9b30
CJMIN <jmintfree@gmail.com>
2021-03-09 07:06:30 +0900

    Chore: makeFruitJuice 함수 변경 및 매개변수명 변경

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -14,6 +14,26 @@ struct FruitInfo {
     var mango: Int
 }
 
+enum Juice: String {
+    case strawberry = "딸기 쥬스"
+    case banana = "바나나 쥬스"
+    case pineapple = "파인애플 쥬스"
+    case kiwi = "키위 쥬스"
+    case mango = "망고 쥬스"
+    case strawberryBanana = "딸바 쥬스"
+    case mangoKiwi = "망고키위 쥬스"
+}
+
+
+
+enum Fruit: String {
+    case strawberry = "strawberry"
+    case banana = "banana"
+    case pineapple = "pineapple"
+    case kiwi = "kiwi"
+    case mango = "mango"
+}
+
 /// 쥬스 메이커 타입 
 class JuiceMaker {
     var fruitStock : FruitInfo
@@ -28,40 +48,64 @@ class JuiceMaker {
         }
     }
     
-    func makeFruitJuice(juiceName: String){
-        switch juiceName {
-        case "딸기 쥬스":
-            if fruitStock.strawberry >= 16 {
-                fruitStock.strawberry -= 16
-            }
-        case "바나나 쥬스":
-            if fruitStock.banana >= 2 {
-                fruitStock.banana -= 2
-            }
-        case "키위 쥬스":
-            if fruitStock.kiwi >= 3 {
-                fruitStock.kiwi -= 3
-            }
-        case "파인애플 쥬스":
-            if fruitStock.pineapple >= 2 {
-                fruitStock.pineapple -= 2
-            }
-        case "딸바 쥬스":
-            if fruitStock.strawberry >= 10 && fruitStock.banana >= 1 {
-                fruitStock.strawberry -= 10
-                fruitStock.banana -= 1
-            }
-        case "망고 쥬스":
-            if fruitStock.mango >= 3 {
-                fruitStock.mango -= 3
-            }
-        case "망고키위 쥬스":
-            if fruitStock.mango >= 2 && fruitStock.kiwi >= 1 {
-                fruitStock.mango -= 2
-                fruitStock.kiwi -= 1
-            }
+    func makeFruitJuice(name: String) {
+        var currentStock: [Int] = []
+        var minimumNumber: [Int] = []
+        switch name {
+        case Juice.strawberry.rawValue:
+            currentStock.append(fruitStock.strawberry)
+            minimumNumber.append(16)
+        case Juice.banana.rawValue:
+            currentStock.append(fruitStock.banana)
+            minimumNumber.append(2)
+        case Juice.kiwi.rawValue:
+            currentStock.append(fruitStock.kiwi)
+            minimumNumber.append(3)
+        case Juice.pineapple.rawValue:
+            currentStock.append(fruitStock.pineapple)
+            minimumNumber.append(2)
+        case Juice.strawberryBanana.rawValue:
+            currentStock.append(fruitStock.strawberry)
+            minimumNumber.append(10)
+            currentStock.append(fruitStock.banana)
+            minimumNumber.append(1)
+        case Juice.mango.rawValue:
+            currentStock.append(fruitStock.mango)
+            minimumNumber.append(3)
+        case Juice.mangoKiwi.rawValue:
+            currentStock.append(fruitStock.mango)
+            minimumNumber.append(2)
+            currentStock.append(fruitStock.kiwi)
+            minimumNumber.append(1)
         default:
-            print("잘못된 입력입니다.")
+            break
+        }
+        
+        for index in 0...currentStock.count {
+            guard currentStock[index] > minimumNumber[index] else {
+                return
+            }
+        }
+        
+        switch name {
+        case Juice.strawberry.rawValue:
+            fruitStock.strawberry -= 16
+        case Juice.banana.rawValue:
+            fruitStock.banana -= 2
+        case Juice.kiwi.rawValue:
+            fruitStock.kiwi -= 3
+        case Juice.pineapple.rawValue:
+            fruitStock.pineapple -= 2
+        case Juice.strawberryBanana.rawValue:
+            fruitStock.strawberry -= 10
+            fruitStock.banana -= 1
+        case Juice.mango.rawValue:
+            fruitStock.mango -= 3
+        case Juice.mangoKiwi.rawValue:
+            fruitStock.mango -= 2
+            fruitStock.kiwi -= 1
+        default:
+            break
         }
     }
     
