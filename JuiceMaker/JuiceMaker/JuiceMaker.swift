efffa3be18941b1b8b1ebea954e78809a5c72a87
unme88 <sunny.on.a.roll@gmail.com>
2021-03-09 17:40:52 +0900

    feat: Stock의 재고 추가, 소모 메소드, JuiceMaker 클래스 생성

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -33,12 +33,59 @@ class Stock {
     var kiwiCount: Int = 10
     var mangoCount: Int = 10
     
-    func add() {
-        
+    func add(input: Fruit) {
+        switch input {
+        case Fruit.strawberry:
+            strawberryCount += 1
+        case Fruit.banana:
+            bananaCount += 1
+        case Fruit.kiwi:
+            kiwiCount += 1
+        case Fruit.pineapple:
+            pineappleCount += 1
+        case Fruit.mango:
+            mangoCount += 1
+        }
     }
     
-    func subtract() {
-        
+    func subtract(input: Fruit, number: Int) {
+        switch input {
+        case Fruit.strawberry:
+            strawberryCount -= number
+        case Fruit.banana:
+            bananaCount -= number
+        case Fruit.kiwi:
+            kiwiCount -= number
+        case Fruit.pineapple:
+            pineappleCount -= number
+        case Fruit.mango:
+            mangoCount -= number
+        }
     }
     
+}
+
+class JuiceMaker {
+    var stockBox: Stock = Stock()
+    
+    func makeJuice(juice: FruitJuice) {
+        switch juice {
+        case FruitJuice.strawberry:
+            stockBox.subtract(input: Fruit.strawberry, number: 16)
+        case FruitJuice.banana:
+            stockBox.subtract(input: Fruit.banana, number: 2)
+        case FruitJuice.kiwi:
+            stockBox.subtract(input: Fruit.kiwi, number: 3)
+        case FruitJuice.pineapple:
+            stockBox.subtract(input: Fruit.pineapple, number: 2)
+        case FruitJuice.strawberryBanana:
+            stockBox.subtract(input: Fruit.strawberry, number: 10)
+            stockBox.subtract(input: Fruit.banana, number: 1)
+        case FruitJuice.mango:
+            stockBox.subtract(input: Fruit.mango, number: 3)
+        case FruitJuice.mangoKiwi:
+            stockBox.subtract(input: Fruit.mango, number: 2)
+            stockBox.subtract(input: Fruit.kiwi, number: 1)
+        }
+    }
 }
