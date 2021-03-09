d9f1351bc36d7950aebfeb9f1e5b36084a6bf2e4
kangkyung <rudgns0508@naver.com>
2021-03-09 21:08:26 +0900

    Feat: Create JuiceRecipe.swift
    - struct Recipe 추가
    - class JuiceRecipe 추가

---
+++ JuiceMaker/JuiceMaker/JuiceRecipe.swift
@@ -0,0 +1,26 @@
+//
+//  Recipe.swift
+//  JuiceMaker
+//
+//  Created by 강경 on 2021/03/09.
+//
+
+import Foundation
+
+struct Recipe: Codable {
+
+}
+
+class JuiceRecipe {    
+    init() {
+    }
+    
+    func orderedJuiceRecipe(juice: Juice) -> Recipe? {
+        var recipe: Recipe?
+        
+        return recipe
+    }
+    
+    // json파일로 따로 빼낼예정
+    let jsonString = ""
+}
