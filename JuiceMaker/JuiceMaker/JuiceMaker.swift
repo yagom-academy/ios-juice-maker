85a80ba41e932e621a3c89ac027e5f3781fe9751
kangkyung <rudgns0508@naver.com>
2021-03-09 10:04:46 +0900

    Feat: Create JuiceMaker.swift
    - enum Juice 추가
    - class JuiceMaker 추가

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -5,3 +5,30 @@
 // 
 
 import Foundation
+
+enum Juice: String {
+    case 딸기쥬스 = "{\"Strawberry\":16}"
+    case 바나나쥬스 = "{\"Banana\":2}"
+    case 키위쥬스 = "{\"Kiwi\":3}"
+    case 파인애플쥬스 = "{\"Pineapple\":2}"
+    case 딸바쥬스 = "{\"Strawberry\":10},{\"Banana\":1}"
+    case 망고쥬스 = "{\"Mango\":3}"
+    case 망고키위쥬스 = "{\"Mango\":2},{\"Kiwi\":1}"
+}
+
+class JuiceMaker {
+    private let stockOfFruit = StockOfFruit()
+
+    func makeJuice(juice: Juice) {
+
+    }
+    
+    func necessaryFruit(juice: Juice) -> [String:Int] {
+        var necessaryFruit = [String:Int]()
+        return necessaryFruit
+    }
+    
+    func checkEnough(_ juice: Juice, _ fruitInfo: [String:Int]) -> Bool {
+        return false
+    }
+}
