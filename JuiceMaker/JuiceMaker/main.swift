8cb96e608b949e86482b3e0d89ed622021df8436
dacodaco <imochi@naver.com>
2021-03-10 08:45:45 +0900

    feat: 메서드 구현
    1. 재고를 추가하는 메서드 (addAmount)구현
    2. 재고에서 과일을 빼는 메서드 (subtractAmount)구현

--- JuiceMaker/JuiceMaker/main.swift
+++ JuiceMaker/JuiceMaker/main.swift
@@ -7,24 +7,23 @@
 
 import Foundation
 
-enum FruitJuice: Int {
-    case strawberryJuice
-    case bananaJuice
-    case kiwiJuice
-    case pineappleJuice
-    case strawberryBananaJuice
-    case mangoJuice
-    case mangoKiwiJuice
-}
-
-let currentFruitJuice :FruitJuice = .strawberryJuice
-
 class FruitStock {
-    var strawberry: Int = 10
-    var banana: Int = 10
-    var kiwi: Int = 10
-    var pineapple: Int = 10
-    var mango: Int = 10
+    
+    enum FruitJuice {
+        case strawberryJuice
+        case bananaJuice
+        case kiwiJuice
+        case pineappleJuice
+        case strawberryBananaJuice
+        case mangoJuice
+        case mangoKiwiJuice
+    }
+    
+    fileprivate(set) var strawberry: Int = 10
+    fileprivate(set) var banana: Int = 10
+    fileprivate(set) var kiwi: Int = 10
+    fileprivate(set) var pineapple: Int = 10
+    fileprivate(set) var mango: Int = 10
 }
 
 let currentFruitStock = FruitStock()
@@ -35,32 +34,38 @@ class JuiceMaker: FruitStock {
         print("과일 재고 : 딸기 \(strawberry), 바나나 \(banana), 파인애플 \(pineapple), 키위 \(kiwi), 망고 \(mango)")
     }
     
-    func manufacture(of: FruitJuice) {
-        switch of {
+    func addAmount(of: inout Int, as much: Int) {
+        of = of + much
+    }
+    
+    func subtractAmount(of: inout Int, as much: Int) {
+        of = of - much
+    }
+    
+    func sellectJuice(juice: FruitJuice) {
+        switch juice {
         case .strawberryJuice:
-            currentFruitStock.strawberry -= 16
+            subtractAmount(of: &strawberry, as: 16)
         case .bananaJuice:
-            currentFruitStock.banana -= 2
+            subtractAmount(of: &banana, as: 2)
         case .kiwiJuice:
-            currentFruitStock.kiwi -= 3
+            subtractAmount(of: &kiwi, as: 3)
         case .pineappleJuice:
-            currentFruitStock.pineapple -= 2
-        case .straw_nanaJuice:
-            currentFruitStock.strawberry -= 10
-            currentFruitStock.banana -= 1
+            subtractAmount(of: &pineapple, as: 2)
+        case .strawberryBananaJuice:
+            subtractAmount(of: &strawberry, as: 10)
+            subtractAmount(of: &banana, as: 1)
         case .mangoJuice:
-            currentFruitStock.mango -= 3
-        case .mango_KiwiJuice:
-            currentFruitStock.mango -= 2
-            currentFruitStock.kiwi -= 1
+            subtractAmount(of: &mango, as: 3)
+        case .mangoKiwiJuice:
+            subtractAmount(of: &mango, as: 2)
+            subtractAmount(of: &kiwi, as: 1)
         }
     }
     
-    
-    
     func startJuiceMaking() {
         checkFruitStock()
-        manufacture(of: currentFruitJuice)
+        sellectJuice(juice: .strawberryJuice)
         print(currentFruitStock.strawberry)
     }
 }
