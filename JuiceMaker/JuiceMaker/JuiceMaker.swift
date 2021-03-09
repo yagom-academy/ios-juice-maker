e2eddc18d2d43498e1d6123783734a8560feaadc
fezravien <yjaewoong@naver.com>
2021-03-09 11:01:34 +0900

    feat : 주스 만들기와 오류처리
    오류 열거형을 통해 재고의 유무를 제어
    과일의 재고에 따른 사용 및 추가 메소드 생성

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -6,8 +6,34 @@
 
 import Foundation
 
+enum MakerError: Error {
+    case nonStock
+    case unknown
+}
 enum Furits {
     case strawberry, banana, pineapple, kiwi, mango
+}
+enum Juices {
+    case strawberryJuice, bananaJuice, pineappleJuice, kiwiJuice, mangoJuice, strawberryBananaJuice, mangoKiwiJuice
+    
+    var recipe : [Furits : Int] {
+        switch self {
+        case .strawberryJuice:
+            return [.strawberry : 16]
+        case .bananaJuice:
+            return [.banana : 2]
+        case .pineappleJuice:
+            return [.pineapple : 2]
+        case .kiwiJuice:
+            return [.kiwi : 3]
+        case .mangoJuice:
+            return [.mango : 3]
+        case .strawberryBananaJuice:
+            return [.strawberry : 10, .banana : 1]
+        case .mangoKiwiJuice:
+            return [.mango : 2, .kiwi : 1]
+        }
+    }
 }
 
 struct Fruit {
@@ -17,6 +43,16 @@ struct Fruit {
         self.whatFruit = whatFruit
         self.thisFruitAmount = thisFruitAmount
     }
+    
+    func isCheckedStock(needFruitAmount: Int) -> Bool {
+            return thisFruitAmount >= needFruitAmount
+        }
+        mutating func useStock(useableFruitAmount: Int) {
+            thisFruitAmount -= useableFruitAmount
+        }
+        mutating func addStock() {
+            thisFruitAmount += 1
+        }
 }
 
 protocol ReadOnlyJuiceMaker {
@@ -30,4 +66,16 @@ class JuiceMaker: ReadOnlyJuiceMaker {
                                           .kiwi : Fruit(whatFruit: .kiwi),
                                           .mango : Fruit(whatFruit: .mango)]
     
+    func makeJuice(juice: Juices) throws {
+            do {
+                try juice.recipe.forEach {
+                    guard var whatFruit = fruitStorage[$0.key], whatFruit.isCheckedStock(needFruitAmount: $0.value) else {
+                        throw MakerError.nonStock
+                    }
+                    whatFruit.useStock(useableFruitAmount: $0.value)
+                }
+            } catch {
+                print("\(juice) 재고 부족")
+            }
+        }
 }
