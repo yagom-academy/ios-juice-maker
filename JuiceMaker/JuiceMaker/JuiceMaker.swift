269af1ce87a647aabcc020d9fcbe739ef5cd888f
unme88 <sunny.on.a.roll@gmail.com>
2021-03-09 16:51:16 +0900

    feat: FruitJuice, Fruit 열거형, Stock 클래스 타입 생성

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -7,3 +7,38 @@
 import Foundation
 
 /// 쥬스 메이커 타입 
+
+enum FruitJuice {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case strawberryBanana
+    case mango
+    case mangoKiwi
+}
+
+enum Fruit {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case mango
+}
+
+class Stock {
+    var strawberryCount: Int = 10
+    var bananaCount: Int = 10
+    var pineappleCount: Int = 10
+    var kiwiCount: Int = 10
+    var mangoCount: Int = 10
+    
+    func add() {
+        
+    }
+    
+    func subtract() {
+        
+    }
+    
+}
