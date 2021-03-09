18126d59c33a1613e7dfb05c2fac037b917da0e6
fezravien <yjaewoong@naver.com>
2021-03-09 10:58:23 +0900

    feat : 과일 저장소 생성
    과일들의 열거형, 각각의 과일을 다루는 Fruit 구조체 생성
    protocol을 활용한 읽기전용 프로퍼티 지정

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -2,8 +2,32 @@
 //  JuiceMaker - JuiceMaker.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
-/// 쥬스 메이커 타입 
+enum Furits {
+    case strawberry, banana, pineapple, kiwi, mango
+}
+
+struct Fruit {
+    let whatFruit: Furits
+    fileprivate var thisFruitAmount: Int
+    init(whatFruit: Furits, thisFruitAmount: Int = 100) {
+        self.whatFruit = whatFruit
+        self.thisFruitAmount = thisFruitAmount
+    }
+}
+
+protocol ReadOnlyJuiceMaker {
+    var fruitStorage: [Furits:Fruit] { get }
+}
+
+class JuiceMaker: ReadOnlyJuiceMaker {
+    var fruitStorage: [Furits : Fruit] = [.strawberry : Fruit(whatFruit: .strawberry),
+                                          .banana : Fruit(whatFruit: .banana),
+                                          .pineapple : Fruit(whatFruit: .pineapple),
+                                          .kiwi : Fruit(whatFruit: .kiwi),
+                                          .mango : Fruit(whatFruit: .mango)]
+    
+}
