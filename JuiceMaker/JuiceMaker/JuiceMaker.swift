f3abe27110c8e8fb58fe8ad9fed83bbd577e2fb3
Wody95 <kwu0626@gmail.com>
2021-03-10 12:26:02 +0900

    #2 UPDATE message according to the result of the makeFruitJuice method

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -13,17 +13,21 @@ class JuiceMaker {
         return result
     }
     
-    func makeFruitJuice(juice: Juice) {
+    func makeFruitJuice(juice: Juice) -> String {
         let recipe: [(Fruit, Int)] = juice.recipe
-
+        var messege: String = ""
+        
         for (fruit, needStock) in recipe {
             if fruit.stock < needStock {
-                print("\(fruit)의 재고량 부족")
+                messege = "재료가 모자라요. 재고를 수정할까요?"
                 break
             } else {
                 fruit.minusStock(count: needStock)
+                messege = "\(juice) 쥬스 나왔습니다! 맛있게 드세요!"
             }
         }
+        
+        return messege
     }
     
     func plusFruitStock(fruit: Fruit, addStock: Int) {
