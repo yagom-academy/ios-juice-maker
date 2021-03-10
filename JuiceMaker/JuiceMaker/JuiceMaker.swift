f62eec002ae93cc9bb7a6b0139635144f8aac31a
babywalnut <anfuza@naver.com>
2021-03-11 03:29:45 +0900

    [#2] step1 수정

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -6,23 +6,26 @@
 
 import Foundation
 
-/// 쥬스 메이커 타입 
 class Fruit {
-    var fruitName: String
-    var fruitAmount: Int
+    private(set) var fruitName: String
+    private(set) var fruitAmount: Int
     
-    init(specie: String, amount: Int) {
-        self.fruitName = specie
+    init(name: String,amount: Int) {
+        self.fruitName = name
         self.fruitAmount = amount
     }
-}
-
-class FruitsStock {
-    private(set) var strawberry = Fruit(specie: "딸기", amount: 10)
-    private(set) var banana = Fruit(specie: "바나나", amount: 10)
-    private(set) var pineapple = Fruit(specie: "파일애플", amount: 10)
-    private(set) var kiwi = Fruit(specie: "키위", amount: 10)
-    private(set) var mango = Fruit(specie: "망고", amount: 10)
+    
+    internal func stockedFruit() -> Int {
+        return self.fruitAmount
+    }
+    
+    internal func addfruit() {
+        self.fruitAmount += 1
+    }
+    
+    internal func reducefruit(amount: Int) {
+        self.fruitAmount -= amount
+    }
 }
 
 enum FruitsJuice {
@@ -30,65 +33,47 @@ enum FruitsJuice {
 }
 
 class JuiceMaker {
-    var fruitStock = FruitsStock()
+    private var strawberry = Fruit(name: "딸기", amount: 10)
+    private var banana = Fruit(name: "바나나", amount: 10)
+    private var pineapple = Fruit(name: "파일애플", amount: 10)
+    private var kiwi = Fruit(name: "키위", amount: 10)
+    private var mango = Fruit(name: "망고", amount: 10)
+    private var fruitStock: [String : Int] = [:]
     
     // 과일 재고 확인
-    func remainedFruitsStock() -> [String : Int] {
-        let fruitsStockList: [String : Int] =
+    internal func remainedFruitsStock() -> [String : Int]{
+        self.fruitStock =
             [
-                fruitStock.strawberry.fruitName: fruitStock.strawberry.fruitAmount,
-                fruitStock.banana.fruitName: fruitStock.banana.fruitAmount,
-                fruitStock.pineapple.fruitName: fruitStock.pineapple.fruitAmount,
-                fruitStock.kiwi.fruitName: fruitStock.kiwi.fruitAmount,
-                fruitStock.mango.fruitName: fruitStock.mango.fruitAmount
-            ]
-        return fruitsStockList
+                strawberry.fruitName: strawberry.fruitAmount,
+                banana.fruitName: banana.fruitAmount,
+                pineapple.fruitName: pineapple.fruitAmount,
+                kiwi.fruitName: kiwi.fruitAmount,
+                mango.fruitName: mango.fruitAmount]
+        
+        return fruitStock
     }
     
-    // 과일 재고 줄이기
-    func reducedFruitsStock(name: Fruit, amount: Int) {
-        name.fruitAmount -= amount
-    }
-        
     // 쥬스 제조
     func produceFruitsJuice(juice: FruitsJuice) {
         switch juice {
         case .strawberryJuice:
-            reducedFruitsStock(name: fruitStock.strawberry, amount: 16)
+            strawberry.reducefruit(amount: 16)
         case .bananaJuice:
-            reducedFruitsStock(name: fruitStock.banana, amount: 2)
+            banana.reducefruit(amount: 2)
         case .pineappleJuice:
-            reducedFruitsStock(name: fruitStock.pineapple, amount: 2)
+            pineapple.reducefruit(amount: 2)
         case .kiwiJuice:
-            reducedFruitsStock(name: fruitStock.kiwi, amount: 3)
+            kiwi.reducefruit(amount: 3)
         case .mangoJuice:
-            reducedFruitsStock(name: fruitStock.mango, amount: 3)
+            mango.reducefruit(amount: 3)
         case .mangoKiwiJuice:
-            reducedFruitsStock(name: fruitStock.mango, amount: 2)
-            reducedFruitsStock(name: fruitStock.kiwi, amount: 1)
+            mango.reducefruit(amount: 2)
+            kiwi.reducefruit(amount: 1)
         case .strawberryBananaJuice:
-            reducedFruitsStock(name: fruitStock.strawberry, amount: 10)
-            reducedFruitsStock(name: fruitStock.banana, amount: 1)
+            strawberry.reducefruit(amount: 10)
+            banana.reducefruit(amount: 1)
         }
     }
     
-    // STEP-3 재고 페이지에서 과일 재고 추가
-    func addToFruitsStock(name: Fruit) {
-        let stock = FruitsStock()
-        
-        switch name.fruitName {
-        case "딸기":
-            stock.strawberry.fruitAmount += 1
-        case "바나나":
-            stock.banana.fruitAmount += 1
-        case "파인애플":
-            stock.pineapple.fruitAmount += 1
-        case "키위":
-            stock.kiwi.fruitAmount += 1
-        case "망고":
-            stock.mango.fruitAmount += 1
-        default:
-            print("과일을 찾을 수 없습니다.")
-        }
-    }
 }
+
