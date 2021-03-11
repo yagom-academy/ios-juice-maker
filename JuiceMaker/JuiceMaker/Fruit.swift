222842c6920cbc451b62d12b7ded1c5b11ceccd0
kio <un19451024@gmail.com>
2021-03-11 14:38:00 +0900

    [Feat] FruitTypes 열거형 및 Fruit 클래스 생성
    - Fruit 클래스에서 cehckStock(), subtractStock(), addStock() 생성

---
+++ JuiceMaker/JuiceMaker/Fruit.swift
@@ -0,0 +1,36 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by 배은서 on 2021/03/08.
+//
+
+import Foundation
+
+enum FruitTypes {
+    case strawberry, banana, kiwi, pineapple, mango
+}
+
+class Fruit {
+    private var fruit: FruitTypes
+    private(set) var stock: Int
+    
+    init(_ fruit: FruitTypes) {
+        self.fruit = fruit
+        self.stock = 10
+    }
+    
+    func checkStock(_ requiredFruitAmount: Int) -> Bool {
+        if stock < requiredFruitAmount { return false }
+        else { return true }
+    }
+    
+    func subtractStock(_ requiredFruitAmount: Int) {
+        stock -= requiredFruitAmount
+    }
+    
+    func addStock() {
+        stock += 1
+    }
+}
+
