08efb721fc5c44de1510b4fa32ead05bf427f374
jaemuyeo <jemking94@gmail.com>
2021-03-11 23:49:55 +0900

    [12] Feat 재고가 없을때 알림구현 및 화면전환

---
+++ JuiceMaker/JuiceMaker/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  JuiceMaker
+//
+//  Created by 잼킹 on 2021/03/11.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    //MARK:- Property
+
+    //MARK:- View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK:- Method
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -31,6 +31,7 @@ class ViewController: UIViewController {
         }
         
         if hasStock {
+            juiceMaker.makeJuice(juice)
             orderCompletedAlert(juiceName: juice.name)
         } else {
             orderFailAlert()
@@ -44,19 +45,22 @@ class ViewController: UIViewController {
         }
     }
 }
-
+// MARK:- Extension
 extension ViewController {
     func orderCompletedAlert(juiceName: String) {
         let alert = UIAlertController(title: "\(juiceName) 나왔습니다!", message: "맛있게 드세요!", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "감사합니다!", style: .default, handler: nil)
         
+        updateFruitStockLabel()
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
     }
     
     func orderFailAlert() {
         let alert = UIAlertController(title: "재고가 모자라요.", message: "재고를 수정할까요?", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "예", style: .default, handler: nil)
+        let okAction = UIAlertAction(title: "예", style: .default, handler: { _ in
+            self.performSegue(withIdentifier: "stockChanger", sender: nil)
+        })
         let cancleAction = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
         
         alert.addAction(okAction)
