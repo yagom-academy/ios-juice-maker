50a2c4dcc33fd8ef9675aaab294b396a435e6cd9
eunseo5355 <eunseo5355@naver.com>
2021-03-11 15:48:44 +0900

    [Feat] JuiceMaker 구조체 생성 - fruits 변수 및 makeJuice() 구현

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -1,9 +1,30 @@
 //
 //  JuiceMaker - JuiceMaker.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
-
+//
 import Foundation
 
-/// 쥬스 메이커 타입 
+struct JuiceMaker {
+    var fruits: [FruitTypes: Fruit] = [
+        .strawberry: Fruit(.strawberry),
+        .banana: Fruit(.banana),
+        .kiwi: Fruit(.kiwi),
+        .mango: Fruit(.mango),
+        .pineapple: Fruit(.pineapple)
+    ]
+    
+    func makeJuice(juice: JuiceTypes) {
+        for (fruitType, requiredAmount) in juice.recipe() {
+            if let fruit = fruits[fruitType] {
+                if fruit.checkStock(requiredAmount) {
+                    fruit.subtractStock(requiredAmount)
+                } else {
+                    print("재고가 모자라요.")
+                    return
+                }
+            }
+        }
+        print("\(juice.name()) 나왔습니다! 맛있게 드세요!")
+    }
+}
