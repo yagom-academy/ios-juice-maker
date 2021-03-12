f3886c4373f29d2926c9b37f5120cd1bf47d6b8a
Wody95 <kwu0626@gmail.com>
2021-03-12 10:43:10 +0900

    #3 REMOVE test code

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -11,19 +11,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        var test = JuiceMaker()
-        var juice: Juice = .strawbaJuice
-        
-        
-        test.makeJuice(juiceName: juice)
-        print(test.messeges)
-        print(test.nowStock(index: 0))
-        print(test.nowStock(index: 1))
-        test.makeJuice(juiceName: juice)
-        print(test.messeges)
-        print(test.nowStock(index: 0))
-        print(test.nowStock(index: 1))
+
     }
 }
 
