d2e9fcd68993a143e1d0b66e87b3fda6785f290b
Wody95 <kwu0626@gmail.com>
2021-03-12 10:09:05 +0900

    newStep1 REFACTOR all code

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -6,4 +6,89 @@
 
 import Foundation
 
+enum printMessege: String {
+    case unknownError = "알 수 없는 에러입니다."
+    case nonExistentRecipe = "존재하지 않는 레시피 입니다."
+    case notEnoughStock = "재고가 부족합니다."
+    case completeMakeJuice = "쥬스가 완성되었습니다."
+}
 
+enum Juice {
+    case strawberryJuice, bananaJuice, pineappleJuice, kiwiJuice, mangoJuice,
+         strawbaJuice, mangkiJuice
+}
+
+class JuiceMaker {
+    private var stock: [Int] = [10, 10, 10, 10, 10]
+    var messeges: printMessege = .unknownError
+    
+    func nowStock(index: Int) -> Int {
+        return stock[index]
+    }
+    
+    func useStock(index: Int, amount: Int) {
+        self.stock[index] -= amount
+    }
+    
+    func addStock(index: Int, amount: Int) {
+        self.stock[index] += amount
+    }
+    
+    
+    func checkCompareAmountToStock(juiceMenu: Juice) -> Bool {
+        var result: Bool = false
+        let recipe = matchJuiceRecipe(targetJuice: juiceMenu)
+        
+        for (target, compareAmount) in recipe {
+            if stock[target] < compareAmount {
+                result = false
+                break
+            } else {
+                result = true
+            }
+        }
+        
+        return result
+    }
+    
+    func matchJuiceRecipe(targetJuice: Juice) -> [Int: Int] {
+        var recipe: [Int: Int] = [:]
+        
+        // strawberry = 0, banana = 1, pineapple = 2, kiwi = 3, mango = 4
+        switch targetJuice {
+        case .strawberryJuice:
+            recipe = [0:16]
+        case .bananaJuice:
+            recipe = [1:2]
+        case .pineappleJuice:
+            recipe = [2:2]
+        case .kiwiJuice:
+            recipe = [3:3]
+        case .mangoJuice:
+            recipe = [4:3]
+        case .strawbaJuice:
+            recipe = [0:10, 1:1]
+        case .mangkiJuice:
+            recipe = [3:1, 4:2]
+        default:
+            messeges = .nonExistentRecipe
+        }
+        
+        return recipe
+    }
+    
+    func makeJuice(juiceName: Juice) {
+        let recipe = matchJuiceRecipe(targetJuice: juiceName)
+        let checkStockStatus: Bool = checkCompareAmountToStock(juiceMenu: juiceName )
+        
+        if checkStockStatus {
+            for (key, value) in recipe {
+                useStock(index: key, amount: value)
+            }
+            messeges = .completeMakeJuice
+        } else {
+            messeges = .notEnoughStock
+        }
+    }
+    
+}
--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -11,8 +11,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        
+        var test = JuiceMaker()
+        var juice: Juice = .strawbaJuice
+        
+        
+        test.makeJuice(juiceName: juice)
+        print(test.messeges)
+        print(test.nowStock(index: 0))
+        print(test.nowStock(index: 1))
+        test.makeJuice(juiceName: juice)
+        print(test.messeges)
+        print(test.nowStock(index: 0))
+        print(test.nowStock(index: 1))
     }
+    
+    
+    
 
 
 }
