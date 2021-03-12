e262fe357a48810de62c3f27c7c78f175a365772
Sookim18 <scstnghks@naver.com>
2021-03-12 19:50:17 +0900

    Feat : Alert 추가

--- JuiceMaker/JuiceMaker/JuiceMakerViewController.swift
+++ JuiceMaker/JuiceMaker/JuiceMakerViewController.swift
@@ -29,6 +29,33 @@ class JuiceMakerViewController: UIViewController {
         mangoLabel.text = String(totalFruitStock[.mango]!)
         kiwiLabel.text = String(totalFruitStock[.kiwi]!)
     }
+    
+    // 에러 안나면
+    @IBAction func orderAlert(_ sender: Any) {
+        let juiceName: String = "**"
+        
+        let alert = UIAlertController(title: "주문 확인", message: "\(juiceName)쥬스 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // 에러 나면
+    
+    @IBAction func failOrder(_ sender: Any) {
+        let failAlert = UIAlertController(title: nil , message: "재료가 모자라요 재고를 수정할까요?", preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "아니오", style: .default, handler : nil )
+        let stockSettingAction = UIAlertAction(title: "예", style: .cancel){ (action) in
+            
+        }
+        failAlert.addAction(stockSettingAction)
+        failAlert.addAction(cancel)
+
+        present(failAlert, animated: true, completion: nil)
+    }
 
 }
 
