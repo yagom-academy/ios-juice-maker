a17393e809d8a7591c881bb58033254be2b26d78
inwoodev <dlsdn1207@gmail.com>
2021-03-13 18:12:38 +0900

    Refactor : makeJuice()와 executeJuiceMaker()구현 - enum Juice 내부에 recipe 변수를 구현해서 recipe와 storage 값 비교 - 추후 alert에 사용하기 위해 Juice 에 customStringConvertible 호출

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -16,8 +16,56 @@ enum Fruit {
     case strawberry, banana, kiwi, pineapple, mango
 }
 enum Juice {
-    case strawberryJuice, bananaJuice, kiwiJuice, pineappleJuice, mangoJuice, strawberryBananaJuice, mangoKiwiJuice
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case mangoJuice
+    case strawberryBananaJuice
+    case mangoKiwiJuice
+    
+    var recipe: [Fruit : Int] {
+        switch self {
+        case .strawberryJuice:
+            return [.strawberry : 16]
+        case .bananaJuice:
+            return [.banana : 2]
+        case .kiwiJuice:
+            return [.kiwi : 3]
+        case .pineappleJuice:
+            return [.pineapple : 2]
+        case .strawberryBananaJuice:
+            return [.strawberry : 10, .banana: 1]
+        case .mangoJuice:
+            return [.mango : 3]
+        case .mangoKiwiJuice:
+            return [.mango : 2, .kiwi : 1]
+            
+        }
+    }
 }
+
+extension Juice: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .strawberryJuice:
+            return "딸기쥬스"
+        case .bananaJuice:
+            return "바나나쥬스"
+        case .kiwiJuice:
+            return "키위쥬스"
+        case .pineappleJuice:
+            return "파인애플쥬스"
+        case .strawberryBananaJuice:
+            return "딸바쥬스"
+        case .mangoJuice:
+            return "망고쥬스"
+        case .mangoKiwiJuice:
+            return "망키쥬스"
+        }
+    }
+}
+
 
 class JuiceMaker {
     var fruitStorage = FruitStorage.shared.refrigerator
@@ -36,62 +84,26 @@ class JuiceMaker {
         fruitStorage.updateValue(stock - 1, forKey: input)
     }
     
-    func consumeFruit(input: Fruit, amount: Int) throws {
-        guard let stock = fruitStorage[input] else {
-            throw AppError.unknownError
-        }
-        fruitStorage.updateValue(stock + amount, forKey: input)
-    }
-    
-    func makeJuice(input: Fruit, for juice: Juice) {
+    func executeJuiceMaker(juice: Juice) {
         do {
-            _ = try isJuiceAvailable(fruit: input, for: juice)
-            print("주스가 나왔습니다.")
-            } catch {
-            print("재고가 부족합니다.")
+            try makeJuice(for: juice)
+        } catch {
+        }
+    }
+    
+    func makeJuice(for juice: Juice) throws -> Bool {
+        for (fruit, requirements) in juice.recipe {
+            guard let stock = fruitStorage[fruit]
+            else {
+                throw AppError.unknownError
             }
-    }
-    func remainingFruits(fruit: Fruit) throws -> Int {
-        guard let remainder = fruitStorage[fruit] else {
-            throw AppError.unknownError
-        }
-        return remainder
-    }
-    
-    func requiredFruitsForRecipe(juice: Juice, fruit: Fruit) throws -> Int {
-        let needs = juiceRecipe(recipe: juice)
-        guard let requirements = needs[fruit] else {
-            throw AppError.unknownError
-        }
-        return requirements
-    }
-    
-    func isJuiceAvailable(fruit: Fruit, for juice: Juice) throws -> Bool {
-        let fruitsInStock = try remainingFruits(fruit: fruit)
-        let requiredFruits = try requiredFruitsForRecipe(juice: juice, fruit: fruit)
-        if fruitsInStock < requiredFruits {
-            throw AppError.outOfStock
+            if stock >= requirements {
+                fruitStorage.updateValue(stock - requirements, forKey: fruit)
+            }
+            else {
+                throw AppError.outOfStock
+            }
         }
         return true
     }
-    func juiceRecipe(recipe: Juice) -> [Fruit: Int] {
-        switch recipe {
-        case .strawberryJuice:
-            return [Fruit.strawberry: 16]
-        case .bananaJuice:
-            return [Fruit.banana: 2]
-        case .kiwiJuice:
-            return [Fruit.kiwi: 3]
-        case .pineappleJuice:
-            return [Fruit.pineapple: 2]
-        case .mangoJuice:
-            return [Fruit.mango: 3]
-        case .strawberryBananaJuice:
-            return [Fruit.strawberry: 10, Fruit.banana: 1]
-        case .mangoKiwiJuice:
-            return [Fruit.mango: 2, Fruit.kiwi: 1]
-        }
-    }
 }
-
-
