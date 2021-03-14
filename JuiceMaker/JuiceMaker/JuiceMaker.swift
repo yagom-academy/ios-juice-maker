ddff03c78fd48ac6dc1f2ff73d42453624bd20de
babywalnut <anfuza@naver.com>
2021-03-14 11:39:04 +0900

    [#10] step-1 전역데이터 정의

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -7,16 +7,14 @@
 import Foundation
 
 class Fruit {
-    private(set) var name: String
     private(set) var amount: Int
     
-    init(name: String, amount: Int) {
-        self.name = name
+    init(amount: Int) {
         self.amount = amount
     }
     
-    func addFruit() {
-        self.amount += 1
+    func addFruit(amount: Int) {
+        self.amount += amount
     }
     
     func reducefruit(amount: Int) {
@@ -24,38 +22,17 @@ class Fruit {
     }
 }
 
-var strawberry = Fruit(name: "딸기", amount: 10)
-var banana = Fruit(name: "바나나", amount: 10)
-var pineapple = Fruit(name: "파일애플", amount: 10)
-var kiwi = Fruit(name: "키위", amount: 10)
-var mango = Fruit(name: "망고", amount: 10)
-
-enum FruitsJuice {
-    case strawberryJuice, bananaJuice, pineappleJuice, kiwiJuice, mangoJuice, strawberryBananaJuice, mangoKiwiJuice
-    
-    func jucieRecipe() -> JuiceIngredients {
-        switch self {
-        case .strawberryJuice:
-            return JuiceIngredients(ingredients: [(strawberry,16)])
-        case .bananaJuice:
-            return JuiceIngredients(ingredients: [(banana,2)])
-        case .pineappleJuice:
-            return JuiceIngredients(ingredients: [(pineapple,2)])
-        case .kiwiJuice:
-            return JuiceIngredients(ingredients: [(kiwi,3)])
-        case .mangoJuice:
-            return JuiceIngredients(ingredients: [(mango,3)])
-        case .strawberryBananaJuice:
-            return JuiceIngredients(ingredients: [(strawberry,10), (banana,1)])
-        case .mangoKiwiJuice:
-            return JuiceIngredients(ingredients: [(mango,2), (kiwi,2)])
-        }
-    }
+enum FruitStock {
+    static let strawberry = Fruit(amount: 10)
+    static let banana = Fruit(amount: 10)
+    static let pineapple = Fruit(amount: 10)
+    static let kiwi = Fruit(amount: 10)
+    static let mango = Fruit(amount: 10)
 }
 
 typealias ingredient = (fruit: Fruit, amount: Int)
 
-class JuiceIngredients {
+class JuiceRecipe {
     private(set) var ingredients: [ingredient]
     
     init(ingredients: [ingredient]) {
@@ -63,13 +40,22 @@ class JuiceIngredients {
     }
 }
 
-let successMessage = "쥬스가 완성되었습니다."
-let failureMessage = "재고가 부족합니다."
+enum JuiceMenu {
+    static let strawberryJuice = JuiceRecipe(ingredients: [(FruitStock.strawberry,16)])
+    static let bananaJuice = JuiceRecipe(ingredients: [(FruitStock.banana,2)])
+    static let pineappleJuice = JuiceRecipe(ingredients: [(FruitStock.pineapple,2)])
+    static let kiwiJuice = JuiceRecipe(ingredients: [(FruitStock.kiwi,3)])
+    static let mangoJuice = JuiceRecipe(ingredients: [(FruitStock.mango,3)])
+}
+
+enum ResultMessages {
+    static let successMessage = "쥬스가 완성되었습니다."
+    static let failureMessage = "재고가 부족합니다."
+}
 
 class JuiceMaker {
-    
-    private func checkFruitStock(of juice: FruitsJuice) -> Bool {
-        for ingredient in juice.jucieRecipe().ingredients {
+    private func checkFruitStock(of juice: JuiceRecipe) -> Bool {
+        for ingredient in juice.ingredients {
             if ingredient.amount < ingredient.fruit.amount {
                 return true
             }
@@ -77,14 +63,18 @@ class JuiceMaker {
         return false
     }
     
-    func informJuiceOrderResult(name: FruitsJuice) -> String {
+    func notifiedOrderedResult() {
+        
+    }
+    
+    func makeJuice(name: JuiceRecipe) -> String {
         if checkFruitStock(of: name) {
-            for ingredient in name.jucieRecipe().ingredients {
+            for ingredient in name.ingredients {
                 ingredient.fruit.reducefruit(amount: ingredient.amount)
             }
-            return successMessage
+            return ResultMessages.successMessage
         } else {
-            return failureMessage
+            return ResultMessages.failureMessage
         }
     }
 }
