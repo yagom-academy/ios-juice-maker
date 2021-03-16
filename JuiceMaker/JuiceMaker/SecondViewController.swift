8acd598b902be2152fbb27a17158f079dada39c7
jaemuyeo <jemking94@gmail.com>
2021-03-16 18:47:26 +0900

    [#19] Feat Stepper로 재고수정 기능

--- JuiceMaker/JuiceMaker/SecondViewController.swift
+++ JuiceMaker/JuiceMaker/SecondViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 class SecondViewController: UIViewController {
     let juiceMaker = JuiceMaker.shared
     @IBOutlet var fruitStockLabels = [UILabel]()
+    var previousStepperValues = [Double]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         update(labels: fruitStockLabels, by: juiceMaker.stock)
+        previousStepperValues = Array(repeating: 0, count: fruitStockLabels.count)
     }
     
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        guard let fruit = Fruit(rawValue: sender.tag) else { return }
+        juiceMaker.manageStock(fruit, by: Int(sender.value - previousStepperValues[sender.tag]))
+        update(labels: fruitStockLabels, by: juiceMaker.stock)
+        previousStepperValues[sender.tag] = sender.value
     }
 }
