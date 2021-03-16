24f9ebf65faf91f6caf51e82fefdedae27231e68
jaemuyeo <jemking94@gmail.com>
2021-03-16 13:43:01 +0900

    [#14] Refactor Alert Extension

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -17,11 +17,13 @@ class ViewController: UIViewController {
     
     @IBAction func juiceOrderButton(_ sender: UIButton) {
         guard let juice = Juice(rawValue: sender.tag) else { return }
+        
         if juiceMaker.stock.hasFruits(for: juice) {
             juiceMaker.make(juice)
-            orderCompletedAlert(with: juice.name)
+            updateFruitStockLabels()
+            alert(title: "\(juice.name) 나왔습니다!", message: "맛있게 드세요!", actionTypes: [.ok("감사합니다!")])
         } else {
-            outOfStockAlert()
+            alert(title: "재고가 모자라요.", message: "재고를 수정할까요?", actionTypes: [.ok("예", { _ in self.performSegue(withIdentifier: "stockChanger", sender: nil)}), .cancel("아니오")])
         }
     }
     
@@ -33,27 +35,14 @@ class ViewController: UIViewController {
             fruitStockLabels[index].text = String(stock)
         }
     }
-}
-
-extension ViewController {
-    func orderCompletedAlert(with juiceName: String) {
-        let alert = UIAlertController(title: "\(juiceName) 나왔습니다!", message: "맛있게 드세요!", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "감사합니다!", style: .default, handler: nil)
-        
-        updateFruitStockLabels()
-        alert.addAction(okAction)
-        present(alert, animated: true, completion: nil)
-    }
     
-    func outOfStockAlert() {
-        let alert = UIAlertController(title: "재고가 모자라요.", message: "재고를 수정할까요?", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "예", style: .default, handler: { _ in
-            self.performSegue(withIdentifier: "stockChanger", sender: nil)
-        })
-        let cancleAction = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
+    func alert(title: String, message: String, actionTypes: [UIAlertAction.ActionType]) {
+        let alertController = UIAlertController(title: title, message: message)
         
-        alert.addAction(okAction)
-        alert.addAction(cancleAction)
-        present(alert, animated: true, completion: nil)
+        for actionType in actionTypes {
+            alertController.addAction(actionType.action())
+        }
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
