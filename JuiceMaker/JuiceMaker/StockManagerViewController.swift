e97e4981a9ce75085b562ee865aeafcd6dda5e85
eunseo5355 <eunseo5355@naver.com>
2021-03-16 10:57:42 +0900

    [Feat] 닫기 버튼 누르면 화면이 닫히는 touchUpCloseButton() 구현

--- JuiceMaker/JuiceMaker/StockManagerViewController.swift
+++ JuiceMaker/JuiceMaker/StockManagerViewController.swift
@@ -5,4 +5,13 @@
 //  Created by 배은서 on 2021/03/15.
 //
 
-import Foundation
+import UIKit
+
+class StockManagerViewController: UIViewController {
+    @IBOutlet weak var closeButton: UIBarButtonItem!
+    
+    @IBAction func touchUpCloseButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
