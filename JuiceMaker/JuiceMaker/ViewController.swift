9a850296f1c5baa2c99e0e790cf027e301bc7f43
Wody95 <kwu0626@gmail.com>
2021-03-17 17:46:52 +0900

    #5 ADD storyboard linked ViewController method

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -7,12 +7,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let juicemaker: JuiceMaker = JuiceMaker()
+    
+    @IBOutlet weak var strawberryStock: UILabel!
+    @IBOutlet weak var bananaStock : UILabel!
+    @IBOutlet weak var pineappleStock : UILabel!
+    @IBOutlet weak var kiwiStock : UILabel!
+    @IBOutlet weak var mangoStock : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        
+        nowStocks()
+//        print(String(juicemaker.fruitStocks.check(fruit: ObjectIdentifier(Strawberry.self))))
     }
 
+    func nowStocks() {
+        strawberryStock.text = String(juicemaker.fruitStocks.check(fruit: ObjectIdentifier(Strawberry.self)))
+        bananaStock.text = String(juicemaker.fruitStocks.check(fruit: ObjectIdentifier(Banana.self)))
+        pineappleStock.text = String(juicemaker.fruitStocks.check(fruit: ObjectIdentifier(Pineapple.self)))
+        kiwiStock.text = String(juicemaker.fruitStocks.check(fruit: ObjectIdentifier(Kiwi.self)))
+        mangoStock.text = String(juicemaker.fruitStocks.check(fruit: ObjectIdentifier(Mango.self)))
+    }
 }
 
