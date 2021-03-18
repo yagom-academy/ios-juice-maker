9ffebc1b7c0e59f9b62937e3583aa9c1d00791fa
DragonTigerXYZ <yb1229@kpu.ac.kr>
2021-03-18 16:50:28 +0900

    #5 Step2

---
+++ JuiceMaker/JuiceMaker/EditStockViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EditStockViewController.swift
+//  JuiceMaker
+//
+//  Created by YB on 2021/03/17.
+//
+
+import UIKit
+
+class EditStockViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton (_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -17,115 +17,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         nowStocks()
-//        print(String(juicemaker.fruitStocks.check(fruit: ObjectIdentifier(Strawberry.self))))
-    }
-    
-    func showSuccesAlert(message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let okAtion = UIAlertAction(title: "OK", style: .default) {
-            (action) in self.nowStocks()
-        }
-        alert.addAction(okAtion)
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func showFailAlert(message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        
-        let yesAtion = UIAlertAction(title: "예", style: .default)
-        let noAtion = UIAlertAction(title: "아니오", style: .cancel)
-        
-        alert.addAction(yesAtion)
-        alert.addAction(noAtion)
-        
-        present(alert, animated: true, completion: nil)
-    }
-    
-    @IBAction func touchStrawberryJuiceButton(_ juiceButton: UIButton) {
-        print("딸기쥬스 버튼을 눌렀습니다")
-        let orderState: ErrorMessage = juiceMaker.order(juice: ObjectIdentifier(StrawberryJuice.self))
-        print("orderState의 상태 : \(orderState)")
-        switch orderState {
-        case .completeMakeJuice:
-            showSuccesAlert(message: "딸기쥬스가 완성되었습니다. 맛있게 드세요")
-        default:
-            showFailAlert(message: "재료가 모자라요. 재고를 수정할까요?")
-        }
-    }
-    
-    @IBAction func touchBananaJuiceButton(_ juiceButton: UIButton) {
-        print("바나나쥬스 버튼을 눌렀습니다")
-        let orderState: ErrorMessage = juiceMaker.order(juice: ObjectIdentifier(BananaJuice.self))
-        print("orderState의 상태 : \(orderState)")
-        switch orderState {
-        case .completeMakeJuice:
-            showSuccesAlert(message: "바나나쥬스가 완성되었습니다. 맛있게 드세요")
-        default:
-            showFailAlert(message: "재료가 모자라요. 재고를 수정할까요?")
-        }
-    }
-    
-    @IBAction func touchPineappleJuiceButton(_ juiceButton: UIButton) {
-        print("파인애플쥬스 버튼을 눌렀습니다")
-        let orderState: ErrorMessage = juiceMaker.order(juice: ObjectIdentifier(PineappleJuice.self))
-        print("orderState의 상태 : \(orderState)")
-        switch orderState {
-        case .completeMakeJuice:
-            showSuccesAlert(message: "파인애플쥬스가 완성되었습니다. 맛있게 드세요")
-        default:
-            showFailAlert(message: "재료가 모자라요. 재고를 수정할까요?")
-        }
-    }
-    
-    @IBAction func touchKiwiJuiceButton(_ juiceButton: UIButton) {
-        print("키위쥬스 버튼을 눌렀습니다")
-        let orderState: ErrorMessage = juiceMaker.order(juice: ObjectIdentifier(KiwiJuice.self))
-        print("orderState의 상태 : \(orderState)")
-        switch orderState {
-        case .completeMakeJuice:
-            showSuccesAlert(message: "키위쥬스가 완성되었습니다. 맛있게 드세요")
-        default:
-            showFailAlert(message: "재료가 모자라요. 재고를 수정할까요?")
-        }
-    }
-    
-    @IBAction func touchMangoJuiceButton(_ juiceButton: UIButton) {
-        print("망고쥬스 버튼을 눌렀습니다")
-        let orderState: ErrorMessage = juiceMaker.order(juice: ObjectIdentifier(MangoJuice.self))
-        print("orderState의 상태 : \(orderState)")
-        switch orderState {
-        case .completeMakeJuice:
-            showSuccesAlert(message: "망고쥬스가 완성되었습니다. 맛있게 드세요")
-        default:
-            showFailAlert(message: "재료가 모자라요. 재고를 수정할까요?")
-        }
-    }
-    
-    @IBAction func touchStrawberryBananaJuiceButton(_ juiceButton: UIButton) {
-        print("딸바쥬스 버튼을 눌렀습니다")
-        let orderState: ErrorMessage = juiceMaker.order(juice: ObjectIdentifier(StrawberryBananaJuice.self))
-        print("orderState의 상태 : \(orderState)")
-        switch orderState {
-        case .completeMakeJuice:
-            showSuccesAlert(message: "딸바쥬스가 완성되었습니다. 맛있게 드세요")
-        default:
-            showFailAlert(message: "재료가 모자라요. 재고를 수정할까요?")
-        }
-    }
-    
-    @IBAction func touchMangoKiwiJuiceButton(_ juiceButton: UIButton) {
-        print("망고키위쥬스 버튼을 눌렀습니다")
-        let orderState: ErrorMessage = juiceMaker.order(juice: ObjectIdentifier(MangoKiwiJuice.self))
-        print("orderState의 상태 : \(orderState)")
-        switch orderState {
-        case .completeMakeJuice:
-            showSuccesAlert(message: "망고키위쥬스가 완성되었습니다. 맛있게 드세요")
-        default:
-            showFailAlert(message: "재료가 모자라요. 재고를 수정할까요?")
-        }
     }
     
     func nowStocks() {
@@ -136,6 +29,69 @@ class ViewController: UIViewController {
         mangoStock.text = String(juiceMaker.fruitStocks.check(fruit: ObjectIdentifier(Mango.self)))
     }
     
+    @IBAction func matchButton(sender: AnyObject) {
+        guard let button = sender as? UIButton else{
+            return
+        }
+        
+        switch button.tag {
+        case 1:
+            juiceButton(ObjectIdentifier(StrawberryJuice.self), "딸기쥬스")
+        case 2:
+            juiceButton(ObjectIdentifier(BananaJuice.self), "바나나쥬스")
+        case 3:
+            juiceButton(ObjectIdentifier(PineappleJuice.self), "파인애플쥬스")
+        case 4:
+            juiceButton(ObjectIdentifier(KiwiJuice.self), "키위쥬스")
+        case 5:
+            juiceButton(ObjectIdentifier(MangoJuice.self), "망고쥬스")
+        case 6:
+            juiceButton(ObjectIdentifier(StrawberryBananaJuice.self), "딸바쥬스")
+        case 7:
+            juiceButton(ObjectIdentifier(MangoKiwiJuice.self), "망고키위쥬스")
+        default:
+            print("모르는 버튼")
+        }
+    }
     
+    func juiceButton(_ juiceRecipe: ObjectIdentifier, _ juiceName: String) {
+                let orderState: ErrorMessage = juiceMaker.order(juice: juiceRecipe)
+                print("orderState의 상태 : \(orderState)")
+                switch orderState {
+                case .completeMakeJuice:
+                    showSuccessAlert(message: juiceName)
+                default:
+                    showFailAlert()
+                }
+    }
+    
+    func showSuccessAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: "\(message)가 완성되었습니다. 맛있게 드세요", preferredStyle: .alert)
+        let okAtion = UIAlertAction(title: "OK", style: .default) {
+            (action) in self.nowStocks()
+        }
+        alert.addAction(okAtion)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showFailAlert() {
+        let alert = UIAlertController(title: nil, message: "재료가 모자라요. 재고를 수정할까요?" , preferredStyle: .alert)
+        let noAtion = UIAlertAction(title: "아니오", style: .cancel)
+        let yesAtion = UIAlertAction(title: "예", style: .default) { action in
+            self.moveViewController(name: "EditStockViewController")
+        }
+        
+        alert.addAction(yesAtion)
+        alert.addAction(noAtion)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func moveViewController(name: String) {
+        let sceneName = self.storyboard?.instantiateViewController(withIdentifier: name)
+        
+        sceneName?.modalTransitionStyle = .crossDissolve
+        self.present(sceneName!, animated: true, completion: nil)
+    }
 }
 
