7a1a4f94448299387dbe15ce3232a7b4a3693d4e
kane-young <lyw2100@naver.com>
2021-03-19 15:45:49 +0900

    Feat : Add IBOutlet variations <description> Button과 Label에 해당하는 IBOutlet 변수 추가

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -8,6 +8,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strawberryCount: UILabel!
+    @IBOutlet weak var bananaCount: UILabel!
+    @IBOutlet weak var pineappleCount: UILabel!
+    @IBOutlet weak var kiwiCount: UILabel!
+    @IBOutlet weak var mangoCount: UILabel!
+    
+    @IBOutlet weak var orderStrawberryBananaJuiceButton: UIButton!
+    @IBOutlet weak var orderStrawberryJuiceButton: UIButton!
+    @IBOutlet weak var orderBananaJuiceButton: UIButton!
+    @IBOutlet weak var orderPineappleJuiceButton: UIButton!
+    @IBOutlet weak var orderMangoKiwiJuiceButton: UIButton!
+    @IBOutlet weak var orderKiwiJuiceButton: UIButton!
+    @IBOutlet weak var orderMangoJuiceButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
