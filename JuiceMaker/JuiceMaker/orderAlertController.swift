be24623b319ef1628ee14f0cd724cf9e81f70dc7
kane-young <lyw2100@naver.com>
2021-03-19 17:37:30 +0900

    Feat : Add ok action's handler <description> handler 안에 stock scene으로 이동하도록 구현

--- JuiceMaker/JuiceMaker/orderAlertController.swift
+++ JuiceMaker/JuiceMaker/orderAlertController.swift
@@ -18,9 +18,15 @@ class OrderAlertController: UIAlertController {
     
     func alertOfFail() -> UIAlertController {
         let alert = UIAlertController(title: "재료가 모자라요", message: "재고를 수정할까요?", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "예", style: .default, handler: { (_) in 
+        let ok = UIAlertAction(title: "예", style: .default, handler: { (_) in
+            guard let rootView = UIApplication.shared.windows.last?.rootViewController else {
+                return
+            }
+            let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let stockView = mainStoryboard.instantiateViewController(identifier: "stockVC")
+            rootView.present(stockView, animated: false, completion: nil)
         })
-        let cancel = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
+        let cancel = UIAlertAction(title: "아니오", style: .default, handler: nil)
         alert.addAction(ok)
         alert.addAction(cancel)
         return alert
