dc7b643ac6522822ebb39cbbc2d22da1a468d185
kane-young <lyw2100@naver.com>
2021-03-19 15:49:14 +0900

    Feat : Add OrderJuiceButton.swift and Create Blueprint

---
+++ JuiceMaker/JuiceMaker/OrderJuiceButton.swift
@@ -0,0 +1,16 @@
+//
+//  OrderJuiceButton.swift
+//  JuiceMaker
+//
+//  Created by 이영우 on 2021/03/19.
+//
+
+import UIKit
+
+class OrderJuiceButton: UIButton {
+    var juice: Juice?
+    
+    func make(using juice: Juice) {
+        
+    }
+}
