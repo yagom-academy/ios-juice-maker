f3da79d12dcadd8fcca31a97dcc4771ff21e2541
ohsg0226 <ohsg0226@naver.com>
2021-06-07 18:09:42 +0900

    feat: step1 FruitStore.swift type 정의

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,18 @@ import Foundation
 
 // 과일 타입
 class FruitStore {
+    var strawberry: Int
+    var banana: Int
+    var pineapple: Int
+    var kiwi: Int
+    var mango: Int
     
+    init() {
+        strawberry = 10
+        banana = 10
+        pineapple = 10
+        kiwi = 10
+        mango = 10
+    }
 }
+
