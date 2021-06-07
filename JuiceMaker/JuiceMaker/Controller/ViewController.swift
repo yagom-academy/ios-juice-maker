fae98258dcf03d3f978982f91e458133ac6a8f25
ohsg0226 <ohsg0226@naver.com>
2021-06-08 00:08:59 +0900

    feat: 초기재고를 가진 클래스 생성 및 과일 종류 담은 enum 생성 하위 클래스 생성

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -12,7 +12,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,19 +7,63 @@
 import Foundation
 
 // 과일 타입
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
+
 class FruitStore {
-    var strawberry: Int
-    var banana: Int
-    var pineapple: Int
-    var kiwi: Int
-    var mango: Int
+    var fruit: Fruit?
+    var count = 10
     
-    init() {
-        strawberry = 10
-        banana = 10
-        pineapple = 10
-        kiwi = 10
-        mango = 10
+    func increment(){
+        count += 1
+    }
+    func increment(amount:Int){
+        count += amount
+    }
+    func decrease(){
+        count -= 1
     }
 }
+
+class Strawberry: FruitStore {
+    
+    override init() {
+        super.init()
+        self.fruit = Fruit.strawberry
+    }
+}
+class Banana: FruitStore {
+    
+    override init() {
+        super.init()
+        self.fruit = Fruit.banana
+    }
+}
+class Pineapple: FruitStore {
+    
+    override init() {
+        super.init()
+        self.fruit = Fruit.pineapple
+    }
+}
+class Kiwi: FruitStore {
+    
+    override init() {
+        super.init()
+        self.fruit = Fruit.kiwi
+    }
+}
+class Mango: FruitStore {
+    
+    override init() {
+        super.init()
+        self.fruit = Fruit.mango
+    }
+}
+
 
