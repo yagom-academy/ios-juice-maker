8ead0a29fae361dc4f780e1a3d784154e2815eae
yaewonLee <yw990324@gmail.com>
2021-06-08 16:47:02 +0900

    feat: JuiceMakert() 와 FruitStore() 타입 정의

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,25 +8,17 @@ import Foundation
 
 // 과일 타입
 class FruitStore {
-    //    private var name: Fruit
-    //    private var amount: Int
-   
     var strawberryStock: Int = 10
     var bananaStock: Int = 10
     var pineappleStock: Int = 10
     var KiwiStock: Int = 10
     var mangoStock: Int = 10
-    
-//    init(name: Fruit, amount: Int) {
-//        self.name = name
-//        self.amount = amount
-//    }
+
     enum Fruit {
-        case strawberry,
-        banana, pineapple, kiwi, mango
+        case strawberry, banana, pineapple, kiwi, mango
     }
     
-    func changeInventory(name: Fruit, amount : Int) {
+    func inventoryManagement(name: Fruit, amount : Int) {
         switch name {
         case .strawberry :
             strawberryStock += amount
@@ -40,5 +32,7 @@ class FruitStore {
             mangoStock += amount
         }
     }
+    
+
 }
 
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -9,6 +9,8 @@ import Foundation
 // 쥬스 메이커 타입 
 struct JuiceMaker {
     
+    var fruitStore = FruitStore()
+    
     enum Juice {
         case strawberryJuice
         case bananaJuice
@@ -18,5 +20,31 @@ struct JuiceMaker {
         case strawberryBananaJuice
         case mangoKiwiJuice
     }
-
+    
+    func manager(btnCliked: Juice) {
+        var result: Bool = true
+        
+        switch btnCliked {
+        case .strawberryJuice:
+            result = strawberryJuiceMaker()
+            
+        default:
+            return
+        }
+    }
+    
+    
+    
+    func strawberryJuiceMaker() -> Bool {
+        if fruitStore.strawberryStock >= 16 {
+            fruitStore.strawberryStock -= 16
+            return true
+        } else {
+            print("재고가 부족합니다!")
+            return false
+        }
+        
+        
+    }
+    
 }
