89257e90884d2948b356d2a2e5468c024d5eb551
hrjy6278 <hrjy6278@naver.com>
2021-06-08 23:51:32 +0900

    feat: 쥬스 종류에 대한 열거형 구현
    *쥬스에 들어가는 재료는 연관값으로 지정 할 것이다.

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 쥬스 메이커 타입 
 struct JuiceMaker {
-    
+    enum JuiceMenu {
+        case strawberry(Fruit, Int)
+        case banana(Fruit, Int)
+        case pineapple(Fruit, Int)
+        case kiwi(Fruit, Int)
+        case mango(Fruit, Int)
+        case strawberryBanana(Fruit, Int, Fruit, Int)
+        case mangoKiwi(Fruit, Int, Fruit, Int)
+    }
 }
