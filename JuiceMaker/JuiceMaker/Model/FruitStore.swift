cf363f9415fa335ae0f4340ef4cb56ec3b2aa801
Ldoy <leedoy7989@gmail.com>
2021-06-08 10:31:16 +0900

    Feat: Fruit Store 클래스 프로퍼티 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,14 @@ import Foundation
 
 // 과일 타입
 class FruitStore {
-    
+	var name: String
+	var count: Int
+	
+	init(fruitname: String, leftcount: Int) {
+		self.name = fruitname
+		self.count = leftcount
+	}
+	
 }
+
+
