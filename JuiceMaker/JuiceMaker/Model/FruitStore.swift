246a02a7984e042e78b104c52b50e1a87f4f2386
taeyoung-Kim-KR <kty72301@naver.com>
2021-06-08 17:19:55 +0900

    feat : enum쥬스타입 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,11 @@ import Foundation
 
 // 과일 타입
 class FruitStore {
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
+    
     
 }
