18ac36d8364f6b91002c72fcac6b80ce56015666
taelee <ritmakid@gmail.com>
2021-06-08 12:13:48 +0900

    feat: Fruit 타입 클래스 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,6 +7,24 @@
 import Foundation
 
 // 과일 타입
-class FruitStore {
+class Fruit {
+    var stock = 10
     
+    func increaseStock() {
+        stock += 1
+    }
+    func decreaseStock() {
+        stock -= 1
+    }
+    func decreaseStock(number: Int) {
+        stock -= number
+    }
+}
+
+class FruitStore {
+    let strawberry = Fruit()
+    let banana = Fruit()
+    let kiwi = Fruit()
+    let pineapple = Fruit()
+    let mango = Fruit()
 }
