6c18312cf003a25e616098cd82a05f0fa678d647
filmcanister <filmcanister97@gmail.com>
2021-06-08 16:52:31 +0900

    feat: JuiceMaker 구조체 작성

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,21 @@ import Foundation
 
 // 쥬스 메이커 타입 
 struct JuiceMaker {
+    var fruitStore = FruitStore()
     
+    func makeJuice(of menu: JuiceMenu) -> Bool {
+        guard checkStocks(of: menu) else { return false }
+        for (fruit, amout) in menu.neededFruits {
+            fruitStore.minusStock(of: fruit, amout: amout)
+        }
+        return true
+    }
+    
+    func checkStocks(of menu: JuiceMenu) -> Bool {
+        var isAvailable = true
+        for (fruit, amout) in menu.neededFruits {
+            isAvailable = isAvailable && fruitStore.getStock(of: fruit) >= amout
+        }
+        return isAvailable
+    }
 }
