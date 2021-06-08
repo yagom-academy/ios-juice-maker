5e9513b8e22c22dd24175ed35a4e331348f432e6
ictechgy <ictechgy@gmail.com>
2021-06-08 19:39:21 +0900

    feat: 과일의 종류를 나타낼 열거형 작성
    과일의 한글 이름이 추후에 필요할 것으로 판단되어 String rawValue 추가
    rawValue를 직접적으로 쓰지 않기 위해 CustomStringConvertible을 채택하고 준수하였다.

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,18 @@
 
 import Foundation
 
-// 과일 타입
+enum Fruit: String, CustomStringConvertible {
+    case strawberry = "딸기"
+    case banana = "바나나"
+    case pineapple = "파인애플"
+    case kiwi = "키위"
+    case mango = "망고"
+    
+    var description: String {
+        self.rawValue
+    }
+}
+
 class FruitStore {
     
 }
