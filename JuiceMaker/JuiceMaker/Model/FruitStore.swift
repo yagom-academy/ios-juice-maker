bfb7b4b86c4ef08017f6817f0e55712a372b3804
filmcanister <filmcanister97@gmail.com>
2021-06-08 16:43:00 +0900

    feat: Fruits.swift, FruitStore 클래스 작성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,54 @@ import Foundation
 
 // 과일 타입
 class FruitStore {
+    var strawberry = 10
+    var banana = 10
+    var kiwi = 10
+    var pineapple = 10
+    var mango = 10
     
+    func getStock(of fruit: Fruits) -> Int {
+        switch fruit {
+        case .strawberry:
+            return strawberry
+        case .banana:
+            return banana
+        case .kiwi:
+            return kiwi
+        case .pienapple:
+            return pineapple
+        case .mango:
+            return mango
+        }
+    }
+    
+    func addStock(of fruit: Fruits, amout: Int) {
+        switch fruit {
+        case .strawberry:
+            strawberry += amout
+        case .banana:
+            banana += amout
+        case .kiwi:
+            kiwi += amout
+        case .pienapple:
+            pineapple += amout
+        case .mango:
+            mango += amout
+        }
+    }
+    
+    func minusStock(of fruit: Fruits, amout: Int) {
+        switch fruit {
+        case .strawberry:
+            strawberry -= amout
+        case .banana:
+            banana -= amout
+        case .kiwi:
+            kiwi -= amout
+        case .pienapple:
+            pineapple -= amout
+        case .mango:
+            mango -= amout
+        }
+    }
 }
---
+++ JuiceMaker/JuiceMaker/Model/Fruits.swift
@@ -0,0 +1,16 @@
+//
+//  Fruits.swift
+//  JuiceMaker
+//
+//  Created by 홍진태 on 2021/06/08.
+//
+
+import Foundation
+
+enum Fruits {
+    case strawberry
+    case banana
+    case kiwi
+    case pienapple
+    case mango
+}
