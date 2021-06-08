dc4de41e9fef8f891a942179154c5719d6c4d6f9
ictechgy <ictechgy@gmail.com>
2021-06-08 21:26:40 +0900

    feat: FruitStore가 관리하는 과일에 대한 프로퍼티 추가
    관리하는 과일에 대한 프로퍼티 `inventory`를 Fruit 열거형을 이용하여 간편하게 만들어주기 위해 reduce 고차함수를 사용하였다.
    * Fruit를 배열로 받기 위해 CaseIterable 프로토콜을 채택시켜 주었다.
    * 매직 넘버를 없애기 위해 타입 프로퍼티 `initialNumberOfFruits`를 만들어주었다.

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,7 @@
 
 import Foundation
 
-enum Fruit: String, CustomStringConvertible {
+enum Fruit: String, CustomStringConvertible, CaseIterable {
     case strawberry = "딸기"
     case banana = "바나나"
     case pineapple = "파인애플"
@@ -19,6 +19,14 @@ enum Fruit: String, CustomStringConvertible {
 }
 
 class FruitStore {
+    static let initialNumberOfFruits = 10
+    
+    let inventory: [Fruit: Int] = Fruit.allCases.reduce([Fruit: Int]()) { bag, crop in
+        var bag = bag
+        bag[crop] = initialNumberOfFruits
+        return bag
+    }
+    
     enum InventoryManagementError: Error {
         case outOfStock
     }
