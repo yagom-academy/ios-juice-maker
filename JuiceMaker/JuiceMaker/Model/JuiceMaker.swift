8b56c64a68dc611ad78439fd6489597e7c68cbb8
shpairo711 <shapiro7@ajou.ac.kr>
2021-06-08 12:35:47 +0900

    feat: recipe 딕셔너리 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,15 @@ import Foundation
 
 // 쥬스 메이커 타입 
 struct JuiceMaker {
+    let fruitStore = FruitStore()
     
+    let recipe = [
+        "strawberryJuice": ["strawberry": 16],
+        "bananaJuice": ["banana": 2],
+        "kiwiJuice": ["kiwi": 3],
+        "pineapple": ["pineapple": 2],
+        "strawberryBananaJuice": ["stawberry": 10, "banana": 1],
+        "mangoJuice": ["mango": 3],
+        "mangoKiwiJuice": ["mango": 2, "kiwi": 1]
+    ]
 }
