e68220f78c96e0133fab8591ee66a0ffd57fa81c
taeyoung-Kim-KR <kty72301@naver.com>
2021-06-09 11:08:23 +0900

    feat : 쥬스메이커 타입 구현
    Fruit,Juice를 열거형으로 생성, 쥬스 종류와 소모될 재료의 양을 가질 변수 생성

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,16 +7,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    enum JuiceType: Int {
-        case strawberryJuice = 1
-        case bananaJuice
-        case pineappleJuice
-        case kiwiJuice
-        case mangoJuice
-        case strawberryBananaJuice
-        case mangoKiwiJuice
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
