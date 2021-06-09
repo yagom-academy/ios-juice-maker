20b4d36296dcb667323137f0fbf1626d9e0fe204
yoonju lee <rsyjlee@gmail.com>
2021-06-09 10:41:32 +0900

    feat: Juice 열거형 및 Recipe struct 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,39 @@
 
 import Foundation
 
-// 쥬스 메이커 타입 
-struct JuiceMaker {
+// 쥬스 메이커 타입
+enum Juice {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
+}
 
+struct Recipe {
+    let juice: Juice
+    var ingredients: [Fruits: Int] {
+        switch juice {
+        case .strawberryJuice:
+            return [.strawberry: 16]
+        case .bananaJuice:
+            return [.banana: 2]
+        case .kiwiJuice:
+            return [.kiwi: 3]
+        case .pineappleJuice:
+            return [.pineapple: 2]
+        case .strawberryBananaJuice:
+            return [.strawberry: 10, .banana: 1]
+        case .mangoJuice:
+            return [.mango: 3]
+        case .mangoKiwiJuice:
+            return [.mango: 2, .kiwi: 1]
+        }
+    }
+}
+
+struct JuiceMaker {
+    
 }
