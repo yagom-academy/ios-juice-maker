2af964d7f09a9b9348c9416bc7317f912fa6c716
산체스 <xognsl87@gmail.com>
2021-06-09 15:56:32 +0900

    feat: juiceMaker 타입에 FruitStore 인스턴스화 진행

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,9 +8,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let fruitStore: [FruitStore] = [
+        StrawberryStock(),
+        BananaStock(),
+        PineappleStock(),
+        KiwiStock(),
+        MangoStock()
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        fruitStore.forEach(({$0.amount}))
     }
 
 
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,44 +8,13 @@ import Foundation
 
 // 쥬스 메이커 타입 
 struct JuiceMaker {
+    let fruitStore: [FruitStore] = [
+        StrawberryStock(),
+        BananaStock(),
+        PineappleStock(),
+        KiwiStock(),
+        MangoStock()
+        ]
     
-    var fruitStore = FruitStore()
-    
-    enum Juice {
-        case strawberryJuice
-        case bananaJuice
-        case pineappleJuice
-        case kiwiJuice
-        case mangoJuice
-        case strawberryBananaJuice
-        case mangoKiwiJuice
-    }
-    
-    func manager(btnCliked: Juice) {
-        var result: Bool = true
-        
-        switch btnCliked {
-        case .strawberryJuice:
-            strawberryJuiceMaker()
-            
-        default:
-            return
-        }
-    }
-    
-    
-    
-    func strawberryJuiceMaker() -> Bool {
-        if fruitStore.strawberryStock >= 16 {
-            fruitStore.inventoryManagement(name: .strawberry, amount: -16)
-            return true
-        } else {
-            print("재고가 부족합니다!")
-            return false
-        }
-        
-        
-        
-    }
     
 }
