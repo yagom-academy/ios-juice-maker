2eb3ab8039154f0314e0ba97aadb8eb6fd246e52
YongwooKim <keepingitflow@gmail.com>
2021-06-09 11:31:53 +0900

    feat : JuiceMaker 구조체 구현
    JuiceMaker 구조체 내부에 FruitStore() 인스턴스를 하나 생성
    Fruit 와 Juice enum을 이용해서 쥬스를 만드는 makeJuice() 메소드와 재고 확인을 하는 checkStock() 메소드를 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -15,32 +15,32 @@ class FruitStore {
     var mango: Int = 10
     
     func currentStock(_ fruit: Fruit) -> Int {
-            switch fruit {
-            case .strawberry:
-                return strawberry
-            case .banana:
-                return banana
-            case .pineapple:
-                return pineapple
-            case .kiwi:
-                return kiwi
-            case .mango:
-                return mango
-            }
+        switch fruit {
+        case .strawberry:
+            return strawberry
+        case .banana:
+            return banana
+        case .pineapple:
+            return pineapple
+        case .kiwi:
+            return kiwi
+        case .mango:
+            return mango
         }
+    }
     
     func changeStock(_ fruit: Fruit, _ changingQuantity: Int) {
-            switch fruit {
-            case .strawberry:
-                strawberry -= changingQuantity
-            case .banana:
-                banana -= changingQuantity
-            case .pineapple:
-                pineapple -= changingQuantity
-            case .kiwi:
-                kiwi -= changingQuantity
-            case .mango:
-                mango -= changingQuantity
-            }
+        switch fruit {
+        case .strawberry:
+            strawberry -= changingQuantity
+        case .banana:
+            banana -= changingQuantity
+        case .pineapple:
+            pineapple -= changingQuantity
+        case .kiwi:
+            kiwi -= changingQuantity
+        case .mango:
+            mango -= changingQuantity
         }
+    }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -63,4 +63,26 @@ struct JuiceMaker {
             }
         }
     }
+    
+    let store = FruitStore()
+    
+    func makeJuice(_ type: Juice) -> Juice? {
+        if checkStock(type.ingredients) {
+            for ingredient in type.ingredients as [(fruit: Fruit, removingQuantity:Int)] {
+                store.changeStock(ingredient.fruit, ingredient.removingQuantity)
+            }
+            return type
+        } else {
+            return nil
+        }
+    }
+    
+    func checkStock(_ ingredients: [(fruit: Fruit, removingQuantity:Int)]) -> Bool {
+        for ingredient in ingredients {
+            if store.currentStock(ingredient.fruit) < ingredient.removingQuantity {
+                return false
+            }
+        }
+        return true
+    }
 }
