d1514efe1121fb3cda525e138b2f1f93255c1235
Ellen <80380535+jcrescent61@users.noreply.github.com>
2021-06-09 09:56:00 +0900

    feat: 재고 확인 후 해당 과일 차감하는 usingStock 함수 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -33,4 +33,13 @@ class FruitStore {
         }
     }
     
+    func usingStock(fruit: Fruit, needs: Int) {
+        guard let stock = fruitStock[fruit] else { return }
+        if stock < needs {
+            print("재고가 부족합니다. 재고를 추가할까요?")
+
+        } else {
+            fruitStock[fruit] = stock - needs
+        }
+    }
 }
