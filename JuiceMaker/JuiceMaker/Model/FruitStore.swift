fda1aa2b39de3b182fb0550a06cb7de856e6987d
홍정아 <isjoeyful@gmail.com>
2021-06-09 10:04:57 +0900

    feat: FruitStore 클래스 및 Fruits 열거형 구현
    - stockLeft 연산 프로퍼티를 사용해 과일 개수를 readOnly로 조회
    - updateStock() - 변경된 재고로 업데이트
    - consumeStock() - 사용한 과일 개수 차감

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,32 @@
 
 import Foundation
 
+enum Fruits {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
+
 // 과일 타입
 class FruitStore {
+    private var stock: Int = 10
+    private let name: String
     
+    init(name: String) {
+        self.name = name
+    }
+    
+    var stockLeft: Int {
+        return stock
+    }
+
+    func updateStock(by number: Int) {
+        stock = number
+    }
+
+    func consumeStock(of number: Int) {
+        stock -= number
+    }
 }
