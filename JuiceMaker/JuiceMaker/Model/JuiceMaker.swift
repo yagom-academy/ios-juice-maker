e23a65e27a5bd483d39ac2270e536b0b3b826646
Ellen <80380535+jcrescent61@users.noreply.github.com>
2021-06-09 10:03:13 +0900

    feat: 레시피를 받아 재고에 차감하는 함수 makeJuice 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,41 +8,47 @@ import Foundation
 
 struct JuiceMaker {
     enum JuiceRecipe: CaseIterable {
-           case strawberryJuice
-           case bananaJuice
-           case kiwiJuice
-           case mangoJuice
-           case pineappleJuice
-
-           case strawberryBananaJuice
-           case mangoKiwiJuice
-           
-           var recipeIngredients: [Fruit: Int] {
-               switch self {
-               
-               case .strawberryJuice:
-                   return [.strawberry: 16]
-                   
-               case .bananaJuice:
-                   return [.banana: 2]
-                   
-               case .kiwiJuice:
-                   return [.kiwi: 3]
-                   
-               case .mangoJuice:
-                   return [.mango: 3]
-                   
-               case .pineappleJuice:
-                   return [.pineapple: 2]
-                   
-               case .strawberryBananaJuice:
-                   return [.strawberry: 10, .banana: 1]
-                   
-               case .mangoKiwiJuice:
-                   return [.mango: 2, .kiwi: 1]
-               }
-           }
-       }
+        case strawberryJuice
+        case bananaJuice
+        case kiwiJuice
+        case mangoJuice
+        case pineappleJuice
+        
+        case strawberryBananaJuice
+        case mangoKiwiJuice
+        
+        var recipeIngredients: [Fruit: Int] {
+            switch self {
+            
+            case .strawberryJuice:
+                return [.strawberry: 16]
+                
+            case .bananaJuice:
+                return [.banana: 2]
+                
+            case .kiwiJuice:
+                return [.kiwi: 3]
+                
+            case .mangoJuice:
+                return [.mango: 3]
+                
+            case .pineappleJuice:
+                return [.pineapple: 2]
+                
+            case .strawberryBananaJuice:
+                return [.strawberry: 10, .banana: 1]
+                
+            case .mangoKiwiJuice:
+                return [.mango: 2, .kiwi: 1]
+            }
+        }
+    }
     
     let fruitstore = FruitStore()
+    
+    func makeJuice(recipe: JuiceRecipe) {
+        for (fruit, amount) in recipe.recipeIngredients {
+            fruitstore.usingStock(fruit: fruit, needs: amount)
+        }
+    }
 }
