a2b9c7cbf155c61a5d6d32dc7fb7a4ee02864617
ictechgy <ictechgy@gmail.com>
2021-06-09 11:41:05 +0900

    feat: 기존 JuiceMenu 열거형을 쥬스를 만들기 위한 쥬스 레시피가 반환되도록 메소드 추가
    * 연관값으로 재료를 받아주려 했던 부분을 메소드로 필요 재료가 반환되도록 수정하였다.
    * warehouse 프로퍼티와 produce(kindOf:)메소드는 개괄적인 내용만 작성한 상태.

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,13 +8,42 @@ import Foundation
 
 // 쥬스 메이커 타입 
 struct JuiceMaker {
+    let warehouse: FruitStore
+    
+    func produce(kindOf menuName: JuiceMenu) {
+        let recipe = menuName.menuRecipe()
+    }
+    
     enum JuiceMenu {
-        case strawberry(Fruit, Int)
-        case banana(Fruit, Int)
-        case pineapple(Fruit, Int)
-        case kiwi(Fruit, Int)
-        case mango(Fruit, Int)
-        case strawberryBanana(Fruit, Int, Fruit, Int)
-        case mangoKiwi(Fruit, Int, Fruit, Int)
+        case strawberry
+        case banana
+        case pineapple
+        case kiwi
+        case mango
+        case strawberryBanana
+        case mangoKiwi
+        
+        func menuRecipe() -> [(Fruit, Int)] {
+            var recipe: [(Fruit, Int)]
+            
+            switch self {
+            case .strawberry:
+                recipe = [(Fruit.strawberry, 16)]
+            case .banana:
+                recipe = [(Fruit.banana, 2)]
+            case .kiwi:
+                recipe = [(Fruit.kiwi, 3)]
+            case .pineapple:
+                recipe = [(Fruit.pineapple, 2)]
+            case .mango:
+                recipe = [(Fruit.mango, 3)]
+            case .strawberryBanana:
+                recipe = [(Fruit.strawberry, 10),(Fruit.banana, 1)]
+            case .mangoKiwi:
+                recipe = [(Fruit.mango, 2), (Fruit.kiwi, 1)]
+            }
+            return recipe
+        }
+        
     }
 }
