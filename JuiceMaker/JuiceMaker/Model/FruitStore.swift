cd287779e625e63af5101defb88b762672d250e9
KimWanKi <seoulkwk95@gmail.com>
2021-06-10 21:58:35 +0900

    feat : JuiceMenu, Fruit enum Type init.

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,27 @@
 
 import Foundation
 
-// 과일 타입
 class FruitStore {
+    let defaultStock: Int = 10
+    var fruitStock: [Fruit:Int] = [:]
     
+    enum Fruit: String, CaseIterable {
+        case strawberry = "딸기"
+        case banana = "바나나"
+        case fineapple = "파인애플"
+        case kiwi = "키위"
+        case mango = "망고"
+    }
+    
+    init() {
+        for i in Fruit.allCases {
+            fruitStock[i] = defaultStock
+        }
+        changeStock(fruit: Fruit.banana, amount: 5)
+    }
+    
+    func changeStock(fruit: Fruit, amount: Int) {
+        fruitStock[fruit] = amount
+    }
 }
+
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 쥬스 메이커 타입 
 struct JuiceMaker {
-    
+    enum JuiceList {
+        case strawberry
+        case banana
+        case kiwi
+        case fineapple
+        case strawberryBanana
+        case mango
+        case mangoKiwi
+    }
 }
