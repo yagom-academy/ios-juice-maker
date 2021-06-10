6083767e9eb1d81563d9fb0b6f6ee8b475e13d4c
Ldoy <leedoy7989@gmail.com>
2021-06-10 22:05:58 +0900

    Feat: Add checkJuiceMakable, checkFruitAmount, findFruitStore method

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,75 +7,87 @@
 import Foundation
 
 struct JuiceMaker {
-    
-    enum JuiceMenu {
-        case strawberryJuice
-        case bananaJuice
-        case kiwiJuice
-        case mangoJuice
-        case pineappleJuice
-        case strawNanaJuice
-        case mangoKiwiJuice
-        
-        var recipe:[(Fruit ,Int)] {
-            switch self {
-            case .strawberryJuice:
-                return [(.strawberry, 16)]
-            case .bananaJuice:
-                return [(.banana, 2)]
-            case .kiwiJuice:
-                return [(.kiwi, 3)]
-            case .mangoJuice:
-                return [(.mango, 3)]
-            case .pineappleJuice:
-                return [(.pineapple, 2)]
-            case .strawNanaJuice:
-                return [(.strawberry, 10), (.banana, 1)]
-            case .mangoKiwiJuice:
-                return [(.mango, 2), (.kiwi, 1)]
-            }
-        }
-    }
-    
-    enum Fruit: String, CaseIterable {
-        case strawberry = "딸기"
-        case banana = "바나나"
-        case kiwi = "키위"
-        case mango = "망고"
-        case pineapple = "파인애플"
-    }
-    
-    let fruitStores =  [FruitStore]()
-    
-    init(){
-        for fruit in Fruit.allCases {
-            fruitStores.append(FruitStore(storeName: fruit.rawValue))
-        }
-    }
-    
-    func checkJuiceMakable(targetStore: FruitStore, amount: Int) -> Bool {
-        if targetStore.stock >= amount {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    func makeJuice(wanted: JuiceMenu){
-        let recipes = wanted.recipe
-        var makable = true
-        for (fruit, amount) in recipes {
-            if let store = fruitStores.first(where: { store in store.name == fruit.rawValue}) {
-                
-                let haveStock = checkJuiceMakable(targetStore: store, amount: amount)
-                
-                if !haveStock {
-                    makable = false
-                    break
-                }
-            }
-        }
-    }
-    
+	
+	enum JuiceMenu {
+		case strawberryJuice
+		case bananaJuice
+		case kiwiJuice
+		case mangoJuice
+		case pineappleJuice
+		case strawNanaJuice
+		case mangoKiwiJuice
+		
+		var recipe:[(Fruit ,Int)] {
+			switch self {
+			case .strawberryJuice:
+				return [(.strawberry, 16)]
+			case .bananaJuice:
+				return [(.banana, 2)]
+			case .kiwiJuice:
+				return [(.kiwi, 3)]
+			case .mangoJuice:
+				return [(.mango, 3)]
+			case .pineappleJuice:
+				return [(.pineapple, 2)]
+			case .strawNanaJuice:
+				return [(.strawberry, 10), (.banana, 1)]
+			case .mangoKiwiJuice:
+				return [(.mango, 2), (.kiwi, 1)]
+			}
+		}
+	}
+	
+	enum Fruit: String, CaseIterable {
+		case strawberry = "딸기"
+		case banana = "바나나"
+		case kiwi = "키위"
+		case mango = "망고"
+		case pineapple = "파인애플"
+	}
+	
+	var fruitStores =  [FruitStore]()
+	
+	init(){
+		for fruit in Fruit.allCases {
+			fruitStores.append(FruitStore(storeName: fruit.rawValue))
+		}
+	}
+	
+	func checkJuiceMakable(recipes: [(Fruit, Int)]) -> Bool {
+		var makable = true
+		
+		for recipe in recipes {
+			makable = checkFruitAmount(receipe: recipe)
+			if !makable {
+				return false
+			}
+		}
+		return makable
+	}
+	
+	func checkFruitAmount(receipe: (name: Fruit, amount: Int)) -> Bool {
+		guard let store = findFruitStore(name: receipe.name) else {
+			return false
+		}
+		if store.stock >= receipe.amount {
+			return true
+		} else {
+			return false
+		}
+	}
+	
+	func findFruitStore(name: Fruit) -> FruitStore? {
+		var targetStore: FruitStore?
+		
+		for store in fruitStores {
+			if store.name == name.rawValue {
+				targetStore = store
+				break
+			}
+		}
+		return targetStore
+	}
 }
+
+
 
