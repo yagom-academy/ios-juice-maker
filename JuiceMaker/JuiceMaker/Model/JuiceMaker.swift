d40b98f1e29ac34389963ee8d62ec34bf9b59b4e
blanche37 <wkddbsghk16@gmail.com>
2021-06-10 20:27:48 +0900

    style: 열거형 Message를 구조체JuiceMaker의 중첩타입으로 이동

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,6 +7,15 @@
 import Foundation
 
 struct JuiceMaker {
+    enum Message {
+        static let outOfStock = "재료가 모자라요. 재고를 수정할까요?"
+        static let nonZeroStock = "재고를 0미만으로 설정 할 수 없습니다"
+        
+        static func completeMakingJuice(on juice: Juice) {
+            print("\(juice) 나왔습니다! 맛있게 드세요!")
+        }
+    }
+    
     let fruitStore: FruitStore
     
     init(fruitStore: FruitStore) {
