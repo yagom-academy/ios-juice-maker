26120999041136570d83117618f7da8022edd7a0
YesCoach <sbsii1020@gmail.com>
2021-06-11 13:18:49 +0900

    feat: ViewController에 Outlet 변수 및 Action 함수 추가, 실행시 label을 초깃값으로 초기화 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,12 +7,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var strawberryStockLabel: UILabel!
+    @IBOutlet var bananaStockLabel: UILabel!
+    @IBOutlet var pineappleStockLabel: UILabel!
+    @IBOutlet var kiwiStockLabel: UILabel!
+    @IBOutlet var mangoStockLabel: UILabel!
+    
+    let juiceMaker = JuiceMaker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        strawberryStockLabel.text = String(juiceMaker.fruitstore.fruitStock[.strawberry] ?? 0)
+        bananaStockLabel.text = String(juiceMaker.fruitstore.fruitStock[.banana] ?? 0)
+        pineappleStockLabel.text = String(juiceMaker.fruitstore.fruitStock[.pineapple] ?? 0)
+        kiwiStockLabel.text = String(juiceMaker.fruitstore.fruitStock[.kiwi] ?? 0)
+        mangoStockLabel.text = String(juiceMaker.fruitstore.fruitStock[.mango] ?? 0)
+        
     }
 
+    @IBAction func strawberryBananaJuiceButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func mangoKiwiJuiceButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func strawberryJuiceButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func bananaJuiceButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func pineappleJuiceButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func kiwiJuiceButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func mangoJuiceButton(_ sender: UIButton) {
+        
+    }
 
 }
 
