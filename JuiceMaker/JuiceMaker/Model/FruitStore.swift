55c34fe52c83b358ec6c84ad2625ffb7efa628e3
jaemuyeo <jemking94@gmail.com>
2021-06-12 16:37:20 +0900

    Feat. FruitStore 타입 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,24 @@
 
 import Foundation
 
-// 과일 타입
 class FruitStore {
+    enum Fruit: Int, CaseIterable {
+        case strawberry, banana, pineapple, kiwi, mango
+    }
     
+    private var fruits = [Fruit: Int]()
+    
+    init(initialStock: Int) {
+        for fruit in Fruit.allCases {
+            fruits[fruit] = initialStock
+        }
+    }
+    
+    func increase(fruit: Fruit, quantity: Int) {
+        fruits[fruit]? += quantity
+    }
+    
+    func decrease(fruit: Fruit, quantity: Int) {
+        fruits[fruit]? -= quantity
+    }
 }
