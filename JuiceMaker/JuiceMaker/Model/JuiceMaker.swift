87d288de0dbaa2c46312debc3bdced9a1bacb03f
ohsg0226 <ohsg0226@naver.com>
2021-06-14 19:25:04 +0900

    chore: 주석제거

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -37,29 +37,5 @@ struct JuiceMaker {
             try fruitStore.decreaseStock(amount: 3, fruit: fruitStore.mango)
         }
     }
-
-//    func makeStrawberryJuice() throws {
-//        try fruitStore.decreaseStock(amount: 16, fruit: fruitStore.strawberry)
-//    }
-//    func makeBananaJuice() throws {
-//        try fruitStore.decreaseStock(amount: 2, fruit: fruitStore.banana)
-//    }
-//    func makeKiwiJuice() throws {
-//        try fruitStore.decreaseStock(amount: 3, fruit: fruitStore.kiwi)
-//    }
-//    func makePineappleJuice() throws {
-//        try fruitStore.decreaseStock(amount: 2, fruit: fruitStore.pineapple)
-//    }
-//    func makeMangoJuice() throws {
-//        try fruitStore.decreaseStock(amount: 3, fruit: fruitStore.mango)
-//    }
-//    func makeStrawberryBananaJuice() throws {
-//        try fruitStore.decreaseStock(amount: 10, fruit: fruitStore.strawberry)
-//        try fruitStore.decreaseStock(amount: 1, fruit: fruitStore.banana)
-//    }
-//    func makeMangoKiwiJuice() throws {
-//        try fruitStore.decreaseStock(amount: 2, fruit: fruitStore.mango)
-//        try fruitStore.decreaseStock(amount: 1, fruit: fruitStore.kiwi)
-//    }
 }
 
