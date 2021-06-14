3c43ff97d71a2fa64587fc9535440536fa5285bc
YongwooKim <keepingitflow@gmail.com>
2021-06-14 23:20:44 +0900

    feat : 하나의 IBAction에서 UIButton 이용해서 Juice case 얻기 구현
    7개의 버튼 IBAction을 하나로 구현하고 동작시 sender를 이용해서 Juice case를 얻는 형태로 구현했습니다.

--- JuiceMaker/JuiceMaker/Controller/MainViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -13,12 +13,17 @@ class MainViewController: UIViewController {
     @IBOutlet weak var kiwiLabel: UILabel!
     @IBOutlet weak var pineappleLabel: UILabel!
 
+    let maker = JuiceMaker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func juiceOrder(_ sender: UIButton) {
+        guard let titleLabel = sender.titleLabel else { print("버튼 타이틀 에러"); return }
+        guard let text = titleLabel.text, let juice = Juice(rawValue: text) else { print("쥬스 구분실패"); return }
+//        print(juice)
+        maker.makeJuice(juice)
     }
     
 }
