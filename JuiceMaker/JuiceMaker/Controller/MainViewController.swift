b1381d55207d033c5d5e741846f4cc7bd5f18265
taeyoung-Kim-KR <kty72301@naver.com>
2021-06-14 23:09:10 +0900

    feat : 수정 뷰 컨트롤러 생성 및 UI 수정
    메인과 수정하는 뷰컨트롤러 분리. 과일 UI레이블 생성. UI버튼 생성 및 연결

---
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  JuiceMaker - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func juiceOrder(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ JuiceMaker/JuiceMaker/Controller/ModifiyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ModifiyViewController.swift
+//  JuiceMaker
+//
+//  Created by 김태영 on 2021/06/14.
+//
+
+import UIKit
+
+class ModifiyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
