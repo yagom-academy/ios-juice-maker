4bf5e95ee2b27552fadb4226718dac2bad982256
taelee <ritmakid@gmail.com>
2021-06-14 16:40:02 +0900

    refactor: alert을 함수로 매번 생성하지 않고 한번만 생성해서 매번 재사용할 수 있도록 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,17 +7,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let juiceMaker = JuiceMaker()
     @IBOutlet weak var strawberryStockLabel: UILabel!
     @IBOutlet weak var bananaStockLabel: UILabel!
     @IBOutlet weak var pineappleStockLabel: UILabel!
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
+    let juiceMaker = JuiceMaker()
+    var successOrderAlert: UIAlertController?
+    var outOfStockErrorAlert: UIAlertController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(updateStockLabels), name: isChangedFruit, object: nil)
         updateStockLabels()
+        successOrderAlert = createSuccessOrderAlert()
+        outOfStockErrorAlert = createOutOfStockErrorAlert()
     }
     
     @objc func updateStockLabels() {
@@ -31,25 +35,6 @@ class ViewController: UIViewController {
             return
         }
     }
-    
-    func showOutOfStockErrorAlert() {
-        let alert = UIAlertController(title: "재료가 모자라요. 재고를 수정할까요?", message: nil, preferredStyle: .alert)
-        let yesAction = UIAlertAction(title: "예", style: .default, handler : { (action) in
-            guard let changeStockVC = self.storyboard?.instantiateViewController(identifier: "changeStock") else { return }
-            self.present(changeStockVC, animated: true, completion: nil)
-        })
-        let noAction = UIAlertAction(title: "아니오", style: .cancel, handler : nil)
-        alert.addAction(noAction)
-        alert.addAction(yesAction)
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func showSuccessOrderAlert(title juice: Juice) {
-        let alert = UIAlertController(title: "\(juice.rawValue) 쥬스 나왔습니다! 맛있게 드세요!", message: nil, preferredStyle: .alert)
-        let checkAction = UIAlertAction(title: "확인", style: .default, handler : nil )
-        alert.addAction(checkAction)
-        present(alert, animated: true, completion: nil)
-    }
 
     @IBAction func touchUpOrderButton(_ sender: UIButton) {
         guard let juiceName = sender.titleLabel?.text?.components(separatedBy: " ").first,
@@ -58,7 +43,7 @@ class ViewController: UIViewController {
         }
         do {
             try juiceMaker.orderJuice(name: juice)
-            showSuccessOrderAlert(title: juice)
+            showSuccessOrderAlert(for: juiceName)
         } catch FruitStoreError.outOfStock {
             showOutOfStockErrorAlert()
         } catch {
@@ -67,3 +52,34 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController {
+    func createSuccessOrderAlert() -> UIAlertController {
+        let alert = UIAlertController(title: "", message: nil, preferredStyle: .alert)
+        let checkAction = UIAlertAction(title: "확인", style: .default, handler : nil )
+        alert.addAction(checkAction)
+        return alert
+    }
+    
+    func createOutOfStockErrorAlert() -> UIAlertController {
+        let alert = UIAlertController(title: "재료가 모자라요. 재고를 수정할까요?", message: nil, preferredStyle: .alert)
+        let yesAction = UIAlertAction(title: "예", style: .default, handler : { (action) in
+            guard let changeStockVC = self.storyboard?.instantiateViewController(identifier: "changeStock") else { return }
+            self.present(changeStockVC, animated: true, completion: nil)
+        })
+        let noAction = UIAlertAction(title: "아니오", style: .cancel, handler : nil)
+        alert.addAction(noAction)
+        alert.addAction(yesAction)
+        return alert
+    }
+    
+    func showSuccessOrderAlert(for juiceName: String) {
+        guard let alert = successOrderAlert else { return }
+        alert.title = "\(juiceName) 쥬스 나왔습니다! 맛있게 드세요!"
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showOutOfStockErrorAlert() {
+        guard let alert = outOfStockErrorAlert else { return }
+        present(alert, animated: true, completion: nil)
+    }
+}
