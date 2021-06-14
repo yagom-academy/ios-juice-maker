bd2c0182346a63a78c732fe1c17b05336accffd8
yoonju lee <rsyjlee@gmail.com>
2021-06-14 20:32:05 +0900

    feat: 과일 재고 UIlabel 연결 및 text에 초기 재고 반영
    - setInitialStock() 함수에서 UILabel에 각 과일의 재고 업데이트

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,8 +7,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var strawberryStock: UILabel!
+    @IBOutlet weak var bananaStock: UILabel!
+    @IBOutlet weak var pineappleStock: UILabel!
+    @IBOutlet weak var kiwiStock: UILabel!
+    @IBOutlet weak var mangoStock: UILabel!
+    
+    let fruitStore = FruitStore.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setInitialStock(of: .strawberry, label: strawberryStock)
+        setInitialStock(of: .banana, label: bananaStock)
+        setInitialStock(of: .pineapple, label: pineappleStock)
+        setInitialStock(of: .kiwi, label: kiwiStock)
+        setInitialStock(of: .mango, label: mangoStock)
+    }
+    
+    func setInitialStock(of fruit: Fruit, label: UILabel) {
+        guard let initialStock = try? fruitStore.showStockLeft(fruit: fruit) else {
+            return
+        }
+        label.text = String(initialStock)
     }
 }
 
