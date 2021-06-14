6b7480c6228922a39b8fcc565e4863bf9402e8cc
taeyoung-Kim-KR <kty72301@naver.com>
2021-06-14 18:20:58 +0900

    feat : currentStock 에러 처리
    반환값을 옵셔널로 주거나 throw로 에러처리를 주는 방식 보다 재고 부족과 같은 상황으로 처리했습니다.

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,6 +9,7 @@ import Foundation
 // 과일 타입
 class FruitStore {
     var fruitStocks: Dictionary<Fruit, Int>
+    let emptyQuantity = 0
     
     init() {
         fruitStocks = [.strawberry:10, .banana:10, .pineapple:10, .kiwi:10, .mango:10]
@@ -17,7 +18,7 @@ class FruitStore {
     func currentStock(_ fruit: Fruit) -> Int {
         guard let stock = fruitStocks[fruit] else {
             print("재고에 존재하지 않는 과일")
-            return
+            return emptyQuantity
         }
         
         return stock
