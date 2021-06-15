f604c04ba54190cbc838c0bc9fe2e890f07a9df3
jgkim1008 <rlawnsrjs100@naver.com>
2021-06-16 01:12:07 +0900

    refactor : 알수없는 오류발생시 오류내용을 알려주는 alert 추가

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -59,21 +59,22 @@ class ViewController: UIViewController {
         do {
             try juiceMaker.makeJuice(of: selectMenu)
                 currentStockOfFruits()
-                alertMessage(Juice: selectMenu , makeJuice: true)
+                alertMessage(juice: selectMenu , makeJuice: true, unknownErrorMessage: nil)
             } catch ErrorCase.outOfStock {
-                alertMessage(Juice: selectMenu, makeJuice: false)
+                alertMessage(juice: selectMenu, makeJuice: false, unknownErrorMessage: nil)
             } catch {
-                print("Unexpected error: \(error).")
+                alertMessage(juice: nil, makeJuice: false, unknownErrorMessage: "Unexpected error: \(error).")
             }
     }
     
-    func alertMessage(Juice: JuiceMenu, makeJuice: Bool) {
+    func alertMessage(juice: JuiceMenu?, makeJuice: Bool, unknownErrorMessage: String?) {
         if makeJuice == true {
-            let sucessAlert = UIAlertController(title: "주문하신 \(Juice.rawValue) 나왔습니다", message: nil, preferredStyle: UIAlertController.Style.alert)
+            guard let juice = juice else { return }
+            let sucessAlert = UIAlertController(title: "주문하신 \(juice.rawValue) 나왔습니다", message: nil, preferredStyle: UIAlertController.Style.alert)
             let defaultAction = UIAlertAction(title: "OK", style: .destructive, handler : nil)
             sucessAlert.addAction(defaultAction)
             present(sucessAlert, animated: true, completion: nil)
-        } else {
+        } else if makeJuice == false && unknownErrorMessage == nil {
             let failAlert = UIAlertController(title: "재료가 모자라요. 재고를 수정할까요?", message: nil, preferredStyle: UIAlertController.Style.alert)
             let okAction = UIAlertAction(title: "OK", style: .default, handler : { action in
                 guard let moveIntoModifyStock = self.storyboard?.instantiateViewController(withIdentifier: "storage") else { return }
@@ -82,6 +83,12 @@ class ViewController: UIViewController {
             failAlert.addAction(cancel)
             failAlert.addAction(okAction)
             present(failAlert, animated: true, completion: nil)
+        } else {
+            guard let unknownErrorMessage = unknownErrorMessage else { return }
+            let errorAlert = UIAlertController(title: "알수없는 오류가 발생했습니다.", message: unknownErrorMessage, preferredStyle: UIAlertController.Style.alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .destructive, handler : nil)
+            errorAlert.addAction(defaultAction)
+            present(errorAlert, animated: true, completion: nil)
         }
     }
 }
