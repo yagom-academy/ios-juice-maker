02a17b7044a93aa1aee920f51c942a18153c0d46
YongwooKim <keepingitflow@gmail.com>
2021-06-15 10:51:15 +0900

    docs : 열거체 코드만 모아놓은 파일생성
    Fruit, Juice, JuiceMakerError 열거형 타입선언 코드를 따로 EnumTypes.swift 파일로 변환해놓음

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,62 +7,6 @@
 import Foundation
 
 // 쥬스 메이커 타입
-enum Fruit {
-    case strawberry
-    case banana
-    case pineapple
-    case kiwi
-    case mango
-}
-
-enum Juice: String, CustomStringConvertible {
-    case strawberryBananaJuice = "딸바쥬스 주문"
-    case mangoKiwiJuice = "망키쥬스 주문"
-    case strawberryJuice = "딸기쥬스 주문"
-    case bananaJuice = "바나나쥬스 주문"
-    case mangoJuice = "망고쥬스 주문"
-    case kiwiJuice = "키위쥬스 주문"
-    case pineappleJuice = "파인애플쥬스 주문"
-    
-    var description: String {
-        switch self {
-        case .strawberryJuice:
-            return "딸기"
-        case .bananaJuice:
-            return "바나나"
-        case .pineappleJuice:
-            return "파인애플"
-        case .kiwiJuice:
-            return "키위"
-        case .mangoJuice:
-            return "망고"
-        case .strawberryBananaJuice:
-            return "딸바"
-        case .mangoKiwiJuice:
-            return "망키"
-        }
-    }
-    
-    var ingredients: Dictionary<Fruit, Int> {
-        switch self {
-        case .strawberryJuice:
-            return [.strawberry:16]
-        case .bananaJuice:
-            return [.banana:2]
-        case .pineappleJuice:
-            return [.pineapple:2]
-        case .kiwiJuice:
-            return [.kiwi:3]
-        case .mangoJuice:
-            return [.mango:3]
-        case .strawberryBananaJuice:
-            return [.strawberry:10, .banana:1]
-        case .mangoKiwiJuice:
-            return [.mango:2, .kiwi:1]
-        }
-    }
-}
-
 struct JuiceMaker {
     let store = FruitStore()
     
@@ -86,8 +30,4 @@ struct JuiceMaker {
             }
         }
     }
-}
-
-enum JuiceMakerError: Error {
-    case outOfStock
 }
