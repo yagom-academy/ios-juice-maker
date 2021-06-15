6c92564ba4c7007e19e6ea95a33a8db705941117
jgkim1008 <rlawnsrjs100@naver.com>
2021-06-15 18:37:01 +0900

    feat : 쥬스 주문시 상황에 맞는 alert 기능 추가

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -19,5 +19,22 @@ class ViewController: UIViewController {
         self.navigationController?.pushViewController(moveIntoModifyStock, animated: true)
     }
     
+    func alertMessage(makeJuice: Bool) {
+        if makeJuice == true {
+            let sucessAlert = UIAlertController(title: "주문하신 \(JuiceMenu.strawberryJuice.rawValue) 나왔습니다", message: nil, preferredStyle: UIAlertController.Style.alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .destructive, handler : nil)
+            sucessAlert.addAction(defaultAction)
+            present(sucessAlert, animated: true, completion: nil)
+        } else {
+            let failAlert = UIAlertController(title: "재료가 모자라요. 재고를 수정할까요?", message: nil, preferredStyle: UIAlertController.Style.alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler : { action in
+                guard let moveIntoModifyStock = self.storyboard?.instantiateViewController(withIdentifier: "storage") else { return }
+                self.navigationController?.pushViewController(moveIntoModifyStock, animated: true)})
+            let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler : nil)
+            failAlert.addAction(cancel)
+            failAlert.addAction(okAction)
+            present(failAlert, animated: true, completion: nil)
+        }
+    }
 }
 
