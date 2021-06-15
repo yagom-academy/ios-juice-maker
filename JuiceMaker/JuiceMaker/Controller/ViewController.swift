c6eaafb7f919294bc9996fa6ae1a0a68a73ec19a
jgkim1008 <rlawnsrjs100@naver.com>
2021-06-15 22:55:23 +0900

    feat : 메뉴 클릭시 주스 제작 함수 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -13,15 +13,49 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-
+    
+    let juiceMaker = JuiceMaker()
+    
     @IBAction func touchUpModifyStock(_ sender: UIBarButtonItem) {
         guard let moveIntoModifyStock = self.storyboard?.instantiateViewController(withIdentifier: "storage") else { return }
         self.navigationController?.pushViewController(moveIntoModifyStock, animated: true)
     }
     
-    func alertMessage(makeJuice: Bool) {
+    @IBAction func touchUpJuiceMenu(_ sender: UIButton) {
+        var selectMenu: JuiceMenu?
+    
+        switch sender.tag {
+        case 0:
+            selectMenu = .bananaJuice
+        case 1:
+            selectMenu = .strawberryBananaJuice
+        case 2:
+            selectMenu = .strawberryJuice
+        case 3:
+            selectMenu = .pineappleJuice
+        case 4:
+            selectMenu = .mangoKiwiJuice
+        case 5:
+            selectMenu = .kiwiJuice
+        case 6:
+            selectMenu = .mangoJuice
+        default:
+            print("잘못된 입력입니다.")
+        }
+        guard let selectMenu = selectMenu else { return }
+        do {
+            try juiceMaker.makeJuice(of: selectMenu)
+                alertMessage(Juice: selectMenu , makeJuice: true)
+            } catch ErrorCase.outOfStock {
+                alertMessage(Juice: selectMenu, makeJuice: false)
+            } catch {
+                print("Unexpected error: \(error).")
+            }
+    }
+    
+    func alertMessage(Juice: JuiceMenu, makeJuice: Bool) {
         if makeJuice == true {
-            let sucessAlert = UIAlertController(title: "주문하신 \(JuiceMenu.strawberryJuice.rawValue) 나왔습니다", message: nil, preferredStyle: UIAlertController.Style.alert)
+            let sucessAlert = UIAlertController(title: "주문하신 \(Juice.rawValue) 나왔습니다", message: nil, preferredStyle: UIAlertController.Style.alert)
             let defaultAction = UIAlertAction(title: "OK", style: .destructive, handler : nil)
             sucessAlert.addAction(defaultAction)
             present(sucessAlert, animated: true, completion: nil)
