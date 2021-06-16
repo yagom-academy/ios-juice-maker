4cc19f2be6be3feb46feb8bc8b23ba3eea84ff8a
brian2931 <sem789456@gmail.com>
2021-06-16 12:41:00 +0900

    feat: add functions, buttons and Label for UI
    feat: add modifyStockButton function to switch screen to
    	modifyStockViewController
    feat: set some buttons and lables for UI

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -12,7 +12,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func modifyStockButton(_ sender: Any) {
+        let showModifyStockView = self.storyboard?.instantiateViewController(withIdentifier: "modifyStockViewController")
+        
+        showModifyStockView?.modalTransitionStyle = .coverVertical
+        self.present(showModifyStockView!, animated: true, completion: nil)
 
+    }
+    
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pinepappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    
+    @IBAction func orderStrawberryBananaJuiceButton(_ sender: Any) {
+    }
+    @IBAction func orderMangoKiwiJuiceButton(_ sender: Any) {
+    }
+    @IBAction func orderStrawberryJuice(_ sender: Any) {
+    }
+    
+    @IBAction func orderBananaJuiceButton(_ sender: Any) {
+    }
+    
+    @IBAction func orderPineappleJuiceButton(_ sender: Any) {
+    }
+    
+    @IBAction func orderKiwiJuiceButton(_ sender: Any) {
+    }
+    
+    @IBAction func orderMangoJuiceButton(_ sender: Any) {
+    }
 }
 
