fe266b25284142e247491258d66c5f8fa7cdc0c9
yaewonLee <yw990324@gmail.com>
2021-06-17 18:33:18 +0900

    feat: 닫기 버튼 클릭시 초기 화면으로 넘어가는 기능 구현 및 화면 제목 추가

---
+++ JuiceMaker/JuiceMaker/Controller/ViewController2.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController2.swift
+//  JuiceMaker
+//
+//  Created by 이예원 on 2021/06/17.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeBtn(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
