27c657d77024ff1d0229d0af3fb7fbc2d34a8fbc
blanche37 <wkddbsghk16@gmail.com>
2021-06-17 22:49:40 +0900

    feat: 재고추가화면에 닫기버튼기능구현
    StockManagerViewController추가
    @IBAction touchUpDismissButton() 추가

---
+++ JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StockManagerViewController.swift
+//  JuiceMaker
+//
+//  Created by yun on 2021/06/17.
+//
+
+import UIKit
+
+class StockManagerViewController: UIViewController {
+    
+    @IBAction func touchUpDismissButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
