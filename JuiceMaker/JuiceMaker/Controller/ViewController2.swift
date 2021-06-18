4d208cfd3d1dea3c11a5e617ce51bec7f976f4c4
yaewonLee <yw990324@gmail.com>
2021-06-18 10:01:13 +0900

    feat: 과일별 스탭퍼 기능 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController2.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController2.swift
@@ -13,12 +13,26 @@ class ViewController2: UIViewController {
     @IBOutlet weak var pineappleStockLabel: UILabel!
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
+ 
     @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        strawberryStepper.value = Double(juiceMaker.getAmount(.strawberry))
+        setFruitLabel()
         showFruitLabel()
+    }
+    
+    func setFruitLabel() {
+        strawberryStepper.value = Double(juiceMaker.getAmount(.strawberry))
+        bananaStepper.value = Double(juiceMaker.getAmount(.banana))
+        pineappleStepper.value = Double(juiceMaker.getAmount(.pineapple))
+        kiwiStepper.value = Double(juiceMaker.getAmount(.kiwi))
+        mangoStepper.value = Double(juiceMaker.getAmount(.mango))
     }
     
     @IBAction func closeBtn(_ sender: UIBarButtonItem) {
@@ -33,8 +47,25 @@ class ViewController2: UIViewController {
         mangoStockLabel.text = String(juiceMaker.getAmount(.mango))
     }
     
-    @IBAction func strawberryStepper(_ sender: UIStepper) {
-        juiceMaker.stockPlus(.strawberry, stock: Int(sender.value))
-        showFruitLabel()
+    @IBAction func fruitStepper(_ sender: UIStepper) {
+        switch sender {
+        case strawberryStepper:
+            juiceMaker.stockPlus(.strawberry, stock: Int(sender.value))
+            showFruitLabel()
+        case bananaStepper:
+            juiceMaker.stockPlus(.banana, stock: Int(sender.value))
+            showFruitLabel()
+        case pineappleStepper:
+            juiceMaker.stockPlus(.pineapple, stock: Int(sender.value))
+            showFruitLabel()
+        case kiwiStepper:
+            juiceMaker.stockPlus(.kiwi, stock: Int(sender.value))
+            showFruitLabel()
+        case mangoStepper:
+            juiceMaker.stockPlus(.mango, stock: Int(sender.value))
+            showFruitLabel()
+        default:
+            return
+        }
     }
 }
