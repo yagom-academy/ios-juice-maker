608890b810b23caa054907ae995c1f1ef626c67f
soo941226 <rhrnakrnakr@naver.com>
2021-06-18 11:10:40 +0900

    Feat: add feature about managing stocks on UIStepper

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
                 style: .default,
                 handler: { _ in
                     self.goNextView()
-					
+                    
                 }
             )
         )
@@ -37,31 +37,117 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
         changeTextOn(label: strawberryStockLabel, about: .strawberry)
         changeTextOn(label: bananaStockLabel, about: .banana)
         changeTextOn(label: kiwiStockLabel, about: .kiwi)
         changeTextOn(label: mangoStockLabel, about: .mango)
         changeTextOn(label: pineappleStockLabel, about: .pineapple)
+        
+        guard let strawberryStepper = strawberryStepper,
+              let strawberryStore = findFruitStore(about: Fruit.strawberry),
+              let bananaStepper = bananaStepper,
+              let bananaStore = findFruitStore(about: Fruit.banana),
+              let pineappleStepper = pineappleStepper,
+              let pineappleStore = findFruitStore(about: Fruit.pineapple),
+              let kiwiStepper = kiwiStepper,
+              let kiwiStore = findFruitStore(about: Fruit.kiwi),
+              let mangoStepper = mangoStepper,
+              let mangoStore = findFruitStore(about: Fruit.mango)
+              else {
+                return
+              }
+        
+        strawberryStepper.value = Double( strawberryStore.stock )
+        bananaStepper.value = Double( bananaStore.stock )
+        pineappleStepper.value = Double( pineappleStore.stock )
+        kiwiStepper.value = Double( kiwiStore.stock )
+        mangoStepper.value = Double( mangoStore.stock )
     }
-	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		
-	}
-	
+    
+    func findFruitStore(about: Fruit) -> FruitStore? {
+        return ViewController
+            .juiceMaker
+            .fruitStores
+            .filter({ store in
+                store.name == about
+            }).first
+    }
+    
+    @IBAction func changedStrawberryStock(_ sender: UIStepper) {
+        guard let store = findFruitStore(about: Fruit.strawberry) else {
+            return
+        }
+        
+        store.setStock(amount: Int(sender.value) - store.stock )
+        
+        changeTextOn(label: strawberryStockLabel, about: .strawberry)
+    }
+    
+    @IBAction func changedPineappleStock(_ sender: UIStepper) {
+        guard let store = findFruitStore(about: Fruit.pineapple) else {
+            return
+        }
+        
+        store.setStock(amount: Int(sender.value) - store.stock )
+        
+        changeTextOn(label: pineappleStockLabel, about: .pineapple)
+    }
+    
+    
+    @IBAction func changedMangoStock(_ sender: UIStepper) {
+        guard let store = findFruitStore(about: Fruit.mango) else {
+            return
+        }
+        
+        store.setStock(amount: Int(sender.value) - store.stock )
+        
+        changeTextOn(label: mangoStockLabel, about: .mango)
+    }
+    
+    
+    @IBAction func changedKiwiStock(_ sender: UIStepper) {
+        guard let store = findFruitStore(about: Fruit.kiwi) else {
+            return
+        }
+        
+        store.setStock(amount: Int(sender.value) - store.stock )
+        
+        changeTextOn(label: kiwiStockLabel, about: .kiwi)
+    }
+    
+    
+    @IBAction func changedBananaStock(_ sender: UIStepper) {
+        guard let store = findFruitStore(about: Fruit.banana) else {
+            return
+        }
+        
+        store.setStock(amount: Int(sender.value) - store.stock )
+        
+        changeTextOn(label: bananaStockLabel, about: .banana)
+    }
+    
     @IBOutlet weak var strawberryStockLabel: UILabel!
     @IBOutlet weak var bananaStockLabel: UILabel!
     @IBOutlet weak var pineappleStockLabel: UILabel!
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
     
-	
-	
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
     @IBAction func makeStrawNanaJuice(_ sender: UIButton) {
         let currentMenu: JuiceMenu = .strawNanaJuice
         
         do {
-			try ViewController.juiceMaker.orderJuice(menu: currentMenu)
+            try ViewController.juiceMaker.orderJuice(menu: currentMenu)
             
             changeTextOn(label: strawberryStockLabel, about: .strawberry)
             changeTextOn(label: bananaStockLabel, about: .banana)
@@ -76,10 +162,10 @@ class ViewController: UIViewController {
         let currentMenu: JuiceMenu = .strawberryJuice
         
         do {
-			try ViewController.juiceMaker.orderJuice(menu: currentMenu)
+            try ViewController.juiceMaker.orderJuice(menu: currentMenu)
             
             changeTextOn(label: strawberryStockLabel, about: .strawberry)
-			            
+            
             showCompleteAlert(about: currentMenu)
         } catch {
             self.present(stockAlert, animated: false, completion: nil)
@@ -90,7 +176,7 @@ class ViewController: UIViewController {
         let currentMenu: JuiceMenu = .mangoKiwiJuice
         
         do {
-			try ViewController.juiceMaker.orderJuice(menu: currentMenu)
+            try ViewController.juiceMaker.orderJuice(menu: currentMenu)
             
             changeTextOn(label: mangoStockLabel, about: .mango)
             changeTextOn(label: kiwiStockLabel, about: .kiwi)
@@ -105,7 +191,7 @@ class ViewController: UIViewController {
         let currentMenu: JuiceMenu = .mangoJuice
         
         do {
-			try ViewController.juiceMaker.orderJuice(menu: currentMenu)
+            try ViewController.juiceMaker.orderJuice(menu: currentMenu)
             
             changeTextOn(label: mangoStockLabel, about: .mango)
             
@@ -120,7 +206,7 @@ class ViewController: UIViewController {
         let currentMenu: JuiceMenu = .kiwiJuice
         
         do {
-			try ViewController.juiceMaker.orderJuice(menu: currentMenu)
+            try ViewController.juiceMaker.orderJuice(menu: currentMenu)
             
             changeTextOn(label: kiwiStockLabel, about: .kiwi)
             
@@ -134,7 +220,7 @@ class ViewController: UIViewController {
         let currentMenu: JuiceMenu = .bananaJuice
         
         do {
-			try ViewController.juiceMaker.orderJuice(menu: currentMenu)
+            try ViewController.juiceMaker.orderJuice(menu: currentMenu)
             
             changeTextOn(label: bananaStockLabel, about: .banana)
             
@@ -148,7 +234,7 @@ class ViewController: UIViewController {
         let currentMenu: JuiceMenu = .pineappleJuice
         
         do {
-			try ViewController.juiceMaker.orderJuice(menu: currentMenu)
+            try ViewController.juiceMaker.orderJuice(menu: currentMenu)
             
             changeTextOn(label: pineappleStockLabel, about: .pineapple)
             
@@ -160,17 +246,16 @@ class ViewController: UIViewController {
     
     @IBAction func touchOnNextButton(){
         goNextView()
-		self.navigationItem.leftBarButtonItem = nil
-
+        self.navigationItem.leftBarButtonItem = nil
+        
     }
     
     func changeTextOn(label: UILabel, about fruit: Fruit) {
-		let fruitStore = ViewController.juiceMaker.fruitStores.filter { $0.name == fruit }.first
+        let fruitStore = ViewController.juiceMaker.fruitStores.filter { $0.name == fruit }.first
         if let stockLeft = fruitStore?.stock {
             label.text = "\(stockLeft)"
         }
     }
-    
     
     func showCompleteAlert(about: JuiceMenu){
         let alert = UIAlertController(title: "확인", message: "\(about.rawValue)가 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
