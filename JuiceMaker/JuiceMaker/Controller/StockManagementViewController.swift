51d527f37bf298e767ab57464472a392bb8ce2f1
hrjy6278 <hrjy6278@naver.com>
2021-06-18 17:03:49 +0900

    feat: 재고관리 화면에서 Label 과 Stepper에 대한 초기설정 구현

--- JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
@@ -7,12 +7,50 @@
 
 import UIKit
 
+//MARK:- Stored Property and LifeCycle
 class StockManagementViewController: UIViewController {
+    
+    @IBOutlet private var numberOfStrawberryLabel: UILabel!
+    @IBOutlet private var numberOfBananaLabel: UILabel!
+    @IBOutlet private var numberOfPineAppleLabel: UILabel!
+    @IBOutlet private var numberOfKiwiLabel: UILabel!
+    @IBOutlet private var numberOfMangoLabel: UILabel!
+    
+    @IBOutlet private var stockOfStrawberryStepper: UIStepper!
+    @IBOutlet private var stockOfBananaStepper: UIStepper!
+    @IBOutlet private var stockOfPineAppleStepper: UIStepper!
+    @IBOutlet private var stockOfKiwiStepper: UIStepper!
+    @IBOutlet private var stockOfMangoStepper: UIStepper!
     
     var fruitStore: FruitStore?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        showNumberOnLabel(fruits: fruitStore?.inventory ?? [:])
     }
 
+}
+
+//MARK:- Label and Stepper Setting
+extension StockManagementViewController {
+    private func setUp(number: Int, on label: UILabel) {
+        label.text = String(number)
+    }
+    
+    private func showNumberOnLabel(fruits: [Fruit: Int]) {
+        for fruit in fruits {
+            switch fruit.key {
+            case .strawberry:
+                setUp(number: fruit.value, on: numberOfStrawberryLabel)
+            case .banana:
+                setUp(number: fruit.value, on: numberOfBananaLabel)
+            case .pineapple:
+                setUp(number: fruit.value, on: numberOfPineAppleLabel)
+            case .kiwi:
+                setUp(number: fruit.value, on: numberOfKiwiLabel)
+            case .mango:
+                setUp(number: fruit.value, on: numberOfMangoLabel)
+            }
+        }
+    }
 }
