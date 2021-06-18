0e32fbc18a5ae837db53b816661fe3ad5ab92eb4
jaemuyeo <jemking94@gmail.com>
2021-06-18 20:16:30 +0900

    Feat. StockChangeView 화면에 현재 재고 수량 표시

--- JuiceMaker/JuiceMaker/Controller/StockChangeViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockChangeViewController.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 class StockChangeViewController: UIViewController {
-
+    @IBOutlet var stockLabels: [StockLabel]!
+    let juiceMaker = JuiceMaker.juiceMaker
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateLabel()
     }
     
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func updateLabel() {
+        StockLabel.update(numbers: stockLabels, of: juiceMaker.fruitStore)
     }
     
 }
