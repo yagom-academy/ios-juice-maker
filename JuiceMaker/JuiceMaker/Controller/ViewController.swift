44389bcb578c8a489ba9897e80d9c55aa9438938
taelee <ritmakid@gmail.com>
2021-06-18 12:46:02 +0900

    refactor: alert을 하나의 함수에서 인자로 2개의 알럿을 별도 관리

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,6 +7,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    enum KindOfAlert {
+        case successOrder
+        case outOfStock
+    }
+    
     @IBOutlet weak var strawberryStockLabel: UILabel!
     @IBOutlet weak var bananaStockLabel: UILabel!
     @IBOutlet weak var pineappleStockLabel: UILabel!
@@ -14,18 +19,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var mangoStockLabel: UILabel!
     
     let juiceMaker = JuiceMaker.shared
-    var successOrderAlert: UIAlertController?
-    var outOfStockErrorAlert: UIAlertController?
+    var juiceMakerAlert: UIAlertController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateStockLabels()
-        successOrderAlert = createSuccessOrderAlert()
-        outOfStockErrorAlert = createOutOfStockErrorAlert()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("hi")
         guard let vc = segue.destination.children.first as? ChangeStockViewController else {
             return
         }
@@ -44,6 +45,21 @@ class ViewController: UIViewController {
         }
     }
     
+    func createAlert(kind: KindOfAlert) {
+        if kind == .successOrder {
+            juiceMakerAlert = createSuccessOrderAlert()
+        } else if kind == .outOfStock {
+            juiceMakerAlert = createOutOfStockErrorAlert()
+        }
+    }
+    
+    func showAlert(name: KindOfAlert, juiceName: String = "") {
+        createAlert(kind: name)
+        guard let alert = juiceMakerAlert, let title = alert.title else { return }
+        alert.title = juiceName + title
+        present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func touchUpOrderButton(_ sender: UIButton) {
         guard let juiceName = sender.titleLabel?.text?.components(separatedBy: " ").first,
               let juice = Juice(rawValue: juiceName) else {
@@ -52,9 +68,9 @@ class ViewController: UIViewController {
         do {
             try juiceMaker.orderJuice(name: juice)
             updateStockLabels()
-            showSuccessOrderAlert(for: juiceName)
+            showAlert(name: .successOrder, juiceName: juiceName)
         } catch FruitStoreError.outOfStock {
-            showOutOfStockErrorAlert()
+            showAlert(name: .outOfStock)
         } catch {
             print(error.localizedDescription)
         }
@@ -63,7 +79,7 @@ class ViewController: UIViewController {
 
 extension ViewController {
     func createSuccessOrderAlert() -> UIAlertController {
-        let alert = UIAlertController(title: "", message: nil, preferredStyle: .alert)
+        let alert = UIAlertController(title: " 나왔습니다! 맛있게 드세요", message: nil, preferredStyle: .alert)
         let checkAction = UIAlertAction(title: "확인", style: .default, handler : nil )
         alert.addAction(checkAction)
         return alert
@@ -81,17 +97,6 @@ extension ViewController {
         alert.addAction(noAction)
         alert.addAction(yesAction)
         return alert
-    }
-    
-    func showSuccessOrderAlert(for juiceName: String) {
-        guard let alert = successOrderAlert else { return }
-        alert.title = "\(juiceName) 쥬스 나왔습니다! 맛있게 드세요!"
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func showOutOfStockErrorAlert() {
-        guard let alert = outOfStockErrorAlert else { return }
-        present(alert, animated: true, completion: nil)
     }
 }
 
