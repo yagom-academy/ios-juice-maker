596f41c2bcd8ca23b41ed704e2891502060b7da0
KimWanki <seoulkwk95@gmail.com>
2021-06-21 00:51:59 +0900

    refactor : Action별 Button 분리, private 키워드 추가

---
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakeViewController.swift
@@ -0,0 +1,156 @@
+//
+//  JuiceMaker - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+extension Notification.Name {
+    static let recieveOrderMenuNotification = Notification.Name("DidRecieveOrderMenu")
+    static let recieveStockChangeNotification = Notification.Name("DidReceiveStockChange")
+}
+
+class JuiceMakeViewController: UIViewController {
+    private let juiceMaker = JuiceMaker()
+    
+    @IBOutlet private weak var strawberryStockLabel: UILabel!
+    @IBOutlet private weak var bananaStockLabel: UILabel!
+    @IBOutlet private weak var fineappleStockLabel: UILabel!
+    @IBOutlet private weak var kiwiStockLabel: UILabel!
+    @IBOutlet private weak var mangoStockLabel: UILabel!
+    
+    @IBOutlet private weak var strawberryJuiceButton: UIButton!
+    @IBOutlet private weak var bananaJuiceButton: UIButton!
+    @IBOutlet private weak var kiwiJuiceButton: UIButton!
+    @IBOutlet private weak var fineappleJuiceButton: UIButton!
+    @IBOutlet private weak var strawberryBananaJuiceButton: UIButton!
+    @IBOutlet private weak var mangoJuiceButton: UIButton!
+    @IBOutlet private weak var mangoKiwiJuiceButton: UIButton!
+    
+    @IBAction func clickStrawberryJuiceButton(_ sender: UIButton) {
+        do {
+            try juiceMaker.makeJuice(menu: .strawberry)
+            self.present(makeJuiceSuccessAlert(menu: .strawberry), animated: true, completion: nil)
+        } catch FruitStoreError.outOfStock {
+            self.present(makeJuiceFailAlert(), animated: true, completion: nil)
+        } catch {
+        }
+    }
+    
+    @IBAction func clickBananaJuiceButton(_ sender: UIButton) {
+        do {
+            try juiceMaker.makeJuice(menu: .banana)
+            self.present(makeJuiceSuccessAlert(menu: .banana), animated: true, completion: nil)
+        } catch FruitStoreError.outOfStock {
+            self.present(makeJuiceFailAlert(), animated: true, completion: nil)
+        } catch {
+        }
+    }
+    
+    @IBAction func clickFineappleJuiceButton(_ sender: UIButton) {
+        do {
+            try juiceMaker.makeJuice(menu: .fineapple)
+            self.present(makeJuiceSuccessAlert(menu: .fineapple), animated: true, completion: nil)
+        } catch FruitStoreError.outOfStock {
+            self.present(makeJuiceFailAlert(), animated: true, completion: nil)
+        } catch {
+        }
+    }
+    
+    @IBAction func clickStrawberryBananaJuiceButton(_ sender: UIButton) {
+        do {
+            try juiceMaker.makeJuice(menu: .strawberryBanana)
+            self.present(makeJuiceSuccessAlert(menu: .strawberryBanana), animated: true, completion: nil)
+        } catch FruitStoreError.outOfStock {
+            self.present(makeJuiceFailAlert(), animated: true, completion: nil)
+        } catch {
+        }
+    }
+    
+    @IBAction func clickKiwiJuiceButton(_ sender: UIButton) {
+        do {
+            try juiceMaker.makeJuice(menu: .kiwi)
+            self.present(makeJuiceSuccessAlert(menu: .kiwi), animated: true, completion: nil)
+        } catch FruitStoreError.outOfStock {
+            self.present(makeJuiceFailAlert(), animated: true, completion: nil)
+        } catch {
+        }
+    }
+    
+    @IBAction func clickMangoJuiceButton(_ sender: UIButton) {
+        do {
+            try juiceMaker.makeJuice(menu: .mango)
+            self.present(makeJuiceSuccessAlert(menu: .mango), animated: true, completion: nil)
+        } catch FruitStoreError.outOfStock {
+            self.present(makeJuiceFailAlert(), animated: true, completion: nil)
+        } catch {
+        }
+    }
+    
+    @IBAction func clickMangoKiwiJuiceButton(_ sender: UIButton) {
+        do {
+            try juiceMaker.makeJuice(menu: .mangoKiwi)
+            self.present(makeJuiceSuccessAlert(menu: .mangoKiwi), animated: true, completion: nil)
+        } catch FruitStoreError.outOfStock {
+            self.present(makeJuiceFailAlert(), animated: true, completion: nil)
+        } catch {
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshFruitStock()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didReceiveStockChangeNotification), name: .recieveStockChangeNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didReceiveMenuMadeNotification), name: .recieveOrderMenuNotification, object: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    @objc private func didReceiveStockChangeNotification(_ notificiation: Notification) {
+        refreshFruitStock()
+    }
+    
+    @objc private func didReceiveMenuMadeNotification(_ notificiation: Notification) {
+        guard let menu: JuiceMaker.JuiceMenu = notificiation.userInfo?["menu"] as? JuiceMaker.JuiceMenu else { return }
+        self.present(makeJuiceSuccessAlert(menu: menu), animated: true, completion: nil)
+    }
+    
+    private func refreshFruitStock(){
+        let fruits = FruitStore.shared.fruitStock
+        for (fruit, amount) in fruits {
+            switch fruit {
+            case.strawberry:
+                strawberryStockLabel.text = "\(amount)"
+            case.banana:
+                bananaStockLabel.text = "\(amount)"
+            case.fineapple:
+                fineappleStockLabel.text = "\(amount)"
+            case.kiwi:
+                kiwiStockLabel.text = "\(amount)"
+            case.mango:
+                mangoStockLabel.text = "\(amount)"
+            }
+        }
+    }
+}
+
+extension JuiceMakeViewController {
+    private func makeJuiceSuccessAlert(menu: JuiceMaker.JuiceMenu) -> UIAlertController {
+        let alert = UIAlertController(title: "\(menu.description()) 쥬스 나왔습니다!", message: "맛있게 드세요!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "예", style: .default, handler: nil))
+        return alert
+    }
+    
+    private func makeJuiceFailAlert() -> UIAlertController {
+        let alert = UIAlertController(title: "재료가 모자라요.", message: "재고를 수정할까요?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("예", comment: "Default action"), style: .default, handler: { _ in
+            guard let nextViewController = self.storyboard?.instantiateViewController(identifier: "StockManageViewController") else { return }
+            self.present(nextViewController, animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("아니오", comment: "Default action"), style: .cancel, handler: nil))
+        return alert
+    }
+}
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,40 +6,40 @@
 
 import Foundation
 
-enum JuiceMenu: String {
-    case strawberry = "딸기쥬스"
-    case banana = "바나나쥬스"
-    case kiwi = "키위쥬스"
-    case fineapple = "파인애플쥬스"
-    case strawberryBanana = "딸바쥬스"
-    case mango = "망고쥬스"
-    case mangoKiwi = "망키쥬스"
-    
-    var recipe: [HandlingFruit: Int] {
-        switch self {
-        case .strawberry:
-            return [.strawberry: 16]
-        case .banana:
-            return [.banana: 10]
-        case .kiwi:
-            return [.kiwi: 3]
-        case .fineapple:
-            return [.fineapple: 2]
-        case .strawberryBanana:
-            return [.strawberry: 10, .banana: 1]
-        case .mango:
-            return [.mango: 3]
-        case .mangoKiwi:
-            return [.mango: 2, .kiwi: 1]
+struct JuiceMaker {
+    enum JuiceMenu: String {
+        case strawberry = "딸기쥬스"
+        case banana = "바나나쥬스"
+        case kiwi = "키위쥬스"
+        case fineapple = "파인애플쥬스"
+        case strawberryBanana = "딸바쥬스"
+        case mango = "망고쥬스"
+        case mangoKiwi = "망키쥬스"
+        
+        var recipe: [HandlingFruit: Int] {
+            switch self {
+            case .strawberry:
+                return [.strawberry: 16]
+            case .banana:
+                return [.banana: 10]
+            case .kiwi:
+                return [.kiwi: 3]
+            case .fineapple:
+                return [.fineapple: 2]
+            case .strawberryBanana:
+                return [.strawberry: 10, .banana: 1]
+            case .mango:
+                return [.mango: 3]
+            case .mangoKiwi:
+                return [.mango: 2, .kiwi: 1]
+            }
+        }
+
+        func description() -> String {
+            return self.rawValue
         }
     }
     
-    func description() -> String {
-        return self.rawValue
-    }
-}
-
-struct JuiceMaker {
     private let store: FruitStore = FruitStore.shared
     
     func makeJuice(menu: JuiceMenu) throws {
