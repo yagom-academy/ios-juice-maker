349ac6e1863e868df9c7a920128beb24cafbc184
July911 <min95911@nate.com>
2021-10-18 23:57:59 +0900

    feat : 과일들을 담은 사용자 정의 타입 Fruit 생성 및 재고를 10개씩 담아주는 initializer 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -4,9 +4,31 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
+
 import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    
+    enum Fruit: CaseIterable {
+        case staryberry
+        case banana
+        case fineapple
+        case kiwi
+        case mango
+    }
+    var defaultStock : Int
+    let allFruits : Array<Fruit>
+    let stock : Array<Int>
+    let fruitStorage : Dictionary<Fruit,Int>
+    init() {
+        defaultStock = 10
+        allFruits = Fruit.allCases
+        stock = Array(repeating: defaultStock, count: allFruits.count)
+        fruitStorage = Dictionary(uniqueKeysWithValues: zip(allFruits, stock))
+    }
 }
+
+
+
+
+
