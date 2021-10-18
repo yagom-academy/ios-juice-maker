ae8a97b9a8ddb03ec06e06018a3fb58095baa5b3
hwangjeha <hyhpwang@gmail.com>
2021-10-18 14:49:07 +0900

    feat: 과일 열거형 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,28 @@
 
 import Foundation
 
-// 과일 저장소 타입
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
+
 class FruitStore {
+    // 프로퍼티 (특성)
+    // 과일의 초기 재고
+    // 딸기 바구니
+    // 바나나 바구니
+    // 파인애플 바구니
+    // 키위 바구니
+    // 망고 바구니
     
+    // 메소드 (기능)
+    // 과일 수량 추가하기
+    // 과일 사용하기
+    // 과일 재고 확인
+        
+    // 이니셜라이져
+    // 과일의 종류를 지정해준다.
 }
