cbb2d7b035fca42cff52bfcb9fdda2e636b6c916
youngwooJang <duddn5906@naver.com>
2021-10-18 18:08:19 +0900

    feat: Juice 열거형 추가
    - Juice 과일 소모량을 반환하는 연산 프로퍼티 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,36 @@
 
 import Foundation
 
-// 쥬스 메이커 타입
+enum Juice {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+    case strawberryBanana
+    case mangoKiwi
+    
+    var recipe: [(Fruit, Int)] {
+        switch self {
+        case .strawberry:
+            return [(.strawberry, 16)]
+        case .banana:
+            return [(.banana, 2)]
+        case .pineapple:
+            return [(.pineapple, 2)]
+        case .kiwi:
+            return [(.kiwi, 3)]
+        case .mango:
+            return [(.mango, 3)]
+        case .strawberryBanana:
+            return [(.strawberry, 10), (.banana, 1)]
+        case .mangoKiwi:
+            return [(.mango, 2), (.kiwi, 1)]
+        }
+    }
+}
+
 struct JuiceMaker {
     
 }
+
