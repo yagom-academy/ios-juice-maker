195408951b5639ac87d60769945e0532677383f2
just1103 <hyojuson1103@gmail.com>
2021-10-18 19:00:43 +0900

    refactor: inventory 타입 변경, Fruit 구조체 분리 #1
    - inventory를 딕셔너리로 구현하면 언래핑이 잦고, 가독성이 떨어져서 배열로 변경
    - 과일과 관련된 상태를 FruitStore 클래스 외부에 별도 구현
    - FruitInformation 구조체의 요소를 Fruit 구조체의 프로퍼티로 변경

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,9 +6,21 @@
 
 import Foundation
 
+struct Fruit {
+    var name: FruitName
+    var count: Int = 10
+    
+    enum FruitName {
+        case 딸기 // rawValue String?
+        case 바나나
+        case 파인애플
+        case 키위
+        case 망고
+    }
+}
+
 // 과일 저장소 타입
 class FruitStore {
-    
     enum FruitStoreError: String, LocalizedError {
         case inValidFruitChoice = "유효하지 않은 선택입니다."
         case lackOfStock = "재료가 부족합니다."
@@ -17,34 +29,12 @@ class FruitStore {
             return rawValue
         }
     }
-    
-    struct FruitInformation {
-//        var price: Int
-        var count: Int
-    }
-    
-    enum Fruit {
-        case 딸기 // rawValue String?
-        case 바나나
-        case 파인애플
-        case 키위
-        case 망고
-    }
-    
-    // 딕셔너리 (과일: 수량) - 수량 관리하는 메서드를 만들기 쉬울듯
-    var inventory: [Fruit: FruitInformation] = [
-        .딸기 : FruitInformation(count: 10),
-        .바나나 : FruitInformation(count: 10),
-        .파인애플 : FruitInformation(count: 10),
-        .키위 : FruitInformation(count: 10),
-        .망고 : FruitInformation(count: 10)
+
+    var inventory: [Fruit] = [
+        Fruit(name: .딸기),
+        Fruit(name: .바나나),
+        Fruit(name: .파인애플),
+        Fruit(name: .키위),
+        Fruit(name: .망고)
     ]
-    
-    func addInventory(count: Int, to fruit: Fruit) throws {
-        guard var item = inventory[fruit] else {
-            throw FruitStoreError.inValidFruitChoice
-        }
-        item.count += count
-        inventory[fruit] = item
-    }
 }
