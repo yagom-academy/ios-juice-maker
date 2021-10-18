a58defad510d99e3a1ee5312a84eb4fcda338ffd
yim2627 <yim2627@gmail.com>
2021-10-19 00:28:58 +0900

    feat : 재고 변경값 입력 함수 및 재고 변경 함수 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -16,15 +16,42 @@ class FruitStore {
         case kiwi
         case mango
     }
+    
+    enum Errors: Error {
+        case noneOfInput
+        case invalidValue
+    }
+    
     var defaultStock : Int
+    var fruitStorage : Dictionary<Fruit, Int>
     let allFruits : Array<Fruit>
     let stock : Array<Int>
-    let fruitStorage : Dictionary<Fruit,Int>
+    
     init() {
         defaultStock = 10
         allFruits = Fruit.allCases
         stock = Array(repeating: defaultStock, count: allFruits.count)
         fruitStorage = Dictionary(uniqueKeysWithValues: zip(allFruits, stock))
+    }
+    
+    func changeFruitStorage(fruit: Fruit, to number: Int) {
+        fruitStorage.keys.forEach{
+            if $0 == fruit {
+                fruitStorage[$0] = number
+            }
+        }
+    }
+    
+    func inputStockToChange() throws -> Int {
+        guard let stockToChange = readLine() else {
+            throw Errors.noneOfInput
+        }
+        
+        guard let stockToChange = Int(stockToChange) else {
+            throw Errors.invalidValue
+        }
+        
+        return stockToChange
     }
 }
 
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,23 +8,7 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    var e = kk()
-    
+
 }
 
-
-
-class kk {
-    var fruitstore:[(fruit:String, akmount:Int)] =  []
-    enum Fruit :CaseIterable{
-        case qq,ww,ee,rr,tt
-    
-    init(){
-        Fruit.allCases.forEach{
-            fruitstore += [(fruit: "\($0)", akmount:10)]
-        }
-    }
-    }
-    
-}
 
