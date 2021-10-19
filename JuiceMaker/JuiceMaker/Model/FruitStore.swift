4530c3d39670b99f329d75f81d42e571732f4874
forestjae <forestsj30@gmail.com>
2021-10-19 14:45:57 +0900

    feat: Fruit 타입(enum) 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,12 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    enum Fruit {
+        case strawberry
+        case banana
+        case pineapple
+        case kiwi
+        case mango
+    }
     
 }
