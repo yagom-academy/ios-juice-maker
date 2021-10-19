01d72f84e7e165c616ebee6ab171f21cd7072583
yeahg-dev <yeahg.dev@gmail.com>
2021-10-19 20:06:36 +0900

    feat: JuiceMenu열거형 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -14,7 +14,7 @@ struct JuiceMaker {
         self.fruitStore = fruitStore
     }
     
-    func make(juice: Juice) throws -> Juice {
+    func make(juice: JuiceRecipe) throws -> JuiceRecipe {
         let unavailableFruits = getUnavailableFruit(juice: juice)
         
         guard unavailableFruits.count == 0 else {
@@ -27,7 +27,7 @@ struct JuiceMaker {
         return juice
     }
   
-    func getUnavailableFruit(juice: Juice) -> [Fruit:Int] {
+    func getUnavailableFruit(juice: JuiceRecipe) -> [Fruit:Int] {
         let unavailableFruits = juice.ingredients.filter( {(fruit: Fruit, amount: Int) -> Bool in
             return fruitStore.isAvailable(fruit: fruit, amount: amount) == false
         })
@@ -35,16 +35,43 @@ struct JuiceMaker {
     }
 }
 
-struct Juice {
-    var name: String
+struct JuiceRecipe {
     var ingredients: [Fruit : Int] = [:]
-    
-    init(name: String, ingredientList: [(Fruit, Int)]) {
-        self.name = name
+
+    init(ingredientList: [(Fruit, Int)]) {
         for (fruit, amount) in ingredientList {
             ingredients[fruit] = amount
         }
     }
 }
+
+enum JuiceMenu: String {
+    case strawberryJuice = "딸기쥬스"
+    case banannaJuice = "바나나쥬스"
+    case kiwiJuice = "키위쥬스"
+    case pineappleJuice = "파인애플쥬스"
+    case strawberryBanannaJuice = "딸바쥬스"
+    case mangoJuice = "망고쥬스"
+    case mangoKiwiJuice = "망고키위쥬스"
     
+    var recipe: JuiceRecipe {
+        switch self {
+        case .strawberryJuice:
+            return JuiceRecipe(ingredientList: [(Fruit.strawberry, 16)])
+        case .banannaJuice:
+            return JuiceRecipe(ingredientList: [(Fruit.bananna, 2)])
+        case .kiwiJuice:
+            return JuiceRecipe(ingredientList: [(Fruit.kiwi, 3)])
+        case .pineappleJuice:
+            return JuiceRecipe(ingredientList: [(Fruit.pineapple, 2)])
+        case .strawberryBanannaJuice:
+            return JuiceRecipe(ingredientList: [(Fruit.strawberry, 10), (Fruit.bananna, 1)])
+        case .mangoJuice:
+            return JuiceRecipe(ingredientList: [(Fruit.mango, 3)])
+        case .mangoKiwiJuice:
+            return JuiceRecipe(ingredientList: [(Fruit.mango, 2), (Fruit.kiwi, 1)])
+        }
+    }
+}
+
 
