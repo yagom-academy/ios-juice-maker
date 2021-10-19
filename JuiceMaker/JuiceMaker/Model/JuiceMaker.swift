bf943fbe453347e3db1e3d6f70cdb0e3d007da3c
just1103 <hyojuson1103@gmail.com>
2021-10-19 17:27:32 +0900

    feat: Juice 구조체 추가 #1

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,6 +6,21 @@
 
 import Foundation
 
+struct Juice {
+    var name: JuiceName
+    var count: Int = 0
+
+    enum JuiceName {
+        case 딸기쥬스
+        case 바나나쥬스
+        case 키위쥬스
+        case 파인애플쥬스
+        case 딸바쥬스
+        case 망고쥬스
+        case 망고키위쥬스
+    }
+}
+
 // 쥬스 메이커 타입
 struct JuiceMaker {
     
