a8d90cf1e4fd4a409737aee09c6f025d22b7225e
llghdud921 <llghdud921@naver.com>
2021-10-19 19:53:33 +0900

    feat: class fruitstore 생성자 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,7 +8,7 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    enum Fruits {
+    enum Fruits: CaseIterable {
         case strawberry
         case banana
         case pineapple
@@ -16,5 +16,13 @@ class FruitStore {
         case mango
     }
     
-    var fruitInventory: [Fruits:Int]?
+    var fruitInventory: [Fruits: Int]
+    
+    init() {
+        let defaultInventory = 10
+        self.fruitInventory = [:]
+        for fruit in Fruits.allCases {
+            fruitInventory[fruit] = defaultInventory
+        }
+    }
 }
