9888965e91fcfb5de5ffe60209562e675ba4790b
smart8612 <smart8612@gmail.com>
2021-10-19 19:39:03 +0900

    feat: 과일 열거형 및 과일 재고 변수 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    enum Fruits {
+        case strawberry
+        case banana
+        case pineapple
+        case kiwi
+        case mango
+    }
     
+    var fruitInventory: [Fruits:Int]?
 }
