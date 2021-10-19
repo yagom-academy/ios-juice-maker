fa9ecddb0cbdae641c8c96b6a0aab9a91e13ca5d
nicholas <graysilver18@gmail.com>
2021-10-19 19:35:52 +0900

    [#2] feat: updateQuantity 함수, searchStorage 함수 정의

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,9 +7,40 @@
 import Foundation
 
 class FruitStore {
-    var storedStrawberry: Fruit = Fruit(fruitName: .strawberry, count: 10)
+    let storedStrawberry: Fruit = Fruit(fruitName: .strawberry, count: 10)
     var storedBanana: Fruit = Fruit(fruitName: .banana, count: 10)
     var storedKiwi: Fruit = Fruit(fruitName: .kiwi, count: 10)
     var storedPineapple: Fruit = Fruit(fruitName: .pineapple, count: 10)
     var storedMango: Fruit = Fruit(fruitName: .mango, count: 10)
+    
+    func searchStorage(fruitNmae: Fruit.FruitName) -> Fruit {
+        switch fruitNmae {
+        case .strawberry:
+            return storedStrawberry
+        case .banana:
+            return storedBanana
+        case .kiwi:
+            return storedKiwi
+        case .pineapple:
+            return storedPineapple
+        case .mango:
+            return storedMango
+        }
+    }
+    
+    func updateQuantity(changingFruit: Fruit) -> Bool {
+        var isSuccess: Bool
+        let changedFruit: Fruit = searchStorage(fruitNmae: changingFruit.name)
+        
+        changedFruit.count = changedFruit.count + changingFruit.count
+        
+        if changedFruit.count == changingFruit.count {
+            isSuccess = true
+        } else {
+            isSuccess = false
+        }
+        
+        return isSuccess
+    }
+    
 }
