ada38330909e36c0fbd7aba281bdca13932276f9
junbangg <wnstjr4620@naver.com>
2021-10-19 12:30:14 +0900

    feat: JuiceMaker 구조체 추가
    - 생성자 추가
    - 부족한 재고를 확인하는 checkFruit 메서드 추가
    - 주스를 만들 수 있는지 확인하는 make 메서드 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,7 +8,24 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    var fruitStore: FruitStore
+    init(amount: Int) {
+        fruitStore = FruitStore(fruits: [.strawberry, .bananna, .pineapple, .kiwi, .mango], amount: amount)
+    }
     
+    func make(juice: Juice) -> Juice? {
+        guard checkFruit(juice: juice).count == 0 else {
+            return nil
+        }
+        return juice
+    }
+    
+    func checkFruit(juice: Juice) -> [Fruit:Int] {
+        let unAvailableFruits = juice.ingredients.filter( {(fruit: Fruit, amount: Int) -> Bool in
+            return fruitStore.isAvailable(fruit: fruit, amount: amount) == false
+        })
+        return unAvailableFruits
+    }
 }
 
 struct Juice {
