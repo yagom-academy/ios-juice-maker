bbb64414d64da9691e298604ebe3915152a6dead
GOSARI <goeunrhdms@gmail.com>
2021-10-19 21:58:28 +0900

    feat: JuiceMaker 타입 내 과일 제조 메소드 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,10 +8,10 @@ import Foundation
 
 class FruitStore {
     let storedStrawberry: Fruit = Fruit(fruitName: .strawberry, count: 10)
-    var storedBanana: Fruit = Fruit(fruitName: .banana, count: 10)
-    var storedKiwi: Fruit = Fruit(fruitName: .kiwi, count: 10)
-    var storedPineapple: Fruit = Fruit(fruitName: .pineapple, count: 10)
-    var storedMango: Fruit = Fruit(fruitName: .mango, count: 10)
+    let storedBanana: Fruit = Fruit(fruitName: .banana, count: 10)
+    let storedKiwi: Fruit = Fruit(fruitName: .kiwi, count: 10)
+    let storedPineapple: Fruit = Fruit(fruitName: .pineapple, count: 10)
+    let storedMango: Fruit = Fruit(fruitName: .mango, count: 10)
     
     func searchStorage(fruitNmae: Fruit.FruitName) -> Fruit {
         switch fruitNmae {
@@ -26,6 +26,34 @@ class FruitStore {
         case .mango:
             return storedMango
         }
+    }
+
+    func checkQuantity(requiredIngredient: Fruit) -> Bool {
+        var isSatisfy: Bool
+        let storedFruit = searchStorage(fruitNmae: requiredIngredient.name)
+        
+        if storedFruit.count < requiredIngredient.count {
+            isSatisfy = false
+        } else {
+            isSatisfy = true
+        }
+        
+        return isSatisfy
+    }
+    
+    func bringIngredients(ingredients: Fruit) -> Bool {
+        var isSuccess: Bool
+        let storedFruit: Fruit = searchStorage(fruitNmae: ingredients.name)
+        
+        storedFruit.count = storedFruit.count - ingredients.count
+        
+        if storedFruit.count >= 0 {
+            isSuccess = true
+        } else {
+            isSuccess = false
+        }
+        
+        return isSuccess
     }
     
     func updateQuantity(changingFruit: Fruit) -> Bool {
@@ -42,5 +70,4 @@ class FruitStore {
         
         return isSuccess
     }
-    
 }
