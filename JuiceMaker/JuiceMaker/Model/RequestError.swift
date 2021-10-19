7b555775e233b00ca6016bf05c102b4f1aaa80df
JT <dd3557@naver.com>
2021-10-19 12:11:12 +0900

    refactor: RequestError의 에러 메세지 출력 로직을 수정

--- JuiceMaker/JuiceMaker/Model/RequestError.swift
+++ JuiceMaker/JuiceMaker/Model/RequestError.swift
@@ -7,19 +7,19 @@
 
 import Foundation
 
-enum RequestError: Error {
+enum RequestError: Error, LocalizedError {
     case wrongCount
     case fruitNotFound
     case fruitStockOut
     
-    func printErrorMessage() {
+    var errorDescription: String {
         switch self {
         case RequestError.wrongCount:
-            print("수량을 잘못 입력하였습니다.")
+            return "수량을 잘못 입력하였습니다."
         case RequestError.fruitNotFound:
-            print("선택한 과일이 존재하지 않습니다.")
+            return "선택한 과일이 존재하지 않습니다."
         case RequestError.fruitStockOut:
-            print("과일의 재고가 부족합니다.")
+            return "과일의 재고가 부족합니다."
         }
     }
 }
