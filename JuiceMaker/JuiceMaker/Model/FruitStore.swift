16997fe8c38f0924f0b577d0fc04433e6a619a88
oksunwoo <oksunwo@naver.com>
2021-10-19 14:52:05 +0900

    feat: stock 딕셔너리 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -16,4 +16,11 @@ class FruitStore {
         case mango
     }
     
+    var stock: [Fruit: Int] = [
+        .strawberry: 10,
+        .banana: 10,
+        .pineapple: 10,
+        .kiwi: 10,
+        .mango: 10
+    ]
 }
