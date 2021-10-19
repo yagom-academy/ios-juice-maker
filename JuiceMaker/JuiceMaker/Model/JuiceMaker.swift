90494e57e49a0255fb48a148cb48023975b80a49
ChaminLee <trueman9512@naver.com>
2021-10-19 15:25:15 +0900

    feat: Juice enum 타입 추가
    - Juice의 각 메뉴별 필요한 재료 정보를 가지는 recipe 변수 추가
    - 재사용되는 타입을 Recipe로 타입 별칭 선언

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,34 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    typealias Recipe = [FruitStore.Fruit: Int]
     
+    enum Juice {
+        case strawberryJuice
+        case bananaJuice
+        case kiwiJuice
+        case pineappleJuice
+        case strawberryAndBananaJuice
+        case mangoJuice
+        case mangoKiwiJuice
+        
+        var recipe: Recipe {
+            switch self {
+            case .strawberryJuice:
+                return [.strawberry: 16]
+            case .bananaJuice:
+                return [.banana: 2]
+            case .kiwiJuice:
+                return [.kiwi: 3]
+            case .pineappleJuice:
+                return [.pineapple: 2]
+            case .strawberryAndBananaJuice:
+                return [.strawberry: 10, .banana: 1]
+            case .mangoJuice:
+                return [.mango: 3]
+            case .mangoKiwiJuice:
+                return [.mango: 2, .kiwi: 1]
+            }
+        }
+    }
 }
