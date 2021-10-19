9048d623cb5fc009d308847c288ed1c583cbfd73
namu <simjh27@gmail.com>
2021-10-19 10:43:12 +0900

    feat: FruitStore의 프로퍼티 및 이니셜라이저 생성
    * FruitStore에서 관리할 과일에 대한 열거형 Fruit 정의
    * Fruit을 키로, Int를 값으로 갖는 딕셔너리 inventory 프로퍼티 선언
    * inventory에 대한 이니셜라이저 정의

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,17 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    enum Fruit: CaseIterable {
+        case strawberry, banana, kiwi, pineapple, mango
+    }
+    
+    var inventory: Dictionary<Fruit, Int>
+    
+    init(defaultValue: Int = 10) {
+        self.inventory = [:]
+        Fruit.allCases.forEach {
+            inventory.updateValue(defaultValue, forKey: $0)
+        }
+    }
     
 }
