a1f49d1a53cdec7126a7ec89e5fa0080decd4fbd
just1103 <hyojuson1103@gmail.com>
2021-10-19 17:57:43 +0900

    feat: ingredient 타입 생성, Recipe 관련 상수 추가 #1

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -9,7 +9,7 @@ import Foundation
 struct Juice {
     var name: JuiceName
     var count: Int = 0
-
+    
     enum JuiceName {
         case strawberryJuice
         case bananaJuice
@@ -23,5 +23,25 @@ struct Juice {
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    var store = FruitStore()
     
+    typealias ingredient = (fruit: Fruit.FruitName, count: Int)
+    
+    let strawberryJuiceRecipe: [ingredient] = [(.strawberry, 16)]
+    let bananaJuiceRecipe: [ingredient] = [(.banana, 2)]
+    let kiwiJuiceRecipe: [ingredient] = [(.kiwi, 3)]
+    let pineappleJuiceRecipe: [ingredient] = [(.pineapple, 2)]
+    let strawberryBananaJuiceRecipe: [ingredient] = [(.strawberry, 10), (.banana, 1)]
+    let mangoJuiceRecipe: [ingredient] = [(.mango, 3)]
+    let mangoKiwiJuiceRecipe: [ingredient] = [(.mango, 2), (.kiwi, 1)]
+    
+    lazy var recipe: [Juice.JuiceName: [ingredient]] = [
+        .strawberryJuice: strawberryJuiceRecipe,
+        .bananaJuice: bananaJuiceRecipe,
+        .kiwiJuice: kiwiJuiceRecipe,
+        .pineappleJuice: pineappleJuiceRecipe,
+        .strawberryBananaJuice: strawberryBananaJuiceRecipe,
+        .mangoJuice: mangoJuiceRecipe,
+        .mangoKiwiJuice: mangoKiwiJuiceRecipe
+    ]
 }
