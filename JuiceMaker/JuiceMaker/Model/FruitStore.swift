68fe277dc5d06f8033b9b0c1c85e36ea8e8cc980
wooyani77 <wooyani77@gmail.com>
2021-10-20 10:47:31 +0900

    feat: FruitStore 클래스 내부에 재고관리 함수 생성
    - 클래스 내부에 Fruit 열거형을 정의해 과일의 종류를 지정하고
    딕셔너리로 과일과 초기재고를 설정함
    - addFruitInventory()와 useFruitInventory() 함수로 재고관리

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,29 @@
 
 import Foundation
 
-// 과일 저장소 타입
 class FruitStore {
+    enum Fruit: CaseIterable {
+        case strawberry, banana, pineapple, kiwi, mango
+    }
     
+    var fruitInventory: [Fruit: Int] = [:]
+    
+    init() {
+        for fruit in Fruit.allCases {
+            fruitInventory[fruit] = 10
+        }
+    }
+    
+    func addFruitInventroy(fruit: Fruit, amount: Int) {
+        guard let inventory = fruitInventory[fruit] else {
+            return
+        }
+        fruitInventory[fruit] = inventory + amount
+    }
+    func useFruitInventroy(fruit: Fruit, amount: Int) {
+        guard let inventory = fruitInventory[fruit] else {
+            return
+        }
+        fruitInventory[fruit] = inventory - amount
+    }
 }
