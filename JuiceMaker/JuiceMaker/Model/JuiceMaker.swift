6540b28bd3cd40cf4f325e4320fcced451350104
llghdud921 <llghdud921@naver.com>
2021-10-20 18:09:41 +0900

    refactor: 주스 열거형의 레시피 메서드를 extension으로 분리하고 private 지정

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -16,25 +16,6 @@ struct JuiceMaker {
         case strawberryBananaJuice
         case mangoJuice
         case kiwiMangoJuice
-        
-        func recipe() -> [FruitStore.Fruits: Int] {
-            switch self {
-            case .strawberryJuice:
-                return [.strawberry: 16]
-            case .bananaJuice:
-                return [.banana: 2]
-            case .kiwiJuice:
-                return [.kiwi: 3]
-            case .pineappleJuice:
-                return [.pineapple: 2]
-            case .strawberryBananaJuice:
-                return [.strawberry: 10, .banana: 1]
-            case .mangoJuice:
-                return [.mango: 3]
-            case .kiwiMangoJuice:
-                return [.mango: 2, .kiwi: 1]
-            }
-        }
     }
     
     enum MakingJuiceError: LocalizedError {
@@ -56,6 +37,27 @@ struct JuiceMaker {
         
         for (fruit, count) in juiceRecipe {
             try fruitStorage.subtractFruit(fruit, of: count)
+        }
+    }
+}
+
+private extension JuiceMaker.Juice {
+    func recipe() -> [FruitStore.Fruits: Int] {
+        switch self {
+        case .strawberryJuice:
+            return [.strawberry: 16]
+        case .bananaJuice:
+            return [.banana: 2]
+        case .kiwiJuice:
+            return [.kiwi: 3]
+        case .pineappleJuice:
+            return [.pineapple: 2]
+        case .strawberryBananaJuice:
+            return [.strawberry: 10, .banana: 1]
+        case .mangoJuice:
+            return [.mango: 3]
+        case .kiwiMangoJuice:
+            return [.mango: 2, .kiwi: 1]
         }
     }
 }
