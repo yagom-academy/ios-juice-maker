fca9402de5dedfc86357f68a19e569fd1112ef37
wooyani77 <wooyani77@gmail.com>
2021-10-20 16:07:24 +0900

    feat: JuiceRecipe namespace 추가
    - 매직넘버 사용 지양을 위해 열거형으로 관리

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -16,24 +16,38 @@ struct JuiceMaker {
         case strawberryBananaJuice
         case mangoKiwiJuice
     }
+    enum JuiceRecipe {
+        static let usageOfStrawberry = 16
+        static let usageOfBanana = 2
+        static let usageOfPineapple = 2
+        static let usageOfKiwi = 3
+        static let usageOfMango = 3
+        static let usageOfMixStrawberry = 10
+        static let usageOfMixBanana = 1
+        static let usageOfMixMango = 2
+        static let usageOfMixKiwi = 1
+    }
+    
     let fruitStore = FruitStore()
     
     func makeFruitJuice(juice: Menu) {
         switch juice {
         case .strawberryJuice:
-            fruitStore.consumeFruit(name: .strawberry, amount: 16)
+            fruitStore.consumeFruit(name: .strawberry, amount: JuiceRecipe.usageOfStrawberry)
         case .bananaJuice:
-            fruitStore.consumeFruit(name: .banana, amount: 2)
+            fruitStore.consumeFruit(name: .banana, amount: JuiceRecipe.usageOfBanana)
         case .pineappleJuice:
-            fruitStore.consumeFruit(name: .pineapple, amount: 2)
+            fruitStore.consumeFruit(name: .pineapple, amount: JuiceRecipe.usageOfPineapple)
         case .kiwiJuice:
-            fruitStore.consumeFruit(name: .kiwi, amount: 3)
+            fruitStore.consumeFruit(name: .kiwi, amount: JuiceRecipe.usageOfKiwi)
         case .mangoJuice:
-            fruitStore.consumeFruit(name: .mango, amount: 3)
+            fruitStore.consumeFruit(name: .mango, amount: JuiceRecipe.usageOfMango)
         case .strawberryBananaJuice:
-            fruitStore.consumeTwoFruits(first: .strawberry, firstAmount: 10, second: .banana, secondAmount: 1)
+            fruitStore.consumeTwoFruits(first: .strawberry, firstAmount: JuiceRecipe.usageOfMixStrawberry,
+                                        second: .banana, secondAmount: JuiceRecipe.usageOfMixBanana)
         case .mangoKiwiJuice:
-            fruitStore.consumeTwoFruits(first: .mango, firstAmount: 2, second: .kiwi, secondAmount: 1)
+            fruitStore.consumeTwoFruits(first: .mango, firstAmount: JuiceRecipe.usageOfMixMango,
+                                        second: .kiwi, secondAmount: JuiceRecipe.usageOfMixKiwi)
         }
     }
 }
