66ce275edc538d176ae6f4802e0d37486d7ce82e
llghdud921 <llghdud921@naver.com>
2021-10-20 13:33:43 +0900

    feat: 주스 종류 열거형에 레시피 알려주는 메서드 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -16,6 +16,25 @@ struct JuiceMaker {
         case strawberryBananaJuice
         case mangoJuice
         case kiwiMangoJuice
+        
+        func recipe() -> [FruitStore.Fruits: Int] {
+            switch self {
+            case .strawberryJuice:
+                return [.strawberry: 16]
+            case .bananaJuice:
+                return [.banana: 2]
+            case .kiwiJuice:
+                return [.kiwi: 3]
+            case .pineappleJuice:
+                return [.pineapple: 2]
+            case .strawberryBananaJuice:
+                return [.strawberry: 10, .banana: 1]
+            case .mangoJuice:
+                return [.mango: 3]
+            case .kiwiMangoJuice:
+                return [.mango: 2, .kiwi: 1]
+            }
+        }
     }
     
     let fruitStorage = FruitStore()
