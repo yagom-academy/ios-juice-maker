a3e7219416cdfbdbdada4fcdc5cd3ff18fb1cfd4
SIWONKIM <siyeon0616@gmail.com>
2021-10-20 11:40:57 +0900

    feat: JuiceMaker 구조체, Juice 열거형 추가
    JuiceMaker 구조체 안에 Juice 열거형을 추가하였고, 딕셔너리를 반환하는 recipe 연산 프로퍼티 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,36 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    let fruitStore: FruitStore = FruitStore()
+    
+    enum Juice {
+        case strawberryJuice
+        case bananaJuice
+        case kiwiJuice
+        case pineappleJuice
+        case mangoJuice
+        case strawbettyBananaJuice
+        case mangoKiwiJuice
+        
+        var recipe: [Fruit: Int] {
+            switch self {
+            case .strawberryJuice:
+                return [.strawberry: 16]
+            case .bananaJuice:
+                return [.banana: 2]
+            case .kiwiJuice:
+                return [.kiwi: 3]
+            case .pineappleJuice:
+                return [.pineapple: 2]
+            case .mangoJuice:
+                return [.mango: 3]
+            case .strawbettyBananaJuice:
+                return [.strawberry: 10, .banana: 1]
+            case .mangoKiwiJuice:
+                return [.mango: 2, .kiwi: 1]
+            }
+        }
+        
+    }
     
 }
