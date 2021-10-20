722e1166ac0239ae2dfd122a53eb3f321b3cf954
smart8612 <smart8612@gmail.com>
2021-10-20 13:18:14 +0900

    feat: 주스 종류를 나타내는 열거형 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,17 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    enum Juice {
+        case strawberryJuice
+        case bananaJuice
+        case kiwiJuice
+        case pineappleJuice
+        case strawberryBananaJuice
+        case mangoJuice
+        case kiwiMangoJuice
+    }
+    
+    let fruitStorage = FruitStore()
+    
     
 }
