fd7371b126476a4c0bd378f0a235a9d25d453831
zoe <kny0651@naver.com>
2021-10-20 11:35:34 +0900

    feat: JuiceMakerError 타입 정의, check 함수 추가
    - 유효한 숫자가 아니거나 남아있는 과일의 개수이상 빼려고할때 에러를 알려주기 위해
    - useFruitInventroy 에서 재고가 남아있는지 확인 후 수량 업데이트

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,6 +6,11 @@
 
 import Foundation
 
+enum JuiceMakerError: Error {
+    case outOfStock
+    case invalidNumber
+}
+
 class FruitStore {
     enum Fruit: CaseIterable {
         case strawberry, banana, pineapple, kiwi, mango
@@ -19,16 +24,32 @@ class FruitStore {
         }
     }
     
+    func check(fruit: Fruit, count: Int) throws {
+        guard count >= 0 else { throw JuiceMakerError.invalidNumber }
+        guard let fruitAmount = fruitInventory[fruit],
+              fruitAmount >= count else { throw JuiceMakerError.outOfStock }
+    }
+    
     func addFruitInventroy(fruit: Fruit, amount: Int) {
         guard let inventory = fruitInventory[fruit] else {
             return
         }
         fruitInventory[fruit] = inventory + amount
     }
+    
     func useFruitInventroy(fruit: Fruit, amount: Int) {
-        guard let inventory = fruitInventory[fruit] else {
-            return
+        do {
+            try check(fruit: fruit, count: amount)
+            guard let inventory = fruitInventory[fruit] else {
+                return
+            }
+            fruitInventory[fruit] = inventory - amount
+        } catch JuiceMakerError.outOfStock {
+            print("재고가 부족합니다")
+        } catch JuiceMakerError.invalidNumber {
+            print("유효하지 않은 숫자입니다")
+        } catch {
+            print(error)
         }
-        fruitInventory[fruit] = inventory - amount
     }
 }
