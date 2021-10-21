e0fec598c9f34181ae30d748a49e855fc3ecbed8
Quokkaaa <rktrkdml@gmail.com>
2021-10-21 18:44:19 +0900

    feat: Juice 열거형 생성 및 fruitStore 인스턴스 생성
    * Juice 열거형 생성
    * Juice 열거형 내부에 쥬스제조에 필요한 과일수량을 담은 recipe 변수 생성
    * fruitStore 인스턴스 생성

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,5 +7,34 @@
 import Foundation
 
 struct JuiceMaker {
+    var fruitStore = FruitStore()
     
+    enum Juice {
+        case strawberryJuice
+        case bananaJuice
+        case kiwiJuice
+        case pineappleJuice
+        case strawberryBananaJuice
+        case mangoJuice
+        case mangoKiwiJuice
+
+        var recipe: [FruitStore.Fruit: Int] {
+            switch self {
+            case .strawberryJuice:
+                return [.strawberry: 16]
+            case .bananaJuice:
+                return [.banana: 2]
+            case .kiwiJuice:
+                return [.kiwi: 3]
+            case .pineappleJuice:
+                return [.pineapple: 2]
+            case .strawberryBananaJuice:
+                return [.strawberry: 2, .banana: 1]
+            case .mangoJuice:
+                return [.mango: 3]
+            case .mangoKiwiJuice:
+                return [.mango: 2, .kiwi: 1]
+            }
+        }
+    }
 }
