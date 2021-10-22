88c724ccb7f51ce6372bf9ccefb29eb6b378e321
junbangg <wnstjr4620@naver.com>
2021-10-22 10:29:54 +0900

    refactor: Fruit 열거형에서 String 타입과 CustomStringConvertible 프로토콜 제거

--- JuiceMaker/JuiceMaker/Model/Fruit.swift
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -7,14 +7,10 @@
 
 import Foundation
 
-enum Fruit: String, CustomStringConvertible, CaseIterable {
-    case strawberry = "딸기"
-    case bananna = "바나나"
-    case pineapple = "파인애플"
-    case kiwi = "키위"
-    case mango = "망고"
-    
-    var description: String {
-        return rawValue
-    }
+enum Fruit: CaseIterable {
+    case strawberry
+    case bananna
+    case pineapple
+    case kiwi
+    case mango
 }
