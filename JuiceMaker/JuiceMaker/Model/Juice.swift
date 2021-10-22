0ea004b1724f10ef9ba4f23a11b1d1833ebc12d2
vayne77 <sky891@naver.com>
2021-10-22 16:15:48 +0900

    refactor: juice 열거형 파일 분리 및 프로퍼티의 접근제어 수정

---
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -0,0 +1,35 @@
+//
+//  Juice.swift
+//  JuiceMaker
+//
+//  Created by hoon on 2021/10/22.
+//
+
+enum Juice {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
+    
+    var recipes: [Fruit: Int] {
+        switch self {
+        case .strawberryJuice:
+            return [.strawberry: 16]
+        case .bananaJuice:
+            return [.banana: 2]
+        case .kiwiJuice:
+            return [.kiwi: 3]
+        case .pineappleJuice:
+            return [.pineapple: 2]
+        case .strawberryBananaJuice:
+            return [.strawberry: 10 , .banana: 1]
+        case .mangoJuice:
+            return [.mango: 3]
+        case .mangoKiwiJuice:
+            return [.mango: 2, .kiwi: 3]
+        }
+    }
+}
