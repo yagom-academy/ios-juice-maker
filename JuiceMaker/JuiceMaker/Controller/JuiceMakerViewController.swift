68927c44055ab44a4aeb8e64214e667a9693a627
Ari <lee_ari95@icloud.com>
2021-10-25 17:08:59 +0900

    feat: 쥬스 주문버튼 터치 시 Alret 표시되는 기능 구현
    - Juice 타입 rawValue 추가후 CustomStringConvertible 프로토콜 채택
    - JuiceMakerViewController 클래스 내부에 orderJuiceButtonTapped 메서드 구현
    - JuiceMakerViewController 클래스 내부에 juiceCompletionAlret 메서드 구현

--- JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
@@ -13,6 +13,39 @@ class JuiceMakerViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func orderJuiceButtonTapped(_ sender: UIButton) {
+        let buttonText = sender.titleLabel?.text
+        
+        switch buttonText {
+        case "딸바쥬스 주문":
+            juiceCompletionAlret(juice: .strawberryBanana)
+        case "망키쥬스 주문":
+            juiceCompletionAlret(juice: .mangoKiwi)
+        case "딸기쥬스 주문":
+            juiceCompletionAlret(juice: .strawberry)
+        case "바나나쥬스 주문":
+            juiceCompletionAlret(juice: .banana)
+        case "파인애플쥬스 주문":
+            juiceCompletionAlret(juice: .pineapple)
+        case "키위쥬스 주문":
+            juiceCompletionAlret(juice: .kiwi)
+        case "망고쥬스 주문":
+            juiceCompletionAlret(juice: .mango)
+        default:
+            let alret = UIAlertController(title: "오류", message: "잘못된 접근입니다.", preferredStyle: .alert)
+            let cencel = UIAlertAction(title: "Cencel", style: .destructive, handler: nil)
+            alret.addAction(cencel)
+            present(alret, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func juiceCompletionAlret(juice: Juice){
+        let alret = UIAlertController(title: nil, message: "\(juice) 쥬스 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alret.addAction(cancel)
+        present(alret, animated: true, completion: nil)
+    }
     
 }
 
