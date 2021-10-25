84fa2775472cc85cd8abeb1a81e153daa4e8256f
oksunwoo <oksunwo@naver.com>
2021-10-25 20:59:00 +0900

    feat: juice 주문 버튼 추가 및 버튼 Action 추가
    - 각 주문버튼에 따라 알맞는 쥬스를 변수 juice에 할당
    - make메소드에 할당받은 juice 전달

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,6 +8,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strawberryBananaJuiceOrderButton: UIButton!
+    @IBOutlet weak var strawberryJuiceOrderButton: UIButton!
+    @IBOutlet weak var bananaJuiceOrderButton: UIButton!
+    @IBOutlet weak var mangoKiwiJuiceOrderButton: UIButton!
+    @IBOutlet weak var pineappleJuiceOrderButton: UIButton!
+    @IBOutlet weak var kiwiJuiceOrderButton: UIButton!
+    @IBOutlet weak var mangoJuiceOrderButton: UIButton!
+    
+    let juiceMaker = JuiceMaker()
+    
+    @IBAction func touchUpJuiceOrderButton(_ sender: UIButton) {
+        var juice: JuiceMaker.Juice?
+        
+        switch sender {
+        case strawberryBananaJuiceOrderButton:
+            juice = .strawberryAndBananaJuice
+        case strawberryJuiceOrderButton:
+            juice = .strawberryJuice
+        case bananaJuiceOrderButton:
+            juice = .bananaJuice
+        case mangoKiwiJuiceOrderButton:
+            juice = .mangoKiwiJuice
+        case pineappleJuiceOrderButton:
+            juice = .pineappleJuice
+        case kiwiJuiceOrderButton:
+            juice = .kiwiJuice
+        case mangoJuiceOrderButton:
+            juice = .mangoJuice
+        default:
+            print("메뉴에 없어요")
+        }
+        
+        guard let orderedJuice = juice else {
+            return
+        }
+        
+        do {
+            try juiceMaker.make(juice: orderedJuice)
+        } catch ServiceError.notEnoughStock {
+            print(ServiceError.notEnoughStock.errorDescription)
+        } catch SystemError.invaildKey {
+            print(SystemError.invaildKey.errorDescription)
+        } catch {
+            print(error)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
