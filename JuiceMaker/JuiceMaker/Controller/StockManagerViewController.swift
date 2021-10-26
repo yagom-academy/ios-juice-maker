5a41d50a3fa76e1ec0109c8c769e3c80da1aa0b3
yeahg-dev <yeahg.dev@gmail.com>
2021-10-26 14:35:41 +0900

    feat: StockManagerViewController 클래스파일 생성

--- JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
@@ -5,6 +5,14 @@
 //  Created by Jun Bang on 2021/10/26.
 //
 
-import Foundation
+import UIKit
+
+class StockManagerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
 
 
