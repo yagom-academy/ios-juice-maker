38388471fdddddba495ef986d386fdbda8a31e3f
just1103 <hyojuson1103@gmail.com>
2021-10-26 15:11:31 +0900

    feat: EditStockViewController 파일 생성 #4
    - ViewController -> OrderJuiceViewController로 파일 이름 수정

---
+++ JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StockViewController.swift
+//  JuiceMaker
+//
+//  Created by Hyoju Son on 2021/10/26.
+//
+
+import UIKit
+
+class EditStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
--- JuiceMaker/JuiceMaker/Controller/OrderJuiceViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/OrderJuiceViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OrderJuiceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
