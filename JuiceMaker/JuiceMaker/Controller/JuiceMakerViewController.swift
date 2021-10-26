cfa90cb8002d248c8fd6b575bc3bbdf0aeaf57e3
llghdud921 <llghdud921@naver.com>
2021-10-26 20:41:09 +0900

    feat: 주문 버튼 클릭 시 alert 띄우기 추가

--- JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
@@ -20,6 +20,21 @@ class JuiceMakerViewController: UIViewController {
     
     @IBOutlet var fruitCountLabels: [UILabel]!
     
+    @IBAction func clickOrderButton(_ sender: UIButton) {
+        let juiceID = sender.restorationIdentifier
+        
+        let alert = UIAlertController(title: "주스 제조 완료", message: "\(juiceID) 제조가 완료되었습니다.", preferredStyle: .alert)
+        
+        let alertOk = UIAlertAction(title: "확인", style: .default)
+        let alertCancel = UIAlertAction(title: "취소", style: .cancel)
+        
+        alert.addAction(alertOk)
+        alert.addAction(alertCancel)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
     func updateFruitCount() {
         let fruitCountList: [FruitStore.Fruits: Int] = FruitStore.shared.inventoryStatus
         
