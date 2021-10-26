98eed560fbf466717b9890a10dbcc8fc0d5b6540
Jager-yoo <y73447jh@gmail.com>
2021-10-26 12:56:52 +0900

    feat: 쥬스 만들기 Button을 @IBOutlet으로 ViewController와 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -6,12 +6,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {  
+class ViewController: UIViewController {
     @IBOutlet weak var strawberryAmountLabel: UILabel!
     @IBOutlet weak var bananaAmountLabel: UILabel!
     @IBOutlet weak var mangoAmountLabel: UILabel!
     @IBOutlet weak var kiwiAmountLabel: UILabel!
     @IBOutlet weak var pineappleAmountLabel: UILabel!
+    
+    @IBOutlet weak var strawberryJuiceButton: UIButton!
+    @IBOutlet weak var bananaJuiceButton: UIButton!
+    @IBOutlet weak var pineappleJuiceButton: UIButton!
+    @IBOutlet weak var kiwiJuiceButton: UIButton!
+    @IBOutlet weak var mangoJuiceButton: UIButton!
+    @IBOutlet weak var strawberryBananaJuiceButton: UIButton!
+    @IBOutlet weak var mangoKiwiJuiceButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
