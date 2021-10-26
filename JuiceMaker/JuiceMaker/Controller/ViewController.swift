4b1cdb9d633214ff04ce9428baac3ea31f11b331
yeahg-dev <yeahg.dev@gmail.com>
2021-10-26 10:43:59 +0900

    feat: Storyboard와 ViewController 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var currentStrawberryStockLabel: UILabel!
+    @IBOutlet var currentBanannaStockLabel: UILabel!
+    @IBOutlet var currentPineappleStockLabel: UILabel!
+    @IBOutlet var currentKiwiStockLabel: UILabel!
+    @IBOutlet var currentMangoStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func orderStrawberryBanannaJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderStrawberryJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderBanannaJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderMangoKiwiJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderPineappleJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderKiwiJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderMangoJuice(_ sender: UIButton) {
+    }
 }
 
