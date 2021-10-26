1dee8dd19d6fc2cf2161e0340a85d3cf29c01b6d
Kim-EunsooSilver <graysilver18@naver.com>
2021-10-26 18:04:25 +0900

    feat: 재고수정 버튼 화면이동 및 dismiss 구현

---
+++ JuiceMaker/JuiceMaker/Controller/ManageStockViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ManageStockViewController.swift
+//  JuiceMaker
+//
+//  Created by Eunsoo KIM on 2021/10/26.
+//
+
+import UIKit
+
+class ManageStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
