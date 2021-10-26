74f95c0cb1ec5909784e4093dcf72c349a897aae
smart8612 <smart8612@gmail.com>
2021-10-26 11:13:15 +0900

    feat: 과일 재고 수정에 대한 ViewController 생성 및 Scene과 연결

---
+++ JuiceMaker/JuiceMaker/Controller/ModifyInventoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ModifyInventoryViewController.swift
+//  JuiceMaker
+//
+//  Created by JeongTaek Han on 2021/10/26.
+//
+
+import UIKit
+
+class ModifyInventoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
