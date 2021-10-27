e469e97e4712bf5d505d5322e5d357f5b9500db4
Kim-EunsooSilver <graysilver18@naver.com>
2021-10-27 17:12:55 +0900

    refactor: JuiceMenu열거 내부 오타 수정

--- JuiceMaker/JuiceMaker/Model/JuiceMenu.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMenu.swift
@@ -16,7 +16,7 @@ enum JuiceMenu: String {
     case ddalbaJuice = "딸바쥬스"
     case mangKiJuice = "망키쥬스"
     
-    enum FruitQuintity {
+    enum FruitQuantity {
         static let strawberryOfStrawberrJuice = 16
         static let bananaOfBananaJuice = 2
         static let kiwiOfKiwiJuice = 3
@@ -32,29 +32,29 @@ enum JuiceMenu: String {
         switch self {
         case .strawberryJuice:
             return [Fruit(name: .strawberry,
-                          quantity: JuiceMenu.FruitQuintity.strawberryOfStrawberrJuice)]
+                          quantity: JuiceMenu.FruitQuantity.strawberryOfStrawberrJuice)]
         case .bananaJuice:
             return [Fruit(name: .banana,
-                          quantity: JuiceMenu.FruitQuintity.bananaOfBananaJuice)]
+                          quantity: JuiceMenu.FruitQuantity.bananaOfBananaJuice)]
         case .kiwiJuice:
             return [Fruit(name: .kiwi,
-                          quantity: JuiceMenu.FruitQuintity.kiwiOfKiwiJuice)]
+                          quantity: JuiceMenu.FruitQuantity.kiwiOfKiwiJuice)]
         case .pineappleJuice:
             return [Fruit(name: .pineapple,
-                          quantity: JuiceMenu.FruitQuintity.pineappleOfPineappleJuice)]
+                          quantity: JuiceMenu.FruitQuantity.pineappleOfPineappleJuice)]
         case .mangoJuice:
             return [Fruit(name: .mango,
-                          quantity: JuiceMenu.FruitQuintity.mangoOfMangoJuice)]
+                          quantity: JuiceMenu.FruitQuantity.mangoOfMangoJuice)]
         case .ddalbaJuice:
             return [Fruit(name: .strawberry,
-                          quantity: JuiceMenu.FruitQuintity.strawberryOfDdalbaJuice),
+                          quantity: JuiceMenu.FruitQuantity.strawberryOfDdalbaJuice),
                     Fruit(name: .banana,
-                          quantity: JuiceMenu.FruitQuintity.bananaOfDdalbaJuice)]
+                          quantity: JuiceMenu.FruitQuantity.bananaOfDdalbaJuice)]
         case .mangKiJuice:
             return [Fruit(name: .mango,
-                          quantity: JuiceMenu.FruitQuintity.mangoOfMangKiJuice),
+                          quantity: JuiceMenu.FruitQuantity.mangoOfMangKiJuice),
                     Fruit(name: .kiwi,
-                          quantity: JuiceMenu.FruitQuintity.KiwiOfMangKiJuice)]
+                          quantity: JuiceMenu.FruitQuantity.KiwiOfMangKiJuice)]
         }
     }
 }
