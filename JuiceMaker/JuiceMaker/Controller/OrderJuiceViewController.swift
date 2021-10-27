0050352b8ea1e5779902125062e8d97baf58663e
just1103 <hyojuson1103@gmail.com>
2021-10-27 14:26:56 +0900

    feat: showLackOfStockAlert 함수 추가 #4

--- JuiceMaker/JuiceMaker/Controller/OrderJuiceViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/OrderJuiceViewController.swift
@@ -14,13 +14,36 @@ class OrderJuiceViewController: UIViewController {
     }
 
     func showSuccessAlert(message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let alert = UIAlertController(title: nil,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK",
+                                     style: .default,
+                                     handler: nil)
         
         alert.addAction(okAction)
         present(alert, animated: true)
     }
-
+    
+    func showLackOfStockAlert(message: String) {
+        guard let editStockViewController = self.storyboard?.instantiateViewController(identifier: "EditStockNavigation") else {
+            return
+        }
+        
+        let alert = UIAlertController(title: nil,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "재고 수정",
+                                     style: .default) { (action) in
+            self.present(editStockViewController, animated: true)
+        }
+        let cancelAction = UIAlertAction(title: "취소",
+                                         style: .cancel,
+                                         handler: nil)
+        
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
