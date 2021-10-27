a9c63bddbe1f3c074df345c7310156f88be8e8d9
SIWONKIM <siyeon0616@gmail.com>
2021-10-27 11:04:50 +0900

    feat: 재고수정 버튼 터치시 재고수정 화면 모달창을 띄우도록 함 #1

---
+++ JuiceMaker/JuiceMaker/Controller/StockModifyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StockModifyViewController.swift
+//  JuiceMaker
+//
+//  Created by si won kim on 2021/10/26.
+//
+
+import UIKit
+
+class StockModifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
