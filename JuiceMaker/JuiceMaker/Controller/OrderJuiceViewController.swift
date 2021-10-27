6b39ea1cedbac715d771292e48e242f8ab926876
ICS-Asan <carpediem1015@gmail.com>
2021-10-27 14:31:58 +0900

    feat: fruit stock label 추가 #4

--- JuiceMaker/JuiceMaker/Controller/OrderJuiceViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/OrderJuiceViewController.swift
@@ -8,6 +8,12 @@ import UIKit
 
 class OrderJuiceViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
