b6cf2208a382172af6e6e2426443ff013bd7228c
just1103 <hyojuson1103@gmail.com>
2021-10-27 16:28:22 +0900

    feat: JuiceName kor 연산 프로퍼티 추가 #5

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,6 +8,25 @@ enum JuiceName: CaseIterable {
     case strawberryBananaJuice
     case mangoJuice
     case mangoKiwiJuice
+    
+    var kor: String {
+        switch self {
+        case .strawberryJuice:
+            return "딸기 쥬스"
+        case .bananaJuice:
+            return "바나나 쥬스"
+        case .kiwiJuice:
+            return "키위 쥬스"
+        case .pineappleJuice:
+            return "파인애플 쥬스"
+        case .strawberryBananaJuice:
+            return "딸바 쥬스"
+        case .mangoJuice:
+            return "망고 쥬스"
+        case .mangoKiwiJuice:
+            return "망키 쥬스"
+        }
+    }
 }
 
 typealias ingredient = (fruit: FruitName, count: Int)
