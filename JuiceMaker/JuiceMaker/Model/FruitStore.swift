5cec3fb589e58f9fdc6712db4d13b82966b512da
just1103 <hyojuson1103@gmail.com>
2021-10-28 15:26:51 +0900

    feat: didChangeStock Notification 추가 #6

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+extension Notification.Name {
+    static let didChangeStock = Notification.Name("Stock Changed")
+}
+
 enum FruitName: CaseIterable {
     case strawberry
     case banana
