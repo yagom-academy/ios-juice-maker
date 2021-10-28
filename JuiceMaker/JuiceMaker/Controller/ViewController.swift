8560ce2abd5f26e38a1a73dfeb5dbc75b43166f3
horeng2 <huski.a.a@gmail.com>
2021-10-28 14:30:29 +0900

    Feat: 과일재고 UILabel 및 orderJuice IBAction 추가

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -6,13 +6,23 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet var stockOfStrawberry: UILabel!
+    @IBOutlet var stockOfBanana: UILabel!
+    @IBOutlet var stockOfPineapple: UILabel!
+    @IBOutlet var stockOfKiwi: UILabel!
+    @IBOutlet var stockOfMango: UILabel!
+    
+    @IBAction func orderJuice(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
