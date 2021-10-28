5f2414a49a832533bf2be29ccf0f1a59cbda062c
smart8612 <smart8612@gmail.com>
2021-10-28 14:03:11 +0900

    feat: JuiceMakerViewController 에러 기능 추가

--- JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
@@ -9,6 +9,23 @@ import UIKit
 class JuiceMakerViewController: UIViewController {
     
     let juiceMaker = JuiceMaker(fruitStorage: FruitStore.shared)
+    
+    enum JuiceMakerError: LocalizedError {
+        case notFoundID
+        case notFoundJuice
+        case notFoundFruitCount
+        
+        var errorDescription: String? {
+            switch self {
+            case .notFoundFruitCount:
+                return "과일의 갯수를 찾을 수 없습니다."
+            case .notFoundID:
+                return "스토리보드에서 UI요소에 ID가 지정되지 않았습니다."
+            case .notFoundJuice:
+                return "과일 ID에 해당하는 과일 케이스를 찾을 수 없습니다."
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,17 +43,26 @@ class JuiceMakerViewController: UIViewController {
     @IBOutlet var fruitCountLabels: [UILabel]!
     
     @IBAction func clickOrderButton(_ sender: UIButton) {
-        guard let juiceID = sender.restorationIdentifier else {
-            return
+        
+        do {
+            guard let juiceID = sender.restorationIdentifier else {
+                throw JuiceMakerError.notFoundID
+            }
+            guard let orderedjuice = JuiceMaker.Juice.findJuice(juiceID: juiceID) else {
+                throw JuiceMakerError.notFoundJuice
+            }
+            
+            if let madejuice = juiceMaker.order(juice: orderedjuice) {
+                alertMessage(title: "주스 제조 완료", message: "\(madejuice) 제조가 완료되었습니다.")
+            } else {
+                alertMessage(title: "주스 제조 실패", message: "재료가 모자라요. 재고를 수정할까요?") { _ in
+                    self.presentModifyView()
+                }
+            }
+            
+        } catch {
+            print("ERROR \(error): \(error.localizedDescription)")
         }
-        guard let orderedjuice = JuiceMaker.Juice.findJuice(juiceID: juiceID) else {
-            return
-        }
-        guard let madejuice = juiceMaker.order(juice: orderedjuice) else {
-            alertMessage(title: "주스 제조 실패", message: "재료가 모자라요. 재고를 수정할까요?", handler:{ _ in self.presentModifyView() })
-            return
-        }
-        alertMessage(title: "주스 제조 완료", message: "\(madejuice) 제조가 완료되었습니다.", handler: nil)
     }
     
     func alertMessage(title: String, message: String, handler: ((UIAlertAction) -> Void)? = nil ) {
@@ -53,14 +79,18 @@ class JuiceMakerViewController: UIViewController {
     
     @objc
     func updateFruitCount() {
-        for fruitCountLabel in fruitCountLabels {
-            guard let fruitID = fruitCountLabel.restorationIdentifier else {
-                return
+        do {
+            for fruitCountLabel in fruitCountLabels {
+                guard let fruitID = fruitCountLabel.restorationIdentifier else {
+                    throw JuiceMakerError.notFoundID
+                }
+                guard let fruitCount = FruitStore.shared.getFruitCount(by: fruitID) else {
+                    throw JuiceMakerError.notFoundFruitCount
+                }
+                fruitCountLabel.text = String(fruitCount)
             }
-            guard let fruitCount = FruitStore.shared.getFruitCount(by: fruitID) else {
-                return
-            }
-            fruitCountLabel.text = String(fruitCount)
+        } catch {
+            print("ERROR \(error): \(error.localizedDescription)")
         }
     }
     
