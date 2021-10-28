7b45441576d4d8bf843a054feb83e98c4d42e6f8
yanghojoon <yanghojoon8487@gmail.com>
2021-10-28 20:51:52 +0900

    feat: 재고수정 모달 닫기 버튼 기능 추가

--- JuiceMaker/JuiceMaker/Controller/ModifyStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ModifyStockViewController.swift
@@ -11,19 +11,10 @@ class ModifyStockViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func touchUpDismissButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
