ea6fa07843787fd5a607be91a3ee39664684fd16
wooyani77 <wooyani77@gmail.com>
2021-10-28 11:50:36 +0900

    feat: viewController와 storyboard 연결
    - IBOutlet으로 과일들의 label 연결
    - IBAction으로 과일주스 주문 button 연결
    - 재고수정 버튼 클릭시, 재고추가 화면으로 이동

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,12 +7,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let juiceMaker = JuiceMaker()
+    
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showInitialStock()
+    }
+    
+    func showInitialStock() {
+        strawberryLabel.text = String(Fruit.initialValue)
+        bananaLabel.text = String(Fruit.initialValue)
+        pineappleLabel.text = String(Fruit.initialValue)
+        kiwiLabel.text = String(Fruit.initialValue)
+        mangoLabel.text = String(Fruit.initialValue)
     }
 
-
+    @IBAction func orderStrawberryBananaJuice(_ sender: UIButton) {
+        juiceMaker.makeFruitJuice(juice: .strawberryBananaJuice)
+    }
+    
+    @IBAction func orderStrawberryJuice(_ sender: UIButton) {
+        juiceMaker.makeFruitJuice(juice: .strawberryJuice)
+    }
+    
+    @IBAction func orderBananaJuice(_ sender: UIButton) {
+        juiceMaker.makeFruitJuice(juice: .bananaJuice)
+    }
+    
+    @IBAction func orderPineappleJuice(_ sender: UIButton) {
+        juiceMaker.makeFruitJuice(juice: .pineappleJuice)
+    }
+    
+    @IBAction func orderKiwiJuice(_ sender: UIButton) {
+        juiceMaker.makeFruitJuice(juice: .kiwiJuice)
+    }
+    
+    @IBAction func orderMangoKiwiJuice(_ sender: UIButton) {
+        juiceMaker.makeFruitJuice(juice: .mangoKiwiJuice)
+    }
+    
+    @IBAction func orderMangoJuice(_ sender: UIButton) {
+        juiceMaker.makeFruitJuice(juice: .mangoKiwiJuice)
+    }
+    
 }
 
