dbaf46a84606db938b98b35e662c893ea48cf070
aCafela-coffee <seym7894@gmail.com>
2021-10-28 18:10:27 +0900

    feat: 재고 수정 화면의 done 버튼 추가

--- JuiceMaker/JuiceMaker/Controller/EditViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/EditViewController.swift
@@ -1,5 +1,15 @@
 import UIKit
 
 class EditViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = ""
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTrigger))
+    }
     
+    @objc
+    func doneButtonTrigger() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
 }
