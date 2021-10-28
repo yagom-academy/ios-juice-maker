041c781867ff61ed2f26b9ee613e3022874919f4
namu <simjh27@gmail.com>
2021-10-28 19:03:02 +0900

    chore: 열거형 정의 컨벤션 통일

--- JuiceMaker/JuiceMaker/Model/JuiceMenu.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMenu.swift
@@ -5,13 +5,13 @@
 //  Created by 박병호 on 2021/10/21.
 //
 enum JuiceMenu: String {
-    case strawberry = "딸기",
-         banana = "바나나",
-         kiwi = "키위",
-         pineapple = "파인애플",
-         mango = "망고",
-         strawberryBanana = "딸바",
-         mangoKiwi = "망키"
+    case strawberry = "딸기"
+    case  banana = "바나나"
+    case  kiwi = "키위"
+    case  pineapple = "파인애플"
+    case  mango = "망고"
+    case  strawberryBanana = "딸바"
+    case  mangoKiwi = "망키"
     
     var recipe: [Fruit: Int] {
         switch self {
