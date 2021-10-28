795e57b5332743fa02bde60f9d273310d6f0bcf2
Kim-EunsooSilver <graysilver18@naver.com>
2021-10-28 14:46:07 +0900

    refactor: ViewController.swif 이름 변경(ViewController.swif -> JuiceMakerViewController.swif)

--- JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class JuiceMakerViewController: UIViewController {
     
     @IBOutlet weak var strawberryQuantityLabel: UILabel!
     @IBOutlet weak var bananaQuantityLabel: UILabel!
