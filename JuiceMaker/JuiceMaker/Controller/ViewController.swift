045fe2b75c714d2864029f5473576f6985e5161f
yeha <dev.yeha@gmail.com>
2021-10-29 23:30:47 +0900

    feat: 주문 결과에 따른 얼럿 표시하는 기능 추가
    * orderSuccessAlert(), orderFailureAlert() 구현
    * 재고가 부족할 때, 확인 버튼을 누르면 재고 수정 스토리보드로 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -58,9 +58,30 @@ class ViewController: UIViewController {
         showStockMango.text = String(mango)
     }
 
-        func order(juice: JuiceMaker.Juice) {
+    func orderSuccessAlert(juiceName: String) {
+        let alert = UIAlertController(title: "재고 있음", message: "\(juiceName)쥬스 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+        let OK = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(OK)
+        present(alert, animated: true, completion: nil)
+    }
+
+    func orderFailureAlert() {
+        let alert = UIAlertController(title: "재고 없음", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
+        let OK = UIAlertAction(title: "확인", style: .default, handler: { _ in
+            self.performSegue(withIdentifier: "changeStock", sender: nil)
+        })
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(cancel)
+        alert.addAction(OK)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func order(juice: JuiceMaker.Juice) {
         if juiceMaker.orderJuice(for: juice) {
+            orderSuccessAlert(juiceName: juice.name)
             showCurrentStock()
+        } else {
+            orderFailureAlert()
         }
     }
 }
