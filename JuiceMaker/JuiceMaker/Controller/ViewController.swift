c8fe855341b318b2dc06e6161e6b9f2c52ece422
yeha <dev.yeha@gmail.com>
2021-10-29 23:25:10 +0900

    feat: 화면에서 현재 재고를 확인하는 기능 추가
    * 스토리보드 내 쥬스 이름 오타 수정
    * showCurrentStock() 메서드 추가
    * IBOutlet 이용하여 showStock(과일이름) UILable 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -1,18 +1,38 @@
 //
 //  JuiceMaker - ViewController.swift
-//  Created by yagom. 
+//  Created by yeha.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var showStockStrawberry: UILabel!
+    @IBOutlet weak var showStockBanana: UILabel!
+    @IBOutlet weak var showStockPineapple: UILabel!
+    @IBOutlet weak var showStockKiwi: UILabel!
+    @IBOutlet weak var showStockMango: UILabel!
+    
+    let juiceMaker = JuiceMaker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showCurrentStock()
     }
-
-
+ 
+    func showCurrentStock() {
+        guard let strawberry = stock.fruitStockList[.strawberry],
+              let banana = stock.fruitStockList[.banana],
+              let pineapple = stock.fruitStockList[.pineapple],
+              let kiwi = stock.fruitStockList[.kiwi],
+              let mango = stock.fruitStockList[.mango] else {
+            return
+        }
+        showStockStrawberry.text = String(strawberry)
+        showStockBanana.text = String(banana)
+        showStockPineapple.text = String(pineapple)
+        showStockKiwi.text = String(kiwi)
+        showStockMango.text = String(mango)
+    }
 }
-
