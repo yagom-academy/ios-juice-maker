1630797354c0a7f414d06defc5b3b31d5e2ae590
yeha <dev.yeha@gmail.com>
2021-10-29 23:20:40 +0900

    feat: 재고 수정하는 뷰 컨트롤러 생성
    * 컨트롤러 폴더 내 재고 수정하는 StockViewController 파일 추가
    * 재고 수정 스토리보드 우측 상단에 바 버튼 아이템 추가
    * 내비게이션 바 자리에 '재고 추가' 문구 삽입

---
+++ JuiceMaker/JuiceMaker/Controller/StockViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StockViewController.swift
+//  Created by Quokkaaa.
+//
+
+import UIKit
+
+class StockViewController: UIViewController {
+    @IBAction func rightBarButtonItem(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
