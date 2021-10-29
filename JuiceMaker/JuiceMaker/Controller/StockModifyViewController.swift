a926924591ff54515439c3096dc84a589a389b15
hoBahk <qudgh849@gmail.com>
2021-10-29 14:57:42 +0900

    feat: 재고수정화면 재고 현황 레이블 출력
    * StockModifyViewController에 과일재고레이블 연결 후 재고 현황 출력하도록 함.

--- JuiceMaker/JuiceMaker/Controller/StockModifyViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockModifyViewController.swift
@@ -10,8 +10,38 @@ import UIKit
 class StockModifyViewController: UIViewController {
     var juiceMaker: JuiceMaking? = nil
     
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initializeFruitStockLabels()
+    }
+    
+    private func initializeFruitStockLabels() {
+        guard let juiceMaker = juiceMaker else {
+            showErrorAlert(error: FruitStoreError.stockDataMissing)
+            return
+        }
+        do {
+            strawberryStockLabel.text = String(try juiceMaker.currentFruitStock(of: .strawberry))
+            bananaStockLabel.text = String(try juiceMaker.currentFruitStock(of: .banana))
+            pineappleStockLabel.text = String(try juiceMaker.currentFruitStock(of: .pineapple))
+            kiwiStockLabel.text = String(try juiceMaker.currentFruitStock(of: .kiwi))
+            mangoStockLabel.text = String(try juiceMaker.currentFruitStock(of: .mango))
+        } catch let error {
+            showErrorAlert(error: error)
+        }
+    }
+
+    private func showErrorAlert(error: Error) {
+        let alert = UIAlertController(title: nil, message: error.localizedDescription, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: AlertMessage.ok.korean, style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
     
     @IBAction private func modifyCompleteButtonDidTap(_ sender: UIBarButtonItem) {
