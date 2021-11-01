cc88778881f127376a7385548eb5c3c961c787b3
aCafela-coffee <seym7894@gmail.com>
2021-11-01 16:50:48 +0900

    refactor: configureNavigationButton() 함수 분리

--- JuiceMaker/JuiceMaker/Controller/EditFruitStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/EditFruitStockViewController.swift
@@ -3,6 +3,10 @@ import UIKit
 class EditFruitStockViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationButton()
+    }
+    
+    fileprivate func configureNavigationButton() {
         navigationItem.title = ""
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTrigger))
     }
