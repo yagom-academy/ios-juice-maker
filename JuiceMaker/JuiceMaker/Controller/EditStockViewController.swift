7f74feb928cd0314bfc081bca04f65873d9378d7
ICS-Asan <carpediem1015@gmail.com>
2021-11-01 15:56:19 +0900

    feat: IBOutlet 변수 선언 #9
    - "재고 추가"화면의 Label, stepper와 연결

--- JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
@@ -2,6 +2,18 @@ import UIKit
 
 class EditStockViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    @IBOutlet weak var strawberryStockStepper: UIStepper!
+    @IBOutlet weak var bananaStockStepper: UIStepper!
+    @IBOutlet weak var pineappleStockStepper: UIStepper!
+    @IBOutlet weak var kiwiStockStepper: UIStepper!
+    @IBOutlet weak var mangoStockStepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
