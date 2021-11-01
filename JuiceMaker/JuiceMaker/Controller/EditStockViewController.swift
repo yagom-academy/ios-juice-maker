527afa034efd909aa495154e6ffb12047512f8b8
just1103 <hyojuson1103@gmail.com>
2021-11-01 16:32:05 +0900

    feat: IBOutlet Collection 변수 추가 #9
    - 여러 개의 Label과 Stepper를 연결

--- JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
@@ -7,12 +7,14 @@ class EditStockViewController: UIViewController {
     @IBOutlet weak var pineappleStockLabel: UILabel!
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
+    @IBOutlet var fruitStockLabels: [UILabel]!
     
     @IBOutlet weak var strawberryStockStepper: UIStepper!
     @IBOutlet weak var bananaStockStepper: UIStepper!
     @IBOutlet weak var pineappleStockStepper: UIStepper!
     @IBOutlet weak var kiwiStockStepper: UIStepper!
     @IBOutlet weak var mangoStockStepper: UIStepper!
+    @IBOutlet var fruitstockSteppers: [UIStepper]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
