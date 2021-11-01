dc6278a7c76223acb2164392ec1f13cb3453b965
yanghojoon <yanghojoon8487@gmail.com>
2021-11-01 15:54:40 +0900

    refactor: 재고수정 소스파일 이름 변경 및 클래스 이름 변경

--- JuiceMaker/JuiceMaker/Controller/StockModificationViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockModificationViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ModifyStockViewController: UIViewController {
+class StockModificationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
