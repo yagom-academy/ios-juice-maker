a7ab3927b7da8e7d911789c160ba5af944c2448a
yim2627 <yim2627@gmail.com>
2021-11-02 14:37:59 +0900

    feat : step2 -> step3 merge
    q
    wq

--- JuiceMaker/JuiceMaker/Controller/FruitStorageViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStorageViewController.swift
@@ -3,13 +3,31 @@ import UIKit
 
 class FruitStorageViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
     @IBAction func completeModifyButton(_ sender: UIButton) {
+        
         dismiss(animated: true, completion: nil)
     }
     
+    
+    
+    @IBAction func stepperTest(_ sender: UIStepper) {
+        switch sender {
+        case strawberryStepper:
+            strawberryStockLabel.text = Int(strawberryStepper.value).description
+        case bananaStepper:
+            bananaStockLabel.text = Int(bananaStepper.value).description
+        default:
+            return
+        }
+    }
 }
