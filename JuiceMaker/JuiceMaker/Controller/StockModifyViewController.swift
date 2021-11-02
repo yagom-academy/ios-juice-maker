5fb4c66055a5aedde389c523677de9097a1f5cd8
SIWONKIM <siyeon0616@gmail.com>
2021-11-02 15:45:45 +0900

    feat: 재고 수정 화면 진입시 과일의 현재 재고 수량 표시 #10
    NotificationCenter을 이용하여 FruitStore의 stock이 바뀌면 JuiceMakerViewController뿐만 아니라 StockModifyViewController도 같은 노티를 받아서 재고 수정 화면에도 재고 수량을 표시함

--- JuiceMaker/JuiceMaker/Controller/StockModifyViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockModifyViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class StockModifyViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initJuiceMakerViewController()
 
+    }
+    
+    func initJuiceMakerViewController() {
+        changeStockLabel()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(changeStockLabel), name: FruitStore.shared.didChangeStock, object: nil)
+    }
+
+    @objc
+    func changeStockLabel() {
+        strawberryStockLabel.text =  FruitStore.shared.showStock(of: .strawberry)
+        bananaStockLabel.text =  FruitStore.shared.showStock(of: .banana)
+        pineappleStockLabel.text =  FruitStore.shared.showStock(of: .pineapple)
+        kiwiStockLabel.text =  FruitStore.shared.showStock(of: .kiwi)
+        mangoStockLabel.text =  FruitStore.shared.showStock(of: .mango)
     }
     
     @IBAction func dismissButton(_ sender: UIBarButtonItem) {
