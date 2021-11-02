4d359bd8c26267941a673f8224e9e3b5eebb987f
yim2627 <yim2627@gmail.com>
2021-11-02 14:54:40 +0900

    feat : stepper 구현 및 label 수정 기능 구현

--- JuiceMaker/JuiceMaker/Controller/FruitStorageViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStorageViewController.swift
@@ -5,9 +5,16 @@ class FruitStorageViewController: UIViewController {
 
     @IBOutlet weak var strawberryStockLabel: UILabel!
     @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    
     
     @IBOutlet weak var strawberryStepper: UIStepper!
     @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,6 +33,12 @@ class FruitStorageViewController: UIViewController {
             strawberryStockLabel.text = Int(strawberryStepper.value).description
         case bananaStepper:
             bananaStockLabel.text = Int(bananaStepper.value).description
+        case mangoStepper:
+            mangoStockLabel.text = Int(mangoStepper.value).description
+        case kiwiStepper:
+            kiwiStockLabel.text = Int(kiwiStepper.value).description
+        case pineappleStepper:
+            pineappleStockLabel.text = Int(pineappleStepper.value).description
         default:
             return
         }
