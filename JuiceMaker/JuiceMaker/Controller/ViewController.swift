6a870ae84b855d0242447bc6091d381f501b0222
Quokkaaa <rktrkdml@gmail.com>
2021-11-03 11:46:40 +0900

    refactor: ViewController내 전반적인 리펙토링
    * IBOutlet 네이밍 showStock+fruitName -> stockOf+fruitName로 변경
    * Alert메서드 네이밍 orderSuccessAlert -> showSuccessAlert로 변경
    * ViewDidLoad내 기본 주석 제거
    * IBAction Button 개행 추가
    * order 메서드 내 메서드 순서 변경

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,30 +7,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var showStockStrawberry: UILabel!
-    @IBOutlet weak var showStockBanana: UILabel!
-    @IBOutlet weak var showStockPineapple: UILabel!
-    @IBOutlet weak var showStockKiwi: UILabel!
-    @IBOutlet weak var showStockMango: UILabel!
+    @IBOutlet weak var stockOfStrawberry: UILabel!
+    @IBOutlet weak var stockOfBanana: UILabel!
+    @IBOutlet weak var stockOfPineapple: UILabel!
+    @IBOutlet weak var stockOfKiwi: UILabel!
+    @IBOutlet weak var stockOfMango: UILabel!
     
     @IBAction func strawberryBananaJuiceButton(_ sender: Any) {
         order(juice: .strawberryBananaJuice)
     }
+    
     @IBAction func strawberryJuiceButton(_ sender: Any) {
         order(juice: .strawberryJuice)
     }
+    
     @IBAction func bananaJuiceButton(_ sender: Any) {
         order(juice: .bananaJuice)
     }
+    
     @IBAction func mangoKiwiJuiceButton(_ sender: Any) {
         order(juice: .mangoKiwiJuice)
     }
+    
     @IBAction func pineappleJuiceButton(_ sender: Any) {
         order(juice: .pineappleJuice)
     }
+    
     @IBAction func kiwiJuiceButton(_ sender: Any) {
         order(juice: .kiwiJuice)
     }
+    
     @IBAction func mangoJuiceButton(_ sender: Any) {
         order(juice: .mangoJuice)
     }
@@ -39,33 +45,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         showCurrentStock()
     }
  
     func showCurrentStock() {
-        guard let strawberry = stock.fruitStockList[.strawberry],
-              let banana = stock.fruitStockList[.banana],
-              let pineapple = stock.fruitStockList[.pineapple],
-              let kiwi = stock.fruitStockList[.kiwi],
-              let mango = stock.fruitStockList[.mango] else {
+        guard let strawberry = juiceMaker.fruitStore.fruits[.strawberry],
+              let banana = juiceMaker.fruitStore.fruits[.banana],
+              let pineapple = juiceMaker.fruitStore.fruits[.pineapple],
+              let kiwi = juiceMaker.fruitStore.fruits[.kiwi],
+              let mango = juiceMaker.fruitStore.fruits[.mango] else {
             return
         }
-        showStockStrawberry.text = String(strawberry)
-        showStockBanana.text = String(banana)
-        showStockPineapple.text = String(pineapple)
-        showStockKiwi.text = String(kiwi)
-        showStockMango.text = String(mango)
+        stockOfStrawberry.text = String(strawberry)
+        stockOfBanana.text = String(banana)
+        stockOfPineapple.text = String(pineapple)
+        stockOfKiwi.text = String(kiwi)
+        stockOfMango.text = String(mango)
     }
 
-    func orderSuccessAlert(juiceName: String) {
+    func showSuccessAlert(juiceName: String) {
         let alert = UIAlertController(title: "재고 있음", message: "\(juiceName)쥬스 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
         let OK = UIAlertAction(title: "확인", style: .default, handler: nil)
         alert.addAction(OK)
         present(alert, animated: true, completion: nil)
     }
 
-    func orderFailureAlert() {
+    func showFailureAlert() {
         let alert = UIAlertController(title: "재고 없음", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
         let OK = UIAlertAction(title: "확인", style: .default, handler: { _ in
             self.performSegue(withIdentifier: "changeStock", sender: nil)
@@ -76,12 +81,12 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func order(juice: JuiceMaker.Juice) {
-        if juiceMaker.orderJuice(for: juice) {
-            orderSuccessAlert(juiceName: juice.name)
+    func order(juice: Juice) {
+        if juiceMaker.order(for: juice) {
             showCurrentStock()
+            showSuccessAlert(juiceName: juice.name)
         } else {
-            orderFailureAlert()
+            showFailureAlert()
         }
     }
 }
