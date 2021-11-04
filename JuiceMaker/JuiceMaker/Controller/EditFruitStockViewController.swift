e5c60dee7604353bf47634fb3986ee46cfd0643d
horeng2 <huski.a.a@gmail.com>
2021-11-04 17:52:20 +0900

    style: 공백 라인 제거

--- JuiceMaker/JuiceMaker/Controller/EditFruitStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/EditFruitStockViewController.swift
@@ -8,7 +8,6 @@ class EditFruitStockViewController: UIViewController {
     @IBOutlet private var stockOfPineappleLabel: UILabel!
     @IBOutlet private var stockOfKiwiLabel: UILabel!
     @IBOutlet private var stockOfMangoLabel: UILabel!
-    
     @IBOutlet private weak var strawberryStepper: UIStepper!
     @IBOutlet private weak var bananaStepper: UIStepper!
     @IBOutlet private weak var pineappleStepper: UIStepper!
@@ -70,25 +69,28 @@ class EditFruitStockViewController: UIViewController {
         fruitStore?.changeQuantity(of: .strawberry, to: value)
         stockOfStrawberryLabel.text = String(value)
     }
+    
     @IBAction private func hitBananaStepper(_ sender: UIStepper) {
         let value = Int(bananaStepper.value)
         fruitStore?.changeQuantity(of: .banana, to: value)
         stockOfBananaLabel.text = String(value)
     }
+    
     @IBAction private func hitPineappleStepper(_ sender: UIStepper) {
         let value = Int(pineappleStepper.value)
         fruitStore?.changeQuantity(of: .pineapple, to: value)
         stockOfPineappleLabel.text = String(value)
     }
+    
     @IBAction private func hitKiwiStepper(_ sender: UIStepper) {
         let value = Int(kiwiStepper.value)
         fruitStore?.changeQuantity(of: .kiwi, to: value)
         stockOfKiwiLabel.text = String(value)
     }
+    
     @IBAction private func hitMangoStepper(_ sender: UIStepper) {
         let value = Int(mangoStepper.value)
         fruitStore?.changeQuantity(of: .mango, to: value)
         stockOfMangoLabel.text = String(value)
     }
-    
 }
