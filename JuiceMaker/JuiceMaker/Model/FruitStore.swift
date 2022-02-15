cd317331449b1dffe2a411089d890544619823bf
JiWon Lee <jiwon.swdev@gmail.com>
2022-02-15 13:59:17 +0900

    feat: FruitStore 초기화

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,6 +7,14 @@
 import Foundation
 
 // 과일 저장소 타입
-class FruitStore {
+final class FruitStore {
+    private var fruits: [Fruit: Int] = [:]
     
+    init(_ value: Int = 10) {
+        for fruit in Fruit.allCases {
+            self.fruits[fruit] = value
+        }
+    }
+    
+    // TODO: 각 과일의 수량 n개를 변경할 수 있어야 한다
 }
