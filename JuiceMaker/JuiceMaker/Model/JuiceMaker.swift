70768bba643f2e56956027de958b282d23d0ace6
JIIN HEO <39071796+JIINHEO@users.noreply.github.com>
2022-02-16 01:00:04 +0900

    feat: JuiceMaker 동작 일부 구현
    TODO: 과일 갯수 부족할 때 실패 원인으로 과일 종류 전달 필요

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    private var fruitStore: FruitStore = FruitStore()
     
+    func makeJuice(juice: Juice) -> JuiceMakeResult {
+        if fruitStore.canMakeJuice(juice: juice) {
+            fruitStore.useFruit(juice: juice)
+            return .success
+        }
+        return .fail(needFruits: []) //TODO: 부족한 과일 종류 전달 필요함
+    }
 }
