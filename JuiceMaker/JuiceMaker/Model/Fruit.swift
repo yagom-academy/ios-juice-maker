02f9ada0c7467e1d76bde62bb8a0d41f117e91ad
JIIN HEO <39071796+JIINHEO@users.noreply.github.com>
2022-02-15 23:42:44 +0900

    feat: Fruit enum 정의

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by temp2 on 2022/02/15.
+//
+
+import Foundation
+
+enum Fruit: Juicable {
+    case Strawberry
+    case Banana
+    case Pineapple
+    case Kiwi
+    case Mango
+}
