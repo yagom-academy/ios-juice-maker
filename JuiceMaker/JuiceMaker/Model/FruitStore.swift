0619253537ac965cc5c556e792f5992f37284e2e
higgs1022 <csh8130@kakao.com>
2022-02-16 00:58:13 +0900

    refactor: FruitStore class의 useFruit 메서드 동작 변경

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -20,15 +20,21 @@ class FruitStore {
         }
     }
     
-    func useFruit(fruit: Fruit, count: Int) {
+    private func reduceFruit(fruit: Fruit, count: Int) {
         guard let currentFruitCount = stock[fruit], currentFruitCount >= count else {
             fatalError()
         }
         stock[fruit] = currentFruitCount - count
     }
     
-    func canMakeJuice(fruit: Juice) -> Bool {
-        for fruitInfo in fruit.recipe.fruitList {
+    func useFruit(juice: Juice) {
+        juice.recipe.fruitList.forEach { fruitInfo in
+            reduceFruit(fruit: fruitInfo.fruit, count: fruitInfo.count)
+        }
+    }
+    
+    func canMakeJuice(juice: Juice) -> Bool {
+        for fruitInfo in juice.recipe.fruitList {
             guard let currentFruitCount = stock[fruitInfo.fruit] else {
                 return false
             }
