4d81616901337fe4cdfddf495d626f46c6915f3f
소영 <sso_0022@naver.com>
2022-02-16 00:03:44 +0900

    feat: [step1] Add FruitScore type

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,28 @@
 
 import Foundation
 
+protocol FruitStorable {
+  func updateStock(of fruit: Fruit, stock: Stock)
+  func stock(of fruit: Fruit) -> Stock?
+}
+
 // 과일 저장소 타입
-class FruitStore {
+class FruitStore: FruitStorable {
+  var store: [Fruit: Stock] = [:]
+  
+  init() {
+    Fruit.allCases.forEach {
+      store[$0] = Stock(10)
+    }
+  }
+  
+  func updateStock(of fruit: Fruit, stock: Stock) {
+    guard store[fruit] != nil else { return }
     
+    store[fruit] = stock
+  }
+  
+  func stock(of fruit: Fruit) -> Stock? {
+    return store[fruit]
+  }
 }
