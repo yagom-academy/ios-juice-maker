984e3649e1d2cbab816a8a0f1e12a6cfdaa1cbc9
JIIN HEO <39071796+JIINHEO@users.noreply.github.com>
2022-02-16 00:45:16 +0900

    feat: FruitStore class에 useFruit, canMakeJuice 기능 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -19,4 +19,24 @@ class FruitStore {
             stock[fruit] = initCount
         }
     }
+    
+    func useFruit(fruit: Fruit, count: Int) {
+        guard let currentFruitCount = stock[fruit], currentFruitCount >= count else {
+            fatalError()
+        }
+        stock[fruit] = currentFruitCount - count
+    }
+    
+    func canMakeJuice(fruit: Juice) -> Bool {
+        for fruitInfo in fruit.recipe.fruitList {
+            guard let currentFruitCount = stock[fruitInfo.fruit] else {
+                return false
+            }
+            
+            if currentFruitCount < fruitInfo.count {
+                return false
+            }
+        }
+        return true
+    }
 }
