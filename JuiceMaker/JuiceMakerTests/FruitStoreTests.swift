3b1834a1ae5c49e18c37a3f03fbd0b122bfe7ecb
Paul <phs880623@gmail.com>
2022-02-15 14:44:09 +0900

    feat: increase 함수 구현
    FruiteStock의 increase 메서드를 구현
    - UniTest 작성

--- JuiceMaker/JuiceMakerTests/FruitStoreTests.swift
+++ JuiceMaker/JuiceMakerTests/FruitStoreTests.swift
@@ -6,7 +6,6 @@
 //
 
 import XCTest
-
 @testable import JuiceMaker
 
 class FruitStoreTests: XCTestCase {
@@ -20,5 +19,19 @@ class FruitStoreTests: XCTestCase {
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         fruitStore = nil
+    }
+    
+    func test_초기재고에서_딸기4개를_더하면_14개가_나온다() {
+        // given
+        let inputFruit: Fruit = .strawberry
+        let inputAmount: Int = 4
+        let expectation: Int = 14
+        
+        // when
+        fruitStore?.increase(fruit: inputFruit, to: inputAmount)
+        let result: Int = fruitStore?.fruits[inputFruit] ?? 0
+        
+        // then
+        XCTAssertEqual(expectation, result)
     }
 }
