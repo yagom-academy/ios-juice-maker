8f30484ae60464d60f21abb5b5f19edeeb795745
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2022-02-15 14:56:39 +0900

    feat: Implement FruitStore initializer

---
+++ JuiceMaker/JuiceMaker/Error/FruitStoreError.swift
@@ -0,0 +1,8 @@
+//
+//  FruitStoreError.swift
+//  JuiceMaker
+//
+//  Created by Jinyoung Kim on 2022/02/15.
+//
+
+import Foundation
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,11 +9,23 @@ import Foundation
 // 과일 저장소 타입
 class FruitStore {
     
-    enum Fruit: Hashable {
+    enum Fruit: Hashable, CaseIterable {
         case strawberry
         case banana
         case pineapple
         case kiwi
         case mango
+    }
+    
+    // MARK: - Properties
+    
+    var store: [Fruit: Int] = [:]
+    
+    // MARK: - Lifecycle
+    
+    init(initialStock: Int) {
+        Fruit.allCases.forEach {
+            store[$0] = initialStock
+        }
     }
 }
