875faaa9c97b5cc72107bdd60346645a3108f11f
momo-youngg <yeongmoj94+dev@gmail.com>
2022-02-16 05:12:00 +0900

    feat: JuiceMaker에 make(of:) 구현
    각각의 쥬스 별로 필요한 과일의 수가 달라지는데, 이를 if로 분기 치고싶지 않았음
    enum Juice에 해당하는 필요한 과일의 수를 알려주는 역할의 자료구조 필요 -> Recipe
    Recipe에서는 각 쥬스 별로 어떠한 과일이 얼만큼 필요한지 NeededFruit로 관리하게 됨.
    현재 레시피가 추가되는 요건은 없으므로 get만 구현.

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -9,7 +9,31 @@ import Foundation
 // 쥬스 메이커 타입
 struct JuiceMaker {
     
-    private let fruitStore: FruitStore = FruitStore()
+    private var fruitStore: FruitStore = FruitStore()
+    
+    mutating func make(of juice: Juice) throws {
+        guard let neededFruits = Recipe.get(juice) else {
+            throw JuiceMakerError.notExistRecipeError
+        }
+        try validateStock(of: neededFruits)
+        try useStock(of: neededFruits)
+    }
+    
+    private func validateStock(of neededFruits: [Recipe.NeededFruit]) throws {
+        let outOfStocks = neededFruits.filter { neededFruit in
+            let stock = fruitStore.getStock(of: neededFruit.fruit)
+            return stock < neededFruit.quantity
+        }
+        if outOfStocks.count > 0 {
+            throw JuiceMakerError.soldOutError
+        }
+    }
+    
+    mutating private func useStock(of neededFruits: [Recipe.NeededFruit]) throws {
+        try neededFruits.forEach { neededFruits in
+            try fruitStore.decreaseStock(of: neededFruits.fruit, neededFruits.quantity)
+        }
+    }
     
 }
 
@@ -18,6 +42,50 @@ extension JuiceMaker {
     enum Juice {
         case strawberryJuice, bananaJuice, kiwiJuice, pineappleJuice
         case strawberryBananaJuice, mangoJuice, mangoKiwiJuice
+    }
+    
+    enum JuiceMakerError: Error {
+        case soldOutError
+        case notExistRecipeError
+    }
+    
+    private struct Recipe {
+        
+        struct NeededFruit {
+            let fruit: FruitStore.Fruit
+            let quantity: Quantity
+        }
+        
+        private static let recipe: [Juice: [NeededFruit]] = [
+            .strawberryJuice: [
+                NeededFruit(fruit: .strawberry, quantity: Quantity(16)),
+            ],
+            .bananaJuice: [
+                NeededFruit(fruit: .banana, quantity: Quantity(2)),
+            ],
+            .kiwiJuice: [
+                NeededFruit(fruit: .banana, quantity: Quantity(3)),
+            ],
+            .pineappleJuice: [
+                NeededFruit(fruit: .pineapple, quantity: Quantity(2)),
+            ],
+            .strawberryBananaJuice: [
+                NeededFruit(fruit: .strawberry, quantity: Quantity(10)),
+                NeededFruit(fruit: .banana, quantity: Quantity(2)),
+            ],
+            .mangoJuice: [
+                NeededFruit(fruit: .mango, quantity: Quantity(3)),
+            ],
+            .mangoKiwiJuice: [
+                NeededFruit(fruit: .mango, quantity: Quantity(2)),
+                NeededFruit(fruit: .kiwi, quantity: Quantity(1)),
+            ],
+        ]
+        
+        static func get(_ juice: Juice) -> [NeededFruit]? {
+            return recipe[juice]
+        }
+
     }
     
 }
--- JuiceMaker/JuiceMakerTests/Model/JuiceMakerTests.swift
+++ JuiceMaker/JuiceMakerTests/Model/JuiceMakerTests.swift
@@ -21,5 +21,21 @@ class JuiceMakerTests: XCTestCase {
         try super.tearDownWithError()
         sut = nil
     }
+    
+    func test_쥬스를_만들_수_있다() {
+        // given
+        let juice = JuiceMaker.Juice.bananaJuice
+        
+        // when then
+        XCTAssertNoThrow(try sut.make(of: juice))
+    }
+    
+    func test_쥬스의_재고가_없을_경우_실패한다() {
+        // given
+        let juice = JuiceMaker.Juice.strawberryJuice
+        
+        // when then
+        XCTAssertThrowsError(try sut.make(of: juice))
+    }
 
 }
