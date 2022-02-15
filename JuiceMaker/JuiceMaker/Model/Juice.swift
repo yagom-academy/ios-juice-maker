f0c38b0d08f0f7dce7d1f231808493a3daf92234
JiWon Lee <jiwon.swdev@gmail.com>
2022-02-15 13:46:49 +0900

    feat: 주스 모델 정의

---
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -0,0 +1,18 @@
+//
+//  Juice.swift
+//  JuiceMaker
+//
+//  Created by 이지원 on 2022/02/15.
+//
+
+import Foundation
+
+enum Juice {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case mangoJuice
+    case strawberryBananaJuice
+    case mangoKiwiJuice
+}
