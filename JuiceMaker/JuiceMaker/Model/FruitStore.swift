12354e1fe9c42642958046602355f0dad54ed139
haeseoklee <haeseoklee.dev@gmail.com>
2022-02-15 20:44:54 +0900

    refactor: FruitStore의 과일 프로퍼티들의 set 을 private으로 수정, increase와 decrease 메소드 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,11 +8,12 @@ import Foundation
 
 // 과일 저장소 타입
 final class FruitStore {
-    var strawberry: Strawberry
-    var banana: Banana
-    var kiwi: Kiwi
-    var pineapple: Pineapple
-    var mango: Mango
+    
+    private(set) var strawberry: Strawberry
+    private(set) var banana: Banana
+    private(set) var kiwi: Kiwi
+    private(set) var pineapple: Pineapple
+    private(set) var mango: Mango
     
     init(strawberryCount: Int, bananaCount: Int, kiwiCount: Int, pineappleCount: Int, mangoCount: Int) {
         self.strawberry = Strawberry(counter: strawberryCount)
@@ -34,6 +35,40 @@ final class FruitStore {
             pineapple.update(num: num)
         case is Mango.Type:
             mango.update(num: num)
+        default:
+            return
+        }
+    }
+    
+    func increase<T: Fruit>(fruitType: T.Type, num: Int) {
+        switch fruitType {
+        case is Strawberry.Type:
+            strawberry.increase(num: num)
+        case is Banana.Type:
+            banana.increase(num: num)
+        case is Kiwi.Type:
+            kiwi.increase(num: num)
+        case is Pineapple.Type:
+            pineapple.increase(num: num)
+        case is Mango.Type:
+            mango.increase(num: num)
+        default:
+            return
+        }
+    }
+    
+    func decrease<T: Fruit>(fruitType: T.Type, num: Int) {
+        switch fruitType {
+        case is Strawberry.Type:
+            strawberry.decrease(num: num)
+        case is Banana.Type:
+            banana.decrease(num: num)
+        case is Kiwi.Type:
+            kiwi.decrease(num: num)
+        case is Pineapple.Type:
+            pineapple.decrease(num: num)
+        case is Mango.Type:
+            mango.decrease(num: num)
         default:
             return
         }
