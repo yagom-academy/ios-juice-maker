f8897d24033fced36182ad9043f26300c59f9904
momo-youngg <yeongmoj94+dev@gmail.com>
2022-02-16 01:42:10 +0900

    feat: Add FruitStore initializer and getStock method
    Initialize all the fruits stock by enum allcases.

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,10 +9,21 @@ import Foundation
 // 과일 저장소 타입
 struct FruitStore {
     
-    enum Fruit {
+    enum Fruit: CaseIterable {
         case strawberry, banana, pineapple, kiwi, mango
     }
     
-    private let fruits: [Fruit: Int] = [:]
+    private let stock: [Fruit: Int]
+    
+    init() {
+        let allFruits = Fruit.allCases
+        stock = allFruits.reduce(into: [:], { partialResult, fruit in
+            partialResult[fruit] = 10
+        })
+    }
+    
+    func getStock(of fruit: Fruit) -> Int {
+        return stock[fruit] ?? 0
+    }
     
 }
--- JuiceMaker/JuiceMakerTests/Model/FruitStoreTests.swift
+++ JuiceMaker/JuiceMakerTests/Model/FruitStoreTests.swift
@@ -6,30 +6,37 @@
 //
 
 import XCTest
+@testable import JuiceMaker
 
 class FruitStoreTests: XCTestCase {
+    
+    private var sut: FruitStore!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = FruitStore()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_과일의_초기_재고는_10개이다() {
+        // when
+        let strawberryStock = sut.getStock(of: .strawberry)
+        let bananaStock = sut.getStock(of: .banana)
+        let pineappleStock = sut.getStock(of: .pineapple)
+        let kiwiStock = sut.getStock(of: .kiwi)
+        let mangoStock = sut.getStock(of: .mango)
+        let expectedStock = 10
+        
+        // then
+        XCTAssertEqual(strawberryStock, expectedStock)
+        XCTAssertEqual(bananaStock, expectedStock)
+        XCTAssertEqual(pineappleStock, expectedStock)
+        XCTAssertEqual(kiwiStock, expectedStock)
+        XCTAssertEqual(mangoStock, expectedStock)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
