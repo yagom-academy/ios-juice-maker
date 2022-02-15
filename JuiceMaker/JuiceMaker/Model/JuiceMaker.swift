5df948e36179b867a2a2b8724fddb86d1cd2022c
samsamilsa <hyejeekim@me.com>
2022-02-15 14:38:42 +0900

    feat: #4 - Add Juice enum

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -9,6 +9,35 @@ import Foundation
 // 쥬스 메이커 타입
 struct JuiceMaker {
     
+    enum Juice {
+        case strawberryJuice
+        case bananaJuice
+        case kiwiJuice
+        case pineappleJuice
+        case strawberryBananaJuice
+        case mangoJuice
+        case mangoKiwiJuice
+        
+        var recipe: [FruitStore.Fruit: Int] {
+            switch self {
+            case .strawberryJuice:
+                return [.strawberry: 16]
+            case .bananaJuice:
+                return [.banana: 2]
+            case .kiwiJuice:
+                return [.kiwi: 3]
+            case .pineappleJuice:
+                return [.pineapple: 2]
+            case .strawberryBananaJuice:
+                return [.strawberry: 10, .banana: 1]
+            case .mangoJuice:
+                return [.mango: 3]
+            case .mangoKiwiJuice:
+                return [.mango: 2, .kiwi: 1]
+            }
+        }
+    }
+    
     let fruitStore: FruitStore = FruitStore()
     
 }
