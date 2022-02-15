0fd88ff5269de629a6500f87dd338a8f43571027
Oh Donggeon <donggeon.dev@gmail.com>
2022-02-15 14:20:34 +0900

    feat: Adopt Stuff to Fruit

--- JuiceMaker/JuiceMaker/Model/Fruit.swift
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 
-enum Fruit {
+enum Fruit: Stuff {
     
     case strawberry, banana, pineapple, kiwi, mango
+    
+    var name: Fruit {
+        return self
+    }
     
 }
