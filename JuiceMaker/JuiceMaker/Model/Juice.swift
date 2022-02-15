416cea1beb514629722152b1c4a9094b90d767b3
Oh Donggeon <donggeon.dev@gmail.com>
2022-02-15 13:56:07 +0900

    feat: Define Juice.swift

---
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -0,0 +1,37 @@
+//
+//  Juice.swift
+//  JuiceMaker
+//
+//  Created by Oh Donggeon on 2022/02/15.
+//
+
+import Foundation
+
+enum Juice {
+    
+    case strawberry, banana, kiwi, pineapple, strawberryBanana, mango, mangoKiwi
+    
+    var ingredients: [FruitCounter] {
+        var ingredients: [FruitCounter] = []
+        switch self {
+        case .strawberry:
+            ingredients.append(FruitCounter(fruit: .strawberry, count: 16))
+        case .banana:
+            ingredients.append(FruitCounter(fruit: .banana, count: 2))
+        case .kiwi:
+            ingredients.append(FruitCounter(fruit: .kiwi, count: 3))
+        case .pineapple:
+            ingredients.append(FruitCounter(fruit: .pineapple, count: 2))
+        case .strawberryBanana:
+            ingredients.append(FruitCounter(fruit: .strawberry, count: 10))
+            ingredients.append(FruitCounter(fruit: .banana, count: 1))
+        case .mango:
+            ingredients.append(FruitCounter(fruit: .mango, count: 3))
+        case .mangoKiwi:
+            ingredients.append(FruitCounter(fruit: .mango, count: 2))
+            ingredients.append(FruitCounter(fruit: .kiwi, count: 1))
+        }
+        return ingredients
+    }
+    
+}
