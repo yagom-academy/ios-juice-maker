b881c3d91155fe7b85e7e27665d6bb7b6ac61214
haeseoklee <haeseoklee.dev@gmail.com>
2022-02-16 12:51:17 +0900

    refactor: description을 name으로 리팩토링, name 프로퍼티를 static으로 수정

--- JuiceMaker/JuiceMaker/Model/Fruit.swift
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol Fruit {
-    var description: String { get }
+    static var name: String { get }
     var counter: Int { get set }
 }
 
@@ -27,26 +27,26 @@ extension Fruit {
 }
 
 struct Strawberry: Fruit {
-    let description: String = "딸기"
-    var counter: Int = 0
+    static let name: String = "딸기"
+    var counter: Int = 10
 }
 
 struct Banana: Fruit {
-    let description: String = "바나나"
-    var counter: Int = 0
+    static let name: String = "바나나"
+    var counter: Int = 10
 }
 
 struct Kiwi: Fruit {
-    let description: String = "키위"
-    var counter: Int = 0
+    static let name: String = "키위"
+    var counter: Int = 10
 }
 
 struct Pineapple: Fruit {
-    let description: String = "파인애플"
-    var counter: Int = 0
+    static let name: String = "파인애플"
+    var counter: Int = 10
 }
 
 struct Mango: Fruit {
-    let description: String = "망고"
-    var counter: Int = 0
+    static let name: String = "망고"
+    var counter: Int = 10
 }
