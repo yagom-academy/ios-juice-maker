d9e99af5c1215280ef027c21dd8834e96736f73a
ryan-son <ryan.son1002@gmail.com>
2022-02-17 01:34:02 +0900

    docs : 문서화 주석 내용 추가

--- JuiceMaker/JuiceMaker/Utilities/Extensions/Dictionary+Extensions.swift
+++ JuiceMaker/JuiceMaker/Utilities/Extensions/Dictionary+Extensions.swift
@@ -7,7 +7,7 @@
 
 extension Dictionary where Value: Numeric {
     /// 딕셔너리의 Key 값을 기준으로 value간의 빼기 연산을 수행합니다.
-    /// 연산자의 오른편에 있는 요소의 키가 왼쪽편에 있는 요소에 없을 때 해당 키를 추가하여 연산을 수행합니다.
+    /// 연산자의 오른편에 있는 요소의 키가 왼쪽편에 있는 요소에 없으면 해당 키를 추가하여 연산을 수행합니다. 키가 없다면 기본 값은 0입니다.
     static func - (lhs: Self, rhs: Self) -> Self {
         var calculated = lhs
 
