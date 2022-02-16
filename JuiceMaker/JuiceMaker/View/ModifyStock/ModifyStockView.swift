6f88e802250820c0e43c10e0b07fedefffa97244
Oh Donggeon <donggeon.dev@gmail.com>
2022-02-16 16:57:40 +0900

    feat: Init ModifyStockView and Add layout

---
+++ JuiceMaker/JuiceMaker/View/ModifyStock/ModifyStockView.swift
@@ -0,0 +1,14 @@
+//
+//  ModifyStockView.swift
+//  JuiceMaker
+//
+//  Created by Oh Donggeon on 2022/02/16.
+//
+
+import UIKit
+
+class ModifyStockView: UIView {
+    @IBOutlet weak var stockLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var countStepper: UIStepper!
+}
