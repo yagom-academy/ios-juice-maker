5adee5bdabff0732ff95e033a15ae57655e0d1da
samsamilsa <hyejeekim@me.com>
2022-02-16 15:21:39 +0900

    feat: Create ManageStockViewController

---
+++ JuiceMaker/JuiceMaker/Controller/ManageStockViewController/ManageStockViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ManageStockViewController.swift
+//  JuiceMaker
+//
+//  Created by HyeJee Kim on 2022/02/16.
+//
+
+import UIKit
+
+class ManageStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
