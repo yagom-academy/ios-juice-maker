afc973e921d08dde471b7c02c85a7b4afe658937
소영 <sso_0022@naver.com>
2022-02-16 21:55:59 +0900

    feat: [step3] Implement StockViewController and StockViewModel

---
+++ JuiceMaker/JuiceMaker/Controller/StockViewController.swift
@@ -0,0 +1,138 @@
+//
+//  StockViewController.swift
+//  JuiceMaker
+//
+//  Created by soyounglee on 2022/02/16.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class StockViewController: UIViewController {
+  var viewModel: StockViewModelType?
+  let disposeBag = DisposeBag()
+  
+  @IBOutlet weak var closeButton: UIButton!
+  @IBOutlet weak var strawberryStockLabel: UILabel!
+  @IBOutlet weak var bananaStockLabel: UILabel!
+  @IBOutlet weak var pineappleStockLabel: UILabel!
+  @IBOutlet weak var kiwiStockLabel: UILabel!
+  @IBOutlet weak var mangoStockLabel: UILabel!
+  
+  @IBOutlet weak var strawberryMinusButton: UIButton!
+  @IBOutlet weak var strawberryPlusButton: UIButton!
+  @IBOutlet weak var bananaMinusButton: UIButton!
+  @IBOutlet weak var bananaPlusButton: UIButton!
+  @IBOutlet weak var pineappleMinusButton: UIButton!
+  @IBOutlet weak var pineapplePlusButton: UIButton!
+  @IBOutlet weak var kiwiMinusButton: UIButton!
+  @IBOutlet weak var kiwiPlusButton: UIButton!
+  @IBOutlet weak var mangoMinusButton: UIButton!
+  @IBOutlet weak var mangoPlusButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    subscribeUI()
+    bind()
+  }
+  
+  private func subscribeUI() {
+    closeButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.dismiss(animated: true, completion: nil)
+      })
+      .disposed(by: disposeBag)
+    
+    strawberryMinusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.decreaseStock(fruit: .strawberry)
+      })
+      .disposed(by: disposeBag)
+    
+    bananaMinusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.decreaseStock(fruit: .banana)
+      })
+      .disposed(by: disposeBag)
+    
+    pineappleMinusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.decreaseStock(fruit: .pineapple)
+      })
+      .disposed(by: disposeBag)
+    
+    kiwiMinusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.decreaseStock(fruit: .kiwi)
+      })
+      .disposed(by: disposeBag)
+    
+    mangoMinusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.decreaseStock(fruit: .mango)
+      })
+      .disposed(by: disposeBag)
+    
+    strawberryPlusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.increaseStock(fruit: .strawberry)
+      })
+      .disposed(by: disposeBag)
+    
+    bananaPlusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.increaseStock(fruit: .banana)
+      })
+      .disposed(by: disposeBag)
+    
+    pineapplePlusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.increaseStock(fruit: .pineapple)
+      })
+      .disposed(by: disposeBag)
+    
+    kiwiPlusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.increaseStock(fruit: .kiwi)
+      })
+      .disposed(by: disposeBag)
+    
+    mangoPlusButton.rx.tap
+      .subscribe(with: self, onNext: { owner, _ in
+        owner.viewModel?.increaseStock(fruit: .mango)
+      })
+      .disposed(by: disposeBag)
+    
+  }
+  
+  private func bind() {
+    viewModel?.strawberryStockObserbable
+      .map { "\($0)" }
+      .bind(to: strawberryStockLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    viewModel?.bananaStockObserbable
+      .map { "\($0)" }
+      .bind(to: bananaStockLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    viewModel?.pineappleStockObserbable
+      .map { "\($0)" }
+      .bind(to: pineappleStockLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    viewModel?.kiwiStockObserbable
+      .map { "\($0)" }
+      .bind(to: kiwiStockLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    viewModel?.mangoStockObserbable
+      .map { "\($0)" }
+      .bind(to: mangoStockLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+  }
+  
+}
---
+++ JuiceMaker/JuiceMaker/ViewModel/StockViewModel.swift
@@ -0,0 +1,86 @@
+//
+//  StockViewModel.swift
+//  JuiceMaker
+//
+//  Created by soyounglee on 2022/02/16.
+//
+
+import Foundation
+import RxSwift
+
+protocol StockViewModelType {
+  var strawberryStockObserbable: Observable<Int> { get }
+  var bananaStockObserbable: Observable<Int> { get }
+  var pineappleStockObserbable: Observable<Int> { get }
+  var kiwiStockObserbable: Observable<Int> { get }
+  var mangoStockObserbable: Observable<Int> { get }
+  func increaseStock(fruit: Fruit)
+  func decreaseStock(fruit: Fruit)
+}
+
+class StockViewModel: StockViewModelType {
+  var fruitStore: FruitStorable
+  
+  let strawberryStockSubject = BehaviorSubject<Int>(value: 0)
+  let bananaStockSubject = BehaviorSubject<Int>(value: 0)
+  let pineappleStockSubject = BehaviorSubject<Int>(value: 0)
+  let kiwiStockSubject = BehaviorSubject<Int>(value: 0)
+  let mangoStockSubject = BehaviorSubject<Int>(value: 0)
+  
+  // output
+  var strawberryStockObserbable: Observable<Int> { strawberryStockSubject }
+  var bananaStockObserbable: Observable<Int> { bananaStockSubject }
+  var pineappleStockObserbable: Observable<Int> { pineappleStockSubject }
+  var kiwiStockObserbable: Observable<Int> { kiwiStockSubject }
+  var mangoStockObserbable: Observable<Int> { mangoStockSubject }
+  
+  init(_ fruitStore: FruitStorable) {
+    self.fruitStore = fruitStore
+    
+    initFruitStock()
+  }
+  
+  private func initFruitStock() {
+    guard let strawberryStock = fruitStore.stock(of: .strawberry),
+          let bananaStock = fruitStore.stock(of: .banana),
+          let pineappleStock = fruitStore.stock(of: .pineapple),
+          let kiwiStock = fruitStore.stock(of: .kiwi),
+          let mangoStock = fruitStore.stock(of: .mango) else { return }
+    
+    strawberryStockSubject.onNext(strawberryStock.quantity)
+    bananaStockSubject.onNext(bananaStock.quantity)
+    pineappleStockSubject.onNext(pineappleStock.quantity)
+    kiwiStockSubject.onNext(kiwiStock.quantity)
+    mangoStockSubject.onNext(mangoStock.quantity)
+  }
+  
+  func increaseStock(fruit: Fruit) {
+    guard let stock = fruitStore.stock(of: fruit) else { return }
+    
+    updateStock(fruit: fruit, quantity: stock.quantity + 1)
+  }
+  
+  func decreaseStock(fruit: Fruit) {
+    guard let stock = fruitStore.stock(of: fruit),
+          stock.quantity - 1 >= 0 else { return }
+    
+    updateStock(fruit: fruit, quantity: stock.quantity - 1)
+  }
+  
+  private func updateStock(fruit: Fruit, quantity: Int) {
+    fruitStore.updateStock(of: fruit, quantity: quantity)
+    switch fruit {
+    case .strawberry:
+      strawberryStockSubject.onNext(quantity)
+    case .banana:
+      bananaStockSubject.onNext(quantity)
+    case .pineapple:
+      pineappleStockSubject.onNext(quantity)
+    case .kiwi:
+      kiwiStockSubject.onNext(quantity)
+    case .mango:
+      mangoStockSubject.onNext(quantity)
+    }
+    
+  }
+}
