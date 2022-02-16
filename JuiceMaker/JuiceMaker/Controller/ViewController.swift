9f9663a2627ffed23a16309d9c1aca486a1b78d3
JIINHEO <gjwldls123@gmail.com>
2022-02-17 04:04:15 +0900

    refactor: ViewController 메서드 private 지정

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -15,11 +15,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var mangoLabel: UILabel!
     
     let juiceMaker = JuiceMaker()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureView()
-    }
 
     @IBAction func tapOrderButton(_ sender: UIButton) {
         Juice.allCases.forEach { juice in
@@ -29,13 +24,13 @@ class ViewController: UIViewController {
         }
     }
     
-    func checkMatch(order: String, juice: Juice) {
+    private func checkMatch(order: String, juice: Juice) {
         if order == juice.name + " 주문" {
             orderMenu(of: juice)
         }
     }
     
-    func orderMenu(of juice: Juice) {
+    private func orderMenu(of juice: Juice) {
         let result = juiceMaker.make(of: juice)
          
          switch result {
@@ -47,14 +42,14 @@ class ViewController: UIViewController {
          }
     }
     
-    func showConfirmAlert(for message: String) {
+    private func showConfirmAlert(for message: String) {
         let alert = UIAlertController(title: message, message: "", preferredStyle: UIAlertController.Style.alert)
         let defaultAction =  UIAlertAction(title: "확인", style: UIAlertAction.Style.default)
         alert.addAction(defaultAction)
         self.present(alert, animated: false)
     }
     
-    func showNotEnoghAlert() {
+    private func showNotEnoghAlert() {
         let alert = UIAlertController(title: "재료가 모자라요. 재고를 수정할까요?", message: "", preferredStyle: UIAlertController.Style.alert)
         let defaultAction =  UIAlertAction(title: "확인", style: UIAlertAction.Style.default)
         let cancleAction =  UIAlertAction(title: "취소", style: UIAlertAction.Style.cancel)
@@ -62,10 +57,15 @@ class ViewController: UIViewController {
         alert.addAction(cancleAction)
         self.present(alert, animated: false)
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
 }
 
 extension ViewController {
-    func configureView() {
+    private func configureView() {
         strawberryLabel.text = String(juiceMaker.quantity(of: .strawberry))
         bananaLabel.text = String(juiceMaker.quantity(of: .banana))
         pineappleLabel.text = String(juiceMaker.quantity(of: .pineapple))
