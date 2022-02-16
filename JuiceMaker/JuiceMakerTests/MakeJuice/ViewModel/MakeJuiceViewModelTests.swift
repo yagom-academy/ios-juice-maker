e045d6a971c4eadaf8f04e72bee5f684e767d3cc
DayeonJung <ekdus0227@gmail.com>
2022-02-17 02:37:08 +0900

    test: 뷰모델 내에서 과일의 초깃값이 제대로 조회 되는지 확인

--- JuiceMaker/JuiceMakerTests/MakeJuice/ViewModel/MakeJuiceViewModelTests.swift
+++ JuiceMaker/JuiceMakerTests/MakeJuice/ViewModel/MakeJuiceViewModelTests.swift
@@ -6,19 +6,35 @@
 //
 
 import XCTest
+@testable import JuiceMaker
 
 class MakeJuiceViewModelTests: XCTestCase {
 
-    var viewModel: MakeJuiceViewModelTests!
+    var viewModel: MakeJuiceViewModel!
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        viewModel = MakeJuiceViewModelTests()
+        viewModel = MakeJuiceViewModel(juiceMaker: JuiceMaker())
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         viewModel = nil
+    }
+    
+    
+    func test_바나나의_초기_재고가_10개로_반환된다() {
+        
+        // given
+        let fruit = FruitStore.Fruit.banana
+        
+        // when
+        let stocks = viewModel.stock(of: fruit)
+        let expected = Quantity(10)
+
+        // then
+        XCTAssertEqual(stocks, expected)
+        
     }
 
 }
