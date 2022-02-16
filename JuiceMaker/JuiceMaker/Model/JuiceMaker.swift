cf69ae7ba18c36af0d58091326fb5a380ff6acd2
haeseoklee <haeseoklee.dev@gmail.com>
2022-02-16 13:10:56 +0900

    refactor: 쥬스제작에 사용되는 과일의 개수에 따라 클래스 분리

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,32 +7,12 @@
 import Foundation
 
 // 쥬스 메이커 타입
-final class JuiceMaker {
-    
+
+class JuiceMaker {
     lazy var emptyJuiceMakerState: EmptyJuiceMakerState = {
         return EmptyJuiceMakerState(juiceMaker: self)
     }()
-    lazy var strawberryJuiceMakerState: StrawberryJuiceMakerState = {
-        return StrawberryJuiceMakerState(juiceMaker: self)
-    }()
-    lazy var bananaJuiceMakerState: BananaJuiceMakerState = {
-        return BananaJuiceMakerState(juiceMaker: self)
-    }()
-    lazy var kiwiJuiceMakerState: KiwiJuiceMakerState = {
-        return KiwiJuiceMakerState(juiceMaker: self)
-    }()
-    lazy var pineappleJuiceMakerState: PineappleJuiceMakerState = {
-        return PineappleJuiceMakerState(juiceMaker: self)
-    }()
-    lazy var strawberryBananaJuiceMakerState: StrawberryBananaJuiceMakerState = {
-        return StrawberryBananaJuiceMakerState(juiceMaker: self)
-    }()
-    lazy var mangoJuiceMakerState: MangoJuiceMakerState = {
-        return MangoJuiceMakerState(juiceMaker: self)
-    }()
-    lazy var mangoKiwiJuiceMakerState: MangoKiwiJuiceMakerState = {
-        return MangoKiwiJuiceMakerState(juiceMaker: self)
-    }()
+    
     private lazy var state: State? = {
         return self.emptyJuiceMakerState
     }()
@@ -56,4 +36,32 @@ final class JuiceMaker {
     func setState(state: State?) {
         self.state = state
     }
+}
+
+
+class SingleFruitJuiceMaker: JuiceMaker {
+    lazy var strawberryJuiceMakerState: StrawberryJuiceMakerState = {
+        return StrawberryJuiceMakerState(juiceMaker: self)
+    }()
+    lazy var bananaJuiceMakerState: BananaJuiceMakerState = {
+        return BananaJuiceMakerState(juiceMaker: self)
+    }()
+    lazy var kiwiJuiceMakerState: KiwiJuiceMakerState = {
+        return KiwiJuiceMakerState(juiceMaker: self)
+    }()
+    lazy var pineappleJuiceMakerState: PineappleJuiceMakerState = {
+        return PineappleJuiceMakerState(juiceMaker: self)
+    }()
+    lazy var mangoJuiceMakerState: MangoJuiceMakerState = {
+        return MangoJuiceMakerState(juiceMaker: self)
+    }()
+}
+
+class MixedFruitJuiceMaker: JuiceMaker {
+    lazy var strawberryBananaJuiceMakerState: StrawberryBananaJuiceMakerState = {
+        return StrawberryBananaJuiceMakerState(juiceMaker: self)
+    }()
+    lazy var mangoKiwiJuiceMakerState: MangoKiwiJuiceMakerState = {
+        return MangoKiwiJuiceMakerState(juiceMaker: self)
+    }()
 }
