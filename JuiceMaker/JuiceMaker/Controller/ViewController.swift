8fe2d9b2441566cc37e02398ef8a79ae7c86d67d
JIINHEO <gjwldls123@gmail.com>
2022-02-16 16:33:29 +0900

    test: Test 코드 삭제

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -11,10 +11,5 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        // TEST
-        let jucieMaker = JuiceMaker()
-        print(jucieMaker.makeJuice(juice: .strawberryJuice).description)
-        print(jucieMaker.makeJuice(juice: .strawberryBananaJuice).description)
     }
 }
