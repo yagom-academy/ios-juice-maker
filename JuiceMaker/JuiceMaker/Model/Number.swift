12e951584da764533810c6749193532f76f4b704
Paul <phs880623@gmail.com>
2022-02-16 15:20:43 +0900

    feat: Number Type 구현
    - Number Type 구현
     - 연산에 대한 책임 분리하기 위해서 구현했습니다.
     - 유닛 테스트를 작성했습니다.

---
+++ JuiceMaker/JuiceMaker/Model/Number.swift
@@ -0,0 +1,44 @@
+//
+//  Number.swift
+//  JuiceMaker
+//
+//  Created by 박형석 on 2022/02/16.
+//
+
+import Foundation
+
+struct Number {
+    private(set) var value: Int
+    
+    init(_ value: Int = 10) {
+        self.value = value
+    }
+    
+    mutating func increase() {
+        self.value += 1
+    }
+    
+    mutating func increase(_ amount: Number) throws {
+        guard amount.value >= 0 else {
+            throw NumberError.isNegativeValue
+        }
+        self.value += amount.value
+    }
+    
+    mutating func decrease() {
+        self.value -= 1
+    }
+    
+    mutating func decrease(_ amount: Number) throws {
+        guard amount.value >= 0 else {
+            throw NumberError.isNegativeValue
+        }
+        self.value -= amount.value
+    }
+}
+
+extension Number: Equatable {
+    static func == (lhs: Number, rhs: Number) -> Bool {
+        return lhs.value == rhs.value
+    }
+}
