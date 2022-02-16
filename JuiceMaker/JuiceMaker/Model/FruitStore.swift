97de85cb6efd912d92e8c998cd6261befb941d53
p41155a <p41155a@gmail.com>
2022-02-16 11:50:01 +0900

    feat: Fruit enum 생성
    fruit enum을 생성 후 fruitStore에 stock 변수를 만들엇습니다

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var stocks: [Fruit: Int]
     
+    init(initalValue: Int = 10) {
+        var stocks: [Fruit: Int] = [:]
+        for fruit in Fruit.allCases {
+            stocks[fruit] = initalValue
+        }
+        self.stocks = stocks
+    }
 }
