d4bee7033d86e9b3917dfb511680ef14c9881449
DayeonJung <ekdus0227@gmail.com>
2022-02-17 01:58:08 +0900

    feat: MakeJuiceViewModel을 테스트하기 위한 테스트 파일 생성

---
+++ JuiceMaker/JuiceMakerTests/MakeJuice/ViewModel/MakeJuiceViewModelTests.swift
@@ -0,0 +1,24 @@
+//
+//  MakeJuiceViewModelTests.swift
+//  JuiceMakerTests
+//
+//  Created by Dayeon Jung on 2022/02/17.
+//
+
+import XCTest
+
+class MakeJuiceViewModelTests: XCTestCase {
+
+    var viewModel: MakeJuiceViewModelTests!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = MakeJuiceViewModelTests()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
+    }
+
+}
