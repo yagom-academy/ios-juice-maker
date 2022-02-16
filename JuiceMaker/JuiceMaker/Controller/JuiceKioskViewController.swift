cd7d56d744a8b4f20809ebb0f31254f6dad8215c
beansBin <beans_bin@naver.com>
2022-02-16 22:41:09 +0900

    feat: [JuiceKioskViewController] patchData() 구현

--- JuiceMaker/JuiceMaker/Controller/JuiceKioskViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceKioskViewController.swift
@@ -8,8 +8,34 @@
 import UIKit
 
 class JuiceKioskViewController: UIViewController {
+    
+    @IBOutlet weak var strawberryCountLabel: UILabel!
+    @IBOutlet weak var bananaCountLabel: UILabel!
+    @IBOutlet weak var pineappleCountLabel: UILabel!
+    @IBOutlet weak var kiwiCountLabel: UILabel!
+    @IBOutlet weak var mangoCountLabel: UILabel!
+    
+    let juiceMaker = JuiceMaker()
+    var fruitsLabels: [Fruit: UILabel] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        patchData()
+    }
+    
+    func patchData() {
+        fruitsLabels = [.strawberry: strawberryCountLabel,
+                       .banana: bananaCountLabel,
+                       .pineapple: pineappleCountLabel,
+                       .kiwi: kiwiCountLabel,
+                       .mango: mangoCountLabel]
+        
+        for fruitLabel in fruitsLabels {
+            guard let fruitCount = juiceMaker.fruitStore.stocks[fruitLabel.key] else {
+                return
+            }
+            fruitLabel.value.text = "\(fruitCount)"
+        }
     }
     
     @IBAction func modifyButtonTap(_ sender: Any) {
