d585143a94dfd36737621a8738ac7c1ef6d943f6
kangkyung <rudgns0508@naver.com>
2022-02-16 20:43:28 +0900

    feat: 과일타입, 쥬스레시피타입 생성

---
+++ JuiceMaker/JuiceMaker/Util/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by 강경 on 2022/02/16.
+//
+
+import Foundation
+
+enum Fruit: Hashable, CaseIterable {
+    case strawberry
+    case banana
+    case kiwi
+    case mango
+    case pineapple
+}
---
+++ JuiceMaker/JuiceMaker/Util/JuiceRecipeType.swift
@@ -0,0 +1,18 @@
+//
+//  JuiceRecipeType.swift
+//  JuiceMaker
+//
+//  Created by 강경 on 2022/02/16.
+//
+
+import Foundation
+
+enum JuiceRecipeType: CaseIterable {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberryBanananJuice
+    case mangoJuice
+    case mangoKiwiJuice
+}
