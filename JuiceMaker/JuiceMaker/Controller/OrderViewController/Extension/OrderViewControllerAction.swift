ba9c957c585cfa8391dcb162b8372ab075b43ed6
samsamilsa <hyejeekim@me.com>
2022-02-16 18:38:11 +0900

    chore: Move Alert type to separate file

--- JuiceMaker/JuiceMaker/Controller/OrderViewController/Extension/OrderViewControllerAction.swift
+++ JuiceMaker/JuiceMaker/Controller/OrderViewController/Extension/OrderViewControllerAction.swift
@@ -107,28 +107,3 @@ extension OrderViewController {
     }
     
 }
-
-struct Alert {
-    
-    enum AlertType {
-        case success
-        case error
-    }
-    
-    static func makeAlert(of type: AlertType, title: String? = nil, message: String? = nil, actionHandler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
-        let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        switch type {
-        case .success:
-            let okAction: UIAlertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alertController.addAction(okAction)
-        case .error:
-            let yesAction: UIAlertAction = UIAlertAction(title: "예", style: .default, handler: actionHandler)
-            alertController.addAction(yesAction)
-            let noAction: UIAlertAction = UIAlertAction(title: "아니오", style: .destructive, handler: nil)
-            alertController.addAction(noAction)
-        }
-        
-        return alertController
-    }
-    
-}
