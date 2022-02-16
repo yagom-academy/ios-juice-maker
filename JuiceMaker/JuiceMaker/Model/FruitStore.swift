5c6384316a199e3f78b6f9cb4b8833c686775d0a
higgs1022 <csh8130@kakao.com>
2022-02-16 20:16:52 +0900

    refactor: FruitStore에 남아있던 fatalError() 제거

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,6 +6,10 @@
 
 import Foundation
 
+enum FruitStoreError: String, Error {
+    case notEnoughFruit = "과일이 부족합니다."
+}
+
 // 과일 저장소 타입
 class FruitStore {
     private var stock: [Fruit: Int] = [:]
@@ -20,16 +24,22 @@ class FruitStore {
         }
     }
     
-    private func reduceFruit(fruit: Fruit, count: Int) {
+    private func reduceFruit(fruit: Fruit, count: Int) throws -> Void {
         guard let currentFruitCount = stock[fruit], currentFruitCount >= count else {
-            fatalError()
+            throw FruitStoreError.notEnoughFruit
         }
         stock[fruit] = currentFruitCount - count
     }
     
     func useFruit(juice: Juice) {
         juice.recipe.fruitList.forEach { fruitInfo in
-            reduceFruit(fruit: fruitInfo.fruit, count: fruitInfo.count)
+            do {
+                try reduceFruit(fruit: fruitInfo.fruit, count: fruitInfo.count)
+            } catch let error as FruitStoreError {
+                print(error.rawValue)
+            } catch {
+                print(error)
+            }
         }
     }
     
