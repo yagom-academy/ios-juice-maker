70bc37db69b4eb367644d492b976455e20241d95
samsamilsa <hyejeekim@me.com>
2022-02-16 14:53:14 +0900

    chore: Use computed property instead of raw value

--- JuiceMaker/JuiceMaker/Error/FruitStoreError.swift
+++ JuiceMaker/JuiceMaker/Error/FruitStoreError.swift
@@ -16,7 +16,7 @@ extension FruitStoreError {
     var errorDescription: String? {
         switch self {
         case .outOfStock(let outOfStockFruits):
-            let outOfStockFruitsString = outOfStockFruits.map({ $0.rawValue }).joined(separator: ", ")
+            let outOfStockFruitsString = outOfStockFruits.map({ $0.name }).joined(separator: ", ")
             return "\(outOfStockFruitsString)의 재고가 부족합니다."
         case .unkown:
             return "알 수 없는 문제가 발생했습니다."
