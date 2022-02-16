5843a04ade31e880b88789d6b61cce054e6490d1
beansBin <beans_bin@naver.com>
2022-02-16 20:23:27 +0900

    feat: [UIViewController+Extension] makeAlert(), makeRequestAlert() 구현

---
+++ JuiceMaker/JuiceMaker/Extension/UIViewController+Extension.swift
@@ -0,0 +1,42 @@
+//
+//  UIViewController+Extension.swift
+//  JuiceMaker
+//
+//  Created by 박예빈 on 2022/02/16.
+//
+
+import UIKit
+
+extension UIViewController {
+    func makeAlert(title: String,
+                   message: String,
+                   okAction: ((UIAlertAction) -> Void)? = nil,
+                   completion : (() -> Void)? = nil) {
+        let alertViewController = UIAlertController(title: title,
+                                                    message: message,
+                                                    preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        self.present(alertViewController, animated: true, completion: completion)
+    }
+
+    func makeRequestAlert(title: String,
+                          message: String,
+                          okAction: ((UIAlertAction) -> Void)?,
+                          cancelAction: ((UIAlertAction) -> Void)? = nil,
+                          completion : (() -> Void)? = nil) {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+
+        let cancelAction = UIAlertAction(title: "취소", style: .destructive, handler: cancelAction)
+        alertViewController.addAction(cancelAction)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+
+        self.present(alertViewController, animated: true, completion: completion)
+    }
+}
