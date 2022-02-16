c8a4665a38dcfe1274b3de61d4482c6961b1d903
HOONHA CHOI <sosqsz2@naver.com>
2022-02-17 00:48:02 +0900

    style: AppDelegate, SceneDelegate 불필요한 주석제거

---
+++ JuiceMaker/JuiceMaker/AppCycle/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  JuiceMaker - AppDelegate.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ JuiceMaker/JuiceMaker/AppCycle/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceMaker - SceneDelegate.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
