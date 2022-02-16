3651a6bbac0b77e2760e63fd74a3d64737adebe2
kch1285 <kch1285@naver.com>
2022-02-17 00:55:30 +0900

    test: JuiceResult 타입 생성으로 인한 테스트코드 수정

--- JuiceMaker/JuiceMakerTests/JuiceMakerTests.swift
+++ JuiceMaker/JuiceMakerTests/JuiceMakerTests.swift
@@ -14,25 +14,22 @@ class JuiceMakerTests: XCTestCase {
     
     func test_과일재고가_있는_경우_원하는_쥬스가_만들어진다() {
         let input = Juice.kiwiJuice
-        let expectation = "성공"
-        let result = juiceMaker.makeJuice(juice: input)
+        let result = juiceMaker.makeJuice(type: input)
         
-        XCTAssertEqual(expectation, result)
+        XCTAssertTrue(result.isSuccess)
     }
     
     func test_과일재고가_부족한_경우_실패한다() {
         let input = Juice.strawberryJuice
-        let expectation = FruitError.outOfStock.description
-        let result = juiceMaker.makeJuice(juice: input)
+        let result = juiceMaker.makeJuice(type: input)
         
-        XCTAssertEqual(expectation, result)
+        XCTAssertFalse(result.isSuccess)
     }
 
     func test_두가지_과일이_필요할_때_과일재고가_있는_경우_원하는_쥬스가_만들어진다() {
         let input = Juice.strawberryBananaJuice
-        let expectation = "성공"
-        let result = juiceMaker.makeJuice(juice: input)
+        let result = juiceMaker.makeJuice(type: input)
         
-        XCTAssertEqual(expectation, result)
+        XCTAssertTrue(result.isSuccess)
     }
 }
