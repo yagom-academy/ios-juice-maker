a3c350c7b8db8fef68d9cc96f926832f4e933bb2
DayeonJung <ekdus0227@gmail.com>
2022-02-17 01:54:18 +0900

    feat: 첫화면의 비즈니스 로직을 담당할 ViewModel 생성

--- JuiceMaker/JuiceMaker/MakeJuice/View/MakeJuiceViewController.swift
+++ JuiceMaker/JuiceMaker/MakeJuice/View/MakeJuiceViewController.swift
@@ -8,11 +8,12 @@ import UIKit
 
 class MakeJuiceViewController: UIViewController {
 
+    // 뷰에서 이루어질 비즈니스 로직을 담당할 뷰모델 생성
+    private var viewModel = MakeJuiceViewModel(juiceMaker: JuiceMaker())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        
+
         
     }
 
---
+++ JuiceMaker/JuiceMaker/MakeJuice/ViewModel/MakeJuiceViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MakeJuiceViewModel.swift
+//  JuiceMaker
+//
+//  Created by Dayeon Jung on 2022/02/17.
+//
+
+import Foundation
+
+struct MakeJuiceViewModel {
+    
+    private var juiceMaker: JuiceMaker
+    
+    init(juiceMaker: JuiceMaker) {
+        self.juiceMaker = juiceMaker
+    }
+    
+}
