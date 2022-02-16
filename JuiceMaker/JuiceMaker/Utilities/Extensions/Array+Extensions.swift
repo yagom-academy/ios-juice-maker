6f7ae8c8a4fda71650adf38cf4255b90a019c375
ryan-son <ryan.son1002@gmail.com>
2022-02-17 01:14:15 +0900

    feat : `toDictionary(with:)` 불필요한 제약사항 제거

--- JuiceMaker/JuiceMaker/Utilities/Extensions/Array+Extensions.swift
+++ JuiceMaker/JuiceMaker/Utilities/Extensions/Array+Extensions.swift
@@ -8,9 +8,9 @@
 extension Array where Element: Hashable {
     typealias Key = Element
 
-    /// 배열의 요소를 Key로 가지는 딕셔너리로 생성합니다.
-    /// 모든 Key 동일한 초기값을 가지도록 합니다.
-    func toDictionary<Value: Numeric>(with initialValue: Value) -> [Key: Value] {
+    /// 배열의 요소를 Key로 가지는 딕셔너리를 생성합니다.
+    /// 모든 Key에 동일한 초기값을 가지도록 설정합니다.
+    func toDictionary<Value>(with initialValue: Value) -> [Key: Value] {
         var dictionary: [Key: Value] = [:]
 
         for key in self {
