22ed40a3c18a8328635a9007b53bea7eb62da869
DayeonJung <ekdus0227@gmail.com>
2022-02-17 10:40:58 +0900

    feat: 과일의 재고를 표시하는 레이블의 텍스트를 재고의 변경에 따라 업데이트

--- JuiceMaker/JuiceMaker/MakeJuice/View/MakeJuiceViewController.swift
+++ JuiceMaker/JuiceMaker/MakeJuice/View/MakeJuiceViewController.swift
@@ -8,13 +8,19 @@ import UIKit
 
 class MakeJuiceViewController: UIViewController {
 
+    @IBOutlet var quantityLabels: [UILabel]!
+    
     // 뷰에서 이루어질 비즈니스 로직을 담당할 뷰모델 생성
     private var viewModel = MakeJuiceViewModel(juiceMaker: JuiceMaker())
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        viewModel.displayingStocks.bind { [weak self] stocks in
+            stocks.forEach { (index: Int, quantity: Quantity) in
+                self?.quantityLabels[index].text = "\(quantity.text)"
+            }
+        }
     }
 
 
