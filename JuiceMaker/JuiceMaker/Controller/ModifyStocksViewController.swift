8a587ae43f0bcab19015ef52717d734041281d3e
beansBin <beans_bin@naver.com>
2022-02-17 17:42:36 +0900

    feat: [ModifyStocksViewController] closeButtonTap() 구현

--- JuiceMaker/JuiceMaker/Controller/ModifyStocksViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ModifyStocksViewController.swift
@@ -9,23 +9,21 @@ import UIKit
 
 class ModifyStocksViewController: UIViewController {
 
+    // MARK: - Outlet Property
     @IBOutlet weak var strawberryStepper: UIStepper!
     
+    // MARK: - Property
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - func
+    // MARK: - Action
+    
+    @IBAction func closeButtonTap(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
     }
-    */
-
 }
