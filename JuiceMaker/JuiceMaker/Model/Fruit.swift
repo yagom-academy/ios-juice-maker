429c51fc13f4897f01fddb5496d50fe3727225fd
JIINHEO <gjwldls123@gmail.com>
2022-02-17 18:09:12 +0900

    refactor: Fruit 프로퍼티 name 제거

--- JuiceMaker/JuiceMaker/Model/Fruit.swift
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -13,19 +13,4 @@ enum Fruit: CaseIterable {
     case pineapple
     case kiwi
     case mango
-    
-    var name: String {
-        switch self {
-        case .strawberry:
-            return "딸기"
-        case .banana:
-            return "바나나"
-        case .pineapple:
-            return "파인애플"
-        case .kiwi:
-            return "키위"
-        case .mango:
-            return "망고"
-        }
-    }
 }
