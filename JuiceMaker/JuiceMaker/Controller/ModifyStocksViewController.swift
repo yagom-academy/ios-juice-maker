e208bcd8ca963a12b1ee7a346540ecfd005f5c7a
beansBin <beans_bin@naver.com>
2022-02-17 18:58:08 +0900

    feat: [ModifyStocksViewController] fruitStepper IBOutlet 및 option 설정

--- JuiceMaker/JuiceMaker/Controller/ModifyStocksViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ModifyStocksViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 class ModifyStocksViewController: UIViewController {
 
     // MARK: - Outlet Property
+    
     @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
     
     @IBOutlet weak var strawberryCountLabel: UILabel!
     @IBOutlet weak var bananaCountLabel: UILabel!
@@ -21,11 +26,14 @@ class ModifyStocksViewController: UIViewController {
     // MARK: - Property
     var juiceMaker: JuiceMaker?
     private var fruitsLabels: [Fruit: UILabel] = [:]
+    private var fruitsSteppers: [Fruit: UIStepper] = [:]
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setFruitLabels()
+        setFruitSteppers()
+        setSteppersOptions()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -42,6 +50,14 @@ class ModifyStocksViewController: UIViewController {
                         .mango: mangoCountLabel]
     }
     
+    private func setFruitSteppers() {
+        fruitsSteppers = [.strawberry: strawberryStepper,
+                          .banana: bananaStepper,
+                          .pineapple: pineappleStepper,
+                          .kiwi: kiwiStepper,
+                          .mango: mangoStepper]
+    }
+    
     private func patchData() {
         for fruitLabel in fruitsLabels {
             guard let fruitCount = juiceMaker?.getFruitCount(fruit: fruitLabel.key) else {
@@ -51,8 +67,30 @@ class ModifyStocksViewController: UIViewController {
         }
     }
     
+    private func setSteppersOptions() {
+        for fruitStepper in fruitsSteppers {
+            fruitStepper.value.minimumValue = 0
+        }
+    }
+    
     // MARK: - Action
     @IBAction func closeButtonTap(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func strawberryStepperTap(_ sender: UIStepper) {
+        print(sender.value.description)
+    }
+    
+    @IBAction func bananaStepperTap(_ sender: UIStepper) {
+    }
+    
+    @IBAction func pineappleStepperTap(_ sender: UIStepper) {
+    }
+    
+    @IBAction func kiwiStepperTap(_ sender: UIStepper) {
+    }
+    
+    @IBAction func mangoStepperTap(_ sender: UIStepper) {
     }
 }
