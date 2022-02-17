2038ae572bf9a7a315f7945ca34f77f57bb20872
samsamilsa <hyejeekim@me.com>
2022-02-17 16:39:56 +0900

    feat: Add error case for invalid stock change

--- JuiceMaker/JuiceMaker/Error/FruitStoreError.swift
+++ JuiceMaker/JuiceMaker/Error/FruitStoreError.swift
@@ -9,6 +9,7 @@ import Foundation
 
 enum FruitStoreError: Error {
     case outOfStock([Fruit])
+    case invalidStockChange
     case unkown
 }
 
@@ -20,6 +21,8 @@ extension FruitStoreError {
             return "\(outOfStockFruitsString)의 재고가 부족합니다."
         case .unkown:
             return "알 수 없는 문제가 발생했습니다."
+        case .invalidStockChange:
+            return "재고는 음수가 될 수 없습니다."
         }
     }
 }
