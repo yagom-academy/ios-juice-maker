9d1d71245076fd0d4c1c13dfb1967b015ebc6577
todayssky <rudwns1113@gmail.com>
2022-02-17 23:26:28 +0900

    reactor: showAlert  네이밍 변경 및 파라미터 추가

--- JuiceMaker/JuiceMaker/Extension/UIViewContollerExtension.swift
+++ JuiceMaker/JuiceMaker/Extension/UIViewContollerExtension.swift
@@ -5,11 +5,10 @@
 //  Created by 이경준 on 2022/02/16.
 //
 
-import Foundation
 import UIKit
 
 extension UIViewController {
-    func showAlert(
+    func showBasicAlert(
         title: String = "알림",
         message: String
     ) {
@@ -19,15 +18,17 @@ extension UIViewController {
         present(alert, animated: true)
     }
     
-    func showAlert(
+    func showActionAlert(
         title: String = "알림",
         message: String,
+        okActionTitle: String = "예",
+        cancelActionTitle: String = "아니오",
         action : ((UIAlertAction) -> Void)?
     ){
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "예", style: .default, handler: action)
-        let cancelAction = UIAlertAction(title: "아니오", style: .cancel)
-        alert.addAction(action)
+        let okAction = UIAlertAction(title: okActionTitle, style: .default, handler: action)
+        let cancelAction = UIAlertAction(title: cancelActionTitle, style: .cancel)
+        alert.addAction(okAction)
         alert.addAction(cancelAction)
         present(alert, animated: true)
     }
