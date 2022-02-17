955d5f73c23ae8810cdcbee553462dff4d175d1f
db97828 <db97828@naver.com>
2022-02-17 23:23:16 +0900

    feat : 스토리보드 button, label MainViewController에 연결

--- JuiceMaker/JuiceMaker/Controller/MainViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -8,11 +8,42 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStock: UILabel!
+    @IBOutlet weak var bananaStock: UILabel!
+    @IBOutlet weak var pineappleStock: UILabel!
+    @IBOutlet weak var kiwiStock: UILabel!
+    @IBOutlet weak var mangoStock: UILabel!
+    
+    let fruitStore = FruitStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    @IBAction func orderStrawberryBananaJuice(_ sender: Any) {
+    }
+    
+    @IBAction func orderMangoKiwiJuice(_ sender: Any) {
+    }
+    
+    @IBAction func orderStrawberryJuice(_ sender: Any) {
+    }
+    
+    @IBAction func orderBananaJuice(_ sender: Any) {
+    }
+    
+    @IBAction func orderPineappleJuice(_ sender: Any) {
+    }
+    
+    @IBAction func orderKiwiJuice(_ sender: Any) {
+    }
+    
+    @IBAction func orderMangoJuice(_ sender: Any) {
+    }
 }
 
