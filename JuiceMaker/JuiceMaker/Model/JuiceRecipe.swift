4c3eff6dcafaecac12785d1bd8cf6d2cb51e1dda
kangkyung <rudgns0508@naver.com>
2022-02-18 23:49:56 +0900

    refactor: JuiceRecipe의 name을 JuiceRecipeType으로 변경

--- JuiceMaker/JuiceMaker/Model/JuiceRecipe.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceRecipe.swift
@@ -9,24 +9,4 @@ import Foundation
 
 struct JuiceRecipe {
     let ingredients: [Ingredient]
-    let name: RecipeName
-    
-    enum RecipeName: String, CustomStringConvertible {
-        case strawberryJuice = "딸기쥬스"
-        case bananaJuice = "바나나쥬스"
-        case kiwiJuice = "키위쥬스"
-        case pineappleJuice = "파인애플쥬스"
-        case strawberryBanananJuice = "딸바쥬스"
-        case mangoJuice = "망고쥬스"
-        case mangoKiwiJuice = "망고키위쥬스"
-        
-        var description: String {
-            self.rawValue
-        }
-    }
-    
-    init(ingredients: [Ingredient], name: RecipeName) {
-        self.ingredients = ingredients
-        self.name = name
-    }
 }
--- JuiceMaker/JuiceMaker/Util/JuiceRecipeType.swift
+++ JuiceMaker/JuiceMaker/Util/JuiceRecipeType.swift
@@ -19,21 +19,40 @@ enum JuiceRecipeType: Hashable, CaseIterable {
     var recipe: JuiceRecipe {
         switch self {
         case .strawberryJuice:
-            return JuiceRecipe(ingredients: [.init(fruit: .strawberry, number: 16)], name: .strawberryJuice)
+            return JuiceRecipe(ingredients: [.init(fruit: .strawberry, number: 16)])
         case .bananaJuice:
-            return JuiceRecipe(ingredients: [.init(fruit: .banana, number: 2)], name: .bananaJuice)
+            return JuiceRecipe(ingredients: [.init(fruit: .banana, number: 2)])
         case .kiwiJuice:
-            return JuiceRecipe(ingredients: [.init(fruit: .kiwi, number: 3)], name: .kiwiJuice)
+            return JuiceRecipe(ingredients: [.init(fruit: .kiwi, number: 3)])
         case .pineappleJuice:
-            return JuiceRecipe(ingredients: [.init(fruit: .pineapple, number: 2)], name: .pineappleJuice)
+            return JuiceRecipe(ingredients: [.init(fruit: .pineapple, number: 2)])
         case .strawberryBanananJuice:
             return JuiceRecipe(ingredients: [.init(fruit: .strawberry, number: 10),
-                                      .init(fruit: .banana, number: 1)], name: .strawberryBanananJuice)
+                                             .init(fruit: .banana, number: 1)])
         case .mangoJuice:
-            return JuiceRecipe(ingredients: [.init(fruit: .mango, number: 3)], name: .mangoJuice)
+            return JuiceRecipe(ingredients: [.init(fruit: .mango, number: 3)])
         case .mangoKiwiJuice:
             return JuiceRecipe(ingredients: [.init(fruit: .mango, number: 2),
-                                      .init(fruit: .kiwi, number: 1)], name: .mangoKiwiJuice)
+                                             .init(fruit: .kiwi, number: 1)])
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .strawberryJuice:
+            return "딸기쥬스"
+        case .bananaJuice:
+            return "바나나쥬스"
+        case .kiwiJuice:
+            return "키위쥬스"
+        case .pineappleJuice:
+            return "파인애플쥬스"
+        case .strawberryBanananJuice:
+            return "딸바쥬스"
+        case .mangoJuice:
+            return "망고쥬스"
+        case .mangoKiwiJuice:
+            return "망고키위쥬스"
         }
     }
 }
