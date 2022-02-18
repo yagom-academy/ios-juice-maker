3d74d26813a30ba05f5d20b9f673f4634b6725a7
kangkyung <rudgns0508@naver.com>
2022-02-18 23:45:00 +0900

    refactor: JuiceRecipeType recipe 추가

--- JuiceMaker/JuiceMaker/Util/JuiceRecipeType.swift
+++ JuiceMaker/JuiceMaker/Util/JuiceRecipeType.swift
@@ -15,4 +15,25 @@ enum JuiceRecipeType: Hashable, CaseIterable {
     case strawberryBanananJuice
     case mangoJuice
     case mangoKiwiJuice
+    
+    var recipe: JuiceRecipe {
+        switch self {
+        case .strawberryJuice:
+            return JuiceRecipe(ingredients: [.init(fruit: .strawberry, number: 16)], name: .strawberryJuice)
+        case .bananaJuice:
+            return JuiceRecipe(ingredients: [.init(fruit: .banana, number: 2)], name: .bananaJuice)
+        case .kiwiJuice:
+            return JuiceRecipe(ingredients: [.init(fruit: .kiwi, number: 3)], name: .kiwiJuice)
+        case .pineappleJuice:
+            return JuiceRecipe(ingredients: [.init(fruit: .pineapple, number: 2)], name: .pineappleJuice)
+        case .strawberryBanananJuice:
+            return JuiceRecipe(ingredients: [.init(fruit: .strawberry, number: 10),
+                                      .init(fruit: .banana, number: 1)], name: .strawberryBanananJuice)
+        case .mangoJuice:
+            return JuiceRecipe(ingredients: [.init(fruit: .mango, number: 3)], name: .mangoJuice)
+        case .mangoKiwiJuice:
+            return JuiceRecipe(ingredients: [.init(fruit: .mango, number: 2),
+                                      .init(fruit: .kiwi, number: 1)], name: .mangoKiwiJuice)
+        }
+    }
 }
