ee2ba262654533978a09f33a7b581f522e0b15fd
DayeonJung <ekdus0227@gmail.com>
2022-02-19 00:36:36 +0900

    refactor: 프로퍼티의 메서드를 호출해야 할 경우, 호출방식 변경
    - 메서드를 생성하여 프로퍼티의 메서드를 호출하도록 했었음.
    - 외부에서 해당 메서드에 접근하지 않는 한, 바로 프로퍼티의 메서드를 호출하는 것이 낫다는 모모의 피드백 반영

--- JuiceMaker/JuiceMaker/MakeJuice/ViewModel/MakeJuiceViewModel.swift
+++ JuiceMaker/JuiceMaker/MakeJuice/ViewModel/MakeJuiceViewModel.swift
@@ -26,12 +26,6 @@ struct MakeJuiceViewModel {
         }
     }
     
-    /// 특정 과일의 현재 재고를 확인
-    func stock(of fruit: FruitStore.Fruit) -> Quantity {
-        return juiceMaker.stock(of: fruit)
-    }
-    
-    
     /// 특정 과일을 주문
     /// 제조 성공 시 화면에 보여질 재고를 인덱스별로 업데이트 합니다.
     ///
@@ -42,16 +36,6 @@ struct MakeJuiceViewModel {
         displayingStocks.value = stocks
     }
     
-    /// 화면에 보여지는 과일 배열 중 특정 과일의 인덱스를 반환
-    ///
-    /// - Returns: `Int?` 특정 과일이 배열 내 존재할 경우 해당 인덱스 반환. 없을 경우 nil
-    ///
-    /// - Parameter fruit: 과일의 종류
-    private func index(of fruit: FruitStore.Fruit) -> Int? {
-        return displayingFruits.firstIndex(of: fruit)
-    }
-    
-    
     /// 특정 과일들의 현재 재고를 인덱스별로 반환
     ///
     /// - Returns: `[(Int, Quantity)]` 업데이트 할 과일들의 인덱스별 재고 튜플 반환
@@ -61,10 +45,10 @@ struct MakeJuiceViewModel {
         with fruits: [FruitStore.Fruit]
     ) -> [(Int, Quantity)] {
         return fruits.compactMap({ fruit -> (Int, Quantity)? in
-            guard let fruitIndex = index(of: fruit) else {
+            guard let fruitIndex = displayingFruits.firstIndex(of: fruit) else {
                 return nil
             }
-            return (fruitIndex, stock(of: fruit))
+            return (fruitIndex, juiceMaker.stock(of: fruit))
         })
     }
 }
