9f074e63f8114f8e7310c80a7c9d27edb459f427
Paul <phs880623@gmail.com>
2022-02-18 14:04:52 +0900

    feat: UpdateStockViewController에 과일 재고 표시
    - JuiceMakerViewController에서 UpdateStockViewController로 데이터를 전달하기 위해 프로퍼티 사용

--- JuiceMaker/JuiceMaker/Controller/UpdateStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/UpdateStockViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 final class UpdateStockViewController: UIViewController, StoryboardBindable {
+    // MARK: - Views
     @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    var fruitStore: FruitStore?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setupNavigationBar()
+        self.setupNavigationBar()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.updateFruitStock()
     }
 }
 
@@ -23,8 +35,19 @@ extension UpdateStockViewController {
         self.navigationBar.shadowImage = UIImage()
         self.navigationBar.backgroundColor = .clear
     }
-
+    
     @IBAction func back() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func updateFruitStock() {
+        guard let fruitStore: FruitStore = self.fruitStore else {
+            return
+        }
+        self.strawberryStockLabel.text = "\(fruitStore.fruits[.strawberry, default: Number()])"
+        self.bananaStockLabel.text = "\(fruitStore.fruits[.banana, default: Number()])"
+        self.pineappleStockLabel.text = "\(fruitStore.fruits[.pineapple, default: Number()])"
+        self.kiwiStockLabel.text = "\(fruitStore.fruits[.kiwi, default: Number()])"
+        self.mangoStockLabel.text = "\(fruitStore.fruits[.mango, default: Number()])"
     }
 }
