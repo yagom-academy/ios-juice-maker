656d68def7c4b33e8dbfede13baa54812bd7b1d5
kch1285 <kch1285@naver.com>
2022-02-18 23:37:20 +0900

    feat: 재고수정 뷰컨트롤러 IBOutlet 연결 및 화면 구성 함수 구현

--- JuiceMaker/JuiceMaker/Controller/StockManageViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManageViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class StockManageViewController: UIViewController {
-
+    var fruitStore: FruitStore?
+    
+    @IBOutlet weak var strawberryStock: UILabel!
+    @IBOutlet weak var bananaStock: UILabel!
+    @IBOutlet weak var kiwiStock: UILabel!
+    @IBOutlet weak var pineappleStock: UILabel!
+    @IBOutlet weak var mangoStock: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
+    private func configureUI() {
+        strawberryStock.text = "\(fruitStore?.store[.strawberry] ?? 0)"
+        bananaStock.text = "\(fruitStore?.store[.banana] ?? 0)"
+        pineappleStock.text = "\(fruitStore?.store[.pineapple] ?? 0)"
+        kiwiStock.text = "\(fruitStore?.store[.kiwi] ?? 0)"
+        mangoStock.text = "\(fruitStore?.store[.mango] ?? 0)"
+    }
 
 }
