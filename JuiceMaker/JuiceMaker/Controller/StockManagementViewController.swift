25af7bb3ffe40940db2995f17508d3a8774bb466
haeseoklee <haeseoklee.dev@gmail.com>
2022-02-18 14:44:21 +0900

    refactor: 메서드 이름 변경 및 Stepper 메서드 리팩토링

--- JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagementViewController.swift
@@ -29,19 +29,25 @@ final class StockManagementViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
+        setupStockManagementVC()
         setupNavigationBar()
     }
     
-    private func setup() {
+    private func setupStockManagementVC() {
         guard let fruitStore = fruitStore else { return }
         updateFruitLabels(fruitStore: fruitStore)
+        setupFruitSteppers()
         updateFruitSteppers(fruitStore: fruitStore)
     }
     
     private func setupNavigationBar() {
         navigationItem.title = "재고 추가"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "닫기", style: .plain, target: self, action: #selector(touchUpCloseRightBarButton))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "닫기",
+            style: .plain,
+            target: self,
+            action: #selector(touchUpCloseRightBarButton)
+        )
     }
     
     private func updateFruitLabels(fruitStore: FruitStore) {
@@ -52,6 +58,14 @@ final class StockManagementViewController: UIViewController {
         mangoCountLabel.text = "\(fruitStore.mango.counter)"
     }
     
+    private func setupFruitSteppers() {
+        strawberryStepper.tag = Constants.Tag.strawberry
+        bananaStepper.tag = Constants.Tag.banana
+        pineappleStepper.tag = Constants.Tag.pineapple
+        kiwiStepper.tag = Constants.Tag.kiwi
+        mangoStepper.tag = Constants.Tag.mango
+    }
+    
     private func updateFruitSteppers(fruitStore: FruitStore) {
         strawberryStepper.value = Double(fruitStore.strawberry.counter)
         bananaStepper.value = Double(fruitStore.banana.counter)
@@ -60,39 +74,47 @@ final class StockManagementViewController: UIViewController {
         mangoStepper.value = Double(fruitStore.mango.counter)
     }
     
+    private func valueChangedStepper(sender: UIStepper) {
+        guard let fruitStore = fruitStore else { return }
+        switch sender.tag {
+        case Constants.Tag.strawberry:
+            fruitStore.update(fruitType: Strawberry.self, num: Int(sender.value))
+        case Constants.Tag.banana:
+            fruitStore.update(fruitType: Banana.self, num: Int(sender.value))
+        case Constants.Tag.pineapple:
+            fruitStore.update(fruitType: Pineapple.self, num: Int(sender.value))
+        case Constants.Tag.kiwi:
+            fruitStore.update(fruitType: Kiwi.self, num: Int(sender.value))
+        case Constants.Tag.mango:
+            fruitStore.update(fruitType: Mango.self, num: Int(sender.value))
+        default:
+            return
+        }
+        updateFruitLabels(fruitStore: fruitStore)
+    }
+    
     @objc private func touchUpCloseRightBarButton() {
-        self.dismiss(animated: true, completion: {[weak self] in
-            self?.delegate?.touchUpCloseButton()
-        })
+        delegate?.touchUpCloseButton()
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func valueChangedStrawberryStepper(_ sender: UIStepper) {
-        guard let fruitStore = fruitStore else { return }
-        fruitStore.update(fruitType: Strawberry.self, num: Int(sender.value))
-        updateFruitLabels(fruitStore: fruitStore)
+        valueChangedStepper(sender: sender)
     }
     
     @IBAction func valueChangedBananaStepper(_ sender: UIStepper) {
-        guard let fruitStore = fruitStore else { return }
-        fruitStore.update(fruitType: Banana.self, num: Int(sender.value))
-        updateFruitLabels(fruitStore: fruitStore)
+        valueChangedStepper(sender: sender)
     }
     
     @IBAction func valueChangedPineappleStepper(_ sender: UIStepper) {
-        guard let fruitStore = fruitStore else { return }
-        fruitStore.update(fruitType: Pineapple.self, num: Int(sender.value))
-        updateFruitLabels(fruitStore: fruitStore)
+        valueChangedStepper(sender: sender)
     }
     
     @IBAction func valueChangedKiwiStepper(_ sender: UIStepper) {
-        guard let fruitStore = fruitStore else { return }
-        fruitStore.update(fruitType: Kiwi.self, num: Int(sender.value))
-        updateFruitLabels(fruitStore: fruitStore)
+        valueChangedStepper(sender: sender)
     }
     
     @IBAction func valueChangedMangoStepper(_ sender: UIStepper) {
-        guard let fruitStore = fruitStore else { return }
-        fruitStore.update(fruitType: Mango.self, num: Int(sender.value))
-        updateFruitLabels(fruitStore: fruitStore)
+        valueChangedStepper(sender: sender)
     }
 }
