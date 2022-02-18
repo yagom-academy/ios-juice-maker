0eb5ebedd0c6e65449588dfb2e6eec46bd9fcf0a
kangkyung <rudgns0508@naver.com>
2022-02-18 23:54:23 +0900

    refactor: JuiceMaker takeOrder 변경
    - 변경된 레시피타입으로 전달하도록 변경

--- JuiceMaker/JuiceMaker/Model/JucieRecipes.swift
+++ JuiceMaker/JuiceMaker/Model/JucieRecipes.swift
@@ -13,18 +13,4 @@ struct JuiceRecipes {
     init(recipes: [JuiceRecipe]) {
         self.recipes = recipes
     }
-    
-    init() {
-        self.recipes = [
-            JuiceRecipe(ingredients: [.init(fruit: .strawberry, number: 16)], name: .strawberryJuice),
-            JuiceRecipe(ingredients: [.init(fruit: .banana, number: 2)], name: .bananaJuice),
-            JuiceRecipe(ingredients: [.init(fruit: .kiwi, number: 3)], name: .kiwiJuice),
-            JuiceRecipe(ingredients: [.init(fruit: .pineapple, number: 2)], name: .pineappleJuice),
-            JuiceRecipe(ingredients: [.init(fruit: .strawberry, number: 10),
-                                      .init(fruit: .banana, number: 1)], name: .strawberryBanananJuice),
-            JuiceRecipe(ingredients: [.init(fruit: .mango, number: 3)], name: .mangoJuice),
-            JuiceRecipe(ingredients: [.init(fruit: .mango, number: 2),
-                                      .init(fruit: .kiwi, number: 1)], name: .mangoKiwiJuice)
-        ]
-    }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -10,23 +10,18 @@ struct JuiceMaker {
     
     private var fruitStore: Store
     private let juiceRecipes: JuiceRecipes
-    private let juiceRecipesMapper: JuiceRecipesMapper
     
     init(fruitStore: Store = FruitStore(), recipes: JuiceRecipes = JuiceRecipes()) {
         self.fruitStore = fruitStore
         self.juiceRecipes = recipes
-        self.juiceRecipesMapper = JuiceRecipesMapper(jucieRecipeTypes: JuiceRecipeType.allCases, recipes: juiceRecipes.recipes)
     }
     
     mutating func takeOrder(recipeType: JuiceRecipeType) throws -> String {
-        guard let juiceRecipe = juiceRecipesMapper[recipeType] else {
-            throw JuiceMakerError.notFindReceipe
-        }
-        if !fruitStore.checkStock(of: juiceRecipe.ingredients) {
+        let ingredients = recipeType.recipe.ingredients
+        if !fruitStore.checkStock(of: ingredients) {
             throw JuiceMakerError.notEnough
         }
-        fruitStore.makeDrink(of: juiceRecipe.ingredients)
-        return juiceRecipe.name.description
+        fruitStore.makeDrink(of: ingredients)
+        return recipeType.name
     }
-    
 }
