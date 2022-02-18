975ee3085541bd46880c57f673e21e4720c5d173
Lina.L <lina@weversecompany.com>
2022-02-18 23:22:57 +0900

    Revert "쥬스 메이커 [STEP 1] Ohtt, JAEYOUNGYUN"

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,34 +6,7 @@
 
 import Foundation
 
-protocol JuiceMakable {
-  func make(juice: Juice) throws
-}
-
 // 쥬스 메이커 타입
-struct JuiceMaker: JuiceMakable {
-  private var fruitStore: FruitStorable
-  
-  init(fruitStore: FruitStorable) {
-    self.fruitStore = fruitStore
-  }
-  
-  func make(juice: Juice) throws {
-    let ingredients = juice.ingredients
-    var updatedStocks: [Int] = []
-    try ingredients.forEach { (fruit, quantity) in
-      let originQuantity = fruitStore.stock(of: fruit) ?? 0
-      let updatedStock = originQuantity - quantity
-      
-      guard updatedStock >= 0 else {
-        throw JuiceMakerError.notEnoughStock
-      }
-      
-      updatedStocks.append(updatedStock)
-    }
+struct JuiceMaker {
     
-    for (index, (fruit, _)) in ingredients.enumerated() {
-      fruitStore.updateStock(of: fruit, quantity: updatedStocks[index])
-    }
-  }
 }
