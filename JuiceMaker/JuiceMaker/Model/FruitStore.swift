db0112fbeeef62e2bdcba0fe159e82f527428c3d
saafaaari <111dltldnjs@naver.com>
2022-02-21 17:54:23 +0900

    feat : enum Fruit타입 추가, inventory 변수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,4 +9,19 @@ import Foundation
 // 과일 저장소 타입
 class FruitStore {
     
+    enum Fruit {
+        case strawberry
+        case banana
+        case pineapple
+        case kiwi
+        case mango
+    }
+    
+    let defaultInventory: Int = 10
+    var inventory: [Fruit: Int] = [.strawberry: 10,
+                                   .banana: 10,
+                                   .pineapple: 10,
+                                   .kiwi: 10,
+                                   .mango: 10]
+    
 }
