8a1b59393ced555ea7d3d2d40ee660b7cdd6d589
malrang <ruehd7924@gmail.com>
2022-02-22 16:20:51 +0900

    feat: makeJuice 함수 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -9,4 +9,17 @@ import Foundation
 // 쥬스 메이커 타입
 struct JuiceMaker {
     
+    func makeJuice(name: String, firstIngredient: Int, secondIngredient: Int?, name1: FruitStore.Fruit, name2: FruitStore.Fruit?) -> (Int, Int) {
+        var remainCounts: (firstIngredient: Int, secondIngredient: Int)
+        remainCounts.firstIngredient = name1.count - firstIngredient
+        
+        if let second = secondIngredient {
+            remainCounts.secondIngredient = name2?.count ?? 0 - second
+        } else {
+            remainCounts.secondIngredient = 0
+        }
+        
+        return remainCounts
+    }
+    
 }
