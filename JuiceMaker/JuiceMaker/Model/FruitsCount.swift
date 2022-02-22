e4f5ce1bbed99df1a0a2a57ee789ea3b5d51e2ce
Roy-wonji <shuwj81@daum.net>
2022-02-22 10:24:28 +0900

    feat: 과일재고 초기화 class 구현

---
+++ JuiceMaker/JuiceMaker/Model/FruitsCount.swift
@@ -0,0 +1,15 @@
+//
+//  FruitsCount.swift
+//  JuiceMaker
+//
+//  Created by 서원지 on 2022/02/22.
+//
+
+import Foundation
+
+class FruitsCount {
+    var count: Int
+    init() {
+        count = 10
+    }
+}
