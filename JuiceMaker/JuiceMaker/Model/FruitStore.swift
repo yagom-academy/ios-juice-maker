421482a4c64cc668270940704804f744d6c5b00c
GoMyungju171 <mooooongdu171@gmail.com>
2022-02-22 10:31:17 +0900

    Feat: FruitStore 구현
    FruitStore가 관리하는 과일 종류 설정, 과일의 초기 재고값 설정, 과일 수량 변경하는 메서드 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -1,12 +1,24 @@
-//
-//  JuiceMaker - FruitStore.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
+enum Fruit: CaseIterable {
+    case strawberry, banana, pineapple, kiwi, mango
+}
 // 과일 저장소 타입
 class FruitStore {
+    let defaultStock = 10
+    var fruits: [Fruit: Int] = [:]
     
+    init() {
+        for fruit in Fruit.allCases {
+            fruits[fruit] = defaultStock
+        }
+    }
+    
+    func adjustStock(fruit: Fruit, amount: Int) {
+        guard let currentStock = fruits[fruit] else {
+            return
+        }
+        fruits.updateValue(currentStock + amount, forKey: fruit)
+    }
 }
+
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -1,9 +1,3 @@
-//
-//  JuiceMaker - JuiceMaker.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import Foundation
 
 // 쥬스 메이커 타입
