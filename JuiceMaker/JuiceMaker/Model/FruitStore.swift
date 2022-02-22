a56f7ee461aaf9c18681ab7f0944d3e3b55b4ef1
doogie <chris_97@naver.com>
2022-02-22 18:28:32 +0900

    feat:JuiceMaker 타입 변경, makeJuice수정,FruitNumber 생성, fruitLists 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,7 +8,6 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    
     struct Fruit {
         var name: String
         var count: Int
@@ -19,5 +18,11 @@ class FruitStore {
     var pineapple = Fruit(name: "pineapple", count: 10)
     var kiwi = Fruit(name: "kiwi", count: 10)
     var mango = Fruit(name: "mango", count: 10)
-    
+    var fruitLists = [
+        Fruit(name: "strawberry", count: 10),
+        Fruit(name: "banana", count: 10),
+        Fruit(name: "pineapple", count: 10),
+        Fruit(name: "kiwi", count: 10),
+        Fruit(name: "mango", count: 10)
+    ]
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,19 +7,19 @@
 import Foundation
 
 // 쥬스 메이커 타입
-struct JuiceMaker {
-    
-    func makeJuice(name: String, firstIngredient: Int, secondIngredient: Int?, name1: FruitStore.Fruit, name2: FruitStore.Fruit?) -> (Int, Int) {
-        var remainCounts: (firstIngredient: Int, secondIngredient: Int)
-        remainCounts.firstIngredient = name1.count - firstIngredient
-        
-        if let second = secondIngredient {
-            remainCounts.secondIngredient = name2?.count ?? 0 - second
-        } else {
-            remainCounts.secondIngredient = 0
-        }
-        
-        return remainCounts
+class JuiceMaker: FruitStore {
+    enum FruitNumber: Int {
+        case strawberry = 0
+        case banana = 1
+        case pineapple = 2
+        case kiwi = 3
+        case mango = 4
     }
     
+    func makeJuice(JuiceName: String, firstIngredientCounts: Int, secondIngredientCounts: Int, firstIngredient: FruitNumber, secondIngredient: FruitNumber?) {
+        fruitLists[firstIngredient.rawValue].count -= firstIngredientCounts
+        if let fruitName2 = secondIngredient {
+            fruitLists[fruitName2.rawValue].count -= secondIngredientCounts
+        }
+    }
 }
