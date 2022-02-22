9df7f4522b08382ab95204adbd1b96f83214311d
dnwhd0112 <dnwhd0112@naver.com>
2022-02-22 22:19:02 +0900

    feat: JuiceMaker 프로퍼티 추가, 기능 추가
    recipe 프로퍼티, fruitStore, 생성자 추가, make 함수 ,getAJuiceRecipe 함수 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,41 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    var recipe: Dictionary<Juice,Dictionary<Fruit,Int>>
+    var fruitStore: FruitStore
     
+    init(recipe: Dictionary<Juice,Dictionary<Fruit,Int>>, fruitStore: FruitStore) {
+        self.recipe = recipe
+        self.fruitStore = fruitStore
+    }
+    
+    func getAJuiceRecipe(juice: Juice) throws -> Dictionary<Fruit,Int> {
+        guard let juiceRecipe = self.recipe[juice] else {
+            throw JuiceMakingError.noRecipe
+        }
+        return juiceRecipe
+    }
+    
+    func make(juice: Juice) -> Juice? {
+        do {
+            let juiceRecipe = try getAJuiceRecipe(juice: juice)
+            for (fruit,amount) in juiceRecipe {
+                try self.fruitStore.checkStock(fruit: fruit, amount: amount)
+            }
+            for (fruit,amount) in juiceRecipe {
+                try self.fruitStore.useFruit(fruit: fruit, amount: amount)
+            }
+            return juice
+        } catch JuiceMakingError.notEnoughStock {
+            print("재고가 부족합니다.")
+            return nil
+        } catch JuiceMakingError.noOption {
+            print("존재하지 않는 과일입니다.")
+            return nil
+        }
+        catch {
+            print("알 수 없는 에러가 났습니다.")
+            return nil
+        }
+    }
 }
