902db51234474c1d7ad114a6233c93aeae0650ec
Eddy <kimkyunghun3@naver.com>
2022-02-22 10:13:17 +0900

    feat: FruitsTypes 열거형 구현

---
+++ JuiceMaker/JuiceMaker/Model/FruitsType.swift
@@ -0,0 +1,16 @@
+//
+//  FruitsType.swift
+//  JuiceMaker
+//
+//  Created by Eddy on 2022/02/22.
+//
+
+import Foundation
+
+enum FruitsTypes: String {
+    case strawberry
+    case banna
+    case pineapple
+    case kiwi
+    case mango
+}
