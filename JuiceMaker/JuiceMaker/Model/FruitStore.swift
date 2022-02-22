4e2d91e5a6030d7c3db23fd1c71cb21595b7774c
JoSH0318 <jsh9870@naver.com>
2022-02-22 10:49:20 +0900

    [feat]: Add fruitList: Dictionary, func updateQuantity()
    - fruitList: 초안으로 Dic에 모든 키값의 value를 10으로 지정
    - func updateQuantity(): view controller에서 받아온 값을 파라미터로 받고
       해당 과일 키값의 수를 업데이트

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,14 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var fruitList: Dictionary = [FruitType.strawberry: 10,
+                                 FruitType.banana: 10,
+                                 FruitType.pineapple: 10,
+                                 FruitType.kiwi: 10,
+                                 FruitType.mango: 10]
     
+    func updateQuantity(_ fruit: FruitType, _ quantity: Int) {
+        guard var updatedFruitList = fruitList[fruit] else { return }
+        updatedFruitList += quantity
+    }
 }
