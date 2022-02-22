b872f57cdcae55ded4719f2c18d2bac0e578a02c
xogns8696@gmail.com <xogns8696@gmail.com>
2022-02-22 15:18:07 +0900

    ➕[ADD] : Fruit, Juice 열거형 추가

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,14 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by taehun kim on 2022/02/22.
+//
+
+enum Fruit {
+    case strawberry
+    case kiwi
+    case banana
+    case pineapple
+    case mango
+}
