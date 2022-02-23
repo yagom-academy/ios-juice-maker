8dc22af054ee5e35c6ae0820e7583c4ce4d1ec49
Eddy <kimkyunghun3@naver.com>
2022-02-23 21:02:57 +0900

    feat: 주스 타입 선언 및 주스에 따른 분류 구현

--- JuiceMaker/JuiceMaker/Model/FruitsType.swift
+++ JuiceMaker/JuiceMaker/Model/FruitsType.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 enum FruitsTypes: CaseIterable {
-    case strawberry, banna, pineapple, kiwi, mango
+    case strawberry, banana, pineapple, kiwi, mango, strawberryBanana, mangoKiwi
+    private var juiceName: String {
+        switch self {
+        case .strawberryBanana:
+            return "딸바쥬스"
+        case .mangoKiwi:
+            return "망고키위쥬스"
+        case .strawberry:
+            return "딸기 쥬스"
+        case .banana:
+            return "바나나 쥬스"
+        case .pineapple:
+            return "파인애플 쥬스"
+        case .kiwi:
+            return "키위 쥬스"
+        case .mango:
+            return "망고 쥬스"
+        }
+    }
 }
