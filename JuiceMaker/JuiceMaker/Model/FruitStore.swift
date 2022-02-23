f853067f65f1c05c5ea0d2d72aae5e1bb9253287
Eddy <kimkyunghun3@naver.com>
2022-02-23 21:21:43 +0900

    feat: 과일 초기 재고 초기화 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,11 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    
+    var fruits: [FruitsTypes: Int] = [:]
+    init() {
+        let initialStock = 10
+        for fruitQuantity in FruitsTypes.allCases {
+            fruits[fruitQuantity] = initialStock
+        }
+    }
 }
