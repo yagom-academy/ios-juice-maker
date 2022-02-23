02322a16e099ff0abc42ba84a1528b0254a84cc5
Papri <98319774+papriOS@users.noreply.github.com>
2022-02-23 23:32:48 +0900

    feat: init() , isEnoughStock() 구현
    * init() : 인스턴스가 생성될 때, fruitsInventory에 저장될 과일과 과일의 개수 초기화
    * isEnoughStock() : 음료 제조에 필요한 과일과 과일의 개수가 충분한지 확인

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -1,5 +1,22 @@
 import Foundation
 
+enum DefaulSetting {
+    static let stock = 10
+}
+
 class FruitStore {
+    var fruitsInventory : [Fruit : Int] = [:]
     
+    init() {
+        Fruit.allCases.forEach { fruitsInventory[$0] = DefaulSetting.stock }
+    }
+    
+    func isEnoughStock(for juiceIngredient: [Fruit : Int]) -> Bool {
+        for (fruit, requiredAmount) in juiceIngredient {
+            guard let fruitStoreStock = fruitsInventory[fruit], fruitStoreStock >= requiredAmount  else {
+                return false
+            }
+        }
+        return true
+    }
 }
