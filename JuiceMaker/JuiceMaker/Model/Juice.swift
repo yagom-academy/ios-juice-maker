8d643e07d52b682c4cba7c041c81bca89d5794b5
Lingo <llingo@kakao.com>
2022-02-23 14:38:02 +0900

    feat: Juice 열거형 타입에 recipe 연산 프로퍼티 생성
    switch 문을 사용하여 딕셔너리에 쥬스 제작에 필요한 과일과 개수를 담은 레시피를 생성

--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -10,4 +10,23 @@ import Foundation
 enum Juice {
   case strawberry, banana, pineapple, kiwi, mango
   case strawberryBanana, mangoKiwi
+  
+  var recipe: [Fruit: Int] {
+    switch self {
+    case .strawberry:
+      return [.strawberry: 16]
+    case .banana:
+      return [.banana: 2]
+    case .pineapple:
+      return [.pineapple: 2]
+    case .kiwi:
+      return [.kiwi: 3]
+    case .mango:
+      return [.mango: 3]
+    case .strawberryBanana:
+      return [.strawberry: 10, .banana: 1]
+    case .mangoKiwi:
+      return [.mango: 2, .kiwi: 1]
+    }
+  }
 }
