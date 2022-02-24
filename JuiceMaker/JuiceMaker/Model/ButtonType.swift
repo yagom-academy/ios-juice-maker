43bc01df541bbda0ea7f2120857a55339fd9810a
Marisol Mirim Kim <marisol.develop@gmail.com>
2022-02-24 12:27:52 +0900

    [feat]: add changeFruitStock() in FruitStore, enum ButtonType
    - 과일의 수량을 n개 변경하는 changeFruitStock() 생성
    - +, - 를 구별하는 ButtonType 열거형 생성

---
+++ JuiceMaker/JuiceMaker/Model/ButtonType.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonType.swift
+//  JuiceMaker
+//
+//  Created by Marisol Mirim Kim on 2022/02/24.
+//
+
+import Foundation
+
+enum ButtonType {
+    case add
+    case reduce
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -13,4 +13,39 @@ class FruitStore {
         let defaultQuantity = 10
         Fruit.allCases.forEach{ fruitInventory[$0] = defaultQuantity }
     }
+    
+    func changeFruitStock(by inputFruitQuantity: [Fruit: Int], buttonType: ButtonType) {
+        do {
+            if buttonType == .add {
+                try addFruitStock(by: inputFruitQuantity)
+            } else {
+                try reduceFruitStock(by: inputFruitQuantity)
+            }
+        } catch let error as OrderError {
+            print(error.description)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func addFruitStock(by inputFruitQuantity: [Fruit: Int]) throws {
+        for (fruit, quantity) in inputFruitQuantity {
+            guard let fruitStock = fruitInventory[fruit] else {
+                throw OrderError.wrongFormat
+            }
+            fruitInventory[fruit] = fruitStock + quantity
+        }
+    }
+    
+    func reduceFruitStock(by inputFruitQuantity: [Fruit: Int]) throws {
+        for (fruit, quantity) in inputFruitQuantity {
+            guard let fruitStock = fruitInventory[fruit] else {
+                throw OrderError.wrongFormat
+            }
+            guard fruitStock >= quantity else {
+                throw OrderError.outOfStock
+            }
+            fruitInventory[fruit] = fruitStock - quantity
+        }
+    }
 }
