83c685d8b2190daa002e05084e0dfdf54c42faa0
Lingo <llingo@kakao.com>
2022-02-24 23:06:43 +0900

    refactor: LocalizedError 프로토콜을 채택하여 에러 처리
    MakeJuiceError의 rawValue 타입을 제거하고 extension 로 확장하여 LocalizedError 프로토콜을 채택하여 에러 처리

--- JuiceMaker/JuiceMaker/Model/Error/MakeJuiceError.swift
+++ JuiceMaker/JuiceMaker/Model/Error/MakeJuiceError.swift
@@ -7,7 +7,18 @@
 
 import Foundation
 
-enum MakeJuiceError: String, Error {
-  case outOfStock = "재고가 부족합니다."
-  case notExistFruit = "존재하지 않는 과일입니다."
+enum MakeJuiceError: Error {
+  case outOfStock
+  case notExistFruit
+}
+
+extension MakeJuiceError: LocalizedError {
+  var errorDescription: String? {
+    switch self {
+    case .outOfStock:
+      return "재료가 모자라요. 재고를 수정할까요?"
+    case .notExistFruit:
+      return "존재하지 않는 과일입니다."
+    }
+  }
 }
