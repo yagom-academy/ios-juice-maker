9017dd21a2b4353776914d500367eea0030ae1e5
Red <seryprk@gmail.com>
2022-02-24 13:42:41 +0900

    correct : neededStrock 이름 오타 수정 및 indent 수정

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -48,14 +48,14 @@ struct JuiceMaker {
     }
     
     private func checkAllStock(of juice: Juice) -> Bool {
-            let listOfIngredient = juice.recipe.count
-            for list in Int.zero..<listOfIngredient {
-                guard checkOneStock(of: juice, with: list) else {
-                    return false
-                }
+        let listOfIngredient = juice.recipe.count
+        for list in Int.zero..<listOfIngredient {
+            guard checkOneStock(of: juice, with: list) else {
+                return false
             }
-            return true
         }
+        return true
+    }
     
     private func checkOneStock(of juice: Juice, with listOfIngredient: Int) -> Bool {
         let (neededFruit, neededStock) = juice.recipe[listOfIngredient]
@@ -68,13 +68,13 @@ struct JuiceMaker {
     }
     
     private func makeJuice(_ juice: Juice) {
-            for list in Int.zero..<juice.recipe.count {
-                let (neededFruit, neededStrock) = juice.recipe[list]
-                let inStock = fruitStore.stock[neededFruit] ?? Int.zero
-                fruitStore.updateStock(of: neededFruit, to: inStock - neededStrock)
-                print("\(juice) 나왔습니다.")
-            }
+        for list in Int.zero..<juice.recipe.count {
+            let (neededFruit, neededStock) = juice.recipe[list]
+            let inStock = fruitStore.stock[neededFruit] ?? Int.zero
+            fruitStore.updateStock(of: neededFruit, to: inStock - neededStock)
+            print("\(juice) 나왔습니다.")
         }
+    }
 }
 
 
