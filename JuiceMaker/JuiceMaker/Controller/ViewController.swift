24f52a039f168d1f8be0ec4db37ff03bbe306a86
cathy171 <mooooongdu171@gmail.com>
2022-02-27 18:18:53 +0900

    feat: 쥬스 주문 시 쥬스 이름 출력하는 함수 생성

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var orderStrawberryBananaButton: UIButton!
+    @IBOutlet weak var orderMangoKiwiButton: UIButton!
+    @IBOutlet weak var orderStrawberryButton: UIButton!
+    @IBOutlet weak var orderBananaButton: UIButton!
+    @IBOutlet weak var orderPineappleButton: UIButton!
+    @IBOutlet weak var orderKiwiButton: UIButton!
+    @IBOutlet weak var orderMangoButton: UIButton!
+    
+    @IBAction func orderJuiceAction(_ sender: UIButton) {
+        print(sender.currentTitle!.components(separatedBy: " ")[0])
+    }
 }
 
