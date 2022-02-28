bf7b18e574492a78c3d0fd75f536d2c28f758b6f
Marisol Mirim Kim <marisol.develop@gmail.com>
2022-02-28 11:39:04 +0900

    [feat]: IBOutlet, IBAction 연결
    과일 라벨과 쥬스만들기 버튼을 viewController에 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    @IBOutlet weak var strawberryBananaJuiceOrderButton: UIButton!
+    @IBOutlet weak var mangoKiwiJuiceOrderButton: UIButton!
+    @IBOutlet weak var strawberryJuiceOrderButton: UIButton!
+    @IBOutlet weak var bananaJuiceOrderButton: UIButton!
+    @IBOutlet weak var pineappleJuiceOrderButton: UIButton!
+    @IBOutlet weak var kiwiJuiceOrderButton: UIButton!
+    @IBOutlet weak var mangoJuiceOrderButton: UIButton!
+    
+    let juiceMaker = JuiceMaker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func strawberryBananaJuiceOrderButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func mangoKiwiJuiceOrderButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func strawberryJuiceOrderButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func bananaJuiceOrderButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func pineappleJuiceOrderButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func kiwiJuiceOrderButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func mangoJuiceOrderButtonClicked(_ sender: UIButton) {
+    }
+    
 }
 
