592fb1d35d84abd34b2b544396663bee9f637b8e
Taeangel <wngks2050@gmail.com>
2022-02-28 23:00:51 +0900

    feat: FruitViewController 생성 및 IBOutlet, IBAction 연결
    Fruit Count Label 및 각각 Stepper 연결, FruitViewController 생성

---
+++ JuiceMaker/JuiceMaker/Controller/FruitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FruitViewController.swift
+//  JuiceMaker
+//
+//  Created by song on 2022/02/28.
+//
+
+import UIKit
+
+final class FruitViewController: UIViewController {
+  
+  @IBOutlet weak var strawberryCountLabel: UILabel!
+  @IBOutlet weak var bananaCountLabel: UILabel!
+  @IBOutlet weak var pineappleCountLabel: UILabel!
+  @IBOutlet weak var kiwiCountLabel: UILabel!
+  @IBOutlet weak var mangoCountLabel: UILabel!
+  
+  @IBOutlet weak var strawberryStepper: UIStepper!
+  @IBOutlet weak var bananaStepper: UIStepper!
+  @IBOutlet weak var pineappleStepper: UIStepper!
+  @IBOutlet weak var kiwiStepper: UIStepper!
+  @IBOutlet weak var mangoStepper: UIStepper!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func didStepperValueChanged(_ sender: UIStepper) {}
+}
