8f0918935f690352387e6d4961813d5b3b9dddb9
papriOS <bluecrab30@naver.com>
2022-02-28 12:14:04 +0900

    feat: 화면 전환과 VC에 UI연결

---
+++ JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FruitStoreViewController.swift
+//  JuiceMaker
+//
+//  Created by 허윤영 on 28/02/2022.
+//
+
+import UIKit
+
+class FruitStoreViewController: UIViewController {
+    
+    @IBOutlet var fruitLabelCollection: [UILabel]!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func stawberryStepper(_ sender: UIStepper) {
+    }
+    
+    @IBAction func bananaStepper(_ sender: UIStepper) {
+    }
+    
+    @IBAction func pineappleStepper(_ sender: UIStepper) {
+    }
+    
+    @IBAction func kiwiStepper(_ sender: UIStepper) {
+    }
+    
+    @IBAction func mangoStepper(_ sender: UIStepper) {
+    }
+    
+}
+
--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,9 +7,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var fruitLabelCollection: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func makeStrawberryBananaJuiceButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func makeMangoKiwiJuiceButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func makeStrawberryJuiceButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func makeBananaJuiceButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func makePineappleJuiceButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func makeKiwiJuiceButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func makeMangoJuiceButton(_ sender: UIButton) {
+    }
+    
 }
