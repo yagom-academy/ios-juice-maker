3eb5cbf3afa37df29744d04b252bc41f5b4d27c8
Lingo <llingo@kakao.com>
2022-02-28 22:39:58 +0900

    feat:  ViewController IBOutlet, IBAction 연결
    Juice Order Button 및 Fruit Count Label IBOutlet과 IBAction 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,26 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  // IBOutlet
+  @IBOutlet weak var strawberryCountLabel: UILabel!
+  @IBOutlet weak var bananaCountLabel: UILabel!
+  @IBOutlet weak var pineappleCountLabel: UILabel!
+  @IBOutlet weak var kiwiCountLabel: UILabel!
+  @IBOutlet weak var mangoCountLabel: UILabel!
+  
+  @IBOutlet weak var strawberryBananaButton: UIButton!
+  @IBOutlet weak var mangoKiwiButton: UIButton!
+  @IBOutlet weak var strawberryButton: UIButton!
+  @IBOutlet weak var bananaButton: UIButton!
+  @IBOutlet weak var pineappleButton: UIButton!
+  @IBOutlet weak var kiwiButton: UIButton!
+  @IBOutlet weak var mangoButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
   
+  // IBAction
+  @IBAction func didTapOrderButton(_ sender: UIButton) {}
   
 }
-
