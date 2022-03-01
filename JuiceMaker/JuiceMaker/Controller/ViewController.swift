aa33a4222e1cfae86e9e9ef29f0aaa14f934381d
Lingo <llingo@kakao.com>
2022-03-01 18:10:16 +0900

    refactor: ViewController의 누락된 final 키워드 추가

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
   
   @IBOutlet weak var strawberryCountLabel: UILabel!
   @IBOutlet weak var bananaCountLabel: UILabel!
