b918f632162f7a354fc3b562397ef89a16c0e109
minseong <msb0987@naver.com>
2022-03-01 15:28:45 +0900

    feat: 얼럿표시 구현
    * 재고가 있을 경우 successAlert 얼럿
    * 재고가 없을 경우 failAlert 얼럿표시 후 수정버튼 누를시FuitStoreVC로 화면전환

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -26,6 +26,23 @@ class ViewController: UIViewController {
         }
     }
     
+    func showSuccessAlert(juice: String) {
+        let successAlert = UIAlertController(title: "\(juice)쥬스 나왔습니다", message: "맛있게드세요", preferredStyle: .alert)
+        successAlert.addAction(UIAlertAction(title: "네", style: .default))
+        present(successAlert, animated: true)
+    }
+    
+    func showFailAlert() {
+        let failAlert = UIAlertController(title: "재료부족", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
+        failAlert.addAction(UIAlertAction(title: "네", style: .default, handler: { _ in
+            guard let fruitStoreVC = self.storyboard?.instantiateViewController(identifier: "FruitStoreVC") else {
+                return
+            }
+            self.navigationController?.pushViewController(fruitStoreVC, animated: true)}))
+        failAlert.addAction(UIAlertAction(title: "아니요", style: .destructive))
+        present(failAlert, animated: true)
+    }
+    
     @IBAction func makeStrawberryBananaJuiceButton(_ sender: UIButton) {
     }
     
