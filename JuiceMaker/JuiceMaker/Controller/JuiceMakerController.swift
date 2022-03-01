b1c7ad7ca917cac55787dc5113d457bbc5b5a068
xogns8696@gmail.com <xogns8696@gmail.com>
2022-03-01 13:51:12 +0900

    ➕[ADD] : JuiceMakerController에 Label과 Button을 outlet으로 연결

--- JuiceMaker/JuiceMaker/Controller/JuiceMakerController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakerController.swift
@@ -7,7 +7,21 @@
 import UIKit
 
 class JuiceMakerController: UIViewController {
-
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    @IBOutlet weak var strawberryBananaButton: UIButton!
+    @IBOutlet weak var strawberryButton: UIButton!
+    @IBOutlet weak var bananaButton: UIButton!
+    @IBOutlet weak var pineappleButton: UIButton!
+    @IBOutlet weak var mangoKiwiButton: UIButton!
+    @IBOutlet weak var kiwiButton: UIButton!
+    @IBOutlet weak var mangoButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
