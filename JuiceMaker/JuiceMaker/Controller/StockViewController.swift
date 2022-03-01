3972156bfa8e5dd381da08679c39f83634293e4e
dudu <whqtkf12@naver.com>
2022-03-01 18:58:02 +0900

    ✨[FEAT] :  StockViewController에 이니셜라이저 추가

--- JuiceMaker/JuiceMaker/Controller/StockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class StockViewController: UIViewController {
+    private var stock: [Fruit: Int]
+    
+    init?(coder: NSCoder, stock: [Fruit: Int]) {
+        self.stock = stock
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("You must create this view controller with code")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
