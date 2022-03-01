d97b1e07686428d8f44abbb376781dcdc9b5c2f1
malrang <ruehd7924@gmail.com>
2022-03-01 17:41:24 +0900

    feat: 과일 재고 표시 기능 구현

--- JuiceMaker/JuiceMaker/Controller/MainViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -7,11 +7,37 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    let juiceMaker = JuiceMaker()
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showStock()
     }
+    
+    func showStock() {
+        for (fruit, stock) in  juiceMaker.store.fruitList {
+            switch fruit {
+            case .strawberry:
+                self.strawberryStockLabel.text = String(stock)
+            case .banana:
+                self.bananaStockLabel.text = String(stock)
+            case .pineapple:
+                self.pineappleStockLabel.text = String(stock)
+            case .kiwi:
+                self.kiwiStockLabel.text = String(stock)
+            case .mango:
+                self.mangoStockLabel.text = String(stock)
+            }
+        }
+    }
+    
     @IBAction func moveToManageView(_ sender: UIBarButtonItem) {
         guard let manageVC = self.storyboard?.instantiateViewController(withIdentifier: "ManageViewController") else { return }
         self.navigationController?.present(manageVC, animated: true, completion: nil)
