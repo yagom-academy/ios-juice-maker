c68b7b7510afeaedb71dee2dcb6dfce3b4c28d49
minseong <msb0987@naver.com>
2022-03-02 17:00:36 +0900

    refactor: 접근제어자, updateFruitStock() 1차refactor
    * Exstension Array를 이용해 updateFruitStock()방어로직 구현

--- JuiceMaker/JuiceMaker/Controller/RootViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/RootViewController.swift
@@ -1,38 +1,49 @@
-//
-//  JuiceMaker - ViewController.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet var fruitLabelCollection: [UILabel]!
+extension Array {
+    public subscript(safeIndex index: Int) -> Element? {
+        guard index >= .zero, index < endIndex else {
+            return nil
+        }
+
+        return self[index]
+    }
+}
+
+final class RootViewController: UIViewController {
+    @IBOutlet private var fruitLabelCollection: [UILabel]!
     private let juiceMaker = JuiceMaker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         updateFruitsStock()
     }
     
     private func updateFruitsStock() {
-        var index:Int = .zero
-        for fruit in Fruit.allCases {
+        for (index, fruit) in Fruit.allCases.enumerated() {
             guard let stock = FruitStore.shared.inventory[fruit] else {
                 return
             }
-            fruitLabelCollection[index].text = String(stock)
-            index += 1
+            
+            guard let fruitLabel = fruitLabelCollection[safeIndex: index] else {
+                return
+            }
+            
+            fruitLabel.text = String(stock)
         }
     }
     
-    func showSuccessAlert(juice: String) {
+    private func showSuccessAlert(juice: String) {
         let successAlert = UIAlertController(title: "\(juice)쥬스 나왔습니다", message: "맛있게드세요", preferredStyle: .alert)
         successAlert.addAction(UIAlertAction(title: "네", style: .default))
         present(successAlert, animated: true)
     }
     
-    func showFailAlert() {
+    private func showFailAlert() {
         let failAlert = UIAlertController(title: "재료부족", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
         failAlert.addAction(UIAlertAction(title: "네", style: .default, handler: { _ in
             guard let fruitStoreVC = self.storyboard?.instantiateViewController(identifier: "FruitStoreVC") else {
