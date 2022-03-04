4f9f7bf23477fb94d13cd791203554a99f2295e0
saafaaari <111dltldnjs@naver.com>
2022-03-04 11:58:30 +0900

    refactor : Juice타입 CustomStringConvertible프로토콜 채용

--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum Juice: String {
+enum Juice: String, CustomStringConvertible {
     case strawberryJuice = "딸기쥬스"
     case bananaJuice = "바나나쥬스"
     case pineappleJuice = "파인애플쥬스"
@@ -16,7 +16,7 @@ enum Juice: String {
     case strawberryAndBananaJuice = "딸바쥬스"
     case mangoAndKiwiJuice = "망키쥬스"
     
-    var koreanName: String {
+    var description: String {
         return self.rawValue
     }
 }
