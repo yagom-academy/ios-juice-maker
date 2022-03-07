a84d727882c66c5f1b35e3069e27db06d3951052
doogie97 <chris_97@naver.com>
2022-03-07 18:12:30 +0900

    feat: 각 stepper value에 재고 연동 및 조작시 label 변경 기능 구현

--- JuiceMaker/JuiceMaker/Controller/ManageViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ManageViewController.swift
@@ -15,6 +15,12 @@ class ManageViewController: UIViewController {
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
     
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
     var fruitDic: [Fruits: Int] = [:]
 
     override func viewDidLoad() {
@@ -23,20 +29,40 @@ class ManageViewController: UIViewController {
             switch fruit {
             case .strawberry:
                 self.strawberryStockLabel.text = String(stock)
+                self.strawberryStepper.value = Double(stock)
             case .banana:
                 self.bananaStockLabel.text = String(stock)
+                self.bananaStepper.value = Double(stock)
             case .pineapple:
                 self.pineappleStockLabel.text = String(stock)
+                self.pineappleStepper.value = Double(stock)
             case .kiwi:
                 self.kiwiStockLabel.text = String(stock)
+                self.kiwiStepper.value = Double(stock)
             case .mango:
                 self.mangoStockLabel.text = String(stock)
+                self.mangoStepper.value = Double(stock)
             }
         }
     }
-    
+
     @IBAction func touchCloseButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
-    
+    @IBAction func touchStepper(_ sender: UIStepper) {
+        switch sender {
+        case strawberryStepper:
+            self.strawberryStockLabel.text = String(Int(sender.value))
+        case bananaStepper:
+            self.bananaStockLabel.text = String(Int(sender.value))
+        case pineappleStepper:
+            self.pineappleStockLabel.text = String(Int(sender.value))
+        case kiwiStepper:
+            self.kiwiStockLabel.text = String(Int(sender.value))
+        case mangoStepper:
+            self.mangoStockLabel.text = String(Int(sender.value))
+        default:
+            print("에러")
+        }
+    }
 }
