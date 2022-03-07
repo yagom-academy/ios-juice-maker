9fd9c163a1e2096ee1f422c793208076c1451570
xogns8696@gmail.com <xogns8696@gmail.com>
2022-03-07 19:01:40 +0900

    ✨[FEAT] : JuiceMakerError에 LocalizedError 프로토콜 채택

--- JuiceMaker/JuiceMaker/Model/JuiceMakerError.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMakerError.swift
@@ -5,7 +5,20 @@
 //  Created by 원툴, 두두 on 2022/02/22.
 //
 
+import Foundation
+
 enum JuiceMakerError: Error {
     case outOfStock
     case invalidButton
+}
+
+extension JuiceMakerError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .outOfStock:
+            return "재료가 모자라요. 재고를 수정할까요?"
+        case .invalidButton:
+            return "버튼을 잘못 누르셨습니다."
+        }
+    }
 }
