f9e348c92cd97cadf2ca492f483f2ff602da143b
JoSH0318 <jsh9870@naver.com>
2022-03-07 21:24:23 +0900

    [feat]: add showCurrentStock method, @IBAction method implement
    - StockViewController에 라벨 갱신을 위한 showCurrentStock 메서드 추가
    - StockViewController @IBAction func stepperClicked 로직 구현
    - 에러 알럿 extension으로 구현

--- JuiceMaker/JuiceMaker/Controller/StockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockViewController.swift
@@ -27,13 +27,63 @@ final class StockViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        showCurrentStock()
+    }
+    
+    private func showCurrentStock() {
+        changedStock.keys.forEach {
+            switch $0 {
+            case .strawberry:
+                strawberryStockLabel.text = changedStock[.strawberry]?.description
+                strawberryStepper.value = Double(changedStock[.strawberry] ?? 0)
+            case .banana:
+                bananaStockLabel.text = changedStock[.banana]?.description
+                bananaStepper.value = Double(changedStock[.banana] ?? 0)
+            case .pineapple:
+                pineappleStockLabel.text = changedStock[.pineapple]?.description
+                pineappleStepper.value = Double(changedStock[.pineapple] ?? 0)
+            case .kiwi:
+                kiwiStockLabel.text = changedStock[.kiwi]?.description
+                kiwiStepper.value = Double(changedStock[.kiwi] ?? 0)
+            case .mango:
+                mangoStockLabel.text = changedStock[.mango]?.description
+                mangoStepper.value = Double(changedStock[.mango] ?? 0)
+            }
+        }
+    }
+    @IBAction func stepperClicked(_ sender: UIStepper) {
+        switch sender {
+        case strawberryStepper:
+            strawberryStockLabel.text = Int(sender.value).description
+            changedStock[.strawberry] = Int(sender.value)
+        case bananaStepper:
+            bananaStockLabel.text = Int(sender.value).description
+            changedStock[.banana] = Int(sender.value)
+        case pineappleStepper:
+            pineappleStockLabel.text = Int(sender.value).description
+            changedStock[.pineapple] = Int(sender.value)
+        case kiwiStepper:
+            kiwiStockLabel.text = Int(sender.value).description
+            changedStock[.kiwi] = Int(sender.value)
+        case mangoStepper:
+            mangoStockLabel.text = Int(sender.value).description
+            changedStock[.mango] = Int(sender.value)
+        default:
+            alertUnknownError()
+        }
     }
     
     @IBAction func closeButtonClicked(_ sender: Any) {
         delegate?.updateStock(changedStock)
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
-    
-    @IBAction func stepperClicked(_ sender: UIStepper) {
+}
+
+extension StockViewController {
+    private func alertUnknownError() {
+        let alert = UIAlertController(title: Alert.unknownError.title, message: Alert.unknownError.message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: AlertButton.confirm.title, style: .default, handler: nil)
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
     }
 }
