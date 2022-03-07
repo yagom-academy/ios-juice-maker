f3c2faed6941b156e04b42559a17086843f9c9fc
Roy-wonji <shuwj81@daum.net>
2022-03-07 20:15:25 +0900

    feat: closedbutton 액션  추가 및 이전 화면으로 돌아가는 popup방식으로 구현

--- JuiceMaker/JuiceMaker/Controller/ChangeStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ChangeStockViewController.swift
@@ -9,4 +9,11 @@ import UIKit
 
 class ChangeStockViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closedButtonTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
