801e2906ba52a5b4b6e5ee5a8dbe5db01797ac2d
saafaaari <111dltldnjs@naver.com>
2022-03-07 21:31:22 +0900

    feat : Stepper를 이용한 딸기 재고 수정 기능 추가
    updateLabel()함수 Stepper.value초기값 설정 기능 추가

--- JuiceMaker/JuiceMaker/Controller/SubViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/SubViewController.swift
@@ -16,6 +16,16 @@ class SubViewController: UIViewController {
     @IBOutlet weak var kiwiLabel: UILabel!
     @IBOutlet weak var mangoLabel: UILabel!
     
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
+    @IBAction func strawberryStockchange(_ sender: UIStepper) {
+        strawberryLabel.text = String(format: "%.0f", strawberryStepper.value)
+    }
+    
     @IBAction func closeView(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
@@ -34,21 +44,22 @@ class SubViewController: UIViewController {
             switch fruit {
             case.strawberry:
                 strawberryLabel.text = String(count)
+                strawberryStepper.value = Double(count)
             case.banana:
                 bananaLabel.text = String(count)
+                bananaStepper.value = Double(count)
             case.pineapple:
                 pineappleLabel.text = String(count)
+                pineappleStepper.value = Double(count)
             case.kiwi:
                 kiwiLabel.text = String(count)
+                kiwiStepper.value = Double(count)
             case.mango:
                 mangoLabel.text = String(count)
+                mangoStepper.value = Double(count)
                 
             }
         }
-//        self.bananaLabel
-//        self.pineappleLabel
-//        self.kiwiLabel
-//        self.mangoLabel
     }
     
 
