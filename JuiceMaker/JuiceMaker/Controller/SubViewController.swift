57cc92bbb1c0e07e8921dfd71fc6254106c4f3ca
saafaaari <111dltldnjs@naver.com>
2022-03-07 17:41:28 +0900

    add :  SubViewController파일 추가
    재고 추가 View UINavigationItem 추가 및 barButton 추가

---
+++ JuiceMaker/JuiceMaker/Controller/SubViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SubViewController.swift
+//  JuiceMaker
+//
+//  Created by 사파리 and Red on 2022/03/07.
+//
+
+import UIKit
+
+class SubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
