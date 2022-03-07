91470810cefe780c9ce954d6b6d32b94e9657ec0
malrang <ruehd7924@gmail.com>
2022-03-07 11:27:57 +0900

    refactor: 중복되는 문자열  중복 제거

--- JuiceMaker/JuiceMaker/Model/Menu.swift
+++ JuiceMaker/JuiceMaker/Model/Menu.swift
@@ -13,7 +13,6 @@ enum Menu: Int {
     case kiwiJuice
     case mangoJuice
     var recipe: [Fruits : Int] {
-        get {
             switch self {
             case .strawberryJuice:
                 return [.strawberry : 16]
@@ -30,27 +29,28 @@ enum Menu: Int {
             case .mangKiJuice:
                 return [.mango : 2, .kiwi : 1]
             }
-        }
+    }
+    
+    var juiceName: String {
+            switch self {
+            case .strawberryJuice:
+                return "딸기 쥬스"
+            case .bananaJuice:
+                return "바나나 쥬스"
+            case .kiwiJuice:
+                return "키위 쥬스"
+            case .pineappleJuice:
+                return "파인애플 쥬스"
+            case .ddalBaJuice:
+                return "딸바 쥬스"
+            case .mangoJuice:
+                return "망고 쥬스"
+            case .mangKiJuice:
+                return "망키 쥬스"
+            }
     }
     
     var orderMessage: String {
-        get {
-            switch self {
-            case .strawberryJuice:
-                return "딸기 쥬스 나왔습니다! 맛있게 드세요!"
-            case .bananaJuice:
-                return "바나나 쥬스 나왔습니다! 맛있게 드세요!"
-            case .kiwiJuice:
-                return "키위 쥬스 나왔습니다! 맛있게 드세요!"
-            case .pineappleJuice:
-                return "파인애플 쥬스 나왔습니다! 맛있게 드세요!"
-            case .ddalBaJuice:
-                return "딸바 쥬스 나왔습니다! 맛있게 드세요!"
-            case .mangoJuice:
-                return "망고 쥬스 나왔습니다! 맛있게 드세요!"
-            case .mangKiJuice:
-                return "망키 쥬스 나왔습니다! 맛있게 드세요!"
-            }
-        }
+        return "\(juiceName) 나왔습니다! 맛있게 드세요!"
     }
 }
