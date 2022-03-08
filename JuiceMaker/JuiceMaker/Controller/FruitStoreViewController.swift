df8522a646fd27e2f0ebb6407dca8432f4d0bf63
papriOS <bluecrab30@naver.com>
2022-03-08 21:40:32 +0900

    feat: stepper의 -, + 를 통한 재고 수정

--- JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
@@ -1,13 +1,27 @@
 import UIKit
 
 class FruitStoreViewController: UIViewController {
-    
     @IBOutlet var fruitLabelCollection: [UILabel]!
+    @IBOutlet var fruitStockStepper: [UIStepper]!
     var juiceMaker: JuiceMaker?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateFruitsStock()
+        updateStepperValue()
+    }
+    
+    func updateStepperValue() {
+        guard let juiceMaker = juiceMaker else {
+            return
+        }
+        
+        for (index, stock) in juiceMaker.getFruitsStock().enumerated() {
+            guard let stepper = fruitStockStepper[safeIndex: index] else {
+                return
+            }
+            stepper.value = Double(stock)
+        }
     }
     
     private func updateFruitsStock() {
@@ -27,19 +41,22 @@ class FruitStoreViewController: UIViewController {
         presentingViewController?.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func stawberryStepper(_ sender: UIStepper) {
+    @IBAction func tapStepper(_ sender: UIStepper) {
+        switch sender.tag {
+        case 1:
+            fruitLabelCollection[0].text = Int(sender.value).description
+        case 2:
+            fruitLabelCollection[1].text = Int(sender.value).description
+        case 3:
+            fruitLabelCollection[2].text = Int(sender.value).description
+        case 4:
+            fruitLabelCollection[3].text = Int(sender.value).description
+        case 5:
+            fruitLabelCollection[4].text = Int(sender.value).description
+        default:
+            return
+        }
     }
     
-    @IBAction func bananaStepper(_ sender: UIStepper) {
-    }
-    
-    @IBAction func pineappleStepper(_ sender: UIStepper) {
-    }
-    
-    @IBAction func kiwiStepper(_ sender: UIStepper) {
-    }
-    
-    @IBAction func mangoStepper(_ sender: UIStepper) {
-    }
 }
 
