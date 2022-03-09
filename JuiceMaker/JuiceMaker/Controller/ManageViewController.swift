671584d39921cab656f29fe6799e0f324fb4bd12
malrang <ruehd7924@gmail.com>
2022-03-09 17:14:51 +0900

    refactor: ConfirmButton Button 과 기능제거

--- JuiceMaker/JuiceMaker/Controller/ManageViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ManageViewController.swift
@@ -31,11 +31,13 @@ class ManageViewController: UIViewController {
                                             .pineapple: pineappleStockLabel,
                                             .kiwi: kiwiStockLabel,
                                             .mango: mangoStockLabel]
+    
     lazy var stepperDictionary: [Fruits: UIStepper] = [.strawberry: strawberryStepper,
                                                 .banana: bananaStepper,
                                                 .pineapple: pineappleStepper,
                                                 .kiwi: kiwiStepper,
                                                 .mango: mangoStepper]
+    
     var delegate: ManageViewControllerDelegate?
 
     override func viewDidLoad() {
@@ -44,24 +46,13 @@ class ManageViewController: UIViewController {
     }
     
     @IBAction func touchCloseButton(_ sender: UIBarButtonItem) {
+        changeStocks()
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func touchStepper(_ sender: UIStepper) {
         let selectedStepperDictionary = stepperDictionary.filter{ $0.value == sender }
         selectedStepperDictionary.forEach{ fruit, stepper in labelDictionary[fruit]?.text = String(Int(sender.value)) }
-    }
-    
-    @IBAction func touchConfirmButton(_ sender: UIButton) {
-        let alert = UIAlertController(title: "경고", message: "재고를 수정하시겠습니까?", preferredStyle: .alert)
-        let yesAction = UIAlertAction(title: "예", style: .default, handler: {_ in
-            self.changeStocks()
-            self.dismiss(animated: true, completion: nil)
-        })
-        let noAction = UIAlertAction(title: "아니오", style: .destructive, handler: nil)
-        alert.addAction(yesAction)
-        alert.addAction(noAction)
-        self.present(alert, animated: true, completion: nil)
     }
     
     func showStock() {
