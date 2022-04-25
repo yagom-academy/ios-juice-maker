ffa8805fba99477109da4cd7aa7e5e5a38e44fe2
MinsuJeon <chso294@naver.com>
2022-04-25 23:35:19 +0900

    feat: Fruit 열거형 및 재고 관련 에러 열거형 추가

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,20 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by 전민수 on 2022/04/25.
+//
+
+import Foundation
+
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
+
+enum stockError: Error {
+    case invalidSelection
+}
