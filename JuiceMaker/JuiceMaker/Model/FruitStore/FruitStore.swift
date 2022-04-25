64022f1cf405171e4783e83a60b3ae5e3d705ff4
derrickkim0109 <derrickkim0109@naver.com>
2022-04-25 19:39:14 +0900

    refactor : "FruitStore" 초기값 init으로 설정

--- JuiceMaker/JuiceMaker/Model/FruitStore/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore/FruitStore.swift
@@ -6,14 +6,16 @@
 
 import Foundation
 
-// 과일 저장소 타입
-class FruitStore {
-    var fruits: [Fruit:Int] = [.strawberry: 10,
-                               .banana: 10,
-                               .pineapple: 10,
-                               .kiwi: 10,
-                               .mango: 10]
-
 }
 
+//MARK: 과일 저장소 타입
+class FruitStore: Fruitable {
+    private var fruits: [Fruit:Int] = [:]
+    
+    init(initialValue: Int = 10) {
+        for fruit in Fruit.allCases {
+            self.fruits.updateValue(initialValue, forKey: fruit)
+        }
+    }
+}
 
