a3fcfe65dc03fecb9a8b56db96100162d5c18007
웡빙 <wb277@naver.com>
2022-04-25 16:44:30 +0900

    feat : "Fruit" Enum 생성, "FruitStore"에서 과일 초기 재고 설정

---
+++ JuiceMaker/JuiceMaker/Model/FruitStore/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by Derrik 웡빙 on 2022/04/25.
+//
+
+import Foundation
+
+enum Fruit {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case mango
+}
---
+++ JuiceMaker/JuiceMaker/Model/FruitStore/FruitStore.swift
@@ -0,0 +1,19 @@
+//
+//  JuiceMaker - FruitStore.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+// 과일 저장소 타입
+class FruitStore {
+    var fruits: [Fruit:Int] = [.strawberry: 10,
+                               .banana: 10,
+                               .pineapple: 10,
+                               .kiwi: 10,
+                               .mango: 10]
+
+}
+
+
