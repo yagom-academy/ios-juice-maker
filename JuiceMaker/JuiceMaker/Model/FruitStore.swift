4509fb8614fc1ca171d97921d4411bc3f2026297
bard <schy2503@naver.com>
2022-04-25 23:50:04 +0900

    feat: 과일과 재고를 저장할 딕셔너리 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,5 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    
+    var fruitDictionary: Dictionary<Fruit, Int> = [.strawberry : 10, .banana : 10, .pineapple : 10, .kiwi : 10, .mango : 10]
 }
