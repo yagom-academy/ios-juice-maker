3b58ac1171f24146e1547d1b3398c8879707165f
finnn1 <tgss3535@gmail.com>
2022-04-25 15:27:35 +0900

    feat: Fruit 열거형 생성 및 FruitStore 클래스 이니셜라이저 구현

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by LeeChiheon on 2022/04/25.
+//
+
+import Foundation
+
+enum Fruit: CaseIterable {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var fruitStock: [Fruit: Int] = [:]
     
+    init(quantity: Int) {
+        for fruit in Fruit.allCases {
+            fruitStock.updateValue(quantity, forKey: fruit)
+        }
+    }
 }
+
+
