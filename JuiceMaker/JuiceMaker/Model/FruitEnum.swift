6c0a8dfa35ec37614a46f78c6838619d04db0ccf
KayAhnDS <dkseotn123@gmail.com>
2022-04-25 15:28:57 +0900

    feat: FruitStore 수정 및 Fruit Enum 추가
    재고 변수 및 재고 수량 조정 함수 추가

---
+++ JuiceMaker/JuiceMaker/Model/FruitEnum.swift
@@ -0,0 +1,16 @@
+//
+//  FruitEnum.swift
+//  JuiceMaker
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+enum FruitKind {
+    case strawberry
+    case banana
+    case pineaple
+    case kiwi
+    case mango
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,25 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var stock: Dictionary<FruitKind, Int> =
+    [FruitKind.strawberry: 10,
+     FruitKind.banana: 10,
+     FruitKind.kiwi: 10,
+     FruitKind.pineaple : 10,
+     FruitKind.mango: 10]
     
+    func changeAmountFruit(fruitName: FruitKind, amount: Int) -> Void {
+        switch fruitName {
+        case .strawberry:
+            stock[FruitKind.strawberry] = amount
+        case .banana:
+            stock[FruitKind.banana] = amount
+        case .pineaple:
+            stock[FruitKind.pineaple] = amount
+        case .kiwi:
+            stock[FruitKind.kiwi] = amount
+        case .mango:
+            stock[FruitKind.mango] = amount
+        }
+    }
 }
