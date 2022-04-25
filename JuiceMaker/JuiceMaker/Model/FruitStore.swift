1c8a151c744c5da1519508d293bbb79731676991
MinsuJeon <chso294@naver.com>
2022-04-25 23:54:21 +0900

    feat: 입력한 수만큼 재고를 소모하는 함수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -11,12 +11,22 @@ class FruitStore {
     var fruitDictionary: Dictionary<Fruit, Int> = [.strawberry : 10, .banana : 10, .pineapple : 10, .kiwi : 10, .mango : 10]
     
     func changeStock(fruit: Fruit, stock: Int) {
-            fruitDictionary[fruit] = stock
-        }
+        fruitDictionary[fruit] = stock
+    }
     
     func addStock(fruit: Fruit, stock: Int) {
-         guard let fruitStock = fruitDictionary[fruit] else { return }
-         
-         fruitDictionary[fruit] = fruitStock + stock
-     }
+        guard let fruitStock = fruitDictionary[fruit] else { return }
+        
+        fruitDictionary[fruit] = fruitStock + stock
+    }
+    
+    func reduceStock(fruit: Fruit, stock: Int) {
+        guard let fruitStock = fruitDictionary[fruit] else { return }
+        
+        if fruitStock < stock {
+            print("재고가 부족합니다.")
+        } else {
+            fruitDictionary[fruit] = fruitStock - stock
+        }
+    }
 }
