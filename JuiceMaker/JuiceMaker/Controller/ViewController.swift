673e1a62fbc327450c44b12c47ae13fc2aef521a
finnn1 <tgss3535@gmail.com>
2022-04-25 18:42:50 +0900

    feat: 스토리보드에 뷰컨트롤러 레이블 및 버튼 연결, 기본적인 화면 업데이트 함수 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblStrawberryStock: UILabel!
+    @IBOutlet weak var lblBananaStock: UILabel!
+    @IBOutlet weak var lblPineappleStock: UILabel!
+    @IBOutlet weak var lblKiwiStock: UILabel!
+    @IBOutlet weak var lblMangoStock: UILabel!
+    @IBOutlet weak var btnStrawberryBananaOrder: UIButton!
+    @IBOutlet weak var btnMangoKiwiOrder: UIButton!
+    @IBOutlet weak var btnStrawberryOrder: UIButton!
+    @IBOutlet weak var btnBananaOrder: UIButton!
+    @IBOutlet weak var btnPineappleOrder: UIButton!
+    @IBOutlet weak var btnKiwiOrder: UIButton!
+    @IBOutlet weak var btnMangoOrder: UIButton!
+    
+    var order: JuiceMaker = JuiceMaker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showStock()
     }
 
-
+    func showStock() {
+        lblStrawberryStock.text = String(order.fruitStore.fruitStock[.strawberry] ?? 0)
+        lblBananaStock.text = String(order.fruitStore.fruitStock[.banana] ?? 0)
+        lblPineappleStock.text = String(order.fruitStore.fruitStock[.pineapple] ?? 0)
+        lblKiwiStock.text = String(order.fruitStore.fruitStock[.kiwi] ?? 0)
+        lblMangoStock.text = String(order.fruitStore.fruitStock[.mango] ?? 0)
+    }
+    
+    @IBAction func orderJuice(sender: UIButton) {
+        var juiceMenu: Menu = .mangoJuice
+        switch sender {
+        case btnStrawberryBananaOrder:
+            juiceMenu = .strawberryBananaJuice
+        case btnMangoKiwiOrder:
+            juiceMenu = .mangoKiwiJuice
+        case btnStrawberryOrder:
+            juiceMenu = .strawberryJuice
+        case btnBananaOrder:
+            juiceMenu = .bananaJuice
+        case btnPineappleOrder:
+            juiceMenu = .pineappleJuice
+        case btnKiwiOrder:
+            juiceMenu = .kiwiJuice
+        case btnMangoOrder:
+            juiceMenu = .mangoJuice
+        default:
+            break
+        }
+        
+        order.makeJuice(what: juiceMenu)
+        showStock()
+    }
+    
 }
 
