108a6869b85bf40953ee23f5f17262344579a008
MinsuJeon <chso294@naver.com>
2022-04-25 23:51:46 +0900

    feat: 입력한 재고의 수로 변경하는 함수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,4 +9,8 @@ import Foundation
 // 과일 저장소 타입
 class FruitStore {
     var fruitDictionary: Dictionary<Fruit, Int> = [.strawberry : 10, .banana : 10, .pineapple : 10, .kiwi : 10, .mango : 10]
+    
+    func changeStock(fruit: Fruit, stock: Int) {
+            fruitDictionary[fruit] = stock
+        }
 }
