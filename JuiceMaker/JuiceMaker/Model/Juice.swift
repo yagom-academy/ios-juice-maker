0c2031143267325212e8eb90fcffd7da9ed0473b
bard <schy2503@naver.com>
2022-04-26 00:07:48 +0900

    feat: 주스 파일 및 열거형 생성

---
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -0,0 +1,16 @@
+//
+//  Juice.swift
+//  JuiceMaker
+//
+//  Created by 김동용 on 2022/04/26.
+//
+
+enum Juice {
+    case strawberryJuice
+    case bananaJuice
+    case pineappleJuice
+    case kiwiJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
+}
