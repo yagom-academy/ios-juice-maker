00b63c51f4db94b27c8850f343a2ed191c61415f
minsson <mskaang@gmail.com>
2022-04-26 15:32:26 +0900

    feat: 쥬스 제작에 따라 과일을 소모하는 메서드, 과일을 공급하는 메서드 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -16,7 +16,8 @@ class FruitStore {
         case mango
     }
     
-    var fruitData: [Fruit: Int] = [:]
+    typealias FruitData = [Fruit: Int]
+    var fruitData: FruitData = [:]
     
     init() {
         let initialInventory = 10
@@ -24,4 +25,30 @@ class FruitStore {
             fruitData[element] = initialInventory
         }
     }
+    
+    func reduce(ingredient: FruitData) {
+        for fruit in ingredient.keys {
+            let amountOfSupply = ingredient[fruit] ?? 0
+            guard var fruitData = fruitData[fruit] else {
+                return
+            }
+            print("\(fruit) 사용전 재고: \(fruitData)")
+            fruitData -= amountOfSupply
+            print("\(fruit) 사용후 재고: \(fruitData)")
+        }
+    }
+    
+    func supplyStock(ingredient: FruitData) {
+        for fruit in ingredient.keys {
+            let amountOfIngredient = ingredient[fruit] ?? 0
+            guard var fruitData = fruitData[fruit] else {
+                return
+            }
+            print("\(fruit) 입고전 재고: \(fruitData)")
+            fruitData += amountOfIngredient
+            print("\(fruit) 입고후 재고: \(fruitData)")
+        }
+    }
 }
+
+
---
+++ JuiceMaker/JuiceMaker/Model/Menu.swift
@@ -0,0 +1,8 @@
+//
+//  Menu.swift
+//  JuiceMaker
+//
+//  Created by Minseong Kang on 2022/04/25.
+//
+
+import Foundation
