74664cd5a6bb7fe44cab8f81e34ec79d4496dfd0
yeeton37 <lyeeun37@naver.com>
2022-04-26 15:14:27 +0900

    feat: 재고 수량 변경하는 메서드(decreaseStock) 선언
    메서드(decreaseStock): 주문 받았을 때 재고 수량을 감소시킴

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -10,4 +10,10 @@ import Foundation
 class FruitStore {
     var stock: [Fruits: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
     private let orderQuantity: [Fruits: Int] = [.strawberry: 80, .banana: 10, .pineapple: 10, .kiwi: 15, .mango: 15]
+    
+    func decreaseStock(menu: Menu, numberOfOrder: Int) {
+        for element in menu.recipe.keys {
+            stock[element]! -= menu.recipe[element]! * numberOfOrder
+        }
+    }
 }
