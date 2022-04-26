7ae6732718fc9d459661a9916ddc78af1569eb8c
seohyeon2 <seo_hyeon418@naver.com>
2022-04-26 14:10:51 +0900

    feat: 과일타입 정의 및 초기 재고를 10개로 초기화 하는 init 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,20 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    enum Fruit: CaseIterable {
+        case strawberry
+        case banana
+        case kiwi
+        case pineapple
+        case mango
+    }
     
+    var fruitData: [Fruit: Int] = [:]
+    
+    init() {
+        let initialInventory = 10
+        for element in Fruit.allCases {
+            fruitData[element] = initialInventory
+        }
+    }
 }
