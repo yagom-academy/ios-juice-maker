32b02cdd7db6a088ad11aac4444e835bad397dd2
gjrjs31@naver.com <gjrjs31@naver.com>
2022-04-26 18:18:10 +0900

    feat: 쥬스메이커와 FruitStore 열거형 추가, 초기 값, 쥬스 종류 별 필요한 재료 함수 작성

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,17 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by 허건 on 2022/04/26.
+//
+
+import Foundation
+
+enum Fruit: String {
+    case strawberry = "딸기"
+    case banana = "바나나"
+    case pineapple = "파인애플"
+    case kiwi = "키위"
+    case mango = "망고"
+    
+}
