cd171caf625097f8cf2383875a666aeb99d12d6e
iilim <42.4.ilim@gmail.com>
2022-04-26 12:24:32 +0900

    feat: 과일 갯수 관리 클래스 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,20 @@
 
 import Foundation
 
-// 과일 저장소 타입
 class FruitStore {
-    
+    var strawberry = 10
+    var banana = 10
+    var pineapple = 10
+    var kiwi = 10
+    var mango = 10
+}
+
+extension FruitStore {
+    func useFruits(strawberry: Int = 0, banana: Int = 0, pineapple: Int = 0, kiwi: Int = 0, mango: Int = 0,) {
+        self.strawberry -= strawberry
+        self.banana -= banana
+        self.pineapple -= pineapple
+        self.kiwi -= kiwi
+        self.mango -= mango
+    }
 }
