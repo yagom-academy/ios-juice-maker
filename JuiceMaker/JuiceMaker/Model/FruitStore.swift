7ebb72831caaa52e0c7174eaae9b1e4fae99d2ee
MinsuJeon <chso294@naver.com>
2022-04-26 10:13:06 +0900

    refactor: 매직 넘버 및 매직 스트링 제거

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,7 +8,11 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    var fruitDictionary: Dictionary<Fruit, Int> = [.strawberry : 10, .banana : 10, .pineapple : 10, .kiwi : 10, .mango : 10]
+    enum InitialSetting {
+        static let stock = 10
+    }
+
+    var fruitDictionary: Dictionary<Fruit, Int> = [.strawberry : InitialSetting.stock, .banana : InitialSetting.stock, .pineapple : InitialSetting.stock, .kiwi : InitialSetting.stock, .mango : InitialSetting.stock]
     
     func changeStock(fruit: Fruit, stock: Int) {
         fruitDictionary[fruit] = stock
@@ -16,17 +20,19 @@ class FruitStore {
     
     func addStock(fruit: Fruit, stock: Int) {
         guard let fruitStock = fruitDictionary[fruit] else { return }
+        let changedStock = fruitStock + stock
         
-        fruitDictionary[fruit] = fruitStock + stock
+        fruitDictionary[fruit] = changedStock
     }
     
     func reduceStock(fruit: Fruit, stock: Int) {
         guard let fruitStock = fruitDictionary[fruit] else { return }
+        let changedStock = fruitStock - stock
         
         if fruitStock < stock {
             print("재고가 부족합니다.")
         } else {
-            fruitDictionary[fruit] = fruitStock - stock
+            fruitDictionary[fruit] = changedStock
         }
     }
 }
