a3d4e6ebccf5a0f521c95a7fbee0383b045f8fa3
OldStarKR <tyr2293@naver.com>
2022-04-26 16:46:29 +0900

    refactor: 주스 제작 실패 구현 및 흐름에 맞게 메서드 수정

---
+++ JuiceMaker/JuiceMaker/Model/FruitJuice.swift
@@ -0,0 +1,16 @@
+//
+//  FruitJuice.swift
+//  JuiceMaker
+//
+//  Created by Gordon Choi on 2022/04/26.
+//
+
+enum FruitJuice: String {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case strawberryAndBanana
+    case mango
+    case mangoAndKiwi
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -4,23 +4,63 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
 class FruitStore {
-    var strawberry = 10
-    var banana = 10
-    var pineapple = 10
-    var kiwi = 10
-    var mango = 10
+    private var strawberry = 10
+    private var banana = 10
+    private var pineapple = 10
+    private var kiwi = 10
+    private var mango = 10
 }
 
 extension FruitStore {
-    func useFruits(strawberry: Int = 0, banana: Int = 0, pineapple: Int = 0, kiwi: Int = 0, mango: Int = 0) {
+    
+    func pickUpFruits(for menu: FruitJuice) -> PickUpFruitResult {
+        var selectedStrawberry = 0
+        var selectedBanana = 0
+        var selectedPineapple = 0
+        var selectedKiwi = 0
+        var selectedMango = 0
+        
+        switch menu {
+        case .strawberry:
+            selectedStrawberry += 16
+        case .banana:
+            selectedBanana += 2
+        case .kiwi:
+            selectedKiwi += 3
+        case .pineapple:
+            selectedPineapple += 2
+        case .strawberryAndBanana:
+            selectedStrawberry += 10
+            selectedBanana += 1
+        case .mango:
+            selectedMango += 3
+        case .mangoAndKiwi:
+            selectedKiwi += 1
+            selectedMango += 2
+        }
+        
+        return useFruits(strawberry: selectedStrawberry,
+                               banana: selectedBanana,
+                               pineapple: selectedPineapple,
+                               kiwi: selectedKiwi,
+                               mango: selectedMango)
+    }
+    
+    private func useFruits(strawberry: Int, banana: Int, pineapple: Int, kiwi: Int, mango: Int) -> PickUpFruitResult {
+        guard self.strawberry >= strawberry,
+              self.banana >= banana,
+              self.pineapple >= pineapple,
+              self.kiwi >= kiwi,
+              self.mango >= mango else {
+            return PickUpFruitResult.failure
+        }
+        
         self.strawberry -= strawberry
         self.banana -= banana
         self.pineapple -= pineapple
         self.kiwi -= kiwi
         self.mango -= mango
+        return PickUpFruitResult.success
     }
 }
-
