7be94f50c4b136cd5f1ee245e05730e0ab812909
starting1231 <starting1231@naver.com>
2022-04-26 15:16:50 +0900

    feat: Create FruitStore in property, function
    - fruitInventory property
    - changeInventory function
    - checkGenerationAvailable function

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,16 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var fruitInventory: [Fruit:Int] = [:]
     
+    init() {
+        fruitInventory = [.strawberry: 10, .banana: 10, .kiwi: 10, .pineapple: 10, .mango: 10]
+    }
+    
+    func changeInventory(fruit: [Fruit:Int]) {
+    }
+    
+    func checkGenerationAvailable(fruit: [Fruit:Int]) throws -> Bool {
+        return true
+    }
 }
