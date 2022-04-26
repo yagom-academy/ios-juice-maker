e6b3f0813207e587f3f7d22e29e3e4a5fb934915
웡빙 <wb277@naver.com>
2022-04-26 11:34:36 +0900

    feat : "JuiceMaker" 폴더 생성, "Juice" Enum 생성 및 Juice의 재료값 리턴 함수 구현

---
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker/Juice.swift
@@ -0,0 +1,39 @@
+//
+//  Juice.swift
+//  JuiceMaker
+//
+//  Created by 이원빈 on 2022/04/26.
+//
+
+import Foundation
+
+enum Juice {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case mangoJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoKiwiJuice
+}
+
+extension Juice {
+    func isRequiredIngrediants() -> [Fruit: Int] {
+        switch self {
+        case .strawberryJuice:
+            return [.strawberry : 16]
+        case .bananaJuice:
+            return [.banana : 2]
+        case .kiwiJuice:
+            return [.kiwi : 3]
+        case .mangoJuice:
+            return [.mango : 3]
+        case .pineappleJuice:
+            return [.pineapple : 2]
+        case .strawberryBananaJuice:
+            return [.strawberry : 10, .banana : 1]
+        case .mangoKiwiJuice:
+            return [.mango : 2, .kiwi : 1]
+        }
+    }
+}
---
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker/JuiceMaker.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceMaker - JuiceMaker.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import Foundation
+
+// 쥬스 메이커 타입
+struct JuiceMaker {
+    let fruitStore = FruitStore()
+    
+    func make(of juice: ) {
+        
+    }
+    
+}
