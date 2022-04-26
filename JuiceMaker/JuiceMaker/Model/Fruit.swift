1c361bda9e8fe0cf805007e57573dc963ae85c0b
yusw10 <yusw10@naver.com>
2022-04-26 15:04:04 +0900

    add: Create Fruit Enum

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,14 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by groot, borysarng on 2022/04/26.
+//
+
+enum Fruit {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case mango
+}
