e29f7a52d59658a941db2a64b4a1fd71f943b46e
Hugh-github <tnghk4822@gmail.com>
2022-04-26 15:34:52 +0900

    feat: 메서드(checkStock) 선언
    재고가 쥬스 제작에 필요한 개수보다 많으면 감소시키고 true를 리턴해주고 적으면 재고를 채워주고 false를 리턴한다.

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -11,9 +11,20 @@ class FruitStore {
     var stock: [Fruits: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
     private let orderQuantity: [Fruits: Int] = [.strawberry: 80, .banana: 10, .pineapple: 10, .kiwi: 15, .mango: 15]
     
+    func checkStock(menu: Menu, numberOfOrder: Int) -> Bool {
+        for (fruit, need) in menu.recipe {
+            if stock[fruit]! >= need * numberOfOrder {
+                decreaseStock(menu: menu, numberOfOrder: numberOfOrder)
+                return true
+            }
+        }
+        // fillStock 함수 추가
+        return false
+    }
+    
     func decreaseStock(menu: Menu, numberOfOrder: Int) {
-        for element in menu.recipe.keys {
-            stock[element]! -= menu.recipe[element]! * numberOfOrder
+        for (fruit, need) in menu.recipe {
+            stock[fruit]! -= need * numberOfOrder
         }
     }
 }
