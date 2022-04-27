bc706cf354c9a1db3f4aada1b5d4904fcc26f6cd
yeeton37 <lyeeun37@naver.com>
2022-04-27 14:28:45 +0900

    refactor: 메서드(checkStock, classifyKey)의 접근제어 수준 변경

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,7 +7,7 @@
 struct JuiceMaker: BindingOptional {
     private var store = FruitStore()
         
-    func checkStock(menu: Menu, total: Int) throws {
+    private func checkStock(menu: Menu, total: Int) throws {
         for (fruit, need) in menu.recipe {
             guard unwrapOptional(store.stock[fruit]) >= need * total else { throw JuiceMakerError.lackOfStock }
         }
@@ -22,7 +22,7 @@ struct JuiceMaker: BindingOptional {
         }
     }
     
-    func classifyKey(from juice: Menu) -> [Fruits] {
+    private func classifyKey(from juice: Menu) -> [Fruits] {
         var keys: [Fruits] = []
         
         for keyValue in juice.recipe.keys {
