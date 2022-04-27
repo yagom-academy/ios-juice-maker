b622281f619e89d9e5a8b60b26f7d9fb380b9962
MinsuJeon <chso294@naver.com>
2022-04-27 14:15:50 +0900

    refactor: JuiceMaker 구조체 내 checkStock() 함수와 handle() 함수를 FruitStore Class 내부로 이동

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -48,4 +48,38 @@ class FruitStore {
         
         fruitRecipe[fruit] = changedStock
     }
+    
+    func checkStock(function: (Fruit, Int) throws -> Void, juice: Product) rethrows {
+        switch juice {
+        case Product.strawberryJuice:
+            try function(Fruit.strawberry, CostOfJuiceProduction.strawberryInStrawberryJuice)
+        case Product.bananaJuice:
+            try function(Fruit.banana, CostOfJuiceProduction.bananaInBananaJuice)
+        case Product.kiwiJuice:
+            try function(Fruit.kiwi, CostOfJuiceProduction.kiwiInKiwiJuice)
+        case Product.pineappleJuice:
+            try function(Fruit.pineapple, CostOfJuiceProduction.pineappleInPineappleJuice)
+        case Product.strawberryBananaJuice:
+            try function(Fruit.strawberry, CostOfJuiceProduction.strawberryInStrawberryBananaJuice)
+            try function(Fruit.banana, CostOfJuiceProduction.bananaInStrawberryBananaJuice)
+        case Product.mangoJuice:
+            try function(Fruit.mango, CostOfJuiceProduction.mangoInMangoJuice)
+        case Product.mangoKiwiJuice:
+            try function(Fruit.mango, CostOfJuiceProduction.mangoInMangoKiwiJuice)
+            try function(Fruit.kiwi, CostOfJuiceProduction.kiwiInMangoKiwiJuice)
+        }
+    }
+    
+    func handle(_ error: Error) {
+        switch error {
+        case JuiceMakerError.outOfStock:
+            print(JuiceMakerError.outOfStock.localizedDescription)
+        case JuiceMakerError.missingProduct:
+            print(JuiceMakerError.missingProduct.localizedDescription)
+        case JuiceMakerError.invalidAmount:
+            print(JuiceMakerError.invalidAmount.localizedDescription)
+        default:
+            print("알 수 없는 오류입니다")
+        }
+    }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -10,43 +10,9 @@ struct JuiceMaker {
     
     func make(_ juice: Product) {
         do {
-            try checkStock(function: juiceMakerStore.reduceStock(fruit:amount:), juice: juice)
+            try juiceMakerStore.checkStock(function: juiceMakerStore.reduceStock(fruit:amount:), juice: juice)
         } catch (let error) {
-            handle(error)
-        }
-    }
-    
-    private func checkStock(function: (Fruit, Int) throws -> Void, juice: Product) rethrows {
-        switch juice {
-        case Product.strawberryJuice:
-            try function(Fruit.strawberry, CostOfJuiceProduction.strawberryInStrawberryJuice)
-        case Product.bananaJuice:
-            try function(Fruit.banana, CostOfJuiceProduction.bananaInBananaJuice)
-        case Product.kiwiJuice:
-            try function(Fruit.kiwi, CostOfJuiceProduction.kiwiInKiwiJuice)
-        case Product.pineappleJuice:
-            try function(Fruit.pineapple, CostOfJuiceProduction.pineappleInPineappleJuice)
-        case Product.strawberryBananaJuice:
-            try function(Fruit.strawberry, CostOfJuiceProduction.strawberryInStrawberryBananaJuice)
-            try function(Fruit.banana, CostOfJuiceProduction.bananaInStrawberryBananaJuice)
-        case Product.mangoJuice:
-            try function(Fruit.mango, CostOfJuiceProduction.mangoInMangoJuice)
-        case Product.mangoKiwiJuice:
-            try function(Fruit.mango, CostOfJuiceProduction.mangoInMangoKiwiJuice)
-            try function(Fruit.kiwi, CostOfJuiceProduction.kiwiInMangoKiwiJuice)
-        }
-    }
-    
-    private func handle(_ error: Error) {
-        switch error {
-        case JuiceMakerError.outOfStock:
-            print(JuiceMakerError.outOfStock.localizedDescription)
-        case JuiceMakerError.missingProduct:
-            print(JuiceMakerError.missingProduct.localizedDescription)
-        case JuiceMakerError.invalidAmount:
-            print(JuiceMakerError.invalidAmount.localizedDescription)
-        default:
-            print("알 수 없는 오류입니다")
+            juiceMakerStore.handle(error)
         }
     }
 }
