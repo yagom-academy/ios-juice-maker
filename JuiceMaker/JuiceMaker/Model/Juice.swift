c75eec52de61bcfad750af4336a4c78abf57d384
ic_6_user_28 <toskw@naver.com>
2022-04-28 14:11:52 +0900

    refactor: 주스 열거형 수정 및 들여쓰기

--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -8,32 +8,30 @@
 import Foundation
 
 enum Juice {
-    case strawberryJuice
-    case bananaJuice
-    case pineappleJuice
-    case kiwiJuice
-    case mangoJuice
-    case strawberryBananaJuice
-    case mangoKiwiJuice
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+    case strawberryBanana
+    case mangoKiwi
 
-
-
-var recipe: [Fruits : Int] {
-    switch self {
-    case .strawberryJuice:
-        return [.strawberry: 16]
-    case .bananaJuice:
-        return [.banana: 2]
-    case .kiwiJuice:
-        return [.kiwi: 3]
-    case .pineappleJuice:
-        return [.pineapple: 2]
-    case .mangoJuice:
-        return [.mango: 3]
-    case .strawberryBananaJuice:
-        return [.strawberry: 10, .banana: 1]
-    case .mangoKiwiJuice:
-        return [.mango: 2, .kiwi: 1]
+        var recipe: [Fruits : Int] {
+            switch self {
+            case .strawberry:
+                return [.strawberry: 16]
+            case .banana:
+                return [.banana: 2]
+            case .kiwi:
+                return [.kiwi: 3]
+            case .pineapple:
+                return [.pineapple: 2]
+            case .mango:
+                return [.mango: 3]
+            case .strawberryBanana:
+                return [.strawberry: 10, .banana: 1]
+            case .mangoKiwi:
+                return [.mango: 2, .kiwi: 1]
     }
     
 }
