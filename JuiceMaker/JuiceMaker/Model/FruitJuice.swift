bc928a4e0de74e8dfca431c59b8289dbcb3c5a26
OldStarKR <tyr2293@naver.com>
2022-04-28 18:10:38 +0900

    refactor: 과일 쥬스 자료형에 rawValue 추가

--- JuiceMaker/JuiceMaker/Model/FruitJuice.swift
+++ JuiceMaker/JuiceMaker/Model/FruitJuice.swift
@@ -1,11 +1,11 @@
 enum FruitJuice: String {
-    case strawberry
-    case banana
-    case kiwi
-    case pineapple
-    case strawberryAndBanana
-    case mango
-    case mangoAndKiwi
+    case strawberry = "딸기"
+    case banana = "바나나"
+    case kiwi = "키위"
+    case pineapple = "파인애플"
+    case strawberryAndBanana = "딸기 바나나"
+    case mango = "망고"
+    case mangoAndKiwi = "망고 키위"
     
     func chooseRecipe() -> Fruits {
         switch self {
