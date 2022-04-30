44c366c15af63aaff1104c8c673dd1f01b355a64
apwierk2451 <apwierk2451@gmail.com>
2022-04-30 18:02:20 +0900

    ################
    # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지
    # 예) feat : 로그인 기능 추가
    refact: FruitStore에 선언해 둔 함수 JuiceMaker로 옮기는 작업
    함수의 매개변수 변경
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -10,7 +10,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.        
+        // Do any additional setup after loading the view.
+    }
+    var juiceMaker = JuiceMaker()
+    
+    func btnEvent(juice: Juice) {
+        do {
+            try juiceMaker.makeJuice(of: juice)
+        } catch StockError.outOfStock {
+            print("재고 부족")
+        } catch {
+            print("알 수 없는 에러")
+        }
     }
 }
-
--- JuiceMaker/JuiceMaker/Model/Fruit/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/Fruit/FruitStore.swift
@@ -6,32 +6,9 @@
 //
 
 class FruitStore {
-    
     var stocks: [Fruit: Int] = [:]
     
     init(defaultFruitStock: Int) {
         Fruit.allCases.forEach { stocks[$0] = defaultFruitStock }
-    }
-
-    func chooseRecipe(order: Juice) throws {
-        for fruit in order.recipeOfJuice.keys {
-            try checkFruitStock(juice: order, fruits: fruit)
-        }
-    }
-
-    private func checkFruitStock(juice: Juice, fruits: Fruit) throws {
-        guard let stockFruit = stocks[fruits],
-              let needFruitAmount = juice.recipeOfJuice[fruits],
-              stockFruit >= needFruitAmount else {
-            throw StockError.outOfStock
-        }
-        useStock(juice: juice, fruits: fruits)
-    }
-    
-    private func useStock(juice: Juice, fruits: Fruit) {
-        if let stockFruit = stocks[fruits],
-           let requiredFruit = juice.recipeOfJuice[fruits] {
-            stocks.updateValue(stockFruit - requiredFruit, forKey: fruits)
-        }
     }
 }
