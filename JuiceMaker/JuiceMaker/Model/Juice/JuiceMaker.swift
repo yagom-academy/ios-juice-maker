3e256a5e6e1e776cbd57d2e957428bcde4ca8632
apwierk2451 <apwierk2451@gmail.com>
2022-04-30 18:02:20 +0900

    refact: FruitStore에 선언해 둔 함수 JuiceMaker로 옮기는 작업

--- JuiceMaker/JuiceMaker/Model/Juice/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/Juice/JuiceMaker.swift
@@ -6,14 +6,35 @@
 
 struct JuiceMaker {
     let fruitStore = FruitStore(defaultFruitStock: 10)
+    var insufficientStock: [Fruit:Int] = [:]
     
-    func makeJuice(of juice: Juice){
-        do {
-            try fruitStore.chooseRecipe(order: juice)
-        } catch StockError.outOfStock {
-            print("재고부족")
-        } catch {
-            print(error)
+    mutating func makeJuice(of juice: Juice) throws {
+        try chooseRecipe(order: juice)
+    }
+    
+    private mutating func chooseRecipe(order: Juice) throws {
+        for (fruit, fruitAmount) in order.recipeOfJuice {
+            try checkFruitStock(fruit: fruit, amount: fruitAmount)
+        }
+        if insufficientStock.isEmpty {
+            useStock(juice: order)
+        } else {
+            throw StockError.outOfStock
+        }
+    }
+    
+    private mutating func checkFruitStock(fruit: Fruit, amount: Int) throws {
+        guard let stockFruit = fruitStore.stocks[fruit] else { throw StockError.outOfStock }
+        guard stockFruit >= amount else {
+            return insufficientStock[fruit] = amount - stockFruit
+        }
+    }
+    
+    private func useStock(juice: Juice) {
+        for (fruit, fruitAmount) in juice.recipeOfJuice {
+            if let stockFruit = fruitStore.stocks[fruit] {
+                fruitStore.stocks.updateValue(stockFruit - fruitAmount, forKey: fruit)
+            }
         }
     }
 }
