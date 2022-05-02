0b067eb87dac11596cd81e314d6c2881419ecb27
starting1231 <starting1231@naver.com>
2022-05-02 14:40:53 +0900

    add: Create FruitStoreViewController
    - update naming to JuiceMakerViewController from ViewController

---
+++ JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  JuiceMaker
+//
+//  Created by NAMU on 2022/05/02.
+//
+
+import UIKit
+
+class FruitStoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
