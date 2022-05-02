41fdbc5f208fa21bdf44df3a8d135b1a33173327
derrickkim0109 <derrickkim0109@naver.com>
2022-05-02 15:52:01 +0900

    feat : "JuiceViewController" 버튼 Tag 사용하여 JuiceType 찾는 기능 구현

--- JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
@@ -7,22 +7,26 @@
 import UIKit
 
 class JuiceViewController: UIViewController {
-    
     @IBOutlet weak var strawberryLabel: UILabel!
     @IBOutlet weak var bananaLabel: UILabel!
     @IBOutlet weak var pineappleLabel: UILabel!
     @IBOutlet weak var kiwiLabel: UILabel!
     @IBOutlet weak var mangoLabel: UILabel!
     
+    let juiceMaker = JuiceMaker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     
     @IBAction func orderJuice(_ sender: UIButton) {
+        guard let orderedJuice = JuiceType(rawValue: sender.tag) else {
+            return
+        }
 
+        let juice = Drink(juice: orderedJuice)
+        juiceMaker.make(juice)
     }
-    
 }
 
