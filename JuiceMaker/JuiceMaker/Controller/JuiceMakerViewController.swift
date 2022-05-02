148eee8ba8f59fb8b98225bf49aad4620952b74c
starting1231 <starting1231@naver.com>
2022-05-02 16:24:32 +0900

    feat: Create @IBOutlet, @IBAction, implement function
    - create FruitStockLabel
    - create FruitjuiceButton
    - implement orderFruitJuice function

--- JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
@@ -7,12 +7,45 @@
 import UIKit
 
 class JuiceMakerViewController: UIViewController {
-
+    
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    @IBOutlet weak var strawberryBananaJuiceButton: UIButton!
+    @IBOutlet weak var mangoKiwiJuiceButton: UIButton!
+    @IBOutlet weak var strawberryJuiceButton: UIButton!
+    @IBOutlet weak var bananaJuiceButton: UIButton!
+    @IBOutlet weak var pineappleJuiceButton: UIButton!
+    @IBOutlet weak var kiwiJuiceButton: UIButton!
+    @IBOutlet weak var mangoJuiceButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    @IBAction func orderFruitJuice(_ sender: UIButton) {
+        switch sender {
+        case strawberryBananaJuiceButton:
+            print("StrawberryBananaJuice")
+        case mangoKiwiJuiceButton:
+            print("MangoKiwiJuice")
+        case strawberryJuiceButton:
+            print("StrawberryJuice")
+        case bananaJuiceButton:
+            print("BananaJuice")
+        case pineappleJuiceButton:
+            print("PineappleJuice")
+        case kiwiJuiceButton:
+            print("KiwiJuice")
+        case mangoJuiceButton:
+            print("MangoJuice")
+        default:
+            break
+        }
+    }
 }
 
