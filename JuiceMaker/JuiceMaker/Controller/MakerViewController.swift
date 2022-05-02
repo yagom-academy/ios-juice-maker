a982746c3a4b46a32d5336ea98dbf0571122319b
KayAhnDS <dkseotn123@gmail.com>
2022-05-02 14:11:14 +0900

    feat: ViewController 파일 추가 및 스토리보드 수정

---
+++ JuiceMaker/JuiceMaker/Controller/MakerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MakerViewController.swift
+//  JuiceMaker
+//
+//  Created by Kay on 2022/05/02.
+//
+
+import UIKit
+
+class MakerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpDissmissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func touchUpCompleteButton(_ sender: UIButton) {
+        showAlert(message: "Coming soon!")
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK",
+                                     style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
 }
+
 
