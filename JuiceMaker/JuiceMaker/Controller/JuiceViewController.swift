a206584b83eb94140226a0838709d19740d2e896
웡빙 <wb277@naver.com>
2022-05-02 14:59:53 +0900

    feat : "JuiceViewController" & "StoreViewController" 생성 및 프로퍼티 연결

---
+++ JuiceMaker/JuiceMaker/Controller/JuiceViewController.swift
@@ -0,0 +1,28 @@
+//
+//  JuiceMaker - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class JuiceViewController: UIViewController {
+    
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func orderJuice(_ sender: UIButton) {
+
+    }
+    
+}
+
---
+++ JuiceMaker/JuiceMaker/Controller/StoreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StoreViewController.swift
+//  JuiceMaker
+//
+//  Created by 이원빈 on 2022/05/02.
+//
+
+import UIKit
+
+class StoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.topItem?.backButtonTitle = "뒤로가기"
+    }
+
+
+}
