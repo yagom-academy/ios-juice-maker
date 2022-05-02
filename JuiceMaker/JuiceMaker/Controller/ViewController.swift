769f9dc3578f79a1fb7bf4bb03767f6b4f2ed240
unchain123 <unchain123@kakao.com>
2022-05-02 14:18:08 +0900

    스토리보드 레이블 및 버튼 연결 함수 생성

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStock: UILabel!
+    @IBOutlet weak var bananaStock: UILabel!
+    @IBOutlet weak var pineappleStock: UILabel!
+    @IBOutlet weak var kiwiStock: UILabel!
+    @IBOutlet weak var mangoStock: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    
+    @IBAction func orderStrawberryBananaJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderMangoKiwiJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderStrawberryJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderBananaJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderPineapplerJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderKiwiJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderMangoJuice(_ sender: UIButton) {
+    }
+    
 }
 
