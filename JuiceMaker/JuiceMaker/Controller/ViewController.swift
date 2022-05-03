ad879722a72666adf2a67ec6ed7b0512b38a8412
gjrjs31@naver.com <gjrjs31@naver.com>
2022-05-03 16:00:15 +0900

    feat: alert 이벤트 추가, alert 특정 버튼 클릭 시 재고 수정 ViewController로 이동 기능 구현, 과일 순서 수정

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,37 +7,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var juiceMaker = JuiceMaker()
 
     @IBOutlet weak var strawberryLabel: UILabel!
     @IBOutlet weak var bananaLabel: UILabel!
-    @IBOutlet weak var pineappleLabel: UILabel!
     @IBOutlet weak var kiwiLabel: UILabel!
     @IBOutlet weak var mangoLabel: UILabel!
-    
+    @IBOutlet weak var pineappleLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         updateStock()
     }
     
-    @IBAction func strawberrybananaButton(_ sender: UIButton) {
-        buttonEvent(juice: .strawberryBanana)
-        updateStock()
-    }
-    @IBAction func mangokiwiButton(_ sender: UIButton) {
-        buttonEvent(juice: .mangoKiwi)
-        updateStock()
-    }
     @IBAction func strawberryButton(_ sender: UIButton) {
         buttonEvent(juice: .strawberry)
         updateStock()
     }
     @IBAction func bananaButton(_ sender: UIButton) {
         buttonEvent(juice: .banana)
-        updateStock()
-    }
-    @IBAction func pineappleButton(_ sender: UIButton) {
-        buttonEvent(juice: .pineapple)
         updateStock()
     }
     @IBAction func kiwiButton(_ sender: UIButton) {
@@ -48,32 +36,61 @@ class ViewController: UIViewController {
         buttonEvent(juice: .mango)
         updateStock()
     }
+    @IBAction func pineappleButton(_ sender: UIButton) {
+        buttonEvent(juice: .pineapple)
+        updateStock()
+    }
+    @IBAction func strawberrybananaButton(_ sender: UIButton) {
+        buttonEvent(juice: .strawberryBanana)
+        updateStock()
+    }
+    @IBAction func mangokiwiButton(_ sender: UIButton) {
+        buttonEvent(juice: .mangoKiwi)
+        updateStock()
+    }
     
     func updateStock() {
         guard let strawberry = juiceMaker.fruitStore.stocks[.strawberry],
               let banana = juiceMaker.fruitStore.stocks[.banana],
-              let pineapple = juiceMaker.fruitStore.stocks[.pineapple],
               let kiwi = juiceMaker.fruitStore.stocks[.kiwi],
-              let mango = juiceMaker.fruitStore.stocks[.mango] else {
+              let mango = juiceMaker.fruitStore.stocks[.mango],
+              let pineapple = juiceMaker.fruitStore.stocks[.pineapple]
+        else {
             return
         }
         strawberryLabel.text = "\(strawberry)"
         bananaLabel.text = "\(banana)"
-        pineappleLabel.text = "\(pineapple)"
         kiwiLabel.text = "\(kiwi)"
         mangoLabel.text = "\(mango)"
+        pineappleLabel.text = "\(pineapple)"
     }
-    
-    var juiceMaker = JuiceMaker()
     
     func buttonEvent(juice: Juice) {
         do {
             try juiceMaker.makeJuice(of: juice)
+            alertSuccessMakeJuice(juice: juice)
         } catch StockError.outOfStock {
-            print("재고 부족")
+            alertFailMakeJuice()
             juiceMaker.fruitStore.insufficientStock.removeAll()
+        } catch StockError.defaultError {
+            print("defaultError!!")
         } catch {
-            print("알 수 없는 에러")
+            print("알 수 없는 에러!!")
         }
+    }
+    
+    func alertSuccessMakeJuice(juice: Juice) {
+        let eventSuccess = UIAlertController(title: "성공!", message: "\(juice.name)쥬스 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+        
+        eventSuccess.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(eventSuccess, animated: false)
+    }
+    
+    func alertFailMakeJuice() {
+        let eventFail = UIAlertController(title: "실패", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
+        
+        eventFail.addAction(UIAlertAction(title: "예", style: .default, handler: {_ in self.performSegue(withIdentifier: "moveToFruitStock", sender: nil)}))
+        eventFail.addAction(UIAlertAction(title: "아니요", style: .destructive, handler: nil))
+        self.present(eventFail, animated: false)
     }
 }
--- JuiceMaker/JuiceMaker/Model/Juice/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice/Juice.swift
@@ -8,28 +8,47 @@
 enum Juice: CaseIterable {
     case strawberry
     case banana
-    case strawberryBanana
-    case mango
-    case mangoKiwi
     case kiwi
+    case mango
     case pineapple
+    case strawberryBanana
+    case mangoKiwi
 
+    var name: String {
+        switch self {
+        case .strawberry:
+            return "딸기"
+        case .banana:
+            return "바나나"
+        case .kiwi:
+            return "키위"
+        case .mango:
+            return "망고"
+        case .pineapple:
+            return "파인애플"
+        case .strawberryBanana:
+            return "딸바"
+        case .mangoKiwi:
+            return "망키"
+        }
+    }
+    
     var recipe: [Fruit: Int] {
         switch self {
         case .strawberry:
             return [.strawberry: 16]
         case .banana:
             return [.banana: 2]
-        case .strawberryBanana:
-            return [.strawberry: 10, .banana: 1]
-        case .mango:
-            return [.mango: 3]
-        case .mangoKiwi:
-            return [.mango: 2, .kiwi: 1]
         case .kiwi:
             return [.kiwi: 3]
+        case .mango:
+            return [.mango: 3]
         case .pineapple:
             return [.pineapple: 2]
+        case .strawberryBanana:
+            return [.strawberry: 10, .banana: 1]
+        case .mangoKiwi:
+            return [.mango: 2, .kiwi: 1]
         }
     }
 }
