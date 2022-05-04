0c24f6a99fe7e6a6aafe9c5e3b628fafef73b2c5
MinsuJeon <chso294@naver.com>
2022-05-04 13:33:34 +0900

    style: 네이밍 변경, 주석 제거 및 줄바꿈 처리

--- JuiceMaker/JuiceMaker/Controller/JuiceOrderViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceOrderViewController.swift
@@ -1,16 +1,16 @@
 //
-//  JuiceMaker - ViewController.swift
+//  JuiceMaker - JuiceOrderViewController.swift
 //  Created by 수꿍, 바드. 
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import UIKit
 
-class ViewController: UIViewController {
+class JuiceOrderViewController: UIViewController {
     
     private let juiceMaker = JuiceMaker()
     
-    @IBOutlet weak var strawberryLabel: UILabel?
+    @IBOutlet private weak var strawberryLabel: UILabel?
     @IBOutlet weak var bananaLabel: UILabel?
     @IBOutlet weak var pineappleLabel: UILabel?
     @IBOutlet weak var kiwiLabel: UILabel?
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         updateFruitStock()
     }
     
@@ -30,7 +29,7 @@ class ViewController: UIViewController {
         mangoLabel?.text = String(juiceMaker.fruitStore.fruitWarehouse[.mango] ?? 0)
     }
     
-    @IBAction func makeJuiceButton(_ sender: UIButton) {
+    @IBAction func makeJuice(_ sender: UIButton) {
         guard let juice = Juice(rawValue: sender.currentTitle ?? "") else { return }
         
         make(juice)
@@ -49,33 +48,29 @@ class ViewController: UIViewController {
     }
 
     private func showSuccessAlert(_ juice: Juice) {
-        let successAlert = UIAlertController(title: "\(juice.name) 쥬스 나왔습니다!\n 맛있게 드세요!",
-                                             message: nil,
-                                             preferredStyle: .alert)
+        let successAlert = UIAlertController(title: "\(juice.name) 쥬스 나왔습니다!\n 맛있게 드세요!", message: nil, preferredStyle: .alert)
 
         let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
 
         successAlert.addAction(ok)
-
         present(successAlert, animated: true)
     }
     
     private func showFailAlert() {
-        let failAlert = UIAlertController(title: "재료가 모자라요.\n 재고를 수정할까요?",
-                                          message: nil,
-                                          preferredStyle: .alert)
+        let failAlert = UIAlertController(title: "재료가 모자라요.\n 재고를 수정할까요?", message: nil, preferredStyle: .alert)
         let no = UIAlertAction(title: "아니오", style: .cancel)
         let yes = UIAlertAction(title: "예", style: .default) { action in
             guard let editMenu = self.storyboard?.instantiateViewController(withIdentifier: "FruitStockViewController") else { return }
             
             self.navigationController?.pushViewController(editMenu, animated: true)
         }
+        
         failAlert.addAction(no)
         failAlert.addAction(yes)
         present(failAlert, animated: true)
     }
     
-    @IBAction func editStock(_ sender: UIBarButtonItem) {
+    @IBAction func navigationRightButtonTapped(_ sender: UIBarButtonItem) {
         guard let controller = self.storyboard?.instantiateViewController(withIdentifier: "FruitStockViewController") else { return }
         
         self.navigationController?.pushViewController(controller, animated: true)
