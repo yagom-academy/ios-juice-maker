995edc7766b1fcf111a5e766cdac70d7080ae361
derrickkim0109 <derrickkim0109@naver.com>
2022-05-04 17:02:54 +0900

    chore : 불필요한 부분 제거

--- JuiceMaker/JuiceMaker/Model/JuiceMaker/JuiceType.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker/JuiceType.swift
@@ -18,25 +18,6 @@ enum JuiceType: String {
 }
 
 extension JuiceType {
-//    var localeKorean: String {
-//        switch self {
-//        case .strawberryJuice:
-//            return "딸기"
-//        case .bananaJuice:
-//            return "바나나"
-//        case .kiwiJuice:
-//            return "키위"
-//        case .mangoJuice:
-//            return "망고"
-//        case .pineappleJuice:
-//            return "파인애플"
-//        case .strawberryBananaJuice:
-//            return "딸바"
-//        case .mangoKiwiJuice:
-//            return "망키"
-//        }
-//    }
-    
     func requireIngredients() -> [Fruit: Int] {
         switch self {
         case .strawberryJuice:
