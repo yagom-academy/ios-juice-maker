c029253b7abc6f5aa8a76ac1bd0dc5e003f60c23
yusw10 <yusw10@naver.com>
2022-05-06 17:49:14 +0900

    feat: Regist UIComponents
    - add Stepper to FruitStoreViewController
    - add UILabel to FruitStoreViewController
    - connect IBAction about stepper
    - initialize temporary fruitsStock dictionary

--- JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 final class FruitStoreViewController: UIViewController {
-
+    private var fruitsStock: [Fruit: Int] = [:]
+    
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fruitsStock[.strawberry] = 10
+        fruitsStock[.banana] = 10
+        fruitsStock[.pineapple] = 10
+        fruitsStock[.mango] = 10
+        fruitsStock[.mango] = 10
     }
 
     @IBAction private func pressBackBarButton(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
+    }
+    
+    @IBAction func pressStepper(_ sender: UIStepper) {
     }
 }
