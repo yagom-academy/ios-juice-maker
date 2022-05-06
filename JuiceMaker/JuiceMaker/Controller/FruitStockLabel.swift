94152a95764bf432c1e74e2bc4280f4b0370aadb
Hugh-github <tnghk4822@gmail.com>
2022-05-06 20:41:50 +0900

    feat: UILabel을 관리하는 파일 FruitStockLabel 생성

---
+++ JuiceMaker/JuiceMaker/Controller/FruitStockLabel.swift
@@ -0,0 +1,21 @@
+//
+//  FruitStockLabel.swift
+//  JuiceMaker
+//
+//  Created by dhoney96 on 2022/05/06.
+//
+
+import UIKit
+
+class FruitStockLabel: UILabel {
+    // 예톤만 믿을게요. 저는 틀렸어요.........
+    // 화이팅 예톤!!!🤣
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
