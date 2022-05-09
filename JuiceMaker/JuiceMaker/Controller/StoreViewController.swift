47dc38f34900efec35b0a50335354c1b906703e2
웡빙 <wb277@naver.com>
2022-05-09 20:04:45 +0900

    style : "StoreViewController" UIStepper 하나로 연결

--- JuiceMaker/JuiceMaker/Controller/StoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StoreViewController.swift
@@ -14,43 +14,71 @@ class StoreViewController: UIViewController {
     @IBOutlet weak var kiwiLabel: UILabel!
     @IBOutlet weak var mangoLabel: UILabel!
     
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
     var delegate: ManangingOrderDelegate?
     var fruits: [Fruit: Int]?
+    
+    var isValid: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fruits = delegate?.setUpStock()
-        print(fruits)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         updateUI()
     }
-
+    
     @IBAction func cancelButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
         delegate?.updateUI()
     }
     
-    @IBAction func strawberryStepperDidTapped(_ sender: UIStepper) {
-        
+    @IBAction func stepperButtonDidTapped(_ sender: UIStepper) {
+        switch sender {
+        case strawberryStepper:
+            setMinimumValue(sender, fruit: .strawberry)
+            strawberryLabel.text = String(change(for: .strawberry, number: Int(sender.value)) ?? 0)
+        case bananaStepper:
+            setMinimumValue(sender, fruit: .banana)
+            bananaLabel.text = String(change(for: .banana, number: Int(sender.value)) ?? 0)
+        case pineappleStepper:
+            setMinimumValue(sender, fruit: .pineapple)
+            pineappleLabel.text = String(change(for: .pineapple, number: Int(sender.value)) ?? 0)
+        case kiwiStepper:
+            setMinimumValue(sender, fruit: .kiwi)
+            kiwiLabel.text = String(change(for: .kiwi, number: Int(sender.value)) ?? 0)
+        case mangoStepper:
+            setMinimumValue(sender, fruit: .mango)
+            mangoLabel.text = String(change(for: .mango, number: Int(sender.value)) ?? 0)
+        default:
+            return
+        }
     }
     
-    @IBAction func bananaStepperDidTapped(_ sender: UIStepper) {
-    
+    func change(for fruit: Fruit, number: Int) -> Int? {
+        var previousValue = 0
+        if(previousValue < number) {
+            previousValue = number
+            fruits?[fruit]! += 1
+        } else if (previousValue > number){
+            previousValue = number
+            fruits?[fruit]! -= 1
+        }
+        return fruits?[fruit]
     }
     
-    @IBAction func pineappleStepperDidTapped(_ sender: UIStepper) {
-    
-    }
-    
-    @IBAction func kiwiStepperDidTapped(_ sender: UIStepper) {
-    
-    }
-    
-    @IBAction func mangoStepperDidTapped(_ sender: UIStepper) {
-    
+    func setMinimumValue(_ sender: UIStepper, fruit: Fruit) {
+        if (isValid == false) {
+            sender.minimumValue = Double(((fruits?[fruit])!) * (-1))
+            isValid = true
+        }
     }
 }
 
