97c7a795d5bdd5e6abca76b23a89c8ad3a938b9c
웡빙 <wb277@naver.com>
2022-05-09 14:54:00 +0900

    feat : "StoreViewController" 에 UI elements 연동

--- JuiceMaker/JuiceMaker/Controller/StoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StoreViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class StoreViewController: UIViewController {
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func cancelButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func strawberryStepperDidTapped(_ sender: UIStepper) {
+    }
+    
+    @IBAction func bananaStepperDidTapped(_ sender: UIStepper) {
+    }
+    
+    @IBAction func pineappleStepperDidTapped(_ sender: UIStepper) {
+    }
+    
+    @IBAction func kiwiStepperDidTapped(_ sender: UIStepper) {
+    }
+    
+    @IBAction func mangoStepperDidTapped(_ sender: UIStepper) {
     }
 }
