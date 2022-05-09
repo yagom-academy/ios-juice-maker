2975e4a4d257fb4b400a23ec379f3bd79733022f
minsson <mskaang@gmail.com>
2022-05-09 20:44:34 +0900

    feat: 재고수정 화면 오토레이아웃 적용, 재고 수정을 위한 UIStepper outlet 생성

--- JuiceMaker/JuiceMaker/Controller/InventoryViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/InventoryViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class InventoryViewController: UIViewController {
+    @IBOutlet private weak var strawberryInventoryLabel: UILabel!
+    @IBOutlet private weak var bananaInventoryLabel: UILabel!
+    @IBOutlet private weak var pineappleInventoryLabel: UILabel!
+    @IBOutlet private weak var kiwiInventoryLabel: UILabel!
+    @IBOutlet private weak var mangoInventoryLabel: UILabel!
+    
+    @IBOutlet private weak var strawberryInventoryStepper: UIStepper!
+    @IBOutlet private weak var bananaInventoryStepper: UIStepper!
+    @IBOutlet private weak var pineappleInventoryStepper: UIStepper!
+    @IBOutlet private weak var kiwiInventoryStepper: UIStepper!
+    @IBOutlet private weak var mangoInventoryStepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction private func touchUpDismissButton(_ sender: UIButton) {
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
 }
