ce51bf5b83e27b398057cd476a3c4a115afb7d64
Hugh-github <tnghk4822@gmail.com>
2022-05-10 18:25:19 +0900

    feat: EditStockStepper를 상속받는 Stepper 타입 생성, minimumValue 설정, stepper addTarget 설정

---
+++ JuiceMaker/JuiceMaker/Controller/EditStockStepper.swift
@@ -0,0 +1,44 @@
+//
+//  EditStockStepper.swift
+//  JuiceMaker
+//
+//  Created by dhoney96 on 2022/05/10.
+//
+
+import UIKit
+
+class EditStockStepper: UIStepper {
+    func convertToFruit() -> Fruit? {
+        return nil
+    }
+}
+
+class StrawberryStepper: EditStockStepper {
+    override func convertToFruit() -> Fruit? {
+        return .strawberry
+    }
+}
+
+class BananaStepper: EditStockStepper {
+    override func convertToFruit() -> Fruit? {
+        return .banana
+    }
+}
+
+class PineappleStepper: EditStockStepper {
+    override func convertToFruit() -> Fruit? {
+        return .pineapple
+    }
+}
+
+class KiwiStepper: EditStockStepper {
+    override func convertToFruit() -> Fruit? {
+        return .kiwi
+    }
+}
+
+class MangoStepper: EditStockStepper {
+    override func convertToFruit() -> Fruit? {
+        return .mango
+    }
+}
--- JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
@@ -10,12 +10,40 @@ import UIKit
 class EditStockViewController: UIViewController {
     var fruitStock = [Fruit: Int]()
     
+    @IBOutlet var fruitStockLabel: [FruitStockLabel]!
+    @IBOutlet var editStockStepper: [EditStockStepper]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(fruitStock)
+        
+        updateFruitStockLabel(stock: fruitStock)
+        
+        editStockStepper.forEach { stepper in
+            stepper.addTarget(self, action: #selector(stepFruit(_:)), for: .valueChanged)
+            
+            guard let fruit = stepper.convertToFruit(),
+                    let amount = fruitStock[fruit]
+            else {
+                return
+            }
+            stepper.minimumValue = Double(-amount)
+        }
     }
     
     @IBAction func tapCloseButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func updateFruitStockLabel(stock: [Fruit: Int]) {
+        fruitStockLabel.forEach { label in
+            guard let fruit = label.convertToFruit() else { return }
+            guard let fruitLabel = stock[fruit] else { return }
+            
+            label.text = "\(fruitLabel)"
+        }
+    }
+    
+    @objc func stepFruit(_ sender: UIStepper) {
+        print(sender.value)
     }
 }
