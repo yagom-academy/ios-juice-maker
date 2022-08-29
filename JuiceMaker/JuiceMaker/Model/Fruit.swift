5624e34b9d7bc76dd7221144474e7d41c41e96c5
woong <workplayhard1@naver.com>
2022-08-29 18:58:41 +0900

    feat: fruitStore에 재고를 체크하는 함수구현

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by 서현웅 on 2022/08/29.
+//
+
+import Foundation
+
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,25 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var stock: [Fruit:Int] = [:]
+    
+    
+    func changeAmount(of fruit: Fruit, to amount: Int) {
+        
+    }
+    
+    func checkStock(of fruit: Fruit, amount: Int) -> Result<Int,Error> {
+        guard let fruitCount = stock[fruit] else {
+            return .failure(Error.self as! Error)
+        }
+        guard fruitCount > amount else {
+            return .failure(Error.self as! Error)
+        }
+        stock[fruit]! -= amount
+        return .success(fruitCount)
+    }
+    
     
 }
+
+
