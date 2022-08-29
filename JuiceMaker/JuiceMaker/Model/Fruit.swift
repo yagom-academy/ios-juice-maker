1412a86088c22d2e6af0f13f8cc969c516d26a2a
yjjem <marksim0606@gmail.com>
2022-08-29 12:08:14 +0900

    feat: changeInventory 추가

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,13 @@
+//
+//  JuiceMaker - FruitStore.swift
+//  Created by Jeremy, Baem.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+enum Fruit {
+    case strawBerry
+    case banana
+    case pineApple
+    case kiwi
+    case mango
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,25 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var strawBerry = 10
+    var banana = 10
+    var pineApple = 10
+    var kiwi = 10
+    var mango = 10
     
+    func changeInventory(_ fruit: Fruit,_ count: Int) {
+        switch fruit {
+        case .strawBerry:
+            self.strawBerry += count
+        case .banana:
+            self.banana += count
+        case .pineApple:
+            self.pineApple += count
+        case .kiwi:
+            self.kiwi += count
+        case .mango:
+            self.mango += count
+        }
+    }
 }
+
