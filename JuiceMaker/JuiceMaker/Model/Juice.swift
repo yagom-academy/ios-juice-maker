80e0beb8c01af1f9acd62f84c150d81d5df9fe91
HyosungP <hyosing92@gmail.com>
2022-08-29 13:25:46 +0900

    feat: Juice enum 타입 생성 및 JuiceMaker클래스 manufactorJuice()메서드 정의, 네비게이터:  하모

---
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -0,0 +1,16 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by Kyo, TaeLee on 2022/08/29.
+//
+
+enum Juice {
+    case strawberryJuice
+    case bananaJuice
+    case pineappleJuice
+    case kiwiJuice
+    case mangoJuice
+    case strawberryBananaJuice
+    case mangoKiwiJuice
+}
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,26 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    let fruitStore = FruitStore()
     
+    func manufactorJuice(menu: Juice) {
+        switch menu {
+        case .strawberryJuice:
+            fruitStore.substractFruits(fruit: .strawberry, amount: 16)
+        case .bananaJuice:
+            fruitStore.substractFruits(fruit: .banana, amount: 2)
+        case .pineappleJuice:
+            fruitStore.substractFruits(fruit: .pineapple, amount: 2)
+        case .kiwiJuice:
+            fruitStore.substractFruits(fruit: .kiwi, amount: 3)
+        case .mangoJuice:
+            fruitStore.substractFruits(fruit: .mango, amount: 3)
+        case .strawberryBananaJuice:
+            fruitStore.substractFruits(fruit: .strawberry, amount: 10)
+            fruitStore.substractFruits(fruit: .banana, amount: 1)
+        case .mangoKiwiJuice:
+            fruitStore.substractFruits(fruit: .mango, amount: 2)
+            fruitStore.substractFruits(fruit: .kiwi, amount: 1)
+        }
+    }
 }
