43b1871e415959a4eff89f3a010df2ee33d09a74
lxodud <xodud1998223@naver.com>
2022-08-29 13:00:34 +0900

    feat: Fruit enum 타입 생성 및 addFruits(), substractFruits() 메소드 정의, 네비게이터: Kyo

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,14 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by Kyo, TaeLee on 2022/08/29.
+//
+
+enum Fruit: Int, CaseIterable {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,32 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var fruitStock: [Fruit : Int]
+    
+    init(defaultStock: Int = 10) {
+        fruitStock = [.strawberry : defaultStock,
+                      .banana : defaultStock,
+                      .kiwi : defaultStock,
+                      .mango : defaultStock,
+                      .pineapple : defaultStock]
+    }
+    
+    func addFruits(fruit: Fruit, amount: Int) {
+        guard var fruitAmount = fruitStock[fruit] else {
+            return
+        }
+        
+        fruitAmount += amount
+        fruitStock[fruit] = fruitAmount
+    }
+    
+    func substractFruits(fruit: Fruit, amount: Int) {
+        guard var fruitAmount = fruitStock[fruit] else {
+            return
+        }
+        
+        fruitAmount -= amount
+        fruitStock[fruit] = fruitAmount
+    }
     
 }
