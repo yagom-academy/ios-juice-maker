cb99bd7e4f5ebe658fd91e44a4a6c518691a0e28
dev-summer <summerly812@gmail.com>
2022-08-29 18:48:18 +0900

    feat: juice enum 정의, checkStockOf 메서드 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,41 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    let fruitStore = FruitStore()
     
+    enum Juice {
+        case strawberryJuice
+        case bananaJuice
+        case kiwiJuice
+        case pineappleJuice
+        case strawberryBananaJuice
+        case mangoJuice
+        case mangoKiwiJuice
+    }
+    
+    func makeJuice(_ juice: Juice, total: Int) {
+//        switch juice {
+//        case .strawberryJuice:
+//        case .bananaJuice:
+//        case .kiwiJuice:
+//        case .pineappleJuice
+//        case .strawberryBananaJuice
+//        case .mangoJuice
+//        case .mangoKiwiJuice
+//        }
+        
+//        checkStock(<#T##juice: Juice##Juice#>, total: <#Int#>)
+    }
+    
+    func checkStockOf(fruit: FruitStore.Fruit, count: Int) -> Bool {
+        guard let currentStock = fruitStore.fruitStock[fruit] else {
+            return false
+        }
+        
+        guard currentStock >= count else {
+            return false
+        }
+        
+        return true
+    }
 }
