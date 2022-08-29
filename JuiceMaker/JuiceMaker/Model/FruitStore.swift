a7d88bfc03f35dc910c9ebe5e2a4b2d5e7c115e3
sunnycookie <sunnyday2007@naver.com>
2022-08-29 18:13:09 +0900

    feat: Fruit 열거형타입정의, fruitStock 프로퍼티선언, changeStockOf 메소드생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,32 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    enum Fruit {
+        case strawberry
+        case banana
+        case pineapple
+        case kiwi
+        case mango
+    }
     
+    var fruitStock: Dictionary<Fruit, Int> = [.strawberry : 10,
+                                              .banana : 10,
+                                              .pineapple : 10,
+                                              .kiwi : 10,
+                                              .mango : 10]
+    
+    func changeStockOf(fruit: Fruit, by quantity: Int) {
+        guard let currentStock = fruitStock[fruit] else {
+            return
+        }
+        
+        guard currentStock >= quantity else {
+            return
+        }
+        
+        fruitStock[fruit] = currentStock + quantity
+    }
 }
+
+
+
---
+++ JuiceMaker/JuiceMaker/Model/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  JuiceMaker
+//
+//  Created by 맹선아 on 2022/08/29.
+//
+
+import Foundation
+
+
+let a = FruitStore()
+a.changeStockOf(fruit: .strawberry, by: 2)
+print(a.fruitStock)
