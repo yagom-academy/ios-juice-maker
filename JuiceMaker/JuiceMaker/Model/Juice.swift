c49a9001be887fc76f82d8fd90755e52f71073d2
Jae Hyung Kim <smfc.mene@gmail.com>
2022-08-30 12:01:54 +0800

    feat: Juice enum에 rawValue 추가

--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -5,12 +5,12 @@
 //  Created by Mene, Dragon on 2022/08/30.
 //
 
-enum Juice {
-    case strawBerry
-    case banana
-    case pineApple
-    case kiwi
-    case mango
-    case strawBerryBanana
-    case mangoKiwi
+enum Juice: String {
+    case strawBerry = "딸기"
+    case banana = "바나나"
+    case pineApple = "파인애플"
+    case kiwi = "키위"
+    case mango = "망고"
+    case strawBerryBanana = "딸바"
+    case mangoKiwi = "망고키위"
 }
