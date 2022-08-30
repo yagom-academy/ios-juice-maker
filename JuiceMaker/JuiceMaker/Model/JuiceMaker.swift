7652a7e21b2265dd1cdc98ec164b5a1ef87e25a7
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-08-30 14:20:51 +0900

    feat: JuiceMaker 객체 생성, grindFruit 메서드 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    let fruitStorage = FruitStore(defaultStack: 10)
     
+    func grindFruit(juice: Juice) {
+        for (fruit, fruitAmount) in juice.recipeOFJuice {
+            if let stackEnough = fruitStorage.stack[fruit] {
+                fruitStorage.stack.updateValue(stackEnough - fruitAmount, forKey: fruit)
+            }
+        }
+    }
 }
