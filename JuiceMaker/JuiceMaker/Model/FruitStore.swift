93b3d09b514790a008d0960c0dbf681a13b6bd1d
DragonYG <kioswe92@gmail.com>
2022-08-30 15:01:55 +0900

    style: 네이밍 및 코드컨벤션 수정

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,36 +8,25 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    
     var inventory: [Fruit: Int] = [:]
     
-    init(initialFruitCount: Int) {
-        Fruit.allCases.forEach {inventory[$0] = initialFruitCount}
+    init(initialFruitAmount: Int) {
+        Fruit.allCases.forEach {inventory[$0] = initialFruitAmount}
     }
     
-    func addToInventory (fruit: Fruit, amount: Int) {
+    func addToInventory (of fruit: Fruit, by amount: Int) {
         guard let fruitStock = inventory[fruit] else { return }
         inventory[fruit] = fruitStock + amount
     }
     
-    func removeToInventory (fruit: Fruit, amount: Int) {
+    func removeToInventory (of fruit: Fruit, by amount: Int) {
         guard let fruitStock = inventory[fruit] else { return }
+        
         if fruitStock < amount {
             print("수량이 작습니다!")
             return
-        } else {
-            inventory[fruit] = fruitStock - amount
         }
+        
+        inventory[fruit] = fruitStock - amount
     }
 }
-
-let store = FruitStore.init(initialFruitCount: 10)
-let mene: () = store.removeToInventory(fruit: .strawBerry, amount: 10)
-let test = store.inventory
-
-
-//FruitStore는 다음의 조건을 충족해야 합니다.
-//FruitStore가 관리하는 과일의 종류 : 딸기, 바나나, 파인애플, 키위, 망고
-//각 과일의 초기 재고 : 10개
-//각 과일의 수량 n개를 변경하는 기능이 있습니다.
-//이 외에 더 필요한 기능, 특성, 타입이 있으면 자유롭게 추가해도 좋습니다.
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,7 +8,7 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    let store = FruitStore(initialFruitCount: 10)
+    let store = FruitStore(initialFruitAmount: 10)
     
     func checkRecipe(of juice: Juice) -> [Fruit: Int] {
         switch juice {
@@ -32,35 +32,17 @@ struct JuiceMaker {
     func makeJuice(of juice: Juice) {
         let recipe = checkRecipe(of: juice)
         
-        for (key, value) in recipe {
-            guard var fruitStock = store.inventory[key] else { return }
-            guard let recipeNumber = recipe[key] else { return }
-            if fruitStock < recipeNumber {
+        for (key, _) in recipe {
+            guard let fruitStock = store.inventory[key] else { return }
+            guard let fruitRequiredNumber = recipe[key] else { return }
+            
+            if fruitStock < fruitRequiredNumber {
                 print("재고가 부족합니다.")
-                print(store.inventory)
                 return
-            } else {
-                store.inventory[key] = fruitStock - recipeNumber
             }
+
+            store.inventory[key] = fruitStock - fruitRequiredNumber
         }
         print("\(juice.rawValue) 쥬스를 만듭니다.")
-        print(store.inventory)
     }
-    
 }
-
-let dragon = JuiceMaker().makeJuice(of: .strawBerryBanana)
-
-
-//JuiceMaker.swift 파일에 다음의 조건을 충족하는 JuiceMaker 타입을 정의합니다.
-//FruitStore의 과일을 사용해 과일쥬스를 제조합니다.
-//딸기쥬스 : 딸기 16개 소모
-//바나나쥬스 : 바나나 2개 소모
-//키위쥬스 : 키위 3개 소모
-//파인애플 쥬스 : 파인애플 2개 소모
-//딸바쥬스 : 딸기 10개 + 바나나 1개 소모
-//망고 쥬스 : 망고 3개 소모
-//망고키위 쥬스 : 망고 2개 + 키위 1개 소모
-//과일의 재고가 부족하면 과일쥬스를 제조할 수 없습니다.
-//JuiceMaker는 FruitStore를 소유하고 있습니다.
-//이 외에 더 필요한 기능, 특성, 타입이 있으면 자유롭게 추가해도 좋습니다.
