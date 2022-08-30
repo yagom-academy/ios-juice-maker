8202d19ef36d5f431599b34003beb20c47641df0
chacha <hyhy0429_@nvaer.com>
2022-08-30 15:05:13 +0900

    revert: Juicerecipe 파일을 Juice 파일로 병합 후 삭제

--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -1,6 +1,6 @@
 //  Created by 애종,질리,벨라 on 2022/08/30.
 
-enum Juice {
+enum JuiceRecipe {
     case strawberryJuice
     case bananaJuice
     case kiwiJuice
@@ -8,4 +8,28 @@ enum Juice {
     case strawberryBananaJuice
     case mangoJuice
     case mangoKiwiJuice
+    
+    struct Recipe {
+        let first: (Fruit, Int)
+        let second: (Fruit, Int)?
+    }
+    
+    var recipe: Recipe {
+        switch self {
+        case .strawberryJuice:
+            return Recipe(first: (.strawberry, 16), second: nil)
+        case .bananaJuice:
+            return Recipe(first: (.banana, 2), second: nil)
+        case .kiwiJuice:
+            return Recipe(first: (.kiwi, 3), second: nil)
+        case .pineappleJuice:
+            return Recipe(first: (.pineapple, 2), second: nil)
+        case .strawberryBananaJuice:
+            return Recipe(first: (.strawberry, 10), second: (.banana, 1))
+        case .mangoJuice:
+            return Recipe(first: (.mango, 3), second: nil)
+        case .mangoKiwiJuice:
+            return Recipe(first: (.mango, 2), second: (.kiwi, 1))
+        }
+    }
 }
