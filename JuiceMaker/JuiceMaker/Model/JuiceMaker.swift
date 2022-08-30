002f1dcdc07b6b39ee77ab3c06adc2bc59551cf7
Gundy <korea1880@naver.com>
2022-08-30 15:08:51 +0900

    feat: 쥬스를 만드는 makeJuice 메서드 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -5,5 +5,17 @@
 // 
 
 struct JuiceMaker {
+    let fruitStore: FruitStore = .init(initialStock: 10)
     
+    func makeJuice(of flavor: Juice) {
+        let firstMaterial = flavor.recipe.first
+        guard fruitStore.canSubtract(amount: firstMaterial.amount, of: firstMaterial.fruit) else {
+            return
+        }
+        if let secondMaterial = flavor.recipe.second {
+            guard fruitStore.canSubtract(amount: secondMaterial.amount, of: secondMaterial.fruit) else { return }
+            fruitStore.subtract(amount: secondMaterial.amount, of: secondMaterial.fruit)
+        }
+        fruitStore.subtract(amount: firstMaterial.amount, of: firstMaterial.fruit)
+    }
 }
