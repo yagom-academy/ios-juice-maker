2c753fd16c8057e00b3df4cf614f4c8656b2e5df
chacha <hyhy0429_@nvaer.com>
2022-08-30 13:48:19 +0900

    feat: FruitStore 재고 변수 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -1,12 +1,13 @@
-//
-//  JuiceMaker - FruitStore.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-//
-
-import Foundation
+//  Created by 애종,질리,벨라 on 2022/08/30.
 
 // 과일 저장소 타입
 class FruitStore {
-    
+    var stock: [Fruit: Int] = [
+        .strawberry: 10,
+        .banana: 10,
+        .kiwi: 10,
+        .pineapple: 10,
+        .mango:10
+        
+    ]
 }
