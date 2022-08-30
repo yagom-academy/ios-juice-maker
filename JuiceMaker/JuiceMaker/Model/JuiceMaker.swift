889e240ca42a558c83af4e36e041fcf4c85741c7
hashswim <ss991117@naver.com>
2022-08-30 12:37:53 +0900

    feat: JuiceMaker 타입의 make 메소드 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -2,11 +2,19 @@
 //  JuiceMaker - JuiceMaker.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
     let fruitStore: FruitStore = FruitStore()
+    
+    func make(juice: Juice) throws {
+        do {
+            try fruitStore.reduceInventory(of: juice.recipe)
+        } catch {
+            fatalError()
+        }
+    }
 }
