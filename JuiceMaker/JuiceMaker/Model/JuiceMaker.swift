4882d87cc8d946390af2e68c9197d8d1355c5984
jin <pearlisrich@naver.com>
2022-08-30 13:28:01 +0900

    feat: JuiceMaker에 Menu 및 하위 프로퍼티 추가

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,40 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    enum Menu {
+        case strawberryJuice,
+             bananaJuice,
+             kiwiJuice,
+             pineappleJuice,
+             strawberryBananaJuice,
+             mangoJuice,
+             mangoKiwiJuice
+        struct Ingredient {
+            let fruit: FruitStore.Fruit, count: Int
+        }
+        var ingredient: [Ingredient] {
+            switch self {
+            case .strawberryJuice:
+                return [Ingredient(fruit: .strawberry, count: 16)]
+            case .bananaJuice:
+                return [Ingredient(fruit: .banana, count: 2)]
+            case .kiwiJuice:
+                return [Ingredient(fruit: .kiwi, count: 3)]
+            case .pineappleJuice:
+                return [Ingredient(fruit: .pineapple, count: 2)]
+            case .strawberryBananaJuice:
+                return [Ingredient(fruit: .strawberry, count: 1),
+                        Ingredient(fruit: .banana, count: 2)]
+            case .mangoJuice:
+                return [Ingredient(fruit: .mango, count: 3)]
+            case .mangoKiwiJuice:
+                return [Ingredient(fruit: .mango, count: 2),
+                        Ingredient(fruit: .kiwi, count: 1)]
+            }
+        }
+    }
+    
+    var fruitStore = FruitStore()
+    
     
 }
