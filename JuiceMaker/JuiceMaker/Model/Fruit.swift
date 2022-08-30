4f9546f7ef7ec47f7f085db57577dc86658d2ce3
DragonYG <kioswe92@gmail.com>
2022-08-30 10:57:02 +0900

    feat: Enum-Fruit파일 추가

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,14 @@
+//
+//  Fruit.swift
+//  FruitStore
+//
+//  Created by Mene, Dragon on 2022/08/30.
+//
+
+enum Fruit {
+    case strawBerry
+    case banana
+    case pineApple
+    case kiwi
+    case mango
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -1,6 +1,6 @@
 //
 //  JuiceMaker - FruitStore.swift
-//  Created by yagom. 
+//  Created by Mene, Dragon.
 //  Copyright © yagom academy. All rights reserved.
 //
 
@@ -10,3 +10,9 @@ import Foundation
 class FruitStore {
     
 }
+
+//FruitStore는 다음의 조건을 충족해야 합니다.
+//FruitStore가 관리하는 과일의 종류 : 딸기, 바나나, 파인애플, 키위, 망고
+//각 과일의 초기 재고 : 10개
+//각 과일의 수량 n개를 변경하는 기능이 있습니다.
+//이 외에 더 필요한 기능, 특성, 타입이 있으면 자유롭게 추가해도 좋습니다.
