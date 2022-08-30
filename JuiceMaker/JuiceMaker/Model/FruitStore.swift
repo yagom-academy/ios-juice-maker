67684c986267077ff7b3e3e3ac430e591b1ef43c
미니 <cow970814@naver.com>
2022-08-30 11:36:36 +0900

    feat : 사용자 정의타입 설정

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -3,7 +3,16 @@
 //  Created by Ash, 미니.
 //
 
+enum Fruit {
+	case strawberry
+	case banana
+	case pineapple
+	case kiwi
+	case mango
+}
+
 // 과일 저장소 타입
 class FruitStore {
-    
+	private let fruitStock: [Fruit: Int] =
+	[.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -3,7 +3,17 @@
 //  Created by Ash, 미니.
 // 
 
+enum Juice {
+	case strawberryJuice(Int)
+	case bananaJuice(Int)
+	case kiwiJuice(Int)
+	case pineappleJuice(Int)
+	case mangoJuice(Int)
+	case strawberrybananaJuice(Int)
+	case mangokiwiJuice(Int)
+}
+
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    
+	private let fruitStore: FruitStore = FruitStore()
 }
