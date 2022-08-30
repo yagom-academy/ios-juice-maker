63c95a2001c1b3598aa4bc46502ea5b304bb6d06
zhilly11 <zhzh1x4@gmail.com>
2022-08-30 13:22:34 +0900

    feat: Fruit, Juice Enum 파일 생성

---
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -0,0 +1,11 @@
+//  Created by 애종,질리,벨라 on 2022/08/30.
+
+enum Juice {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
+}
