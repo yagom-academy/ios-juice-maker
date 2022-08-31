5bc356a0839b485d1e112de57e8e741f625c3061
wonbi92 <bin9239@gmail.com>
2022-09-01 02:19:54 +0900

    refactor: 사용자 정의 타입 및 함수 네이밍 리팩토링

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,35 +8,35 @@ class FruitStore {
     private var stock = [Fruits]()
     
     private init() {
-        for fruit in Fruits.FruitName.fruitsList {
-            stock.insert(Fruits(name: fruit, count: 10), at: fruit.rawValue)
+        for fruit in Fruits.FruitType.fruitsList {
+            stock.append(Fruits(name: fruit, count: 10))
         }
     }
     
     private func haveInStock(fruits: [Fruits]) -> Bool {
         for fruit in fruits {
-            let stockCount = stock[fruit.name.rawValue].count
+            let stockCount = stock[fruit.fruitIndex].count
             let requestedCount = fruit.count
             if stockCount < requestedCount { return false }
         }
         return true
     }
     
-    func changeStock(fruit: Fruits) {
-        stock.insert(fruit, at: fruit.name.rawValue)
+    private func changeStock(fruit: Fruits) {
+        stock.insert(fruit, at: fruit.fruitIndex)
     }
     
-    func addStock(fruits: [Fruits]) {
+    private func addStock(fruits: [Fruits]) {
         for fruit in fruits {
-            let stockCount = stock[fruit.name.rawValue].count
+            let stockCount = stock[fruit.fruitIndex].count
             let requestedCount = fruit.count
             changeStock(fruit: Fruits(name: fruit.name, count: stockCount + requestedCount))
         }
     }
     
-    func removeStock(fruits: [Fruits]) {
+    private func removeStock(fruits: [Fruits]) {
         for fruit in fruits {
-            let stockCount = stock[fruit.name.rawValue].count
+            let stockCount = stock[fruit.fruitIndex].count
             let requestedCount = fruit.count
             changeStock(fruit: Fruits(name: fruit.name, count: stockCount - requestedCount))
         }
--- JuiceMaker/JuiceMaker/Model/Fruits.swift
+++ JuiceMaker/JuiceMaker/Model/Fruits.swift
@@ -4,26 +4,32 @@
 //
 
 struct Fruits {
-    enum FruitName: Int {
+    enum FruitType: Int {
         case strawberry = 0
         case banana
         case pineapple
         case kiwi
         case mango
+        
+        static let fruitsList = [strawberry, banana, pineapple, kiwi, mango]
     }
     
-    var name: FruitName
+    var name: FruitType
     var count: Int
+    var fruitIndex: Int {
+        self.name.rawValue
+    }
     
-    init(name: FruitName, count: Int) {
+    init(name: FruitType, count: Int) {
         self.name = name
         self.count = count
     }
 }
 
-extension Fruits.FruitName: CaseIterable {
-    static let fruitsList = Self.allCases
-}
+//extension Fruits.FruitName: CaseIterable {
+//    static let fruitsList = Self.allCases
+//}
+
 // 아래 allCases 때문에 사용한 것 같은데.. CaseIterable를 사용하지 않고 구현하는 방법은 없을까요?
 // Caseliterable을 FruitName의 프로토콜로 채택하지않고 extension을 통해 fruitsList에 접근할 때만 caseIterable을 채택하도록 구현해보았습니다.
 // 이 방법 외에는 매직넘버를 사용할 수 밖에없어서 그 선택지는 선택하지않았습니다.
