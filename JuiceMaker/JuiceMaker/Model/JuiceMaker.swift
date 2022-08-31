61f14f0d1abb882996adbd057d57ed067bd95aa2
dylan-yoon <d.dylany21@gmail.com>
2022-08-31 14:57:23 +0900

    refactor: makeJuice의 의미에 맞게 핸들링 위치 수정

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -9,14 +9,22 @@ import Foundation
 struct JuiceMaker {
     private let store = FruitStore()
     
-    func makeJuice(_ juice: Juice) throws {
+    func makeJuice(_ juice: Juice) {
+        if requestStockAvailability(for: juice) {
+            store.useStockForRecipe(of: juice)
+        }
+    }
+    
+    func requestStockAvailability(for juice: Juice) -> Bool {
         do {
             try store.checkStockAvailability(of: juice)
         } catch StockError.notEnoughFruit {
             print("재고 부족")
         } catch StockError.outOfFruit {
             print("재고 없음")
+        } catch {
+            print("Unknown Error")
         }
-        store.useStockForRecipe(of: juice)
+        return true
     }
 }
