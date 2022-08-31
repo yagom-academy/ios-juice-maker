499724c47dcfd6c6e329f2eebb21f71d32c41e7b
woong <workplayhard1@naver.com>
2022-09-01 00:13:18 +0900

    refactor: makeJuice 메서드 로직 수정

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -4,76 +4,43 @@
 //
 
 struct JuiceMaker {
-    enum Juice {
-        case strawberryJuice
-        case bananaJuice
-        case kiwiJuice
-        case pineappleJuice
-        case mangoJuice
-        case strawberryBananaJuice
-        case mangoKiwiJuice
+    enum Juice: String {
+        case strawberryJuice = "딸기 주스"
+        case bananaJuice = "바나나 주스"
+        case kiwiJuice = "키위 주스"
+        case pineappleJuice = "파인애플 주스"
+        case mangoJuice = "망고 주스"
+        case strawberryBananaJuice = "딸바 주스"
+        case mangoKiwiJuice = "망키 주스"
         
         var name: String {
-            switch self {
-            case .strawberryJuice:
-                return "딸기 주스"
-            case .bananaJuice:
-                return "바나나 주스"
-            case .kiwiJuice:
-                return "키위 주스"
-            case .pineappleJuice:
-                return "파인애플 주스"
-            case .mangoJuice:
-                return "망고 주스"
-            case .strawberryBananaJuice:
-                return "딸바 주스"
-            case .mangoKiwiJuice:
-                return "망키 주스"
-            }
+            self.rawValue
         }
         
-        var recipe: [Fruit: Int] {
+        var recipe: [Fruits] {
             switch self {
             case .strawberryJuice:
-                return [.strawberry: 16]
+                return [Fruits(name: .strawberry, count: 16)]
             case .bananaJuice:
-                return [.banana: 2]
+                return [Fruits(name: .banana, count: 2)]
             case .kiwiJuice:
-                return [.kiwi: 3]
+                return [Fruits(name: .kiwi, count: 3)]
             case .pineappleJuice:
-                return [.pineapple: 2]
+                return [Fruits(name: .pineapple, count: 2)]
             case .mangoJuice:
-                return [.mango: 3]
+                return [Fruits(name: .mango, count: 3)]
             case .strawberryBananaJuice:
-                return [.strawberry: 10, .banana: 1]
+                return [Fruits(name: .strawberry, count: 10), Fruits(name: .banana, count: 1)]
             case .mangoKiwiJuice:
-                return [.mango: 2, .kiwi: 1]
+                return [Fruits(name: .mango, count: 2), Fruits(name: .kiwi, count: 1)]
             }
         }
     }
     
     let stockManager = FruitStore.stockManager
     
-    func turnBackFruit(from receivedFruits: [Fruit: Int]) {
-        for (fruit, numberOfFruits) in receivedFruits {
-            stockManager.addStock(of: fruit, number: numberOfFruits)
-        }
-    }
-    
     func makeJuice(_ juice: Juice) {
-        var receivedFruitsList = [Fruit: Int]()
-        
-        for (fruit, numberOfFruits) in juice.recipe {
-            let receivedNumberOfFruits = stockManager.handOver(of: fruit, number: numberOfFruits)
-            if receivedNumberOfFruits != 0 {
-                receivedFruitsList.updateValue(receivedNumberOfFruits, forKey: fruit)
-            }
-        }
-        
-        if receivedFruitsList.count != juice.recipe.count {
-            turnBackFruit(from: receivedFruitsList)
-        } else {
-            print("(juice.name) 완성")
-        }
+        if stockManager.supplyRequest(fruits: juice.recipe) == nil { return }
+        print("\(juice.name) 완성")
     }
 }
