575297a3525204d6b745bb56ab5abaa4dd713aa2
oneStar92 <onestar7984@gmail.com>
2022-09-01 10:50:41 +0900

    refactor: increaseInventory메소드 기능 수정.

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -15,17 +15,10 @@ class FruitStore {
         }
     }
     
-    func increaseInventory(about fruitsInventory: FruitsInventory) throws {
-        guard fruitsInventory.filter({ $0.value < 0 }).isEmpty else {
-            throw FruitStoreError.invalidParameter
-        }
-        
-        for (fruit, count) in fruitsInventory {
-            guard let inventory = fruitsInventory[fruit] else {
-                throw FruitStoreError.noneFruit
-            }
-            
-            self.fruitsInventory[fruit] = inventory + count
+    func increaseInventory(of fruit: Fruit, by amount: Int) {
+        if amount >= 0,
+           let inventory = fruitsInventory[fruit] {
+            self.fruitsInventory = inventory + amount
         }
     }
     
