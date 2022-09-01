a7ae5c1555066d836d5020535cbfb02848049ee9
LJ <swtw72@gmail.com>
2022-09-01 20:23:28 +0900

    fix: Juice타입 name프로퍼티 추가, recipeOfJuice 프로퍼티 변경

--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -3,35 +3,49 @@
 import Foundation
 
 enum Juice: String {
-    case strawberryJuice = "딸기쥬스"
-    case bananaJuice = "바나나쥬스"
-    case kiwiJuice = "키위쥬스"
-    case pineappleJuice = "파인애플쥬스"
-    case strawberryBananaJuice = "딸바쥬스"
-    case mangoJuice = "망고쥬스"
-    case mangoKiwiJuice = "망고키위쥬스"
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
     
-    struct Ingredient {
-        let first: (Fruit, Int)
-        let second: (Fruit, Int)?
-    }
-    
-    var recipeOfJuice: Ingredient {
+    var name: String {
         switch self {
         case .strawberryJuice:
-            return Ingredient(first: (.strawberry, 16), second: nil)
+            return "딸기쥬스"
         case .bananaJuice:
-            return Ingredient(first: (.banana, 2), second: nil)
+            return "바나나쥬스"
         case .kiwiJuice:
-            return Ingredient(first: (.kiwi, 3), second: nil)
+            return "키위쥬스"
         case .pineappleJuice:
-            return Ingredient(first: (.pineapple, 2), second: nil)
+            return "파인애플쥬스"
         case .strawberryBananaJuice:
-            return Ingredient(first: (.strawberry, 10), second: (.banana, 1))
+            return "딸바쥬스"
         case .mangoJuice:
-            return Ingredient(first: (.mango, 3), second: nil)
+            return "망고쥬스"
         case .mangoKiwiJuice:
-            return Ingredient(first: (.mango, 2), second: (.kiwi, 1))
+            return "망고키위쥬스"
+        }
+    }
+    
+    var recipeOfJuice: [Fruit: Int] {
+        switch self {
+        case .strawberryJuice:
+            return [.strawberry: 16]
+        case .bananaJuice:
+            return [.banana: 2]
+        case .kiwiJuice:
+            return [.kiwi: 3]
+        case .pineappleJuice:
+            return [.pineapple: 2]
+        case .strawberryBananaJuice:
+            return [.strawberry: 10, .banana: 1]
+        case .mangoJuice:
+            return [.mango: 3]
+        case .mangoKiwiJuice:
+            return [.mango: 2, .kiwi: 1]
         }
     }
 }
