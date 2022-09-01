27d731b1e04117d5e430958462cae1f54ce73d39
lnhk98 <hozzang666@gmai.com>
2022-09-01 10:49:04 +0900

    refactor: fruitStock 프로퍼티 초기화 방법 수정

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,14 +8,7 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    private let initialStock = 10
-    private var fruitStock = [Fruit: Int]()
-    
-    init() {
-        Fruit.allCases.forEach { fruit in
-            fruitStock[fruit] = initialStock
-        }
-    }
+    private var fruitStock: [Fruit: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
     
     func addStock(of fruit: Fruit, amount: Int) {
         if let currentStock = fruitStock[fruit] {
