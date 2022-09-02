18e1893a6aa96b00beb8e69445bcc9ff728db2f5
jeongmin <lj845854@gmail.com>
2022-09-02 10:23:54 +0900

    feat: r과일저장소 재고를 레이블에 불러오기

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,8 +8,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fruitStore = FruitStore.shared
+        self.strawberryStockLabel.text = "\(fruitStore.fruitsStock[.strawberry] ?? 0)"
+        self.bananaStockLabel.text = "\(fruitStore.fruitsStock[.banana] ?? 0)"
+        self.pineappleStockLabel.text = "\(fruitStore.fruitsStock[.pineapple] ?? 0)"
+        self.kiwiStockLabel.text = "\(fruitStore.fruitsStock[.kiwi] ?? 0)"
+        self.mangoStockLabel.text = "\(fruitStore.fruitsStock[.mango] ?? 0)"
     }
 }
 
