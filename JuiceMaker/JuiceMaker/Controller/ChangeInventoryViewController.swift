21b63ffe76848a86826727f80e060b82d5405ace
DragonYG <kioswe92@gmail.com>
2022-09-02 12:55:39 +0900

    feat: ChangeInventoryViewController 생성 및 Modality화면전환 구현

---
+++ JuiceMaker/JuiceMaker/Controller/ChangeInventoryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ChangeInventoryViewController.swift
+//  JuiceMaker
+//
+//  Created by yonggeun Kim on 2022/09/02.
+//
+
+import UIKit
+
+class ChangeInventoryViewController: UIViewController {
+
+    @IBAction func CloseButton(_ sender: UIButton) {
+        dismiss(animated: true,
+                completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- JuiceMaker/JuiceMaker/Controller/HomeViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/HomeViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
