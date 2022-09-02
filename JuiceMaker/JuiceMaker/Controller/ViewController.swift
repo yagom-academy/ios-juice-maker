b7d225bf48479dab6a4057852b9d3286f0a69c0e
lxodud <xodud1998223@naver.com>
2022-09-02 11:36:14 +0900

    feat: navigation bar setting 및 과일 재고 가져오기

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var juiceMaker = JuiceMaker()
+    
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setNavigation()
+        settingFruitStockLabel()
+    }
+    
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        guard var orderedJuice = sender.currentTitle else {
+            return
+        }
+        
+        orderedJuice = orderedJuice.replacingOccurrences(of: " 주문", with: "")
     }
 
-
+    func setNavigation() {
+        self.title = "맛있는 주스를 만들어 드려요!"
+        self.navigationController?.navigationBar.backgroundColor = .lightGray
+    }
+    
+    func settingFruitStockLabel() {
+        let fruitStore = juiceMaker.fruitStore
+        self.strawberryStockLabel.text = String(fruitStore.bringValidFruitStock(.strawberry))
+        self.bananaStockLabel.text = String(fruitStore.bringValidFruitStock(.banana))
+        self.pineappleStockLabel.text = String(fruitStore.bringValidFruitStock(.pineapple))
+        self.kiwiStockLabel.text = String(fruitStore.bringValidFruitStock(.kiwi))
+        self.mangoStockLabel.text = String(fruitStore.bringValidFruitStock(.mango))
+    }
 }
 
