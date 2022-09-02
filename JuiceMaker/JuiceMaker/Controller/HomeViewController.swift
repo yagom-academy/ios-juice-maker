fe2f979147283cec0b7bc9a50ea94054feeab854
DragonYG <kioswe92@gmail.com>
2022-09-02 13:10:18 +0900

    feat: HomeViewController에Label&Button 연결

--- JuiceMaker/JuiceMaker/Controller/HomeViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/HomeViewController.swift
@@ -8,6 +8,33 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    @IBAction func orderStrawberryJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderBananaJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderPineappleJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderKiwiJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderMangoJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderStrawberryBananaJuice(_ sender: UIButton) {
+    }
+    
+    @IBAction func orderMangoKiwi(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
