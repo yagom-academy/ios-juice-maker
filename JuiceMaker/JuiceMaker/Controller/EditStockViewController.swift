5349be2d05672033dee5fed1e64180c45b856910
lws2269 <lws2269@kakao.com>
2022-09-02 10:35:02 +0900

    feat: 화면전환 구현

---
+++ JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EditStockViewController.swift
+//  JuiceMaker
+//
+//  Created by leewonseok on 2022/09/02.
+//
+
+import UIKit
+
+class EditStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -14,16 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
     
+    let fruitStore = FruitStore.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let fruitStore = FruitStore.shared
         self.strawberryStockLabel.text = "\(fruitStore.fruitsStock[.strawberry] ?? 0)"
         self.bananaStockLabel.text = "\(fruitStore.fruitsStock[.banana] ?? 0)"
         self.pineappleStockLabel.text = "\(fruitStore.fruitsStock[.pineapple] ?? 0)"
         self.kiwiStockLabel.text = "\(fruitStore.fruitsStock[.kiwi] ?? 0)"
         self.mangoStockLabel.text = "\(fruitStore.fruitsStock[.mango] ?? 0)"
+    }
+    @IBAction func editStockButtonTapped(_ sender: UIBarButtonItem) {
+        guard let editStockViewController = self.storyboard?.instantiateViewController(withIdentifier: "EditStockViewController") else { return }
+        self.navigationItem.backBarButtonItem = UIBarButtonItem.init(title: "뒤로가기", style: .plain, target: nil, action: nil)
+        self.navigationController?.pushViewController(editStockViewController, animated: true)
     }
 }
 
