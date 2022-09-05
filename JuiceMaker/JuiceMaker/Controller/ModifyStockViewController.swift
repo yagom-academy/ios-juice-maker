685f29f04160f68ef157fb61a99bd32ca75f2af5
HyosungP <hyosing92@gmail.com>
2022-09-05 10:38:06 +0900

    feat: 재고수정화면 오토레이아웃 설정 및 화면전환(Modal) 구현

---
+++ JuiceMaker/JuiceMaker/Controller/ModifyStockViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ModifyStockViewController.swift
+//  JuiceMaker
+//
+//  Created by parkhyo on 2022/09/02.
+//
+
+import UIKit
+
+class ModifyStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+}
