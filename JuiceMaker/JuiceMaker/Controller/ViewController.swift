ca4b1815496020ce5ac19b3f238f2fcde80400b2
dev-summer <summerly812@gmail.com>
2022-09-05 20:46:48 +0900

    feat: 재고수정 view controller 생성, 재고 수정 view controller로 이동 함수 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -1,21 +1,36 @@
 //
 //  JuiceMaker - ViewController.swift
-//  Created by yagom. 
+//  Created by 써니쿠키, 써머캣
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var changeFruitStockButton: UIBarButtonItem!
+    
+    @IBOutlet weak var strawberryBannaJuiceButton: UIButton!
+    @IBOutlet weak var mangoKiwiJuiceButton: UIButton!
+    @IBOutlet weak var strawberryJuiceButton: UIButton!
+    @IBOutlet weak var bananaJuiceButton: UIButton!
+    @IBOutlet weak var pineappleJuiceButton: UIButton!
+    @IBOutlet weak var kiwiJuiceButton: UIButton!
+    @IBOutlet weak var mangoJuiceButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myJuiceMaker = JuiceMaker()
-        myJuiceMaker.makeJuice(.strawberryBananaJuice, total: 1)
-        myJuiceMaker.makeJuice(.strawberryBananaJuice, total: 1)
-        
     }
 
+    func moveToFruitStockVC() {
+        guard let fruitStoreStockViewController =
+                self.storyboard?.instantiateViewController(withIdentifier: "fruitStoreStock") else { return }
+        self.present(fruitStoreStockViewController, animated: true, completion: nil)
+    }
+
+    @IBAction func touchUpChangeFruitStockButton(_ sender: Any) {
+        moveToFruitStockVC()
+    }
+    
 }
 
