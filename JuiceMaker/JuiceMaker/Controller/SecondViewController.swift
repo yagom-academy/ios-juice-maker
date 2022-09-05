46a2654d3ba0307ad77b4ae0e35adebdaa606bbe
woong <workplayhard1@naver.com>
2022-09-05 12:54:18 +0900

    feat: SecondViewController 생성 및 SecondView 오토레이아웃 설정

---
+++ JuiceMaker/JuiceMaker/Controller/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  JuiceMaker
+//
+//  Created by 서현웅 on 2022/09/05.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func btnComplete(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+}
