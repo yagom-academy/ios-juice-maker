e76a659ed2c78b879fe397c2a0ebd8b26999f01a
jin <pearlisrich@naver.com>
2022-09-05 11:04:42 +0900

    feat: updateStockLabel 함수 생성 및 하위 함수 생성

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -17,9 +17,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateStockLabel()
         // Do any additional setup after loading the view.
     }
+    
+    func updateStockLabel() {
+        updateStrawberryStockLabel()
+        updateBananaStockLabel()
+        updatePineappleStockLabel()
+        updateKiwiStockLabel()
+        updateMangoStockLabel()
+    }
 
-
+    func updateStrawberryStockLabel() {
+        let strawberryStock = try! juiceMaker.fetchStock(of: .strawberry)
+        strawberryStockLabel.text = String(strawberryStock)
+    }
+    
+    func updateBananaStockLabel() {
+        let bananaStock = try! juiceMaker.fetchStock(of: .banana)
+        bananaStockLabel.text = String(bananaStock)
+    }
+    
+    func updatePineappleStockLabel() {
+        let pineappleStock = try! juiceMaker.fetchStock(of: .pineapple)
+        pineappleStockLabel.text = String(pineappleStock)
+    }
+    
+    func updateKiwiStockLabel() {
+        let kiwiStock = try! juiceMaker.fetchStock(of: .kiwi)
+        kiwiStockLabel.text = String(kiwiStock)
+    }
+    
+    func updateMangoStockLabel() {
+        let mangoStock = try! juiceMaker.fetchStock(of: .mango)
+        mangoStockLabel.text = String(mangoStock)
+    }
 }
 
