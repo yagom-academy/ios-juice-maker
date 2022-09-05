e0140fcde444060d8b5c124456639d199106089e
wonbi92 <bin9239@gmail.com>
2022-09-05 18:03:14 +0900

    feat: 상황에 맞는 Alert 메소드 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -22,8 +22,29 @@ class ViewController: UIViewController {
         guard let juice = Juice(rawValue: juiceName) else { return }
         
         juiceMaker.makeJuice(juice)
+        madeJuiceAlert(message: "\(juiceName) 나왔습니다! 맛있게 드세요!")
+        failedAlert(message: "재료가 모자라요. 재고를 수정할까요?")
     }
+    
+    func madeJuiceAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+    
+    func failedAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let yesAction = UIAlertAction(title: "예", style: .default, handler: nil)
+        let noAction = UIAlertAction(title: "아니오", style: .destructive, handler: nil)
+        
+        alert.addAction(yesAction)
+        alert.addAction(noAction)
 
+        present(alert, animated: true)
+    }
 }
 
 // 주스 제조후에 나오는 얼럿 : 000 쥬스 나왔습니다! 맛있게 드세요! (확인)
