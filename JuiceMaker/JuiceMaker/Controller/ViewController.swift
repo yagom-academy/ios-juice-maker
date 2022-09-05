d57b369776ff847c896ea0f57cbd921c50b5f3a1
hashswim <ss991117@naver.com>
2022-09-05 20:40:06 +0900

    feat: 과일 재고 레이블 뷰컨트롤러와 연결 및 텍스트 출력 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateInventory()
     }
-
+    
+    func updateInventory() {
+        strawberryLabel.text = "10"
+        bananaLabel.text = "10"
+        pineappleLabel.text = "10"
+        kiwiLabel.text = "10"
+        mangoLabel.text = "10"
+    }
 
 }
 
