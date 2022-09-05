b62043ede689f7710d487923197f6c45e3431247
lws2269 <lws2269@kakao.com>
2022-09-05 17:06:21 +0900

    feat: notification 을 통한 값 감시기능 추가

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var pineappleStockLabel: UILabel!
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
-    
+ 
     @IBOutlet weak var strawberryBananaJuiceOrderButton: UIButton!
     @IBOutlet weak var mangoKiwiJuiceOrderButton: UIButton!
     @IBOutlet weak var strawberryJuiceOrderButton: UIButton!
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         setStockLabel()
+        addNotification()
     }
     
     func setStockLabel() {
@@ -40,14 +41,13 @@ class ViewController: UIViewController {
     func transitionView(){
         guard let editStockViewController = self.storyboard?.instantiateViewController(withIdentifier: "EditStockViewController") else { return }
         
-        editStockViewController
-        
         self.present(editStockViewController, animated: true)
     }
     
     func alertSuccess(for juice: Juice) {
         let alert = UIAlertController(title: "알림", message: "\(juice.name) 쥬스 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "예", style: .default) { _ in self.viewDidLoad() })
+        alert.addAction(UIAlertAction(title: "예", style: .default))
+        
         self.present(alert, animated: true)
     }
     
@@ -55,7 +55,26 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: "알림", message: "\(error.localizedDescription)", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "아니오", style: .cancel))
         alert.addAction(UIAlertAction(title: "예", style: .default) { _ in self.transitionView() })
+        
         self.present(alert, animated: true)
+    }
+    
+    func addNotification() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(didReceivedFruitsAmountChanged(_:)),
+                                               name: NSNotification.Name("fruitStockChange"),
+                                               object: nil)
+    }
+    
+    @objc func didReceivedFruitsAmountChanged(_ noti: Notification){
+        guard let userInfo = noti.userInfo?["fruitsStock"],
+              let fruitsStock = userInfo as? Dictionary<Fruit,Int> else { return }
+        
+        self.strawberryStockLabel.text = "\(fruitsStock[.strawberry] ?? 0)"
+        self.bananaStockLabel.text = "\(fruitsStock[.banana] ?? 0)"
+        self.pineappleStockLabel.text = "\(fruitsStock[.pineapple] ?? 0)"
+        self.kiwiStockLabel.text = "\(fruitsStock[.kiwi] ?? 0)"
+        self.mangoStockLabel.text = "\(fruitsStock[.mango] ?? 0)"
     }
     
     @IBAction func editStockButtonTapped(_ sender: UIBarButtonItem) {
