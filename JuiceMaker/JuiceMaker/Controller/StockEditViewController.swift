5ce9737fff804adae7bb0e16ae8c92a930855dfa
jin <pearlisrich@naver.com>
2022-09-06 10:20:21 +0900

    feat: StockEditViewController 추가 및 연결

---
+++ JuiceMaker/JuiceMaker/Controller/StockEditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StockEditViewController.swift
+//  JuiceMaker
+//
+//  Created by jin on 9/6/22.
+//
+
+import UIKit
+
+class StockEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
