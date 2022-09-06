651a1697791ea5f1ff9aa6ab965e013e31b970ab
미니 <cow970814@naver.com>
2022-09-06 10:59:12 +0900

    feat : 화면 전환 구현 및 화면 닫기 구현

---
+++ JuiceMaker/JuiceMaker/Controller/ModifyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ModifyViewController.swift
+//  JuiceMaker
+//
+//  Created by 이경민 on 2022/09/06.
+//
+
+import UIKit
+
+class ModifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+	
+	@IBAction func didTapDismissButton(_ sender: UIBarButtonItem) {
+		dismiss(animated: true)
+	}
+}
+
