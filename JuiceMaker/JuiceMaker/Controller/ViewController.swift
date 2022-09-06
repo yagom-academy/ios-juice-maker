106bba03454827d9ddef66b781f38f65c58053d4
jonghancha <jonghancha@gmail.com>
2022-09-06 11:05:57 +0900

    feat: 과일주스 주문 버튼 연결 및 재고수정버튼 segue연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,9 +8,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strawberryAmountLabel: UILabel!
+    @IBOutlet weak var bananaAmountLabel: UILabel!
+    @IBOutlet weak var pineappleAmountLabel: UILabel!
+    @IBOutlet weak var kiwiAmountLabel: UILabel!
+    @IBOutlet weak var mangoAmountLabel: UILabel!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func touchUpStrawberryBananaJuiceOrderButton(_ sender: Any) {
+    }
+    
+    @IBAction func touchUpstrawberryJuiceOrderButton(_ sender: Any) {
+    }
+    
+    @IBAction func touchUpBananaJuiceOrderButton(_ sender: Any) {
+    }
+    
+    @IBAction func touchUpPineappleJuiceOrderButton(_ sender: Any) {
+    }
+    
+    @IBAction func touchUpKiwiJuiceOrderButton(_ sender: Any) {
+    }
+    
+    @IBAction func touchUpMangoJuiceOrderButton(_ sender: Any) {
+    }
+    
+    @IBAction func touchUpMangoKiwiJuiceOrderButton(_ sender: Any) {
+    }
+    
+    
 }
-
