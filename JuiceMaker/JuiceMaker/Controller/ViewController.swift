2ffa568bcfda40a84437e49853ada44f42c3db9a
LJ <swtw72@gmail.com>
2022-09-06 14:46:56 +0900

    feat: 과일쥬스버튼 추가, tappedOrderButton 메서드 추가

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,12 +7,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var strawberryBananaJuiceButton: UIButton!
+    @IBOutlet weak var mangoKiwiJuiceButton: UIButton!
+    @IBOutlet weak var strawberryJuiceButton: UIButton!
+    @IBOutlet weak var bananaJuiceButton: UIButton!
+    @IBOutlet weak var pineappleJuiceButton: UIButton!
+    @IBOutlet weak var kiwiJuiceButton: UIButton!
+    @IBOutlet weak var mangoJuiceButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    let juiceMaker = JuiceMaker()
+    
+    @IBAction func tappedOrderButton(_ sender: UIButton) {
+        
+        switch sender {
+        case strawberryBananaJuiceButton:
+            juiceMaker.makeJuice(to: .strawberryBananaJuice)
+        case mangoKiwiJuiceButton:
+            juiceMaker.makeJuice(to: .mangoKiwiJuice)
+        case strawberryJuiceButton:
+            juiceMaker.makeJuice(to: .strawberryJuice)
+        case bananaJuiceButton:
+            juiceMaker.makeJuice(to: .bananaJuice)
+        case pineappleJuiceButton:
+            juiceMaker.makeJuice(to: .pineappleJuice)
+        case kiwiJuiceButton:
+            juiceMaker.makeJuice(to: .kiwiJuice)
+        case mangoJuiceButton:
+            juiceMaker.makeJuice(to: .mangoJuice)
+        default:
+            print("버튼을 다시 눌러주세요.")
+        }
+    }
 
 }
 
