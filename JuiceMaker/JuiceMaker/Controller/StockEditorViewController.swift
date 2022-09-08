079da28536a9c4165860b755b03a9a75f49233b9
yjjem <marksim0606@gmail.com>
2022-09-08 13:30:06 +0900

    refactor: 불필요한 주석 삭제

--- JuiceMaker/JuiceMaker/Controller/StockEditorViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockEditorViewController.swift
@@ -1,11 +1,15 @@
+//
+//  Created by Baem, Jeremy
+//
+//
 import UIKit
 
 class StockEditorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
     @IBAction func closeModal(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
