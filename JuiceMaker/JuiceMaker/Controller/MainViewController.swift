497c47b611753346fface6b6f5ad3dabc1aeffdc
yjjem <marksim0606@gmail.com>
2022-09-08 11:35:30 +0900

    refactor: MainViewcontroller 함수 은닉화

--- JuiceMaker/JuiceMaker/Controller/MainViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -21,34 +21,19 @@ class MainViewController: UIViewController {
     @IBOutlet weak var kiwiButton: UIButton!
     @IBOutlet weak var mangoButton: UIButton!
     
-    let juiceMaker = JuiceMaker()
-    lazy var fruitStock = juiceMaker.store.stock
-    var observation: NSKeyValueObservation?
+    private let juiceMaker = JuiceMaker()
+    private lazy var fruitStock = juiceMaker.store.stock
+    private var observation: NSKeyValueObservation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setLabelDefaultValue()
         observation = juiceMaker.store.observe(\.stock, options: [.new], changeHandler: {
             (object, changes) in
-            guard let newValue = changes.newValue else {
-                return
-            }
-            
+            guard let newValue = changes.newValue else { return }
             self.updateStock(newValue)
             self.setLabelDefaultValue()
         })
-    }
-    
-    func updateStock(_ newValue: [String: Int]) {
-        fruitStock = newValue
-    }
-    
-    func setLabelDefaultValue() {
-        strawBerryLabel.text = juiceMaker.store.stock["딸기"]?.description
-        bananaLabel.text = juiceMaker.store.stock["바나나"]?.description
-        pineAppleLabel.text = juiceMaker.store.stock["파인애플"]?.description
-        kiwiLabel.text = juiceMaker.store.stock["키위"]?.description
-        mangoLabel.text = juiceMaker.store.stock["망고"]?.description
     }
     
     @IBAction func orderJuice(_ sender: UIButton) {
@@ -63,12 +48,26 @@ class MainViewController: UIViewController {
         }
     }
     
-    func showStockEditor() {
+    private func updateStock(_ newValue: [String: Int]) {
+        fruitStock = newValue
+    }
+    
+    private func setLabelDefaultValue() {
+        strawBerryLabel.text = juiceMaker.store.stock["딸기"]?.description
+        bananaLabel.text = juiceMaker.store.stock["바나나"]?.description
+        pineAppleLabel.text = juiceMaker.store.stock["파인애플"]?.description
+        kiwiLabel.text = juiceMaker.store.stock["키위"]?.description
+        mangoLabel.text = juiceMaker.store.stock["망고"]?.description
+    }
+    
+    
+    
+    private func showStockEditor() {
         guard let stockEditorViewController = self.storyboard?.instantiateViewController(withIdentifier: "StockEditorViewController") else { return }
         self.present(stockEditorViewController, animated: true, completion: nil)
     }
     
-    func alertOrderIsReady(_ juice: Juice) -> UIAlertController {
+    private func alertOrderIsReady(_ juice: Juice) -> UIAlertController {
         let alert = UIAlertController(title: "완성!", message: "\(juice.rawValue) 나왔습니다! 맛있게 드세요!", preferredStyle: UIAlertController.Style.alert)
         let ok = UIAlertAction(title: "Ok", style: .default, handler: { _ in
             NSLog("The \"OK\" alert occured.")})
@@ -77,7 +76,7 @@ class MainViewController: UIViewController {
         return alert
     }
     
-    func alertOrderIsNotReady() -> UIAlertController {
+    private func alertOrderIsNotReady() -> UIAlertController {
         let alert = UIAlertController(title: "재고 부족!", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: UIAlertController.Style.alert)
         let ok = UIAlertAction(title: "Ok", style: .default, handler: { _ in
             NSLog("The \"OK\" alert occured.")
