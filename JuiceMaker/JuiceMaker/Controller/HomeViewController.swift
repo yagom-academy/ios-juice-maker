b087816b6e3c01bf8686563839f54a715e18abc9
DragonYG <kioswe92@gmail.com>
2022-09-08 12:55:54 +0900

    refactor: ActionButton을 통합하여 눌린버튼의 이름을 takeJuiceMenu->orderFruitJuice함수로 전달되는 식으로 변경

--- JuiceMaker/JuiceMaker/Controller/HomeViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/HomeViewController.swift
@@ -16,82 +16,71 @@ class HomeViewController: UIViewController {
     @IBOutlet weak private var kiwiStockLabel: UILabel!
     @IBOutlet weak private var mangoStockLabel: UILabel!
     
-    @IBAction private func orderStrawberryJuice(_ sender: UIButton) {
-        if juiceMaker.makeJuice(of: .strawBerry) {
-            showSuccessAlert(of: .strawBerry)
+    @IBOutlet weak private var strawberryBananaJuiceOrderButton: UIButton!
+    @IBOutlet weak private var mangoKiwiJuiceOrderButton: UIButton!
+    @IBOutlet weak private var strawberryJuiceOrderButton: UIButton!
+    @IBOutlet weak private var bananaJuiceOrderButton: UIButton!
+    @IBOutlet weak private var pineappleJuiceOrderButton: UIButton!
+    @IBOutlet weak private var kiwiJuiceJuiceOrderButton: UIButton!
+    @IBOutlet weak private var mangoJuiceOrderButton: UIButton!
+    
+    @IBAction private func orderFruitJuice(_ sender: UIButton) {
+        guard let juice = takeJuiceMenu(of: sender) else { return }
+        
+        if juiceMaker.makeJuice(of: juice) {
+            showSuccessAlert(of: juice)
             checkInventory()
         } else {
             showFailAlert()
         }
     }
     
-    @IBAction private func orderBananaJuice(_ sender: UIButton) {
-        if juiceMaker.makeJuice(of: .banana) {
-            showSuccessAlert(of: .banana)
-            checkInventory()
-        } else {
-            showFailAlert()
-        }
-    }
-    
-    @IBAction private func orderPineappleJuice(_ sender: UIButton) {
-        if juiceMaker.makeJuice(of: .pineApple) {
-            showSuccessAlert(of: .pineApple)
-            checkInventory()
-        } else {
-            showFailAlert()
-        }
-    }
-    
-    @IBAction private func orderKiwiJuice(_ sender: UIButton) {
-        if juiceMaker.makeJuice(of: .kiwi) {
-            showSuccessAlert(of: .kiwi)
-            checkInventory()
-        } else {
-            showFailAlert()
-        }
-    }
-    
-    @IBAction private func orderMangoJuice(_ sender: UIButton) {
-        if juiceMaker.makeJuice(of: .mango) {
-            showSuccessAlert(of: .mango)
-            checkInventory()
-        } else {
-            showFailAlert()
-        }
-    }
-    
-    @IBAction private func orderStrawberryBananaJuice(_ sender: UIButton) {
-        if juiceMaker.makeJuice(of: .strawBerryBanana) {
-            showSuccessAlert(of: .strawBerryBanana)
-            checkInventory()
-        } else {
-            showFailAlert()
-        }
-    }
-    
-    @IBAction private func orderMangoKiwi(_ sender: UIButton) {
-        if juiceMaker.makeJuice(of: .mangoKiwi) {
-            showSuccessAlert(of: .mangoKiwi)
-            checkInventory()
-        } else {
-            showFailAlert()
+    func takeJuiceMenu(of sender: UIButton) -> Juice? {
+        switch sender {
+        case strawberryBananaJuiceOrderButton:
+            return .strawBerryBanana
+        case mangoKiwiJuiceOrderButton:
+            return .mangoKiwi
+        case strawberryJuiceOrderButton:
+            return .strawBerry
+        case bananaJuiceOrderButton:
+            return .banana
+        case pineappleJuiceOrderButton:
+            return .pineApple
+        case kiwiJuiceJuiceOrderButton:
+            return .kiwi
+        case mangoJuiceOrderButton:
+            return .mango
+        default:
+            return nil
         }
     }
     
     private func showSuccessAlert(of juice: Juice) {
-        let alert = UIAlertController(title: "제조 완료", message: "\(juice.rawValue)쥬스 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+        let alert = UIAlertController(title: "제조 완료",
+                                      message: "\(juice.rawValue)쥬스 나왔습니다! 맛있게 드세요!",
+                                      preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인",
+                               style: .default,
+                               handler: nil)
+        
         alert.addAction(ok)
         present(alert, animated: true, completion: nil)
     }
     
     private func showFailAlert() {
-        let alert = UIAlertController(title: "재고 부족", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "재고수정", style: .default, handler: {_ in
+        let alert = UIAlertController(title: "재고 부족",
+                                      message: "재료가 모자라요. 재고를 수정할까요?",
+                                      preferredStyle: .alert)
+        let ok = UIAlertAction(title: "재고수정",
+                               style: .default,
+                               handler: {_ in
             self.presentChangeInventoryViewController()
         })
-        let cancel = UIAlertAction(title: "취소", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "취소",
+                                   style: .default,
+                                   handler: nil)
+        
         alert.addAction(cancel)
         alert.addAction(ok)
         present(alert, animated: true, completion: nil)
