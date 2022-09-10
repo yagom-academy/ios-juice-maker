db6cb3022f32ce3b6d6d9485eef3ca21cd585414
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-09-10 21:15:47 +0900

    fix: makeJuice 메서드 수정

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,16 +7,19 @@ struct JuiceMaker {
     let fruitStorage = FruitStore(defaultStock: 10)
     
     func makeJuice(to order: Juice, in view: ViewController) {
-        do {
-            try fruitStorage.checkStockBeUsed(in: order)
-            view.showSuccessAlert(to: order)
-        } catch OrderError.outOfStock {
-            view.showFailedAlert()
-        } catch OrderError.emptyStock {
-            print(OrderError.emptyStock.message)
-        } catch {
-            print("알 수 없는 오류입니다.")
+        for (fruit, amountOfFruit) in order.recipeOfJuice.ingredient {
+            do {
+                let stock = try fruitStorage.checkEmptyStock(to: fruit)
+                let remainingStock = try fruitStorage.checkStockAmount(to: stock, with: amountOfFruit)
+                fruitStorage.changeFruitStock(to: remainingStock, to: fruit)
+                view.showSuccessAlert(to: order) // 최종 목적지
+            } catch OrderError.outOfStock {
+                view.showFailedAlert()
+            } catch OrderError.emptyStock {
+                print(OrderError.emptyStock.message)
+            } catch {
+                print("알 수 없는 오류입니다.")
+            }
         }
-        fruitStorage.changeFruitStock(to: order)
     }
 }
