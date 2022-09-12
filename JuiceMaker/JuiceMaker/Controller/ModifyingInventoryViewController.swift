a4e7be26ef99620e2b13dca16ac0f4a6334dfeb8
oneStar92 <onestar7984@gmail.com>
2022-09-12 19:12:12 +0900

    feat: stepper 기능 구현.

--- JuiceMaker/JuiceMaker/Controller/ModifyingInventoryViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ModifyingInventoryViewController.swift
@@ -9,6 +9,12 @@ class ModifyingInventoryViewController: UIViewController {
     @IBOutlet weak var pineappleLabel: UILabel!
     @IBOutlet weak var kiwiLabel: UILabel!
     
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    
     var receivedFruitStore: FruitStore?
     
     override func viewDidLoad() {
@@ -26,14 +32,19 @@ class ModifyingInventoryViewController: UIViewController {
             switch fruit {
             case .strawberry:
                 strawberryLabel.text = "\(inventory)"
+                strawberryStepper.value = Double(inventory)
             case .banana:
                 bananaLabel.text = "\(inventory)"
+                bananaStepper.value = Double(inventory)
             case .kiwi:
                 kiwiLabel.text = "\(inventory)"
+                kiwiStepper.value = Double(inventory)
             case .pineapple:
                 pineappleLabel.text = "\(inventory)"
+                pineappleStepper.value = Double(inventory)
             case .mango:
                 mangoLabel.text = "\(inventory)"
+                mangoStepper.value = Double(inventory)
             }
         }
     }
@@ -42,6 +53,23 @@ class ModifyingInventoryViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func changeStrawberryInventory(_ sender: UIStepper) {
+        strawberryLabel.text = Int(sender.value).description
+    }
     
+    @IBAction func changeBananaInventory(_ sender: UIStepper) {
+        bananaLabel.text = Int(sender.value).description
+    }
     
+    @IBAction func changeMangoInventory(_ sender: UIStepper) {
+        mangoLabel.text = Int(sender.value).description
+    }
+    
+    @IBAction func changePineappleInventory(_ sender: UIStepper) {
+        pineappleLabel.text = Int(sender.value).description
+    }
+    
+    @IBAction func changeKiwiInventory(_ sender: UIStepper) {
+        kiwiLabel.text = Int(sender.value).description
+    }
 }
