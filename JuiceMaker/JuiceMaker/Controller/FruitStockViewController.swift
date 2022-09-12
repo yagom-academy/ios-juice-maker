b336cdd6c0362d37d0a522f3f88980a36b877939
sunnycookie <sunnyday2007@naver.com>
2022-09-12 11:21:50 +0900

    feat: stepper 작동에 반응하는 changeFruitStock 메서드 구현

--- JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
@@ -8,6 +8,12 @@ class FruitStockViewController: UIViewController {
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
     
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
     lazy var fruitLabel: [Fruit: UILabel] = [
         .strawberry: strawberryStockLabel,
         .banana: bananaStockLabel,
@@ -31,5 +37,29 @@ class FruitStockViewController: UIViewController {
     @IBAction func touchUpDismissButton(_ sender: Any) {
         self.dismiss(animated: true)
     }
+    
+    @IBAction func changeFruitStock(_ sender: UIStepper) {
+        let fruitStepper: [UIStepper: Fruit] = [
+            strawberryStepper: .strawberry,
+            bananaStepper: .banana,
+            pineappleStepper: .pineapple,
+            kiwiStepper: .kiwi,
+            mangoStepper: .mango
+        ]
+
+        sender.minimumValue = -1
+        
+        guard let fruit = fruitStepper[sender],
+              let fruitLabel = fruitLabel[fruit] else { return }
+        
+        FruitStore.sharedFruitStore.changeStockOf(fruit: fruit, by: Int(sender.value))
+        
+        fruitLabel.text = String(FruitStore.sharedFruitStore.fetchStockOf(fruit))
+        
+        sender.value = 0
+    }
+    
+    
+    
     
 }
