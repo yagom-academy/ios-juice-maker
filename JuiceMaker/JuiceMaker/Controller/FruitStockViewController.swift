baa201bb15ac87c8573e998be2308edb98c9a7b9
dev-summer <summerly812@gmail.com>
2022-09-12 10:53:12 +0900

    feat: FruitStockVC fruitLabel값 업데이트 메서드 구현

--- JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
@@ -7,9 +7,25 @@ class FruitStockViewController: UIViewController {
     @IBOutlet weak var pineappleStockLabel: UILabel!
     @IBOutlet weak var kiwiStockLabel: UILabel!
     @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    lazy var fruitLabel: [Fruit: UILabel] = [
+        .strawberry: strawberryStockLabel,
+        .banana: bananaStockLabel,
+        .pineapple: pineappleStockLabel,
+        .kiwi: kiwiStockLabel,
+        .mango: mangoStockLabel
+    ]
+    
+    func updateFruitStockLabel() {
+        for (fruit, label) in fruitLabel {
+            label.text = String(FruitStore.sharedFruitStore.fetchStockOf(fruit))
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateFruitStockLabel()
     }
     
     @IBAction func touchUpDismissButton(_ sender: Any) {
