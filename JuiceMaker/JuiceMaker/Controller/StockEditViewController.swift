d73497b8440aff199d78b0bb099483f8797000d5
lnhk98 <hozzang666@gmai.com>
2022-09-12 11:47:25 +0900

    feat: StockEditViewController에 stockLabel 생성

--- JuiceMaker/JuiceMaker/Controller/StockEditViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockEditViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class StockEditViewController: UIViewController {
+    
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
