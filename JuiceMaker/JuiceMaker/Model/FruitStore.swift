6076da6f6c8e5b3eb68a249d21ceef1cfd2b0d27
lws2269 <lws2269@kakao.com>
2022-09-12 21:35:54 +0900

    refactor: 필요없는 주석 삭제

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -11,7 +11,6 @@ class FruitStore {
     static let shared = FruitStore()
     var fruitsStock: [Fruit : Int] = [:] {
         didSet {
-            print("코딱지")
             NotificationCenter.default.post(name: NSNotification.Name("fruitStockChange"),
                                             object: nil,
                                             userInfo: ["fruitsStock": fruitsStock])
