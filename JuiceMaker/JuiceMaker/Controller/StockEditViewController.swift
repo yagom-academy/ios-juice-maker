cba4d45ba178807501b2edf54a72eaf6fad202f6
Gundy <korea1880@naver.com>
2022-09-12 12:11:49 +0900

    feat: Stepper를 통해 입력되는 값을 통해 재고를 변경시켜주는 changeStock 메서드 추가 및 기존 touchUpStepper 메서드 기능 분리

--- JuiceMaker/JuiceMaker/Controller/StockEditViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockEditViewController.swift
@@ -10,11 +10,6 @@ import UIKit
 class StockEditViewController: UIViewController {
     
     weak var delegate: FruitStoreDelegate?
-    var amountOfStrawberryStockToChange: Int = 0
-    var amountOfBananaStockToChange: Int = 0
-    var amountOfKiwiStockToChange: Int = 0
-    var amountOfPineappleStockToChange: Int = 0
-    var amountOfMangoStockToChange: Int = 0
     
     @IBOutlet weak var strawberryStockLabel: UILabel!
     @IBOutlet weak var bananaStockLabel: UILabel!
@@ -29,7 +24,7 @@ class StockEditViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateFruitStockLabel()
+        updateFruitStock()
     }
     
     @IBAction func touchUpDismissButton(_ sender: UIButton) {
@@ -39,83 +34,67 @@ class StockEditViewController: UIViewController {
     @IBAction func touchUpStepper(_ sender: UIStepper) {
         switch sender {
         case straberryStockStepper:
-            amountOfStrawberryStockToChange = Int(sender.value)
-            guard let currentStock = try? delegate?.fruitStore.currentStock(of: .strawberry) else {
-                return
+            if let changedStock = changeStock(of: .strawberry, by: sender) {
+                strawberryStockLabel.text = "\(changedStock)"
             }
-            guard currentStock + amountOfStrawberryStockToChange >= 0 else {
-                amountOfStrawberryStockToChange += 1
-                return
-            }
-            strawberryStockLabel.text = "\(currentStock + amountOfStrawberryStockToChange)"
         case bananaStockStepper:
-            amountOfBananaStockToChange = Int(sender.value)
-            guard let currentStock = try? delegate?.fruitStore.currentStock(of: .banana) else {
-                return
+            if let changedStock = changeStock(of: .banana, by: sender) {
+                bananaStockLabel.text = "\(changedStock)"
             }
-            guard currentStock + amountOfBananaStockToChange >= 0 else {
-                amountOfBananaStockToChange += 1
-                return
-            }
-            bananaStockLabel.text = "\(currentStock + amountOfBananaStockToChange)"
         case kiwiStockStepper:
-            amountOfKiwiStockToChange = Int(sender.value)
-            guard let currentStock = try? delegate?.fruitStore.currentStock(of: .kiwi) else {
-                return
+            if let changedStock = changeStock(of: .kiwi, by: sender) {
+                kiwiStockLabel.text = "\(changedStock)"
             }
-            guard currentStock + amountOfKiwiStockToChange >= 0 else {
-                amountOfKiwiStockToChange += 1
-                return
-            }
-            kiwiStockLabel.text = "\(currentStock + amountOfKiwiStockToChange)"
         case pineappleStockStepper:
-            amountOfPineappleStockToChange = Int(sender.value)
-            guard let currentStock = try? delegate?.fruitStore.currentStock(of: .pineapple) else {
-                return
+            if let changedStock = changeStock(of: .pineapple, by: sender) {
+                pineappleStockLabel.text = "\(changedStock)"
             }
-            guard currentStock + amountOfPineappleStockToChange >= 0 else {
-                amountOfPineappleStockToChange += 1
-                return
-            }
-            pineappleStockLabel.text = "\(currentStock + amountOfPineappleStockToChange)"
         case mangoStockStepper:
-            amountOfMangoStockToChange = Int(sender.value)
-            guard let currentStock = try? delegate?.fruitStore.currentStock(of: .mango) else {
-                return
+            if let changedStock = changeStock(of: .mango, by: sender) {
+                mangoStockLabel.text = "\(changedStock)"
             }
-            guard currentStock + amountOfMangoStockToChange >= 0 else {
-                amountOfMangoStockToChange += 1 
-                return
-            }
-            mangoStockLabel.text = "\(currentStock + amountOfMangoStockToChange)"
         default:
             return
         }
     }
     
-    func updateFruitStockLabel() {
+    func changeStock(of fruit: Fruit, by stockStepper: UIStepper) -> Int? {
+        guard let currentStock = try? delegate?.fruitStore.currentStock(of: fruit) else {
+            return nil
+        }
+        let stockAmountToChange: Int = Int(stockStepper.value) - currentStock
+        delegate?.fruitStore.changeStock(of: fruit, by: stockAmountToChange)
+        return currentStock + stockAmountToChange
+    }
+    
+    func updateFruitStock() {
         if let strawberryStock = try? delegate?.fruitStore.currentStock(of: .strawberry) {
             strawberryStockLabel.text = "\(strawberryStock)"
+            straberryStockStepper.value = Double(strawberryStock)
         } else {
             strawberryStockLabel.text = FruitStoreError.notExist
         }
         if let bananaStock = try? delegate?.fruitStore.currentStock(of: .banana) {
             bananaStockLabel.text = "\(bananaStock)"
+            bananaStockStepper.value = Double(bananaStock)
         } else {
             bananaStockLabel.text = FruitStoreError.notExist
         }
         if let kiwiStock = try? delegate?.fruitStore.currentStock(of: .kiwi) {
             kiwiStockLabel.text = "\(kiwiStock)"
+            kiwiStockStepper.value = Double(kiwiStock)
         } else {
             kiwiStockLabel.text = FruitStoreError.notExist
         }
         if let pineappleStock = try? delegate?.fruitStore.currentStock(of: .pineapple) {
             pineappleStockLabel.text = "\(pineappleStock)"
+            pineappleStockStepper.value = Double(pineappleStock)
         } else {
             pineappleStockLabel.text = FruitStoreError.notExist
         }
         if let mangoStock = try? delegate?.fruitStore.currentStock(of: .mango) {
             mangoStockLabel.text = "\(mangoStock)"
+            mangoStockStepper.value = Double(mangoStock)
         } else {
             mangoStockLabel.text = FruitStoreError.notExist
         }
