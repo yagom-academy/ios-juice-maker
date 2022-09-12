2adccf6804e3a94057923cab67e97ff8cc56b4b5
ash-youu <ash.you.dev@gmail.com>
2022-09-12 14:17:13 +0900

    feat: ModifyViewController 재고수정 화면  데이터 설정 기능 추가

--- JuiceMaker/JuiceMaker/Controller/ModifyViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ModifyViewController.swift
@@ -5,19 +5,45 @@
 
 import UIKit
 
+protocol Stockable {
+}
+
 class ModifyViewController: UIViewController {
 	static let identifier = "presentModifyViewController"
+	var inventory: [Fruit: Int] = Fruit.beginningStock
 	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        changeNavBackgroundColor()
-    }
-    
-    func changeNavBackgroundColor() {
-        navigationController?.navigationBar.backgroundColor = .systemGray5
-    }
+	@IBOutlet weak var strawberryStockLabel: UILabel!
+	@IBOutlet weak var bananaStockLabel: UILabel!
+	@IBOutlet weak var pineAppleStockLabel: UILabel!
+	@IBOutlet weak var kiwiStockLabel: UILabel!
+	@IBOutlet weak var mangoStockLabel: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		changeNavBackgroundColor()
+		setUpLabels()
+	}
+	
+	func changeNavBackgroundColor() {
+		navigationController?.navigationBar.backgroundColor = .systemGray5
+	}
 	
 	@IBAction func didTapDismissButton(_ sender: UIBarButtonItem) {
 		dismiss(animated: true)
+	}
+	
+	func setUpLabels() {
+		[
+			strawberryStockLabel,
+			bananaStockLabel,
+			pineAppleStockLabel,
+			kiwiStockLabel,
+			mangoStockLabel,
+		].compactMap { $0 }.forEach {
+			if let fruit = Fruit(rawValue: $0.tag),
+			   let stock = inventory[fruit]?.description {
+				$0.text = stock
+			}
+		}
 	}
 }
