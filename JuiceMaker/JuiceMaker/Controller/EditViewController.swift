cbfa3aadd7e3f71e3a116cc3e9a7dbd3e323744c
woong <workplayhard1@naver.com>
2022-09-13 10:43:59 +0900

    feat: Stepper의 value를 설정하는 함수 구현

--- JuiceMaker/JuiceMaker/Controller/EditViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/EditViewController.swift
@@ -7,12 +7,19 @@ import UIKit
 
 class EditViewController: UIViewController {
     var store: FruitStore?
+    var stock: [Int]?
     
     @IBOutlet var fruitCountLabels: [UILabel]!
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateStockCount()
+        setStepperValue()
     }
     
     @IBAction private func tappedApplyButton(_ sender: UIButton) {
@@ -23,8 +30,11 @@ class EditViewController: UIViewController {
         dismiss(animated: true)
     }
     
+    @IBAction func strawberryStepper(_ sender: UIStepper) {
+        print(Int(sender.value))
+    }
     private func updateStockCount() {
-        guard var newStock = store?.stock else { return }
+        guard var newStock = stock else { return }
         
         for fruitCountLabel in fruitCountLabels {
             if newStock.isEmpty { return }
@@ -34,5 +44,15 @@ class EditViewController: UIViewController {
     
     func setStore(from store: FruitStore) {
         self.store = store
+        self.stock = store.stock
     }
+    
+    func setStepperValue() {
+        strawberryStepper.value = Double(stock?[0] ?? 0)
+        bananaStepper.value = Double(stock?[1] ?? 0)
+        pineappleStepper.value = Double(stock?[2] ?? 0)
+        kiwiStepper.value = Double(stock?[3] ?? 0)
+        mangoStepper.value = Double(stock?[4] ?? 0)
+    }
+    
 }
