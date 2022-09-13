2694cf9205b99fff3dfca1c586e65b6abf4a349d
zhilly11 <zhzh1x4@gmail.com>
2022-09-13 18:29:54 +0900

    feat: 재고 수정화면에 label과 action button 작성 및 연결

--- JuiceMaker/JuiceMaker/Controller/FruitStockEditorViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStockEditorViewController.swift
@@ -3,6 +3,18 @@
 import UIKit
 
 class FruitStockEditorViewController: UIViewController {
+
+    @IBOutlet weak var mutableStrawberryAmountLabel: UILabel!
+    @IBOutlet weak var mutableBananaAmountLabel: UILabel!
+    @IBOutlet weak var mutablePineappleAmountLabel: UILabel!
+    @IBOutlet weak var mutableKiwiAmountLabel: UILabel!
+    @IBOutlet weak var mutableMangoAmountLabel: UILabel!
+    
+    @IBOutlet weak var strawberryAmountStepper: UIStepper!
+    @IBOutlet weak var bananaAmountStepper: UIStepper!
+    @IBOutlet weak var pineappleAmountStepper: UIStepper!
+    @IBOutlet weak var kiwiAmountStepper: UIStepper!
+    @IBOutlet weak var mangoAmountStepper: UIStepper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,4 +25,23 @@ class FruitStockEditorViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func touchupStrawberryAmountStepper(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func touchupBananaAmountStepper(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func touchupPineappleAmountStepper(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func touchupKiwiAmountStepper(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func touchupMangoAmountStepper(_ sender: UIStepper) {
+        
+    }
 }
