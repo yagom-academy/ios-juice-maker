5871f7e7dd9e8cf02d8d5b352e7d0f7192636b1e
yjjem <marksim0606@gmail.com>
2022-09-14 21:13:15 +0900

    refactor: 불필요한 함수 제거

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -9,16 +9,4 @@ struct JuiceMaker {
     func makeJuice(_ juice: Juice) {
         JuiceMaker.sharedStore.useStockForRecipe(of: juice)
     }
-    
-    func requestStockAvailability(for juice: Juice) -> Bool {
-        do {
-            try JuiceMaker.sharedStore.checkStockAvailability(of: juice)
-            return true
-        } catch StockError.notEnoughFruit {
-            print("재고 부족")
-        } catch {
-            print("Unknown Error")
-        }
-        return false
-    }
 }
