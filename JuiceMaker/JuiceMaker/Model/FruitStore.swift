cc03ceb4a64da7e9168365c2ba8c7a8a1684f44e
parkmuri <smxhmu@naver.com>
2023-01-02 14:18:11 +0900

    feat: fruitStore타입 정의

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,29 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
     
+    func buyStrawberry(howMany: Int) {
+        strawberry += howMany
+    }
+    
+    func buyBanana(howMany: Int) {
+        banana += howMany
+    }
+    
+    func buyPineapple(howMany: Int) {
+        pineapple += howMany
+    }
+    
+    func buyKiwi(howMany: Int) {
+        kiwi += howMany
+    }
+    
+    func buyMango(howMany: Int) {
+        mango += howMany
+    }
 }
