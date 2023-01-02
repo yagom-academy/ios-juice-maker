6555f4c2ee2366a192b4b594f06032ecdbce912e
HarryHyeon <hharry0728@gmail.com>
2023-01-02 15:46:47 +0900

    feat: JuiceMaker 클래스에서 딸기,바나나,키위 주스 만들기 함수 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,17 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    var fruitStore = FruitStore.shared
     
+    func makeStrawberryJuice() {
+        fruitStore.subtractStock(fruit: .strawberry, amount: 16)
+    }
+    
+    func makeBananaJuice() {
+        fruitStore.subtractStock(fruit: .banana, amount: 2)
+    }
+    
+    func makeKiwiJuice() {
+        fruitStore.subtractStock(fruit: .kiwi, amount: 3)
+    }
 }
