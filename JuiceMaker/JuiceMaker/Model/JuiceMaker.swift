41339435658a6168835cd4c76e8de60b6d9c6730
minsu3007 <minsu3007@gmail.com>
2023-01-02 16:19:27 +0900

    feat: JuiceMenu 열거형 레시피 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -14,6 +14,25 @@ enum JuiceMenu {
     case mangoJuice
     case strawberryBananaJuice
     case mangoKiwiJuice
+    
+    var recipe: [Fruit: Int] {
+        switch self {
+        case .strawberryJuice:
+            return [.strawberry: 16]
+        case .bananaJuice:
+            return [.banana: 2]
+        case .kiwiJuice:
+            return [.kiwi: 3]
+        case .pineappleJuice:
+            return [.pineapple: 2]
+        case .mangoJuice:
+            return [.mango: 3]
+        case .strawberryBananaJuice:
+            return [.strawberry: 10, .banana: 1]
+        case .mangoKiwiJuice:
+            return [.mango: 2, .kiwi: 1]
+        }
+    }
 }
 
 // 쥬스 메이커 타입
@@ -21,14 +40,6 @@ struct JuiceMaker {
     var fruit = FruitStore()
     
     func makeJuice(juice: JuiceMenu) {
-        switch juice {
-        case .strawberryJuice:
-        case .bananaJuice:
-        case .kiwiJuice:
-        case .pineappleJuice:
-        case .mangoJuice:
-        case .strawberryBananaJuice:
-        case .mangoKiwiJuice:
-        }
+        
     }
 }
