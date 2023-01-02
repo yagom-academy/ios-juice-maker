3d10b6b0a69c7b42a7d29e6894fda7d4beb58867
이주명 <gzzjk159@naver.com>
2023-01-02 15:31:54 +0900

    feat: 열거형 함수 구현
    error타입 열거형 생성, 재고확인 함수 구현
    
    feat

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -7,6 +7,10 @@ enum Fruit {
     case strawberry, banana, kiwi, mango, pineapple
 }
 
+enum JuiceError: Error {
+    case lackedInventory
+}
+
 // 과일 저장소 타입
 class FruitStore {
     var fruits: [Fruit: Int] = [.strawberry: 10, .banana: 10, .kiwi: 10, .mango: 10, .pineapple: 10]
@@ -22,10 +26,17 @@ class FruitStore {
         guard let storedFruit = self.fruits[fruit] else {
             return
         }
+        self.fruits[fruit] = storedFruit - number
+    }
+    
+    func checkFruit(fruit: Fruit, number: Int) throws -> Bool {
+        guard let storedFruit = self.fruits[fruit] else {
+            return false
+        }
         guard storedFruit >= number else {
-            return
+            throw JuiceError.lackedInventory
         }
         
-        self.fruits[fruit] = storedFruit - number
+        return true
     }
 }
