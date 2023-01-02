665435fca5be5b7d18a2160880d317b6723a3007
Hanseul Lee <61936306+christy-hs-lee@users.noreply.github.com>
2023-01-02 18:57:12 +0900

    feat: ErrorMessage 열거형 생성, addOne 메서드 생성

---
+++ JuiceMaker/JuiceMaker/Model/ErrorMessage.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorMessage.swift
+//  JuiceMaker
+//
+//  Created by Christy Lee on 2023/01/02.
+//
+
+import Foundation
+
+enum ErrorMessage: Error {
+    case outOfRange
+}
--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,14 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    //var fruits = [String: Int]()
+    var fruits = ["딸기": 10, "바나나": 10, "파인애플": 10, "키위": 10, "망고": 10]
     
+    func addOne(selectedFruit: String) throws {
+        guard var number = fruits[selectedFruit] else {
+            throw ErrorMessage.outOfRange
+        }
+        number += 1
+        fruits[selectedFruit] = number
+    }
 }
