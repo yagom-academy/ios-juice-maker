85e36b72df5edac8c2aded6121529db628f8d69b
kimseongj <ksj8277@naver.com>
2023-01-02 15:10:36 +0900

    feat: checkStock 함수 생성, Fruit클래스에서 name프로퍼티 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -9,16 +9,22 @@ import Foundation
 // 과일 저장소 타입
 class FruitStore {
     class Fruit {
+        let name: String
         var stock = 10
     }
     
-    let strawberry = Fruit()
-    let banana = Fruit()
-    let kiwi = Fruit()
-    let pineapple = Fruit()
-    let mango = Fruit()
+    let strawberry = Fruit(name: "딸기")
+    let banana = Fruit(name: "바나나")
+    let kiwi = Fruit(name: "키위")
+    let pineapple = Fruit(name: "파인애플")
+    let mango = Fruit(name: "망고")
     
     func changeInventory(fruit: Fruit, number: Int) {
         fruit.stock -= number
+    }
+    
+    func checkStock(fruit: Fruit) {
+        let resultMessage = "\(fruit.name)의 재고는 \(fruit.stock)개입니다."
+        print(resultMessage)
     }
 }
