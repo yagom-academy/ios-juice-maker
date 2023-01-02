f1ed713e2509348629db448919571006f876c209
minsu3007 <minsu3007@gmail.com>
2023-01-02 14:37:55 +0900

    chore: 주석, 함수 생성
    주석내용변경 및 increaseFruit, decresaeFruit 함수 선언
    
    chore

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -1,12 +1,21 @@
-//
 //  JuiceMaker - FruitStore.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-//
+//  Created by Vetto, 레옹아범 on 2023.1.2
 
 import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var strawberry = 10
+    var banana = 10
+    var kiwi = 10
+    var mango = 10
+    var pineapple = 10
     
+    func increaseFruit(number: Int) {
+        
+    }
+    
+    func decreaseFruit(number: Int) {
+        
+    }
 }
