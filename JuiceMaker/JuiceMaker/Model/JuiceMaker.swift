0467f9b15433c4c9c864ae22428ed24a17d2eacc
HarryHyeon <hharry0728@gmail.com>
2023-01-02 17:25:07 +0900

    refactor: Recipe 타입을 이용해 주스만들기 makeJuice 메서드로 통합

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,35 +8,16 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    var fruitStore = FruitStore.shared
+    let fruitStore = FruitStore.shared
     
-    func makeStrawberryJuice() {
-        fruitStore.subtractStock(fruit: .strawberry, amount: 16)
-    }
-    
-    func makeBananaJuice() {
-        fruitStore.subtractStock(fruit: .banana, amount: 2)
-    }
-    
-    func makeKiwiJuice() {
-        fruitStore.subtractStock(fruit: .kiwi, amount: 3)
-    }
-    
-    func makePineappleJuice() {
-        fruitStore.subtractStock(fruit: .pineapple, amount: 2)
-    }
-    
-    func makeStrawberryBananaJuice() {
-        fruitStore.subtractStock(fruit: .strawberry, amount: 10)
-        fruitStore.subtractStock(fruit: .banana, amount: 1)
-    }
-    
-    func makeMangoJuice() {
-        fruitStore.subtractStock(fruit: .mango, amount: 3)
-    }
-    
-    func makeMangoKiwiJuice() {
-        fruitStore.subtractStock(fruit: .mango, amount: 2)
-        fruitStore.subtractStock(fruit: .kiwi, amount: 1)
+    func makeJuice(recipe: [Fruit: Int]) {
+        guard fruitStore.isStocked(recipe: recipe) else {
+            return
+        }
+        
+        for ingrediant in recipe {
+            fruitStore.subtractStock(fruit: ingrediant.key, amount: ingrediant.value)
+        }
+        //print(fruitStore.fruitStock)
     }
 }
