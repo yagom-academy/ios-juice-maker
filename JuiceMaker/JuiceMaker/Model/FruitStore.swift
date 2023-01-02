3aef0752278d4fba511ca647f32ceba9c6bc90fa
HarryHyeon <hharry0728@gmail.com>
2023-01-02 15:14:00 +0900

    feat: FruitStore 클래스에서 보유하고 있는 과일의 딕셔너리 선언, addStock함수 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,31 @@
 
 import Foundation
 
+enum Fruit: String {
+    case strawberry = "strawberry"
+    case banana = "banana"
+    case pineapple = "pineapple"
+    case kiwi = "kiwi"
+    case mango = "mango"
+}
+
 // 과일 저장소 타입
 class FruitStore {
+    var fruitStock: [Fruit: Int] = [
+        .strawberry: 10,
+        .banana: 10,
+        .pineapple: 10,
+        .kiwi: 10,
+        .mango: 10
+    ]
     
+    func addStock(fruit: Fruit, amount: Int) {
+        guard let selectedFruitStock = fruitStock[fruit] else {
+            return
+        }
+        
+        fruitStock.updateValue(selectedFruitStock + amount, forKey: fruit)
+    }
 }
+
+
