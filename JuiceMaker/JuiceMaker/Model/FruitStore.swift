a1a97fec7aaa6e9aa8ce5a939d445be35321b5b5
Rhode-park <jinah.park.16@gmail.com>
2023-01-02 19:13:58 +0900

    feat: minusOne 메서드 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,7 +8,6 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    //var fruits = [String: Int]()
     var fruits = ["딸기": 10, "바나나": 10, "파인애플": 10, "키위": 10, "망고": 10]
     
     func addOne(selectedFruit: String) throws {
@@ -18,4 +17,13 @@ class FruitStore {
         number += 1
         fruits[selectedFruit] = number
     }
+    
+    func minusOne(selectedFruit: String) throws {
+        guard var number = fruits[selectedFruit] else {
+            throw ErrorMessage.outOfRange
+        }
+        number -= 1
+        fruits[selectedFruit] = number
+    }
 }
+
