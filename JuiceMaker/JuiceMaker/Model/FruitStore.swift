b783f1df15f8d7140803ecd1b2acbbbfebf205dd
kimseongj <ksj8277@naver.com>
2023-01-02 14:53:05 +0900

    feat: 클래스 Fruit 선언, 과일인스턴스 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,14 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    class Fruit {
+        var stock = 10
+    }
+    
+    let strawberry = Fruit()
+    let banana = Fruit()
+    let kiwi = Fruit()
+    let pineapple = Fruit()
+    let mango = Fruit()
     
 }
