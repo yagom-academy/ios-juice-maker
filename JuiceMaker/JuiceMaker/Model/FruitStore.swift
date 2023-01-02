68202d82c417789793870950f36a8ddca0762b2d
whk1140 <whk1140@gmail.com>
2023-01-02 14:36:15 +0900

    feat: Fruit 열거형 생성, stock 프로퍼티 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -4,9 +4,17 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
 // 과일 저장소 타입
 class FruitStore {
+    enum Fruit {
+        case strawberry
+        case banana
+        case pineapple
+        case mango
+    }
+    
+    var stock: [Fruit: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .mango: 10]
+    
+    
     
 }
