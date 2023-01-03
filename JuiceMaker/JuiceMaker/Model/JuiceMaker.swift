b591500bf2bc1d7afea57ba8ef7d3cb781baabd3
이주명 <gzzjk159@naver.com>
2023-01-03 10:51:43 +0900

    feat: 함수 로직 변경
    makeJuice함수 로직 변경, checkFruitInJuice 로직 변경
    
    feat

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,23 +6,38 @@ struct JuiceMaker {
     let fruitStore = FruitStore()
     
     func makeJuice(juice: JuiceMenu) {
-        do {
-            try checkFruitInJuice(juice: juice)
+//        do {
+//            try checkFruitInJuice(juice: juice)
+//            useFruit(juice: juice)
+//            print(fruitStore.fruits)
+//        } catch FruitStoreError.noExistInventory {
+//            print("해당 과일 존재하지 않음")
+//        } catch FruitStoreError.lackedInventory {
+//            print("과일 재고 부족")
+//        } catch {
+//            print(error)
+//        }
+        if checkFruitInJuice(juice: juice) {
             useFruit(juice: juice)
-            print(fruitStore.fruits)
-        } catch FruitStoreError.noExistInventory {
-            print("해당 과일 존재하지 않음")
-        } catch FruitStoreError.lackedInventory {
-            print("과일 재고 부족")
-        } catch {
-            print(error)
         }
     }
     
-    func checkFruitInJuice(juice: JuiceMenu) throws {
+    func checkFruitInJuice(juice: JuiceMenu) -> Bool {
         for (fruit, num) in juice.recipe {
-            try fruitStore.checkFruit(fruit: fruit, number: num)
+            do {
+                try fruitStore.checkFruit(fruit: fruit, number: num)
+            } catch FruitStoreError.noExistInventory {
+                print("해당 과일 존재하지 않음")
+                return false
+            } catch FruitStoreError.lackedInventory {
+                print("과일 재고 부족")
+                return false
+            } catch {
+                print(error)
+                return false
+            }
         }
+        return true
     }
     
     func useFruit(juice: JuiceMenu) {
