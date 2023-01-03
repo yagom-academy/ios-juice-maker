3e3bf0fcac873ebbd19ca41f9670d7e7d668a6f9
ROWAN <kimkj93@naver.com>
2023-01-03 10:24:25 +0900

    feat: fruitsStock 딕셔너리 추가, findFruit/makeFruitList 메서드 삭제, class Fruit 삭제

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,45 +8,19 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    class Fruit {
-        let name: String
-        var stock = 10
+    var fruitsStock = ["딸기":10, "망고":10, "바나나":10, "키위":10, "파인애플":10]
+    
+    func changeInventory(fruit: String, number: Int) {
+        guard var selectedStock = self.fruitsStock[fruit],
+              selectedStock > 0 else {
+            return 
+        }
         
-        init(name: String, stock: Int = 10) {
-            self.name = name
-            self.stock = stock
-        }
+        selectedStock -= number
     }
     
-    let strawberry = Fruit(name: "딸기")
-    let banana = Fruit(name: "바나나")
-    let kiwi = Fruit(name: "키위")
-    let pineapple = Fruit(name: "파인애플")
-    let mango = Fruit(name: "망고")
-    
-    //MARK: 배열선언을 안하고 싶은데 굳이 해야되는 사태 발생
-    func makeFruitsList() -> [Fruit] {
-        let fruitsList = [self.strawberry, self.banana, self.kiwi, self.pineapple, self.mango]
-        return fruitsList
-    }
-    
-    
-    
-    func changeInventory(fruit: Fruit, number: Int) {
-        fruit.stock -= number
-    }
-    
-    func findFruit(fruitName: String) -> Fruit? {
-        for i in 0..<makeFruitsList().count {
-            if makeFruitsList()[i].name == fruitName {
-                return makeFruitsList()[i]
-            }
-        }
-        return nil
-    }
-    
-    func checkStock(fruit: Fruit) {
-        let resultMessage = "\(fruit.name)의 재고는 \(fruit.stock)개입니다."
+    func checkStock(fruit: String) {
+        let resultMessage = "\(fruit)의 재고는 \(fruitsStock[fruit])개입니다."
         print(resultMessage)
     }
 }
