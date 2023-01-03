f1c17cc5fb0dd55a96bb49f30463f8fc691a4049
ROWAN <kimkj93@naver.com>
2023-01-03 20:31:07 +0900

    feat: FruitStore initializer 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,12 +6,11 @@ class FruitStore {
     
     var fruitsStock = [Fruits: Int]()
     
-    func append() {
+    func fillFruitsStock() {
         for fruit in Fruits.allCases {
             fruitsStock[fruit] = 10
         }
     }
-    //[:10, "망고":10, "바나나":10, "키위":10, "파인애플":10]
     
     func subtractInventory(fruit: Fruits, number: Int) throws {
         guard let selectedStock = self.fruitsStock[fruit],
@@ -33,6 +32,10 @@ class FruitStore {
             let resultMessage = "\(fruit)의 재고는 \(stock)개입니다."
             print(resultMessage)
         }
+    }
+    
+    init() {
+        self.fillFruitsStock()
     }
 }
 
