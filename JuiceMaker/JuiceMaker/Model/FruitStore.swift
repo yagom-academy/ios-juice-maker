18a1770d251f28297fa15d1bfec9134439d1e8fb
kimseongj <ksj8277@naver.com>
2023-01-03 20:27:04 +0900

    feat: Fruits 열거형 생성, FruitStore클래스에 append 함수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -3,9 +3,17 @@
 //  Copyright © yagom academy. All rights reserved.
 
 class FruitStore {
-    var fruitsStock = ["딸기":10, "망고":10, "바나나":10, "키위":10, "파인애플":10]
     
-    func subtractInventory(fruit: String, number: Int) throws {
+    var fruitsStock = [Fruits: Int]()
+    
+    func append() {
+        for fruit in Fruits.allCases {
+            fruitsStock[fruit] = 10
+        }
+    }
+    //[:10, "망고":10, "바나나":10, "키위":10, "파인애플":10]
+    
+    func subtractInventory(fruit: Fruits, number: Int) throws {
         guard let selectedStock = self.fruitsStock[fruit],
               selectedStock - number >= 0 else {
             throw StockError.outOfStock
@@ -14,16 +22,17 @@ class FruitStore {
         fruitsStock[fruit] = selectedStock - number
     }
     
-    func addInventory(fruit: String, number: Int) {
+    func addInventory(fruit: Fruits, number: Int) {
         if let selectedStock = self.fruitsStock[fruit] {
             fruitsStock[fruit] = selectedStock + number
         }
     }
     
-    func checkStock(fruit: String) {
+    func checkStock(fruit: Fruits) {
         if let stock = fruitsStock[fruit] {
             let resultMessage = "\(fruit)의 재고는 \(stock)개입니다."
             print(resultMessage)
         }
     }
 }
+
---
+++ JuiceMaker/JuiceMaker/Model/Fruits.swift
@@ -0,0 +1,14 @@
+//
+//  Fruits.swift
+//  JuiceMaker
+//
+//  Created by kimseongjun on 2023/01/03.
+//
+
+enum Fruits: CaseIterable {
+    case strawberry
+    case mango
+    case pineapple
+    case kiwi
+    case banana
+}
