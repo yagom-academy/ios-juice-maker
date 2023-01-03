b9498008d63b983be5378e40a6f8a090dfb15915
seunghyunCheon <csh7215@naver.com>
2023-01-03 10:00:55 +0900

    feat: FruiteStore 프로퍼티 정의

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,19 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var strawberry: Int
+    var banana: Int
+    var pineapple: Int
+    var kiwi: Int
+    var mango: Int
     
+    init(number: Int) {
+        strawberry = 10
+        banana = 10
+        pineapple = 10
+        kiwi = 10
+        mango = 10
+    }
 }
+
+
