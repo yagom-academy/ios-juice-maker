44690cab2f16b8318180f70e67a6c6209c5ea609
Andrew-0411 <whk1140@naver.com>
2023-01-04 10:09:46 +0900

    test: ViewController 테스트 코드 삭제

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -10,14 +10,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let juiceMaker: JuiceMaker = JuiceMaker()
-        juiceMaker.fruitStore.addStock(fruit: .strawberry, amount: 100)
-        juiceMaker.makeJuice(juice: .strawberryBananaJuice)
-        
-        
     }
-
-
 }
 
