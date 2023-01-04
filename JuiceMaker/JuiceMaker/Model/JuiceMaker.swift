614ae558287c6b5e51fcd2ed179d79d46b4ca3e4
ROWAN <kimkj93@naver.com>
2023-01-04 10:38:15 +0900

    refactor: JuiceMaker 구조체 수정
    Recipe 열거형 이름 변경 -> Juice, make 메서드 argument type 변경

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -5,9 +5,9 @@
 struct JuiceMaker {
     let songroFruit = FruitStore()
     
-    enum Recipe: String {
-        case mango = "망고", pineapple = "파인애플", banana = "바나나", kiwi = "키위", strawberry = "딸기"
-        case strawberryBanana = "딸바", mangoKiwi = "망고키위"
+    enum Juice: String {
+        case mango = "망고쥬스", pineapple = "파인애플쥬스", banana = "바나나쥬스", kiwi = "키위쥬스", strawberry = "딸기쥬스"
+        case strawberryBanana = "딸바쥬스", mangoKiwi = "망고키위쥬스"
         
         var ingredients: [Fruits: Int] {
             switch self {
@@ -22,15 +22,15 @@ struct JuiceMaker {
         }
     }
     
-    func make(juice: Recipe) {
+    func make(juice: Juice) {
         let ingredients = juice.ingredients
         
         do {
             for (key, value) in ingredients {
                 try songroFruit.subtractStock(of: key, count: value)
-                songroFruit.checkStock(fruit: key)
+                songroFruit.checkStock(of: key)
             }
-            print("주문하신 \(juice.rawValue)쥬스가 나왔습니다!")
+            print("주문하신 \(juice.rawValue)가 나왔습니다!")
         } catch StockError.outOfStock {
             print("재고가 부족합니다.")
         } catch {
