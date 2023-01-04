6c68805b2ad618108bf1d13a3bf26936b06e8a38
seunghyunCheon <csh7215@naver.com>
2023-01-04 10:17:47 +0900

    refactor: ViewController네이밍 수정

--- JuiceMaker/JuiceMaker/Controller/JuiceMakeViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakeViewController.swift
@@ -6,9 +6,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class JuiceMakeViewController: UIViewController {
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
