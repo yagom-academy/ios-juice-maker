e357761b8160f6c9ff06f30d97acb14eabd66aa1
HarryHyeon <hharry0728@gmail.com>
2023-01-04 19:28:53 +0900

    refactor: Recipe 열거형에서 타입 프로퍼티를 case로 변경, 연산프로퍼티 정의, Juice로 이름 변경

--- JuiceMaker/JuiceMaker/Model/Recipe.swift
+++ JuiceMaker/JuiceMaker/Model/Recipe.swift
@@ -5,28 +5,31 @@
 //  Created by Harry, kokkilE.
 //
 
-enum Recipe {
-    static let strawberryJuice: [Fruit: Int] = [
-        .strawberry: 16
-    ]
-    static let bananaJuice: [Fruit: Int] = [
-        .banana: 2
-    ]
-    static let kiwiJuice: [Fruit: Int] = [
-        .kiwi: 3
-    ]
-    static let pineappleJuice: [Fruit: Int] = [
-        .pineapple: 2
-    ]
-    static let strawberryBananaJuice: [Fruit: Int] = [
-        .strawberry: 10,
-        .banana: 1
-    ]
-    static let mangoJuice: [Fruit: Int] = [
-        .mango: 3
-    ]
-    static let mangoKiwiJuice: [Fruit: Int] = [
-        .mango: 2,
-        .kiwi: 1
-    ]
+enum Juice {
+    case strawberryJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberryBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
+    
+    var recipe: [Fruit: Int] {
+        switch self {
+        case .strawberryJuice:
+            return [.strawberry: 16]
+        case .bananaJuice:
+            return [.banana: 2]
+        case .kiwiJuice:
+            return [.kiwi: 3]
+        case .pineappleJuice:
+            return [.pineapple: 2]
+        case .strawberryBananaJuice:
+            return [.strawberry: 10, .banana: 1]
+        case .mangoJuice:
+            return [.mango: 3]
+        case .mangoKiwiJuice:
+            return [.mango: 2, .kiwi: 1]
+        }
+    }
 }
