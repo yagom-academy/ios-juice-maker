4d2941d43ddd9007988c9b799a21a04a3c7d146f
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-01-05 16:10:28 +0900

    feat: navigation controller를 이용한 화면전환과 fruitBasketVC 화면구성

---
+++ JuiceMaker/JuiceMaker/Controller/FruitBasketViewController.swift
@@ -0,0 +1,66 @@
+//
+//  FruitBasketViewController.swift
+//  JuiceMaker
+//
+//  Created by 송종환 on 2023/01/05.
+//
+import UIKit
+
+final class FruitBasketViewController: UIViewController {
+    
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineapppleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpStepper()
+        setUpLabel()
+    }
+
+    func setUpStepper() {
+        [strawberryStepper, bananaStepper, pineapppleStepper, kiwiStepper, mangoStepper].forEach {
+            $0.wraps = true
+            $0.maximumValue = 1000
+            $0.minimumValue = 0
+        }
+    }
+    
+    func setUpLabel() {
+        [strawberryLabel, bananaLabel, pineappleLabel, kiwiLabel, mangoLabel].forEach {
+            $0?.sizeToFit()
+        }
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+    
+        switch sender {
+        case strawberryStepper:
+            strawberryLabel.text = Int(sender.value).description
+            strawberryLabel.sizeToFit()
+        case bananaStepper:
+            bananaLabel.text = Int(sender.value).description
+            bananaLabel.sizeToFit()
+        case pineapppleStepper:
+            pineappleLabel.text = Int(sender.value).description
+            pineappleLabel.sizeToFit()
+        case kiwiStepper:
+            kiwiLabel.text = Int(sender.value).description
+            kiwiLabel.sizeToFit()
+        case mangoStepper:
+            mangoLabel.text = Int(sender.value).description
+            mangoLabel.sizeToFit()
+        default:
+            print("no")
+        }
+    }
+}
+
