17facfa5cd6f1b09acac2f528e4bdfeff0a001ba
ROWAN <kimkj93@naver.com>
2023-01-06 16:44:37 +0900

    feat: failAlert 구현, JuiceMaker Juice열거형 name 연산 프로퍼티 추가

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -1,5 +1,5 @@
 //  JuiceMaker - ViewController.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 
 import UIKit
@@ -18,101 +18,107 @@ final class ViewController: UIViewController {
     @IBOutlet weak var stockOfMango: UILabel!
     
     @IBAction func ClickChangeStock(_ sender: Any) {
-        let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "ChangeStock") as! ChangeStockViewController
+        moveToChangeStockViewController()
+    }
+    
+    func moveToChangeStockViewController() {
+        guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "ChangeStock") as? ChangeStockViewController else { return }
         self.navigationController?.pushViewController(nextVC, animated: false)
+    }
+    
+    func setSuccessAlert(juiceName: JuiceMaker.Juice) {
+        let successAlert = UIAlertController(title: "\(juiceName.name)가 나왔습니다 맛있게 드세요", message: nil, preferredStyle: UIAlertController.Style.alert)
+        let offAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
+        successAlert.addAction(offAction)
+        present(successAlert, animated: true, completion: nil)
+    }
+    
+    func setFailAlert() {
+        let failAlert = UIAlertController(title: "재료가 모자라요. 재고를 수정할까요?", message: nil, preferredStyle: UIAlertController.Style.alert)
+        let yesAction = UIAlertAction(title: "예",
+                                      style: UIAlertAction.Style.default,
+                                      handler: { Action in self.moveToChangeStockViewController() })
+        
+        let noAction = UIAlertAction(title: "아니오", style: UIAlertAction.Style.default, handler: nil)
+        failAlert.addAction(yesAction)
+        failAlert.addAction(noAction)
+        present(failAlert, animated: true, completion: nil)
     }
     
     @IBAction func orderStrawberryBananaJuice(_ sender: UIButton) {
         do {
             try juiceMaker.make(juice: .strawberryBanana)
-            let successAlert = UIAlertController(title: "딸바쥬스가 나왔습니다 맛있게 드세요", message: nil, preferredStyle: UIAlertController.Style.alert)
-            let offAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
-            successAlert.addAction(offAction)
-            present(successAlert, animated: true, completion: nil)
+            setSuccessAlert(juiceName: .strawberryBanana)
         } catch StockError.outOfStock {
-            //let Alert: UIAlertController()
+            setFailAlert()
         } catch {
             print(error)
         }
     }
+    
     @IBAction func orderStrawberryJuice(_ sender: UIButton) {
         do {
             try juiceMaker.make(juice: .strawberry)
-            let successAlert = UIAlertController(title: "딸기쥬스가 나왔습니다 맛있게 드세요", message: nil, preferredStyle: UIAlertController.Style.alert)
-            let offAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
-            successAlert.addAction(offAction)
-            present(successAlert, animated: true, completion: nil)
+            setSuccessAlert(juiceName: .strawberry)
         } catch StockError.outOfStock {
-            //let Alert: UIAlertController()
+            setFailAlert()
         } catch {
             print(error)
         }
     }
+    
     @IBAction func orderBananaJuice(_ sender: UIButton) {
         do {
             try juiceMaker.make(juice: .banana)
-            let successAlert = UIAlertController(title: "바나나쥬스가 나왔습니다 맛있게 드세요", message: nil, preferredStyle: UIAlertController.Style.alert)
-            let offAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
-            successAlert.addAction(offAction)
-            present(successAlert, animated: true, completion: nil)
+            setSuccessAlert(juiceName: .banana)
         } catch StockError.outOfStock {
-            //let Alert: UIAlertController()
+            setFailAlert()
         } catch {
             print(error)
         }
     }
+    
     @IBAction func orderPineappleJuice(_ sender: UIButton) {
         do {
             try juiceMaker.make(juice: .pineapple)
-            let successAlert = UIAlertController(title: "파인애플쥬스가 나왔습니다 맛있게 드세요", message: nil, preferredStyle: UIAlertController.Style.alert)
-            let offAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
-            successAlert.addAction(offAction)
-            present(successAlert, animated: true, completion: nil)
+            setSuccessAlert(juiceName: .pineapple)
         } catch StockError.outOfStock {
-            //let Alert: UIAlertController()
+            setFailAlert()
         } catch {
             print(error)
         }
     }
+    
     @IBAction func orderKiwiJuice(_ sender: UIButton) {
         do {
             try juiceMaker.make(juice: .kiwi)
-            let successAlert = UIAlertController(title: "키위쥬스가 나왔습니다 맛있게 드세요", message: nil, preferredStyle: UIAlertController.Style.alert)
-            let offAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
-            successAlert.addAction(offAction)
-            present(successAlert, animated: true, completion: nil)
+            setSuccessAlert(juiceName: .kiwi)
         } catch StockError.outOfStock {
-            //let Alert: UIAlertController()
+            setFailAlert()
         } catch {
             print(error)
         }
     }
+    
     @IBAction func orderMangoJuice(_ sender: UIButton) {
         do {
             try juiceMaker.make(juice: .mango)
-            let successAlert = UIAlertController(title: "망고쥬스가 나왔습니다 맛있게 드세요", message: nil, preferredStyle: UIAlertController.Style.alert)
-            let offAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
-            successAlert.addAction(offAction)
-            present(successAlert, animated: true, completion: nil)
+            setSuccessAlert(juiceName: .mango)
         } catch StockError.outOfStock {
-            //let Alert: UIAlertController()
+            setFailAlert()
         } catch {
             print(error)
         }
     }
+    
     @IBAction func orderMangoKiwiJuice(_ sender: UIButton) {
         do {
             try juiceMaker.make(juice: .mangoKiwi)
-            let successAlert = UIAlertController(title: "망키쥬스가 나왔습니다 맛있게 드세요", message: nil, preferredStyle: UIAlertController.Style.alert)
-            let offAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
-            successAlert.addAction(offAction)
-            present(successAlert, animated: true, completion: nil)
+            setSuccessAlert(juiceName: .mangoKiwi)
         } catch StockError.outOfStock {
-            //let Alert: UIAlertController()
+            setFailAlert()
         } catch {
             print(error)
         }
     }
 }
-
-
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -1,13 +1,13 @@
 //  JuiceMaker - JuiceMaker.swift
-//  Created by 송준, Rowan. 
+//  Created by 송준, Rowan.
 //  Copyright © yagom academy. All rights reserved.
 
 struct JuiceMaker {
     private let fruitStorage = FruitStore()
     
-    enum Juice: String {
-        case mango = "망고쥬스", pineapple = "파인애플쥬스", banana = "바나나쥬스", kiwi = "키위쥬스", strawberry = "딸기쥬스"
-        case strawberryBanana = "딸바쥬스", mangoKiwi = "망고키위쥬스"
+    enum Juice {
+        case mango, pineapple, banana, kiwi, strawberry
+        case strawberryBanana, mangoKiwi
         
         var ingredients: [Fruits: Int] {
             switch self {
@@ -18,6 +18,18 @@ struct JuiceMaker {
             case .strawberry: return [.strawberry: 16]
             case .strawberryBanana: return [.strawberry: 10, .banana: 1]
             case .mangoKiwi: return [.mango: 2, .kiwi: 1]
+            }
+        }
+        
+        var name: String {
+            switch self {
+            case .mango: return "망고쥬스"
+            case .pineapple: return "파인애플쥬스"
+            case .banana: return "바나나쥬스"
+            case .kiwi: return "키위쥬스"
+            case .strawberry: return "딸기쥬스"
+            case .strawberryBanana: return "딸바쥬스"
+            case .mangoKiwi: return "망키쥬스"
             }
         }
     }
@@ -34,7 +46,7 @@ struct JuiceMaker {
         for (key, value) in ingredients {
             fruitStorage.subtractStock(of: key, count: value)
         
-        print("주문하신 \(juice.rawValue)가 나왔습니다!")
+        print("주문하신 \(juice.name)가 나왔습니다!")
         }
     }
 }
