0aa6289f458f213e072b35e46da46db962c311bd
이주명 <gzzjk159@naver.com>
2023-01-06 20:01:03 +0900

    feat: updateLabel 함수 구현
    updateLabel 함수 구현
    
    feat

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,9 +8,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let juiceMaker = JuiceMaker()
+    
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateLabel(juice: juiceMaker.getFruitsInStore())
+    }
+    
+    func updateLabel(juice: [Fruit: Int]) {
+        guard let strawberry = juice[.strawberry],
+              let banana = juice[.banana],
+              let kiwi = juice[.kiwi],
+              let pineapple = juice[.pineapple],
+              let mango = juice[.mango] else { return }
+        
+        strawberryLabel.text = String(strawberry)
+        bananaLabel.text = String(banana)
+        kiwiLabel.text = String(kiwi)
+        pineappleLabel.text = String(pineapple)
+        mangoLabel.text = String(mango)
     }
 }
 
