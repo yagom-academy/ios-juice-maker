3be2cf5118dfcf2bfb5cbcc026047b1a83517400
HarryHyeon <hharry0728@gmail.com>
2023-01-06 10:15:15 +0900

    feat: 재고수정 버튼 클릭시 화면전환 기능 구현

--- JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakerViewController.swift
@@ -10,9 +10,17 @@ class JuiceMakerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
     
+    @IBAction func touchUpEditStockButton(_ sender: UIBarButtonItem) {
+        presentStockManager()
+    }
+    
+    func presentStockManager() {
+        guard let stockManagerNC = self.storyboard?.instantiateViewController(withIdentifier: "StockManagerNavigationController") as? UINavigationController else { return }
+                        
+        self.present(stockManagerNC, animated: true)
+    }
 }
 
