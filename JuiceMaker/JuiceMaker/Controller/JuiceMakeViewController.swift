d69f0a274a34fbcf913703c4903b5fbf195093e7
seunghyunCheon <csh7215@naver.com>
2023-01-09 15:49:25 +0900

    feat: fruitStore 데이터 레이블 연결

--- JuiceMaker/JuiceMaker/Controller/JuiceMakeViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakeViewController.swift
@@ -8,10 +8,16 @@ import UIKit
 
 final class JuiceMakeViewController: UIViewController {
 
+    let fruitStore = FruitStore.shared
+    
+    var currentFruitBasket: [Fruit: Int] {
+        return FruitStore.shared.shareFruitBasket()
+    }
+    
     @IBOutlet weak var strawberryLabel: UILabel!
     @IBOutlet weak var bananaLabel: UILabel!
     @IBOutlet weak var pineappleLabel: UILabel!
-    @IBOutlet weak var kiwilabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
     @IBOutlet weak var mangoLabel: UILabel!
     
     override func viewDidLoad() {
@@ -21,6 +27,23 @@ final class JuiceMakeViewController: UIViewController {
         juiceMaker.make(.mangoKiwiJuice)
         
    }
+        setUpLabel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setUpLabel()
+    }
+    
+    func setUpLabel() {
+        strawberryLabel.text = currentFruitBasket[.strawberry]?.description
+        bananaLabel.text = currentFruitBasket[.banana]?.description
+        pineappleLabel.text = currentFruitBasket[.pineapple]?.description
+        kiwiLabel.text = currentFruitBasket[.kiwi]?.description
+        mangoLabel.text = currentFruitBasket[.mango]?.description
+        [strawberryLabel, bananaLabel, pineappleLabel, kiwiLabel, mangoLabel].forEach {
+            $0.sizeToFit()
+        }
+    }
 
 
 }
