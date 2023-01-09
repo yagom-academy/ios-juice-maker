28a302c0b17efc3b121ad04ff51544fcf4d4838c
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-01-09 15:13:02 +0900

    feat: JuiceMakerViewController Label 연결

--- JuiceMaker/JuiceMaker/Controller/JuiceMakeViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMakeViewController.swift
@@ -8,9 +8,16 @@ import UIKit
 
 final class JuiceMakeViewController: UIViewController {
 
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwilabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let juiceMaker = JuiceMaker()
+        
         
     }
 
