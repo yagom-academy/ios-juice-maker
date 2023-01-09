9cfa4241b55a54abf08881470d5c1f3d635f05a7
parkmuri <smxhmu@naver.com>
2023-01-09 15:07:01 +0900

    feat: 뷰컨트롤 라벨에 과일 재고 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,12 +7,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet weak var bananaLabel: UILabel!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
+    let fruitStore = FruitStore.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateStockLabel()
         // Do any additional setup after loading the view.
     }
-
+    
+    func updateStockLabel() {
+        let fruits = fruitStore.fruits.mapValues{ String($0) }
+        
+        strawberryLabel.text = fruits[Fruit.strawberry]
+        bananaLabel.text = fruits[Fruit.banana]
+        pineappleLabel.text = fruits[Fruit.pineapple]
+        kiwiLabel.text = fruits[Fruit.kiwi]
+        mangoLabel.text = fruits[Fruit.mango]
+    }
 
 }
 
