c7dfdd5e3eb7e977c60c2bd64993f0d7a8cef375
Hanseul Lee <61936306+christy-hs-lee@users.noreply.github.com>
2023-01-10 12:08:14 +0900

    refactor: 네이밍 수정

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -32,63 +32,7 @@ final class ViewController: UIViewController {
     }
     
     @IBAction func touchJuiceButton(_ sender: UIButton) {
-        setButton(tag: sender.tag)
-    }
-    
-    func makeAlertJuiceDidMade(juice: Juice) {
-        let alertJuiceDidMade = UIAlertController(title: "성공", message: "\(juice) 나왔습니다! 맛있게 드세요!", preferredStyle: UIAlertController.Style.alert)
-        let takeJuice = UIAlertAction(title: "쥬스 받기", style: .default)
-        alertJuiceDidMade.addAction(takeJuice)
-        present(alertJuiceDidMade, animated: false, completion: nil)
-    }
-    
-    func makeAlertOutOfStock() {
-        let alertOutOfStock = UIAlertController(title: "실패", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: UIAlertController.Style.alert)
-        let modifyYes = UIAlertAction(title: "예", style: .default) { (action) in
-            guard let stockModifyButton = self.storyboard?.instantiateViewController(withIdentifier: "stockModifyViewController") else { return }
-            self.navigationController?.pushViewController(stockModifyButton, animated: true)
-        }
-        let modifyNo = UIAlertAction(title: "아니오", style: .cancel)
-        alertOutOfStock.addAction(modifyYes)
-        alertOutOfStock.addAction(modifyNo)
-        present(alertOutOfStock, animated: false, completion: nil)
-    }
-    
-    func setButton(tag: Int) {
-        let juiceMaker = JuiceMaker()
-        switch tag {
-        case 1:
-            if juiceMaker.makeJuice(juiceName: .딸바쥬스) != nil {
-                makeAlertJuiceDidMade(juice: .딸바쥬스)
-            } else { makeAlertOutOfStock() }
-        case 2:
-            if juiceMaker.makeJuice(juiceName: .딸기쥬스) != nil {
-                makeAlertJuiceDidMade(juice: .딸기쥬스)
-            } else { makeAlertOutOfStock() }
-        case 3:
-            if juiceMaker.makeJuice(juiceName: .바나나쥬스) != nil {
-                makeAlertJuiceDidMade(juice: .바나나쥬스)
-            } else { makeAlertOutOfStock() }
-        case 4:
-            if juiceMaker.makeJuice(juiceName: .파인애플쥬스) != nil {
-                makeAlertJuiceDidMade(juice: .파인애플쥬스)
-            } else { makeAlertOutOfStock() }
-        case 5:
-            if juiceMaker.makeJuice(juiceName: .망고키위쥬스) != nil {
-                makeAlertJuiceDidMade(juice: .망고키위쥬스)
-            } else { makeAlertOutOfStock() }
-        case 6:
-            if juiceMaker.makeJuice(juiceName: .키위쥬스) != nil {
-                makeAlertJuiceDidMade(juice: .키위쥬스)
-            } else { makeAlertOutOfStock() }
-        case 7:
-            if juiceMaker.makeJuice(juiceName: .망고쥬스) != nil {
-                makeAlertJuiceDidMade(juice: .망고쥬스)
-            } else { makeAlertOutOfStock() }
-        default:
-            break
-        }
-        displayStocks()
+        selectJuiceButton(tag: sender.tag)
     }
     
     func displayStocks() {
@@ -109,12 +53,67 @@ final class ViewController: UIViewController {
         }
     }
     
+    func selectJuiceButton(tag: Int) {
+        let juiceMaker = JuiceMaker()
+        switch tag {
+        case 1:
+            if juiceMaker.makeJuice(juiceName: .딸바쥬스) != nil {
+                presentAlertJuiceDidMade(juice: .딸바쥬스)
+            } else { presentAlertOutOfStock() }
+        case 2:
+            if juiceMaker.makeJuice(juiceName: .딸기쥬스) != nil {
+                presentAlertJuiceDidMade(juice: .딸기쥬스)
+            } else { presentAlertOutOfStock() }
+        case 3:
+            if juiceMaker.makeJuice(juiceName: .바나나쥬스) != nil {
+                presentAlertJuiceDidMade(juice: .바나나쥬스)
+            } else { presentAlertOutOfStock() }
+        case 4:
+            if juiceMaker.makeJuice(juiceName: .파인애플쥬스) != nil {
+                presentAlertJuiceDidMade(juice: .파인애플쥬스)
+            } else { presentAlertOutOfStock() }
+        case 5:
+            if juiceMaker.makeJuice(juiceName: .망고키위쥬스) != nil {
+                presentAlertJuiceDidMade(juice: .망고키위쥬스)
+            } else { presentAlertOutOfStock() }
+        case 6:
+            if juiceMaker.makeJuice(juiceName: .키위쥬스) != nil {
+                presentAlertJuiceDidMade(juice: .키위쥬스)
+            } else { presentAlertOutOfStock() }
+        case 7:
+            if juiceMaker.makeJuice(juiceName: .망고쥬스) != nil {
+                presentAlertJuiceDidMade(juice: .망고쥬스)
+            } else { presentAlertOutOfStock() }
+        default:
+            break
+        }
+        displayStocks()
+    }
+    
+    func presentAlertJuiceDidMade(juice: Juice) {
+        let alertJuiceDidMade = UIAlertController(title: "성공", message: "\(juice) 나왔습니다! 맛있게 드세요!", preferredStyle: UIAlertController.Style.alert)
+        let takeJuice = UIAlertAction(title: "쥬스 받기", style: .default)
+        alertJuiceDidMade.addAction(takeJuice)
+        present(alertJuiceDidMade, animated: false, completion: nil)
+    }
+    
+    func presentAlertOutOfStock() {
+        let alertOutOfStock = UIAlertController(title: "실패", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: UIAlertController.Style.alert)
+        let yesAction = UIAlertAction(title: "예", style: .default) { (action) in
+            guard let stockModifyButton = self.storyboard?.instantiateViewController(withIdentifier: "stockModifyViewController") else { return }
+            self.navigationController?.pushViewController(stockModifyButton, animated: true)
+        }
+        let noAction = UIAlertAction(title: "아니오", style: .cancel)
+        alertOutOfStock.addAction(yesAction)
+        alertOutOfStock.addAction(noAction)
+        present(alertOutOfStock, animated: false, completion: nil)
+    }
+    
     @IBAction func stockModifyTapped(_ sender: UIBarButtonItem) {
         guard let stockModifyButton = self.storyboard?.instantiateViewController(withIdentifier: "stockModifyViewController") else {
             return
         }
         self.navigationController?.pushViewController(stockModifyButton, animated: true)
     }
-    
 }
 
