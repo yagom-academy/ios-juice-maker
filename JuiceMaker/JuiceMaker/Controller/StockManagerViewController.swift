e8cf4cadcc396be588442cf1ae58591e24d50828
kokkilE <kkuldak1@naver.com>
2023-01-10 15:33:46 +0900

    feat: Stepper 변수 생성, Stepper 조작에 따라 Label 업데이트하는 함수 구현

--- JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockManagerViewController.swift
@@ -15,19 +15,37 @@ final class StockManagerViewController: UIViewController {
     @IBOutlet weak private var kiwiCountLabel: UILabel!
     @IBOutlet weak private var mangoCountLabel: UILabel!
     
+    @IBOutlet weak private var strawberryStepper: UIStepper!
+    @IBOutlet weak private var bananaStepper: UIStepper!
+    @IBOutlet weak private var pineappleStepper: UIStepper!
+    @IBOutlet weak private var kiwiStepper: UIStepper!
+    @IBOutlet weak private var mangoStepper: UIStepper!
+    
     private let fruitStore = FruitStore.shared
     
     private var fruitLabelsDictionary: [Fruit: UILabel] = [:]
+    private var fruitSteppersDictionary: [UIStepper: Fruit] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         defineDictionary()
         configureFruitCountLabels()
+        
+        //strawberryStepper.value = Double(fruitStore.getStockCountToString(of: .strawberry)!)!
     }
     
     @IBAction private func touchUpDismissButton(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)        
+        dismiss(animated: true)
+    }
+    
+    @IBAction private func touchUpStepper(_ sender: UIStepper) {
+        guard let fruit = fruitSteppersDictionary[sender] else {
+            return
+        }
+        
+        fruitLabelsDictionary[fruit]?.text = Int(sender.value).description
+        
     }
     
     private func defineDictionary() {
@@ -37,6 +55,14 @@ final class StockManagerViewController: UIViewController {
             .kiwi: kiwiCountLabel,
             .pineapple: pineappleCountLabel,
             .mango: mangoCountLabel
+        ]
+        
+        fruitSteppersDictionary = [
+            strawberryStepper: .strawberry,
+            bananaStepper: .banana,
+            kiwiStepper: .kiwi,
+            pineappleStepper: .pineapple,
+            mangoStepper: .mango
         ]
     }
 
