5da353d7632d7fbf695a94757505a5897dfa005b
Jun <sa9343@naver.com>
2023-01-11 22:49:19 +0900

    feat : EditStockViewController의 identifier 선언

--- JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/EditStockViewController.swift
@@ -7,6 +7,8 @@
 import UIKit
 
 final class EditStockViewController: UIViewController {
+    static let identifier = "EditStockViewController"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
