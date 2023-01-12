f1933d73ec9a1187eca91067a48acf3a653f8bdf
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-01-12 13:54:58 +0900

    feat: setUpStepper함수 구현과 stepper초기 세팅

--- JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStoreViewController.swift
@@ -7,6 +7,11 @@
 import UIKit
 
 final class FruitStoreViewController: UIViewController {
+    let fruitStore = FruitStore.shared
+        
+    var currentFruitBasket: [Fruit: Int] {
+        return FruitStore.shared.fruitsBasket
+    }
     
     @IBOutlet weak var strawberryLabel: UILabel!
     @IBOutlet weak var bananaLabel: UILabel!
@@ -16,11 +21,29 @@ final class FruitStoreViewController: UIViewController {
     
     @IBOutlet weak var strawberryStepper: UIStepper!
     @IBOutlet weak var bananaStepper: UIStepper!
-    @IBOutlet weak var pineapppleStepper: UIStepper!
+    @IBOutlet weak var pineappleStepper: UIStepper!
     @IBOutlet weak var kiwiStepper: UIStepper!
     @IBOutlet weak var mangoStepper: UIStepper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func setUpStepper() {
+        if let strawBerryValue = currentFruitBasket[.strawberry],
+           let bananaValue = currentFruitBasket[.banana],
+           let pineappleValue = currentFruitBasket[.pineapple],
+           let kiwiValue = currentFruitBasket[.kiwi],
+           let mangoValue = currentFruitBasket[.mango] {
+            strawberryStepper.value = Double(strawBerryValue)
+            bananaStepper.value = Double(bananaValue)
+            pineappleStepper.value = Double(pineappleValue)
+            kiwiStepper.value = Double(kiwiValue)
+            mangoStepper.value = Double(mangoValue)
+        }
+        [strawberryStepper, bananaStepper, pineappleStepper, kiwiStepper, mangoStepper].forEach {
+            $0.maximumValue = 1000
+            $0.minimumValue = 0
+        }
     }
 }
