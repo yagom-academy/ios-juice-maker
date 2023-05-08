ed3fcc748d9c052199ce4d0998e85ef5518472e8
Hayeon Kim <qoocrab@gmail.com>
2023-05-08 20:04:27 +0900

    feat: UseFruit 함수 생성
    - UseFruit 함수: 쥬스를 만들기 위해 재고에서 과일을 꺼내는 함수
    - JuiceError, Juice enum 생성

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,28 @@
 
 import Foundation
 
+enum Juice {
+    case strawberry, banana, kiwi, pineapple, strawNana, mango, mangKi
+}
+
+enum JuiceError: Error {
+    case insufficientFruitStock
+    case notFoundFruitInformation
+}
+
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    
+    private let fruitStore = FruitStore()
+
+    private func useFruits(_ fruit: Fruit, _ amount: Int) throws {
+        guard let stock = fruitStore.checkStock(of: fruit) else {
+            throw JuiceError.notFoundFruitInformation
+        }
+        
+        if stock < amount {
+            throw JuiceError.insufficientFruitStock
+        }
+        
+        try fruitStore.consumeStock(of: fruit, by: amount)
+    }
 }
