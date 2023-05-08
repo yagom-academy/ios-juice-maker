fc96f199aec0af018c6b4760d5fb14b95cb00d2a
Hayeon Kim <qoocrab@gmail.com>
2023-05-08 19:34:51 +0900

    feat: enum Fruit 생성, FruitStore - checkStock 함수 생성
    - inventory를 만들어서 10개씩 재고 지정.
    - checkStock 함수: 과일 인벤토리로부터 과일 개수를 확인
    - checkStock dictionary 예외처리

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,25 @@
 
 import Foundation
 
+enum Fruit {
+    case strawberry, banana, pineapple, kiwi, mango
+}
+
 // 과일 저장소 타입
 class FruitStore {
+    private var inventory: [Fruit: Int] = [
+        .strawberry: 10,
+        .banana: 10,
+        .pineapple: 10,
+        .kiwi: 10,
+        .mango: 10
+    ]
     
+    func checkStock(of fruit: Fruit) -> Int? {
+        if let stock = inventory[fruit] {
+            return stock
+        } else {
+            return nil
+        }
+    }
 }
