9e0e2e51a99db9b03f51d061c58bbcd2d55d1ae4
Hoon94 <dleognsdl1@naver.com>
2023-05-08 22:52:35 +0900

    feat: Recipe enum 추가

---
+++ JuiceMaker/JuiceMaker/Model/Recipe.swift
@@ -0,0 +1,18 @@
+//
+//  Recipe.swift
+//  JuiceMaker
+//
+//  Created by Daehoon Lee on 2023/05/08.
+//
+
+import Foundation
+
+enum Recipe {
+    case strawberryJuice(strawberry: Int = 16)
+    case bananaJuice(banana: Int = 2)
+    case kiwiJuice(kiwi: Int = 3)
+    case pineappleJuice(pineapple: Int = 2)
+    case strawberryBananaJuice(strawberry: Int = 10, banana: Int = 1)
+    case mangoJuice(mango: Int = 3)
+    case mangoKiwiJuice(mango: Int = 2, kiwi: Int = 1)
+}
