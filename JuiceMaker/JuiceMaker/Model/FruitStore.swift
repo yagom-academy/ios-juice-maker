8ec2f182c3dabeb8eb8d42fdb6c4e1e637179444
JusBug <pksgwo123@gmail.com>
2023-05-08 11:31:18 +0900

    feat: 과일 딕셔너리 생성 및 addFruit() 함수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,10 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var fruits: [String: Int] = ["딸기": 10, "바나나": 10, "파인애플": 10, "키위":10, "망고": 10]
     
+    func addFruit(_ fruit: String) {
+        
+    }
 }
+
