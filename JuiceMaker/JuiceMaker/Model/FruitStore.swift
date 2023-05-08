ee33e91b99a57af4d9a50be9ebdccb458fb7e59f
h-suo <phstoo1021@naver.com>
2023-05-08 12:24:25 +0900

    feat: InputError 열거형 생성 및 printErrorMessage() 함수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -10,28 +10,55 @@ import Foundation
 class FruitStore {
     var fruitsDictionary: [String: Int] = ["딸기": 10, "바나나": 10, "파인애플": 10, "키위": 10, "망고": 10]
     
-    func getUserInput() -> String {
-        guard let userInput: String = readLine() else { return "" }
+    func printErrorMessage() {
+        do { try getUserInput()
+        } catch InputError.invalidInputError {
+            print("잘못된 입력입니다.")
+        } catch {
+            print("알수없는 오류가 발생하였습니다.")
+        }
+        
+        do { try inputFruit()
+        } catch InputError.invalidInputError {
+            print("잘못된 입력입니다.")
+        } catch {
+            print("알수없는 오류가 발생하였습니다.")
+        }
+        
+        do { try changeFruitsNumber()
+        } catch InputError.invalidInputError {
+            print("잘못된 입력입니다.")
+        } catch {
+            print("알수없는 오류가 발생하였습니다.")
+        }
+    }
+    
+    func getUserInput() throws -> String {
+        guard let userInput: String = readLine() else { throw InputError.invalidInputError }
         
         return userInput
     }
     
-    func inputFruit() -> String {
+    func inputFruit() throws -> String {
         print("과일을 입력해주세요: ", terminator: "")
-        let fruit: String = getUserInput()
-        guard let _ = fruitsDictionary[fruit] else { return "" }
+        let fruit: String = try getUserInput()
+        guard let _ = fruitsDictionary[fruit] else { throw InputError.invalidInputError }
         
         return fruit
     }
     
-    func changeFruitsNumber() {
-        let inputFruits = inputFruit()
+    func changeFruitsNumber() throws {
+        let inputFruits = try inputFruit()
         
         print("변경할 개수를 입력해주세요.: ", terminator: "")
-        let userInput = getUserInput()
-        guard let number = Int(userInput) else { return }
+        let userInput = try getUserInput()
+        guard let number = Int(userInput) else { throw InputError.invalidInputError }
         fruitsDictionary[inputFruits] = number
+    }
+    
+    func checkStoock() {
+        
     }
 }
 
-feat: changeFruitsNumber() 및 getUserInput() 함수 생성
+
---
+++ JuiceMaker/JuiceMaker/Model/InputError.swift
@@ -0,0 +1,12 @@
+//
+//  InputError.swift
+//  JuiceMaker
+//
+//  Created by 표현수 on 2023/05/08.
+//
+
+import Foundation
+
+enum InputError: Error {
+    case invalidInputError
+}
