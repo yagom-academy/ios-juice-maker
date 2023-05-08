e2b9569702fa6b090a3af5a2a9dd2cb66e5e84ba
h-suo <phstoo1021@naver.com>
2023-05-08 11:49:24 +0900

    feat: inputFruit() 함수 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -10,6 +10,12 @@ import Foundation
 class FruitStore {
     var fruits: [String: Int] = ["딸기": 10, "바나나": 10, "파인애플": 10, "키위":10, "망고": 10]
     
+    func inputFruit() {
+        print("과일을 입력해주세요: ", terminator: "")
+        var fruit: String = readLine()
+        
+        guard let fruits[fruit]
+    }
     func addFruit(_ fruit: String) {
         
     }
