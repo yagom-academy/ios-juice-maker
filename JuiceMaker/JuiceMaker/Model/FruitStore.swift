d9799efc39707bb8ce30b0fba814e7e852950280
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-05-08 11:22:21 +0900

    feat: FruitStore 내 Int타입의 과일 프로퍼티들 추가, 각 과일 프로퍼티 수량을 변경하는 메소드 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,29 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    var strawberry: Int = 10
+    var banana: Int = 10
+    var pineapple: Int = 10
+    var kiwi: Int = 10
+    var mango: Int = 10
     
+    func changeStrawberry(amount: Int) {
+        strawberry += amount
+    }
+    
+    func changeBanana(amount: Int) {
+        banana += amount
+    }
+    
+    func changePineapple(amount: Int) {
+        pineapple += amount
+    }
+    
+    func changeKiwi(amount: Int) {
+        kiwi += amount
+    }
+    
+    func changeMango(amount: Int) {
+        mango += amount
+    }
 }
