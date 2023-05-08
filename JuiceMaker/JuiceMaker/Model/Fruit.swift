3c59671ad436fd98396c5bb4a8b470b86378cb57
Hoon94 <dleognsdl1@naver.com>
2023-05-08 21:27:03 +0900

    feat: Fruit 클래스 추가

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,18 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by 훈맹구 on 2023/05/08.
+//
+
+import Foundation
+
+class Fruit {
+    var name: String
+    var quantity: Int
+    
+    init(name: String, quantity: Int) {
+        self.name = name
+        self.quantity = quantity
+    }
+}
