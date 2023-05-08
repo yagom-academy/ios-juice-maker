6d3ea961c344c2b2ff5f2c16681c157822d9981c
hemg2 <hemg2@naver.com>
2023-05-08 18:59:59 +0900

    feat: 시작 함수 추가 및 함수 네이밍 변경

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -4,8 +4,6 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
 // 과일 저장소 타입
 class FruitStore {
     var strawBerry: Int
@@ -14,98 +12,68 @@ class FruitStore {
     var kiwi: Int
     var mango: Int
     
-    init(strawBerry: Int, banana: Int, pineApple: Int, kiwi: Int, mango: Int) {
-        self.strawBerry = 10
-        self.banana = 10
-        self.pineApple = 10
-        self.kiwi = 10
-        self.mango = 10
+    init(strawBerry: Int = 10, banana: Int = 10, pineApple: Int = 10, kiwi: Int = 10, mango: Int = 10) {
+        self.strawBerry = strawBerry
+        self.banana = banana
+        self.pineApple = pineApple
+        self.kiwi = kiwi
+        self.mango = mango
     }
     
-    func addFruit(_ fruit: String, _ count: Int) {
+    private func addStoredFruit(_ fruit: String, _ stock: Int) {
         switch fruit {
         case "딸기":
-            self.strawBerry += count
+            self.strawBerry += stock
         case "바나나":
-            self.banana += count
+            self.banana += stock
         case "파인애플":
-            self.pineApple += count
+            self.pineApple += stock
         case "키위":
-            self.kiwi += count
+            self.kiwi += stock
         case "망고":
-            self.mango += count
+            self.mango += stock
         default:
             print("없는 과일")
         }
     }
     
-    func removeFruit(_ fruit: String, _ count: Int) {
+    private func removeStoredFruit(_ fruit: String, _ stock: Int) {
         switch fruit {
         case "딸기":
-            self.strawBerry -= count
+            self.strawBerry -= stock
         case "바나나":
-            self.banana -= count
+            self.banana -= stock
         case "파인애플":
-            self.pineApple -= count
+            self.pineApple -= stock
         case "키위":
-            self.kiwi -= count
+            self.kiwi -= stock
         case "망고":
-            self.mango -= count
+            self.mango -= stock
         default:
             print("없는 과일")
         }
     }
     
-    func removeFruitJuice(_ fruit: String) throws {
-        switch fruit {
+    func removeFruit(_ juice: String) {
+        switch juice {
         case "딸기주스":
-            guard strawBerry > 16 else {
-                throw FruitStoreError.countError
-            }
             self.strawBerry -= 16
         case "바나나주스":
-            guard banana > 2 else {
-                throw FruitStoreError.countError
-            }
             self.banana -= 2
         case "키위주스":
-            guard kiwi > 3 else {
-                throw FruitStoreError.countError
-            }
             self.kiwi -= 3
         case "파인애플주스":
-            guard pineApple > 2 else {
-                throw FruitStoreError.countError
-            }
             self.pineApple -= 2
         case "딸바주스":
-            guard strawBerry > 10, banana < 1 else {
-                throw FruitStoreError.countError
-            }
             self.strawBerry -= 10
             self.banana -= 1
         case "망고주스":
-            guard mango > 3 else {
-                throw FruitStoreError.countError
-            }
             self.mango -= 3
         case "망고키위주스":
-            guard mango > 2, kiwi > 1 else {
-                throw FruitStoreError.countError
-            }
             self.mango -= 2
             self.kiwi -= 1
         default:
             print("")
-        }
-    }
-    func error(_ fruit: String) {
-        do {
-            try removeFruitJuice(fruit)
-        } catch FruitStoreError.countError {
-            print("재고가 부족합니다.")
-        } catch {
-            print(error)
         }
     }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -4,16 +4,57 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
-
 // 쥬스 메이커 타입
 struct JuiceMaker {
-    let fruitStore: FruitStore
+    private let fruitStore: FruitStore = FruitStore()
     
-    func makeJuice(_ juiceName: String) {
-        fruitStore.error(juiceName)
+    func start(_ juiceName: String) {
+        do {
+            try makeJuice("딸기주스")
+        } catch FruitStoreError.countError {
+            print("재고가 부족합니다.")
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func makeJuice(_ juiceName: String) throws {
+        try checkFruitStock(juiceName)
+        fruitStore.removeFruit(juiceName)
+    }
+    
+    private func checkFruitStock(_ juiceName: String) throws {
+        switch juiceName {
+        case "딸기주스":
+            guard fruitStore.strawBerry > 16 else {
+                throw FruitStoreError.countError
+            }
+        case "바나나주스":
+            guard fruitStore.banana > 2 else {
+                throw FruitStoreError.countError
+            }
+        case "키위주스":
+            guard fruitStore.kiwi > 3 else {
+                throw FruitStoreError.countError
+            }
+        case "파인애플주스":
+            guard fruitStore.pineApple > 2 else {
+                throw FruitStoreError.countError
+            }
+        case "딸바주스":
+            guard fruitStore.strawBerry > 10, fruitStore.banana > 1 else {
+                throw FruitStoreError.countError
+            }
+        case "망고주스":
+            guard fruitStore.mango > 3 else {
+                throw FruitStoreError.countError
+            }
+        case "망고키위주스":
+            guard fruitStore.mango > 2, fruitStore.kiwi > 1 else {
+                throw FruitStoreError.countError
+            }
+        default:
+            print("없는 주스입니다.")
+        }
     }
 }
-
-
-
