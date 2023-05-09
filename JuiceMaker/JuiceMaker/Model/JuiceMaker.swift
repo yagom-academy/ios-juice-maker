bc9d7a30d23a059871e6e075c8c68c488ac9b7b7
iOS-Yetti <yechan123456@gmail.net>
2023-05-09 15:37:35 +0900

    feat: 쥬스를 만드는 기능(재고 체크, 재고 소진, 쥬스 제조) 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -5,13 +5,32 @@
 //  last modified by Yetti, yy-ss99, Mary.
 
 struct JuiceMaker {
+    let fruitStore: FruitStore
     
     func make(_ juiceType: Juice) {
+        let recipe = juiceType.checkRecipe()
+        let checkFruit = checkFruit(recipe)
+        
+        switch checkFruit {
+        case .success:
+            print("\(juiceType)이(가) 준비되었습니다.")
+            makeJuice(recipe)
+        case .failure(let error):
+            print(error)
+        }
     }
     
-    private func checkFruit(_ juice: Juice) -> Result<Juice, BuyError> {
-        
-        return .success(juice)
+    private func checkFruit(_ recipe: [IngredientAndAmount]) -> Result<Bool, BuyError> {
+        for ingredient in recipe {
+            guard let stock = fruitStore.fruitStock[ingredient.name],
+                  stock >= ingredient.amount else {
+                return .failure(BuyError.ingredientShortage)
+            }
+        }
+        return .success(true)
+    }
+    
+    private func makeJuice(_ recipe: [IngredientAndAmount]) {
+        recipe.forEach { fruitStore.useStock(fruit: $0) }
     }
 }
-
