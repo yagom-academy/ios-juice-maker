060cc5d4b3ae0569868e607071bb295da8db1f7e
YaRkyungmin <kkgm94@naver.com>
2023-05-09 20:44:47 +0900

    🩹chore: 출력 메세지 수정

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -20,14 +20,14 @@ struct JuiceMaker {
         }
         
         guard isEnoughStock else {
-            print("재고부족")
+            print("재고 부족으로 \(fruitJuice)를 만들 수 없습니다.")
             return
         }
         
         fruitJuice.recipe.forEach {
             fruitStore.reduceFruitCount(at: $0.key, with: $0.value)
         }
-        print("\(fruitJuice) 제조완료")
+        print("주문하신 \(fruitJuice)가 나왔습니다.")
     }
 }
       
