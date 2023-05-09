cb749505012289de3d5fa850b04d87a8536daa7c
DasanKim <tempzipp@gmail.com>
2023-05-09 17:51:29 +0900

    ✨feat: JuiceMaker Type 내 blendFruitJuice method 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -4,9 +4,30 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
-
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    let fruitStore: FruitStore
     
+    init(fruitStore: FruitStore) {
+        self.fruitStore = fruitStore
+    }
+    
+    func blendFruitJuice(fruitJuice: JuiceType) {
+        var isEnoughStock: Bool = true
+        
+        fruitJuice.recipe.forEach {
+            isEnoughStock = fruitStore.checkFruitStock(fruit: $0.key , amount: $0.value)
+        }
+        
+        guard isEnoughStock else {
+            print("재고부족")
+            return
+        }
+        
+        fruitJuice.recipe.forEach {
+            fruitStore.reduceFruitCount(at: $0.key, with: $0.value)
+        }
+        print("\(fruitJuice) 제조완료")
+    }
 }
+      
