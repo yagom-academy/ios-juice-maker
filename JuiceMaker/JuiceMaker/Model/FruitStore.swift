454b1de8b3885074eab8bc13e584e03f6ff56a21
mint3382 <mskim0926@naver.com>
2023-05-09 15:22:12 +0900

    feat: changeFruitCount 함수, Fruit 열거형 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,35 @@
 
 import Foundation
 
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
+
+
 // 과일 저장소 타입
 class FruitStore {
+    private var strawberry: Int = 10
+    private var banana: Int = 10
+    private var pineapple: Int = 10
+    private var kiwi: Int = 10
+    private var mango: Int = 10
     
+    func changeFruitCount(_ fruit: Fruit, count: Int) {
+        switch fruit {
+        case .strawberry:
+            strawberry -= count
+        case .banana:
+            banana -= count
+        case .pineapple:
+            pineapple -= count
+        case .kiwi:
+            kiwi -= count
+        case .mango:
+            mango -= count
+        }
+    }
 }
