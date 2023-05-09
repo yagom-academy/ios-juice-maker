d282adb795b42fd2588f306bfbfbb11bdc769925
Serena <sha0720@naver.com>
2023-05-09 13:08:06 +0900

    feat: fruiteStore 재고 설정, JuiceMaker 케이스 정의

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,13 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    enum Fruit {
+        case strawberry, banana, pineapple, kiwi, mango
+    }
     
+    var fruitStock: [Fruit: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
+    
+    func changeStock(amount: Int, fruit: Fruit) {
+        fruitStock.updateValue(amount, forKey: fruit)
+    }
 }
