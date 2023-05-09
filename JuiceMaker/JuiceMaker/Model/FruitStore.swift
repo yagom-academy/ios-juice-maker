458c02babcbf45fae8c63e7b2cbe57818ae70994
Kobe <mskang@xingxingmobility.com>
2023-05-09 09:51:09 +0900

    feat: FruitStore 생성 및 과일 재고 변경 매서드 구현.

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,20 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    
+	enum Fruit: CaseIterable {
+		case strawberry
+		case banana
+		case pineapple
+		case kiwi
+		case mango
+	}
+	
+	private var inventory: Dictionary<Fruit, Int> = Fruit.allCases.reduce(into: Dictionary<Fruit, Int>()) { $0[$1] = 10 }
+	
+	func changeStock(of fruit: Fruit, minus amount: Int) {
+		guard let fruitStock = inventory[fruit] else { return }
+		guard fruitStock > amount else { return }
+		
+		inventory[fruit] = fruitStock - amount
+	}
 }
