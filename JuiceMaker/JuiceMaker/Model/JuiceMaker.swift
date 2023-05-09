14f1af0a2e74932f5de16eba99cb89a495829853
Hoon94 <dleognsdl1@naver.com>
2023-05-09 14:06:23 +0900

    refactor: 재고 부족 오류 처리 수정

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -10,26 +10,35 @@ import Foundation
 struct JuiceMaker {
     func makeJuice(with recipe: Recipe) {
         let fruitStore: FruitStore = FruitStore()
+        var fruits: [Fruit] = []
+        var fruitsQuantity: [Int] = []
+        
+        switch recipe {
+        case .strawberryJuice(strawberry: let quantity):
+            fruits.append(fruitStore.strawberry)
+            fruitsQuantity.append(quantity)
+        case .bananaJuice(banana: let quantity):
+            fruits.append(fruitStore.banana)
+            fruitsQuantity.append(quantity)
+        case .kiwiJuice(kiwi: let quantity):
+            fruits.append(fruitStore.kiwi)
+            fruitsQuantity.append(quantity)
+        case .pineappleJuice(pineapple: let quantity):
+            fruits.append(fruitStore.pineapple)
+            fruitsQuantity.append(quantity)
+        case .strawberryBananaJuice(let strawberryQuantity, let bananaQuantity):
+            fruits.append(contentsOf: [fruitStore.strawberry, fruitStore.banana])
+            fruitsQuantity.append(contentsOf: [strawberryQuantity, bananaQuantity])
+        case .mangoJuice(mango: let quantity):
+            fruits.append(fruitStore.mango)
+            fruitsQuantity.append(quantity)
+        case .mangoKiwiJuice(let mangoQuantity, let kiwiQuantity):
+            fruits.append(contentsOf: [fruitStore.mango, fruitStore.kiwi])
+            fruitsQuantity.append(contentsOf: [mangoQuantity, kiwiQuantity])
+        }
         
         do {
-            switch recipe {
-            case .strawberryJuice(strawberry: let quantity):
-                try fruitStore.changeStock(of: fruitStore.strawberry, by: quantity)
-            case .bananaJuice(banana: let quantity):
-                try fruitStore.changeStock(of: fruitStore.banana, by: quantity)
-            case .kiwiJuice(kiwi: let quantity):
-                try fruitStore.changeStock(of: fruitStore.kiwi, by: quantity)
-            case .pineappleJuice(pineapple: let quantity):
-                try fruitStore.changeStock(of: fruitStore.pineapple, by: quantity)
-            case .strawberryBananaJuice(let strawberryQuantity, let bananaQuantity):
-                try fruitStore.changeStock(of: fruitStore.strawberry, fruitStore.banana,
-                                           by: strawberryQuantity, bananaQuantity)
-            case .mangoJuice(mango: let quantity):
-                try fruitStore.changeStock(of: fruitStore.mango, by: quantity)
-            case .mangoKiwiJuice(let mangoQuantity, let kiwiQuantity):
-                try fruitStore.changeStock(of: fruitStore.mango, fruitStore.kiwi,
-                                           by: mangoQuantity, kiwiQuantity)
-            }
+            try fruitStore.changeStock(of: fruits, by: fruitsQuantity)
         } catch FruitStoreError.shortageOfStock(let fruit) {
             print("\(fruit.name)의 재고가 부족합니다.")
         } catch {
