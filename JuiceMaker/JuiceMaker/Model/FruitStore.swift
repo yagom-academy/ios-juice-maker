892d6aba42e52ad33ea121d29296c2e7533b3d92
ysj <yyssjj9999@gmail.com>
2023-05-09 11:22:08 +0900

    feat: FruitStore 클래스 정의(재고 변경 메서드 구현)및 Fruits 열거형 정의

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -2,11 +2,18 @@
 //  JuiceMaker - FruitStore.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-//
+//  last modified by Yetti, yy-ss99, Mary.
 
-import Foundation
-
-// 과일 저장소 타입
 class FruitStore {
+    static let initialStock = 10
+    var fruitStock: [Fruits: Int] = [
+        .strawberry: initialStock, .banana: initialStock,
+        .pineapple: initialStock, .mango: initialStock,
+        .kiwi: initialStock
+    ]
     
+    func changeStock(item: Fruits, amount: Int) {
+        fruitStock[item]? += amount
+    }
 }
+
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -2,11 +2,7 @@
 //  JuiceMaker - JuiceMaker.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//  last modified by Yetti, yy-ss99, Mary.
 
-import Foundation
 
-// 쥬스 메이커 타입
-struct JuiceMaker {
-    
-}
+
