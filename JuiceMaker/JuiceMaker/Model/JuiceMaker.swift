0077e43e4c1577c17804bf88468c572064b01ff4
Serena <sha0720@naver.com>
2023-05-09 13:10:44 +0900

    feat: JuiceMaker 케이스 정의

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,34 @@
 
 import Foundation
 
+enum Juice {
+    case strawberryJuice, bananaJuice, kiwiJuice, pineappleJuice, strawberryBananaJuice, mangoJuice, mangoKiwiJuice
+}
+
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    let fruitStore: FruitStore
     
+    init(fruitStore: FruitStore) {
+        self.fruitStore = fruitStore
+    }
+    
+    func makeJuice(juice: Juice, fruitStock: [FruitStore.Fruit: Int]) -> Juice {
+        switch juice {
+        case .strawberryJuice:
+            return .strawberryJuice
+        case .bananaJuice:
+            return .bananaJuice
+        case .kiwiJuice:
+            return .kiwiJuice
+        case .pineappleJuice:
+            return .pineappleJuice
+        case .strawberryBananaJuice:
+            return .strawberryBananaJuice
+        case .mangoJuice:
+            return .mangoJuice
+        case .mangoKiwiJuice:
+            return .mangoKiwiJuice
+        }
+    }
 }
