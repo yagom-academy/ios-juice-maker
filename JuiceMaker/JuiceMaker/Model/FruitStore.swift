34a5388329e662a98a5ecc00584cd879fd2de2b0
YaRkyungmin <kkgm94@naver.com>
2023-05-09 20:53:27 +0900

    🩹chore:  FruitStore타입 내 네이밍 수정

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,27 +6,27 @@
 
 // 과일 저장소 타입
 class FruitStore {
-    private var fruits: [FruitType: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
+    private var fruitInventory: [FruitType: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
     
-    func checkFruitStock(fruit: FruitType, amount: Int) -> Bool {
-        guard let fruitStock = fruits[fruit],
+    func checkStock(fruit: FruitType, amount: Int) -> Bool {
+        guard let fruitStock = fruitInventory[fruit],
               fruitStock >= amount else {
             return false
         }
         return true
     }
     
-    func addFruitCount(at fruit: FruitType, with addCount: Int) {
-        guard let fruitStock = fruits[fruit] else {
+    func addStock(fruit: FruitType, addCount: Int) {
+        guard let fruitStock = fruitInventory[fruit] else {
             return
         }
-        fruits[fruit] = fruitStock + addCount
+        fruitInventory[fruit] = fruitStock + addCount
     }
     
-    func reduceFruitCount(at fruit: FruitType, with reduceCount: Int) {
-        guard let fruitStock = fruits[fruit] else {
+    func reduceStock(fruit: FruitType, reduceCount: Int) {
+        guard let fruitStock = fruitInventory[fruit] else {
             return
         }
-        fruits[fruit] = fruitStock - reduceCount
+        fruitInventory[fruit] = fruitStock - reduceCount
     }
 }
