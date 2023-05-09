f33cd0d809df8f71c49e55d27c08ae74301e90ba
MaryJo-github <jotjdal0215@gmail.com>
2023-05-09 15:26:22 +0900

    refactor: BuyError 열거형에 description 추가

--- JuiceMaker/JuiceMaker/Model/Error.swift
+++ JuiceMaker/JuiceMaker/Model/Error.swift
@@ -5,6 +5,13 @@
 //  Created by Yetti, yy-ss99, Mary.
 //
 
-enum BuyError: Error {
+enum BuyError: Error, CustomStringConvertible {
     case ingredientShortage
+    
+    var description: String {
+        switch self {
+        case .ingredientShortage:
+            return "재료가 모두 소진되었습니다."
+        }
+    }
 }
