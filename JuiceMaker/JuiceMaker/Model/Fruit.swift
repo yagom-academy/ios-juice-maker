63f2287a4238a26fa92cd0d6f2a1c7f1e1e65ed7
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-05-10 17:21:42 +0900

    feat: Fruit타입 추가

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,14 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by EtialMoon, 비모 on 2023/05/10.
+//
+
+enum Fruit {
+    case strawberry
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
