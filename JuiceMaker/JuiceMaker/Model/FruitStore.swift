513e6720fd6a46f72ea9e4f377a0d317858300d7
bubblecocoa <bubblecocoa@gmail.com>
2023-05-10 17:23:03 +0900

    feat: FruitStore내에 저장 프로퍼티 생성 및 재고 추가, 감소 메서드 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -4,10 +4,29 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
-
 // 과일 저장소 타입
 class FruitStore {
+    var fruits: [Fruit: Int] = [
+        .strawberry: 10,
+        .banana: 10,
+        .pineapple: 10,
+        .kiwi: 10,
+        .mango: 10,
+    ]
     
+    func add(fruit: Fruit, amount: Int) {
+        guard let stock = fruits[fruit] else {
+            return
+        }
+        
+        fruits.updateValue(stock + amount, forKey: fruit)
+    }
+    
+    func subtract(fruit: Fruit, amount: Int) {
+        guard let stock = fruits[fruit] else {
+            return
+        }
+        
+        fruits.updateValue(stock - amount, forKey: fruit)
+    }
 }
-
