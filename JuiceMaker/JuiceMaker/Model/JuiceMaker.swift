94bf78b6d62e8305bf4cd6645422cc740ff25e3c
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-05-11 08:49:00 +0900

    feat: JuiceMaker에 주스 제작 메서드 생성

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,38 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    let fruitStore = FruitStore()
     
+    func make(juice menu: JuiceMenu) {
+        do {
+            let availableFruitAndAmountList = try menu.recipe.map { ingredient in
+                guard let fruit = fruitStore.fruits.first(where: { $0.key == ingredient.fruit }) else {
+                    // TODO: fruitStore에 fruit 없음
+                    throw JuiceError.test
+                }
+                
+                guard fruit.value - ingredient.amount >= 0 else {
+                    // TODO: fruit stock 부족
+                    throw JuiceError.test
+                }
+                
+                return (fruit, ingredient.amount)
+            }
+            
+            availableFruitAndAmountList.forEach { (fruit, amount) in
+                fruitStore.subtract(fruit: fruit.key, amount: amount)
+            }
+        } catch {
+            switch error {
+            case JuiceError.test:
+                print("test")
+            default:
+                print("default")
+            }
+        }
+    }
+}
+
+enum JuiceError: Error {
+    case test
 }
