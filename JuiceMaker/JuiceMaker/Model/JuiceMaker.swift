d9ff7f66d51575dbf62c8958436f0b897a4b9356
Zion <lion2212@naver.com>
2023-05-10 15:25:13 +0900

    refactor: receipt를 얻어오는 getRecipe 함수 생성, followRecipe 삭제

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -15,52 +15,38 @@ enum Menu {
 }
 
 struct JuiceMaker {
+    typealias Recipe = [(Fruit, Int)]
     private let store: FruitStore
     
     init(fruitStore: FruitStore) {
         self.store = fruitStore
     }
-    
-    typealias Recipe = [(Fruit, Int)]
-    
-    let strawberryJuiceRecipe: Recipe = [(.strawberry, 16)]
-    let bananaJuiceRecipe: Recipe = [(.banana, 2)]
-    let pineappleJuiceRecipe: Recipe = [(.pineapple, 2)]
-    let kiwiJuiceRecipe: Recipe = [(.kiwi, 3)]
-    let mangoJuiceRecipe: Recipe = [(.mango, 3)]
-    let strawberryAndBananaJuice: Recipe = [(.strawberry, 10), (.banana, 1)]
-    let mangoAndKiwiJuice: Recipe = [(.mango, 2), (.kiwi, 1)]
+
+    private func getRecipe(_ menu: Menu) -> Recipe {
+        switch menu {
+            case .strawberryJuice:
+                return [(.strawberry, 16)]
+            case .bananaJuice:
+                return [(.banana, 2)]
+            case .pineappleJuice:
+                return [(.pineapple, 2)]
+            case .kiwiJuice:
+                return [(.kiwi, 3)]
+            case .mangoJuice:
+                return [(.mango, 3)]
+            case .strawberryAndBananaJuice:
+                return [(.strawberry, 10), (.banana, 1)]
+            case .mangoAndKiwiJuice:
+                return [(.mango, 2), (.kiwi, 1)]
+        }
+    }
     
     func makeJuice(menu: Menu) -> Bool {
-        let isSuccess: Bool
+        let recipe = getRecipe(menu)
         
-        switch menu {
-        case .strawberryJuice:
-            isSuccess = followRecipe(strawberryJuiceRecipe)
-        case .bananaJuice:
-            isSuccess = followRecipe(bananaJuiceRecipe)
-        case .pineappleJuice:
-            isSuccess = followRecipe(pineappleJuiceRecipe)
-        case .kiwiJuice:
-            isSuccess = followRecipe(kiwiJuiceRecipe)
-        case .mangoJuice:
-            isSuccess = followRecipe(mangoJuiceRecipe)
-        case .strawberryAndBananaJuice:
-            isSuccess = followRecipe(strawberryAndBananaJuice)
-        case .mangoAndKiwiJuice:
-            isSuccess = followRecipe(mangoAndKiwiJuice)
-        }
-        
-        return isSuccess
-    }
-}
+        guard recipe.allSatisfy({ store.verifyFruitCount($0, count: $1) }) else { return false }
 
-// MARK: - Make Juice
-extension JuiceMaker {
-    private func followRecipe(_ receipe: [(fruit: Fruit, count: Int)]) -> Bool {
-        guard receipe.allSatisfy({ store.verifyFruitCount($0, count: $1) }) else { return false }
-
-        receipe.forEach { store.changeFruitCount($0, count: $1) }
+        recipe.forEach { store.changeFruitCount($0, count: $1) }
         return true
     }
 }
