45cf9a78281ac2b01d0d79930128a0619c5cdb1d
Whales <Whales.w.Jin@gmail.com>
2023-05-10 15:49:41 +0900

    feat: 과일 재고 늘리고 줄이는 함수 각각 생성

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -4,6 +4,7 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
+// Todo 과일 늘리고 줄이고,
 struct FruitStore {
     enum Fruit {
         case strawberry, banana, pineapple, mango, kiwi
@@ -12,5 +13,27 @@ struct FruitStore {
     enum Juice {
         case strawberryJuice, bananaJuice, pineappleJuice
         case mangoJuice, kiwiJuice, strawNanaJuice, mangKiJuice
+    }
+    
+    var fruitInventory: [Fruit: Int]
+    
+    init(fruitInventory: [Fruit : Int]) {
+        self.fruitInventory = fruitInventory
+    }
+    
+    mutating func increaseFruitStock(_ fruit: Fruit, amount: Int) {
+        if let currentAmount = fruitInventory[fruit] {
+            fruitInventory[fruit] = currentAmount + amount
+        } else {
+            //오류처리
+        }
+    }
+    
+    mutating func decreaseFruitStock(_ fruit: Fruit, amount: Int) {
+        if let currentAmount = fruitInventory[fruit] {
+            fruitInventory[fruit] = currentAmount - amount
+        } else {
+            //오류처리
+        }
     }
 }
