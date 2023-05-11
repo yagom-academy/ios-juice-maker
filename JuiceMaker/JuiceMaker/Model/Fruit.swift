d06d4518177258ad35fd84d0f84f03a5e6f01110
Hayeon Kim <qoocrab@gmail.com>
2023-05-11 23:50:03 +0900

    feat: juice, fruit sturct를 생성

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,10 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by qoocrab, Whales on 2023/05/11.
+//
+
+struct Fruit: Hashable {
+    let name: String
+}
---
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -0,0 +1,11 @@
+//
+//  Juice.swift
+//  JuiceMaker
+//
+//  Created by qoocrab, Whales on 2023/05/11.
+//
+
+struct Juice {
+    let name: String
+    let recipe: [Fruit: Int]
+}
