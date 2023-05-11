0afe9e0d74043dc8d5b2abe486004302b41c2c35
h-suo <phstoo1021@naver.com>
2023-05-11 11:57:05 +0900

    feat: 재고를 가져오는 getStock 함수 추가, 재고를 더하고 사용하는 addFruits, useFruits 함수 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -17,6 +17,26 @@ enum Fruit: CaseIterable {
 // 과일 저장소 타입
 class FruitStore {
     var fruitsStock: [Fruit: Int] = Dictionary(uniqueKeysWithValues: Fruit.allCases.map{ ($0, 10) })
+    
+    func getStock(_ fruit: Fruit) throws -> Int {
+        guard let stock = fruitsStock[fruit] else { throw InputError.nameError }
+        
+        return stock
+    }
+    
+    func addFruits(_ addStock: Int, _ addFruit: Fruit) throws -> Int {
+        var stock = try getStock(addFruit)
+        stock += addStock
+        
+        return stock
+    }
+    
+    func useFruits(_ useStock: Int, _ useFruit: Fruit) throws -> Int {
+        var stock = try getStock(useFruit)
+        stock -= useStock
+        
+        return stock
+    }
 }
 
 
