0c428bb591403fc0329ae1606562d7b7f42792a3
DasanKim <tempzipp@gmail.com>
2023-05-11 10:31:28 +0900

    ✨feat: JuiceMaker 타입 안에 과일 수량 요청, 과일 수량 받기 메소드 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -12,21 +12,29 @@ struct JuiceMaker {
     }
     
     func blendFruitJuice(menu fruitJuice: JuiceType) {
-        var isEnoughStock: Bool = true
+        guard requestFruitStock(menu: fruitJuice) else {
+            print("\(fruitJuice) 재료가 부족합니다ㅠㅠ")
+            return
+        }
+        
+        receiveFruitStock(menu: fruitJuice)
+        print("주문하신 \(fruitJuice)가 나왔습니다.")
+    }
+    
+    func requestFruitStock(menu fruitJuice: JuiceType) -> Bool {
+        var isEnoughStock: Bool = Bool()
         
         fruitJuice.recipe.forEach {
             isEnoughStock = fruitStore.checkStock(fruit: $0.key, amount: $0.value)
         }
         
-        guard isEnoughStock else {
-            print("재고 부족으로 \(fruitJuice)를 만들 수 없습니다.")
-            return
-        }
-        
+        return isEnoughStock
+    }
+    
+    func receiveFruitStock(menu fruitJuice: JuiceType) {
         fruitJuice.recipe.forEach {
             fruitStore.reduceStock(fruit: $0.key, amount: $0.value)
         }
-        print("주문하신 \(fruitJuice)가 나왔습니다.")
     }
 }
       
