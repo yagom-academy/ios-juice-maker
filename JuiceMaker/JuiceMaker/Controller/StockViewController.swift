241b383d2a278367537a111ce5f63f43584e3bcb
Kobe <mskang@xingxingmobility.com>
2023-05-11 10:43:25 +0900

    feat: '재고수정'버튼을 터치하여 '재고 추가'화면으로 이동 구현, StockViewController 생성.

---
+++ JuiceMaker/JuiceMaker/Controller/StockViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StockViewController.swift
+//  JuiceMaker
+//
+//  Created by Minseong Kang on 2023/05/11.
+//
+
+import UIKit
+
+class StockViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
