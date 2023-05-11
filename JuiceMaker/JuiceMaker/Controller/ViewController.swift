fe3b7a2e549ca19a09f3ff53667c2df9f4efb8a7
Min Hyun <minnimida@gmail.com>
2023-05-11 11:07:54 +0900

    feat: 쥬스 주문 버튼 전체 연결 및 Alert 생성

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,17 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var strawberryBananaJuice: UIButton!
+    @IBOutlet weak var mangoKiwiJuice: UIButton!
+    @IBOutlet weak var strawberryJuice: UIButton!
+    @IBOutlet weak var bananaJuice: UIButton!
+    @IBOutlet weak var pineappleJuice: UIButton!
+    @IBOutlet weak var kiwiJuice: UIButton!
+    @IBOutlet weak var mangoJuice: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let fruitStore = FruitStore(stock: [.strawberry: 20,
-                                            .banana: 10,
-                                            .pineapple: 15,
-                                            .kiwi: 5,
-                                            .mango: 7])
-		var juiceMaker = JuiceMaker(fruitStore: fruitStore)
-		juiceMaker.makeFruitJuice(menu: .kiwiJuice)
-		juiceMaker.makeFruitJuice(menu: .kiwiJuice)
+        strawberryBananaJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
+        mangoKiwiJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
+        strawberryJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
+        bananaJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
+        pineappleJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
+        kiwiJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
+        mangoJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
+    }
+    
+    @objc func orderJuice() {
+        let alert = UIAlertController(title: "재료가 모자라요.", message: "재고를 수정할까요?", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "예", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "아니오", style: .default, handler: nil)
+        
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
