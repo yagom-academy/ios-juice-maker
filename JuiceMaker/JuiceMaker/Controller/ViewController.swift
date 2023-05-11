f002910f77891a5723754127fd487bd47c4d35a1
Kobe <mskang@xingxingmobility.com>
2023-05-11 11:34:31 +0900

    feat: juiceMaker 주문 메서드 1차 연결.

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -15,8 +15,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var pineappleJuice: UIButton!
     @IBOutlet weak var kiwiJuice: UIButton!
     @IBOutlet weak var mangoJuice: UIButton!
-    
-    override func viewDidLoad() {
+	
+	@IBOutlet var strawberryStock: UILabel!
+	@IBOutlet weak var bananaStock: UILabel!
+	@IBOutlet weak var pineappleStock: UILabel!
+	@IBOutlet weak var kiwiStock: UILabel!
+	@IBOutlet weak var manggoStock: UILabel!
+	
+	let fruitStore = FruitStore(equalizedStock: 10)
+	lazy var juiceMaker = JuiceMaker(fruitStore: fruitStore)
+
+	override func viewDidLoad() {
         super.viewDidLoad()
         strawberryBananaJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
         mangoKiwiJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
@@ -27,15 +36,25 @@ class ViewController: UIViewController {
         mangoJuice.addTarget(self, action: #selector(orderJuice), for: .touchUpInside)
     }
     
-    @objc func orderJuice() {
-        let alert = UIAlertController(title: "재료가 모자라요.", message: "재고를 수정할까요?", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "예", style: .default, handler: nil)
-        let cancel = UIAlertAction(title: "아니오", style: .default, handler: nil)
-        
-        alert.addAction(ok)
-        alert.addAction(cancel)
-        
-        present(alert, animated: true, completion: nil)
+	@objc func orderJuice(_ sender: UIButton) {
+		
+		do {
+			try juiceMaker.makeFruitJuice(menu: .kiwiJuice)
+			
+		} catch StockError.fruitNotFound {
+			print(StockError.fruitNotFound.message)
+		} catch StockError.outOfStock {
+			let alert = UIAlertController(title: "재료가 모자라요.", message: "재고를 수정할까요?", preferredStyle: .alert)
+			let ok = UIAlertAction(title: "예", style: .default, handler: nil)
+			let cancel = UIAlertAction(title: "아니오", style: .default, handler: nil)
+			
+			alert.addAction(ok)
+			alert.addAction(cancel)
+			
+			present(alert, animated: true, completion: nil)
+		} catch {
+			print(StockError.unKnown.message)
+		}
     }
 }
 
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -15,7 +15,7 @@ struct JuiceMaker {
 		self.fruitStore = fruitStore
 	}
 	
-	mutating func makeFruitJuice(menu: FruitJuice) {
+	mutating func makeFruitJuice(menu: FruitJuice) throws {
 		let recipe = menu.juiceRecipe
         var changedStock: Recipe = []
         do {
@@ -30,10 +30,13 @@ struct JuiceMaker {
 			print("\(menu.name) 제조가 완료되었습니다.")
         } catch StockError.fruitNotFound {
             print(StockError.fruitNotFound.message)
+			throw StockError.fruitNotFound
         } catch StockError.outOfStock {
             print(StockError.outOfStock.message)
+			throw StockError.outOfStock
         } catch {
             print(StockError.unKnown.message)
+			throw StockError.unKnown
         }
 	}
 }
