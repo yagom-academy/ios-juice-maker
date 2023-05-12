9378a3bbf114c6d11018fdc19ae36ced55f07e3b
JusBug <pksgwo123@gmail.com>
2023-05-12 14:02:06 +0900

    feat: storyboard의 레이블과 버튼을 ViewController에 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,12 +7,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func strawberrybananaJuiceOrderButton(_ sender: UIButton) {
+    }
+    @IBAction func mangokiwiJuiceOrderButton(_ sender: UIButton) {
+    }
+    @IBAction func strawberryJuiceOrderButton(_ sender: UIButton) {
+    }
+    @IBAction func bananaJuiceOrderButton(_ sender: UIButton) {
+    }
+    @IBAction func pineappleJuiceOrderButton(_ sender: UIButton) {
+    }
+    @IBAction func kiwiJuiceOrderButton(_ sender: UIButton) {
+    }
+    @IBAction func mangoJuiceOrderButton(_ sender: UIButton) {
+    }
 }
-
