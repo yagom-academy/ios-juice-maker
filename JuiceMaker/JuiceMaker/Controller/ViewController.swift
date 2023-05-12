3b9ddcfbb1d3c2ef836ae40c664d983f5a4908f4
mint3382 <mskim0926@naver.com>
2023-05-12 14:57:30 +0900

    feat: UILabel 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -6,10 +6,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class JuiceOrderViewController: UIViewController {
+    
+    
+    @IBOutlet weak var strawberryLabel: UILabel!
+    
+    @IBOutlet weak var bananaLabel: UILabel!
+    
+    @IBOutlet weak var pineappleLabel: UILabel!
+    
+    @IBOutlet weak var kiwiLabel: UILabel!
+    
+    @IBOutlet weak var mangoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 }
 
