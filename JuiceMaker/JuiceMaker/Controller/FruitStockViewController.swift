6d89e0e58d61598d0f8df54d8b1f0de8fd94534c
mint3382 <mskim0926@naver.com>
2023-05-12 15:18:53 +0900

    feat: FruitStockViewController에 UILabel 연결

--- JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitStockViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class FruitStockViewController: UIViewController {
     
+    
+    @IBOutlet weak var strawberryLabel: UILabel!
+    @IBOutlet var bananaLabel: UIView!
+    @IBOutlet weak var pineappleLabel: UILabel!
+    @IBOutlet weak var kiwiLabel: UILabel!
+    @IBOutlet weak var mangoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func test(_ sender: UIStepper) {
+        print(sender.value)
+    }
+    
 }
