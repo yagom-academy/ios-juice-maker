549a581b579fe3de6ab7a709000ab9b9a16450a8
YaRkyungmin <kkgm94@naver.com>
2023-05-12 12:04:22 +0900

    ♻️refactor: 오류처리 함수 기능을 모델의 JuiceMaker로 이동

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -11,20 +11,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        orderFruitJuice()
-    }
-    
-    func orderFruitJuice() {
         let juiceMaker = JuiceMaker()
-        
-        do {
-            try juiceMaker.blendFruitJuice(menu: .bananaJuice)
-            try juiceMaker.blendFruitJuice(menu: .strawberryJuice)
-        } catch JuiceMakerError.outOfFruitStock(let fruitJuice) {
-            print("\(fruitJuice)의 재료 재고가 부족합니다.")
-        } catch {
-            print("알 수 없는 에러입니다.")
-        }
+        juiceMaker.blendFruitJuice(menu: .bananaJuice)
+        juiceMaker.blendFruitJuice(menu: .strawberryJuice)
     }
 }
 
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -4,31 +4,45 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-enum JuiceMakerError: Error {
-    case outOfFruitStock(menu: JuiceType)
+enum JuiceMakerError: Error, CustomStringConvertible {
+    case outOfFruitStock
+    case unknownError
+    
+    var description: String {
+        switch self {
+        case .outOfFruitStock:
+            return "재고가 부족하여 음료 제조가 불가능 합니다."
+        case .unknownError:
+            return "알수 없는 오류"
+        }
+    }
 }
 
 struct JuiceMaker {
     private let fruitStore: FruitStore = FruitStore()
     
-    func blendFruitJuice(menu fruitJuice: JuiceType) throws {
-        guard requestFruitStock(menu: fruitJuice) else {
-            throw JuiceMakerError.outOfFruitStock(menu: fruitJuice)
+    func blendFruitJuice(menu fruitJuice: JuiceType) {
+        do {
+            try requestFruitStock(menu: fruitJuice)
+            receiveFruitStock(menu: fruitJuice)
+            print("주문하신 \(fruitJuice)가 나왔습니다.")
+        } catch JuiceMakerError.outOfFruitStock {
+            print(JuiceMakerError.outOfFruitStock)
+        } catch {
+            print(JuiceMakerError.unknownError)
         }
-        
-        receiveFruitStock(menu: fruitJuice)
-        
-        print("주문하신 \(fruitJuice)가 나왔습니다.")
     }
     
-    private func requestFruitStock(menu fruitJuice: JuiceType) -> Bool {
+    private func requestFruitStock(menu fruitJuice: JuiceType) throws {
         var isEnoughStock: Bool = Bool()
         
         fruitJuice.recipe.forEach {
             isEnoughStock = fruitStore.checkStock(fruit: $0.key, amount: $0.value)
         }
         
-        return isEnoughStock
+        guard isEnoughStock else {
+            throw JuiceMakerError.outOfFruitStock
+        }
     }
     
     private func receiveFruitStock(menu fruitJuice: JuiceType) {
