8880533d2c576340c503eb8c37094c7554600aa2
Whales <Whales.w.Jin@gmail.com>
2023-05-16 05:34:24 +0900

    feat: 에러 열거형 타입 생성

---
+++ JuiceMaker/JuiceMaker/Model/JuiceMakerError.swift
@@ -0,0 +1,10 @@
+//
+//  JuiceMakerError.swift
+//  JuiceMaker
+//
+//  Created by Whales on 2023/05/16.
+//
+
+enum JuiceMakerError: Error {
+    case invalidFruit, invalidJuice, insufficientFruit
+}
