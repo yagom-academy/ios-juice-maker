bef867f210d24d11574e029cae19f7665667028e
myungsun <audtjs9987@naver.com>
2023-05-15 17:51:38 +0900

    feat: JuiceOrderViewController에서 필요한 프로퍼티 생성

--- JuiceMaker/JuiceMaker/Controller/JuiceOrderViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceOrderViewController.swift
@@ -7,12 +7,17 @@
 import UIKit
 
 class JuiceOrderViewController: UIViewController {
-
+    @IBOutlet var fruitStockLabels: [UILabel] = []
+    let juiceMaker = JuiceMaker(fruitStore: FruitStore(fruitInventory: [
+        .strawberry: 10,
+        .banana: 10,
+        .pineapple: 10,
+        .kiwi: 10,
+        .mango: 10
+    ]))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
