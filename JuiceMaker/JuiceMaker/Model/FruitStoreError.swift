fdef976e20b0dd2dec1179779a338349986630d9
myungsun <audtjs9987@naver.com>
2023-05-15 17:18:18 +0900

    refactor: FruitStoreError, Juice 열거형에 CustomStringConvertible 프로토콜 채택

--- JuiceMaker/JuiceMaker/Model/FruitStoreError.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStoreError.swift
@@ -5,6 +5,13 @@
 //  Created by Karen, myungsun on 2023/05/09.
 //
 
-enum FruitStoreError: Error {
+enum FruitStoreError: Error, CustomStringConvertible {
     case outOfStock
+    
+    var description: String {
+        switch self {
+        case.outOfStock:
+            return "재료가 모자라요. 재고를 수정할까요?"
+        }
+    }
 }
--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -5,7 +5,7 @@
 //  Created by Karen, myungsun on 2023/05/09.
 //
 
-enum Juice {
+enum Juice: CustomStringConvertible {
     case strawberry
     case banana
     case kiwi
@@ -30,6 +30,25 @@ enum Juice {
             return [.mango: 3]
         case .mangoKiwi:
             return [.mango: 2, .kiwi: 1]
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .strawberry:
+            return "딸기 쥬스"
+        case .banana:
+            return "바나나 쥬스"
+        case .kiwi:
+            return "키위 쥬스"
+        case .pineapple:
+            return "파인애플 쥬스"
+        case .strawberryBanana:
+            return "딸바 쥬스"
+        case .mango:
+            return "망고 쥬스"
+        case .mangoKiwi:
+            return "망고키위 쥬스"
         }
     }
 }
