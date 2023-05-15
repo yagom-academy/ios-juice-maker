64a5464c0630f3ef5f8851b57c311cd15b3ce8ab
Whales <Whales.w.Jin@gmail.com>
2023-05-16 05:48:30 +0900

    feat: 쥬스메이커 구조체 타입 생성
    - 과일 가게 프로퍼티 (재고 10)
    - takeOrder 함수 (do-try-catch)
    - makeJuice 함수
    - checkStock 함수
    - consumeFruit 함수

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -5,5 +5,42 @@
 // 
 
 struct JuiceMaker {
+    private let fruitStore = FruitStore(fruitInventory: [
+        .strawberry: 10,
+        .banana: 10,
+        .pineapple: 10,
+        .kiwi: 10,
+        .mango: 10
+    ])
     
+    func takeOrder(_ juice: Juice) {
+        do {
+            try makeJuice(juice)
+        } catch JuiceMakerError.invalidFruit {
+            print("올바르지 않은 과일 이름입니다.")
+        } catch JuiceMakerError.invalidJuice {
+            print("올바르지 않은 쥬스 이름입니다.")
+        } catch JuiceMakerError.insufficientFruit {
+            print("과일의 재고가 부족하여 쥬스를 만들 수 없습니다.")
+        } catch {
+            print("알 수 없는 에러입니다.")
+        }
+    }
+    
+    private func makeJuice(_ juice: Juice) throws {
+        try checkStock(for: juice)
+        try consumeFruit(for: juice)
+    }
+
+    private func checkStock(for juice: Juice) throws {
+        for (fruit, amount) in juice.recipe {
+            try fruitStore.isStockSufficient(fruit, with: amount)
+        }
+    }
+        
+    private func consumeFruit(for juice: Juice) throws {
+        for (fruit, amount) in juice.recipe {
+            try fruitStore.decreaseFruitStock(fruit, amount: amount)
+        }
+    }
 }
