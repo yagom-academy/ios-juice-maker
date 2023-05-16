7d9b1f5f5cce96a09197d5b9289b9bea03085435
agilestarskim <agilestars@kakao.com>
2023-05-16 10:42:59 +0900

    feat: 알럿 버튼 추가, 재고레이블 연결, 알럿 아니오 클릭시 화면이동

--- JuiceMaker/JuiceMaker/Controller/MainViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/MainViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 class MainViewController: UIViewController {
     let juiceMaker = JuiceMaker(fruitStore: FruitStore())
     
-    @IBOutlet weak var MangoStockLabel: UILabel!
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +21,12 @@ class MainViewController: UIViewController {
     }
     
     private func configureLabel() {
-        self.MangoStockLabel.text = String(juiceMaker.fruitStore.fruitStock[.mango]!)
+        self.strawberryStockLabel.text = String(juiceMaker.fruitStore.fruitStock[.strawberry]!)
+        self.bananaStockLabel.text = String(juiceMaker.fruitStore.fruitStock[.banana]!)
+        self.pineappleStockLabel.text = String(juiceMaker.fruitStore.fruitStock[.pineapple]!)
+        self.kiwiStockLabel.text = String(juiceMaker.fruitStore.fruitStock[.kiwi]!)
+        self.mangoStockLabel.text = String(juiceMaker.fruitStore.fruitStock[.mango]!)
+        
     }
     
     @IBAction func onTouchStockButton(_ sender: UIBarButtonItem) {
@@ -27,7 +36,7 @@ class MainViewController: UIViewController {
         self.navigationController?.present(vcStock, animated: true)
     }
     
-    @IBAction func onTouchOderButton(_ sender: UIButton) {
+    @IBAction func onTouchOrderButton(_ sender: UIButton) {
         var result: Result<Juice, FruitStoreError> = .failure(.unknown)
         
         switch sender.titleLabel?.text {
@@ -44,7 +53,7 @@ class MainViewController: UIViewController {
         case "키위쥬스 주문":
             result = self.juiceMaker.makeJuice(juice: .kiwiJuice)
         case "망고쥬스 주문":
-            result = self.juiceMaker.makeJuice(juice: .mangoKiwiJuice)
+            result = self.juiceMaker.makeJuice(juice: .mangoJuice)
         default:
             print("알 수 없는 주문")
         }
@@ -59,8 +68,12 @@ class MainViewController: UIViewController {
             
         case .failure(_):
             let alert = UIAlertController(title: "재고 부족", message: "재료가 모잘라요. 재고를 수정할까요?", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-                NSLog("The \"OK\" alert occured.")
+            alert.addAction(UIAlertAction(title: "아니오", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "예", style: .default, handler: { _ in
+                guard let vcStock = self.storyboard?.instantiateViewController(identifier: "StockViewController") as? UINavigationController else {
+                    return
+                }
+                self.navigationController?.present(vcStock, animated: true)
             }))
             self.present(alert, animated: true, completion: nil)
         }
