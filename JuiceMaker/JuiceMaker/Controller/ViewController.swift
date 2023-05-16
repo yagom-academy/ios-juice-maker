e0ec123f705eb8edc310bc9bc9fa41c312d1b002
bubblecocoa <bubblecocoa@gmail.com>
2023-05-16 15:34:14 +0900

    feat: 각 주문 버튼 터치시 재고에 따라 alert 표시

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -21,7 +21,63 @@ class ViewController: UIViewController {
 
         setFruitStockLabel()
     }
-
+    
+    @IBAction func orderStrawberryJuice(_ sender: UIButton) {
+        if juiceMaker.make(juice: .strawberryJuice) {
+            present(alertJuiceReady(menu: .strawberryJuice), animated: true)
+        } else {
+            present(alertShortageStock(), animated: true)
+        }
+    }
+    
+    @IBAction func orderBananaJuice(_ sender: UIButton) {
+        if juiceMaker.make(juice: .bananaJuice) {
+            present(alertJuiceReady(menu: .bananaJuice), animated: true)
+        } else {
+            present(alertShortageStock(), animated: true)
+        }
+    }
+    
+    @IBAction func orderPineappleJuice(_ sender: UIButton) {
+        if juiceMaker.make(juice: .pineappleJuice) {
+            present(alertJuiceReady(menu: .pineappleJuice), animated: true)
+        } else {
+            present(alertShortageStock(), animated: true)
+        }
+    }
+    
+    @IBAction func orderKiwiJuice(_ sender: UIButton) {
+        if juiceMaker.make(juice: .kiwiJuice) {
+            present(alertJuiceReady(menu: .kiwiJuice), animated: true)
+        } else {
+            present(alertShortageStock(), animated: true)
+        }
+    }
+    
+    @IBAction func orderMangoJuice(_ sender: UIButton) {
+        if juiceMaker.make(juice: .mangoJuice) {
+            present(alertJuiceReady(menu: .mangoJuice), animated: true)
+        } else {
+            present(alertShortageStock(), animated: true)
+        }
+    }
+    
+    @IBAction func orderStrawberryBananaJuice(_ sender: UIButton) {
+        if juiceMaker.make(juice: .strawberryBananaJuice) {
+            present(alertJuiceReady(menu: .strawberryBananaJuice), animated: true)
+        } else {
+            present(alertShortageStock(), animated: true)
+        }
+    }
+    
+    @IBAction func orderMangoKiwiJuice(_ sender: UIButton) {
+        if juiceMaker.make(juice: .mangoKiwiJuice) {
+            present(alertJuiceReady(menu: .mangoKiwiJuice), animated: true)
+        } else {
+            present(alertShortageStock(), animated: true)
+        }
+    }
+    
     func setFruitStockLabel() {
         do {
             strawberryStockLabel.text = try String(juiceMaker.fruitStore.receiveStock(of: .strawberry))
@@ -38,5 +94,21 @@ class ViewController: UIViewController {
             }
         }
     }
-}
+    
+    func alertJuiceReady(menu: JuiceMenu) -> UIAlertController {
+        let juiceAlert = UIAlertController(title: "제조 완료", message: "\(menu) 쥬스 나왔습니다! 맛있게 드세요!", preferredStyle: .alert)
+        
+        juiceAlert.addAction(UIAlertAction(title: "Yes!", style: .default, handler: { _ in print("yes 클릭") }))
 
+        return juiceAlert
+    }
+    
+    func alertShortageStock() -> UIAlertController {
+        let juiceAlert = UIAlertController(title: "Juice", message: "재료가 모자라요. 재고를 수정할까요?", preferredStyle: .alert)
+        
+        juiceAlert.addAction(UIAlertAction(title: "예", style: .destructive, handler: { _ in print("yes 클릭") }))
+        juiceAlert.addAction(UIAlertAction(title: "아니오", style: .cancel, handler: { _ in print("no 클릭") }))
+        
+        return juiceAlert
+    }
+}
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,21 +7,26 @@
 struct JuiceMaker {
     let fruitStore = FruitStore()
     
-    func make(juice menu: JuiceMenu) {
+    func make(juice menu: JuiceMenu) -> Bool {
         do {
             let availableIngredients = try validIngredients(by: menu.recipe)
             
             try availableIngredients.forEach {
                 try fruitStore.useStock(of: $0.fruit, amount: $0.amount)
             }
+            
+            return true
         } catch {
             switch error {
             case JuiceError.nonexistentFruit:
                 print("FruitStore에 해당 Fruit이 없습니다.")
+                return false
             case JuiceError.shortageFruitStock:
                 print("Fruit의 수량이 부족합니다.")
+                return false
             default:
                 print("알 수 없는 에러")
+                return false
             }
         }
     }
