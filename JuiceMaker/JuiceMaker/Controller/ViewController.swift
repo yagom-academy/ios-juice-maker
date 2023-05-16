4c512cb195ad086ca553b5c013aaaa4d45143745
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-05-16 13:56:19 +0900

    feat: 뷰컨트롤러에 IBOutlet 연결 및 레이블에 데이터 set

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    let juiceMaker = JuiceMaker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setFruitStockLabel()
     }
 
-
+    func setFruitStockLabel() {
+        do {
+            strawberryStockLabel.text = try String(juiceMaker.fruitStore.receiveStock(of: .strawberry))
+            bananaStockLabel.text = try String(juiceMaker.fruitStore.receiveStock(of: .banana))
+            pineappleStockLabel.text = try String(juiceMaker.fruitStore.receiveStock(of: .pineapple))
+            kiwiStockLabel.text = try String(juiceMaker.fruitStore.receiveStock(of: .kiwi))
+            mangoStockLabel.text = try String(juiceMaker.fruitStore.receiveStock(of: .mango))
+        } catch {
+            switch error {
+            case JuiceError.nonexistentFruit:
+                print("FruitStore에 해당 Fruit이 없습니다.")
+            default:
+                print("알 수 없는 에러")
+            }
+        }
+    }
 }
 
