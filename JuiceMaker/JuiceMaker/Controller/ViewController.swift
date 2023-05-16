ed2a4a7b0818369dd04f339d12b32e7edae7c21a
Whales <Whales.w.Jin@gmail.com>
2023-05-17 00:33:37 +0900

    feat: 버튼들 연결

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,11 +8,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var strawberryJuiceButton: UIButton!
+    @IBOutlet var bananaJuiceButton: UIButton!
+    @IBOutlet var pineappleJuiceButton: UIButton!
+    @IBOutlet var kiwiJuiceButton: UIButton!
+    @IBOutlet var mangoJuiceButton: UIButton!
+    @IBOutlet var strawberryBananaJuiceButton: UIButton!
+    @IBOutlet var mangoKiwiJuiceButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func takeOrderStrawberryJuiceButton(_ sender: UIButton) {
+    }
+    @IBAction func takeOrderBananaJuiceButton(_ sender: UIButton) {
+    }
+    @IBAction func takeOrderPineappleJuiceButton(_ sender: UIButton) {
+    }
+    @IBAction func takeOrderKiwiJuiceButton(_ sender: UIButton) {
+    }
+    @IBAction func takeOrderMangoJuiceButton(_ sender: UIButton) {
+    }
+    @IBAction func takeOrderStrawberryBananaJuiceButton(_ sender: UIButton) {
+    }
+    @IBAction func takeOrderMangoKiwiJuiceButton(_ sender: UIButton) {
+    }
+    
+    
 }
 
