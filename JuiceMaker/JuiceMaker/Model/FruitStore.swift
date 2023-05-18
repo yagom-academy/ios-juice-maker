49097ff06ea43420fa66336e67c5b60d61fa0053
Whales <Whales.w.Jin@gmail.com>
2023-05-19 02:31:02 +0900

    refactor: 과일 재고함 은닉화
    - 과일 하나의 재고를 불러오는 함수 삭제
    - 과일 재고함 불러오는 함수 생성
    - 연관된 함수들 수정

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -5,33 +5,27 @@
 //
 
 class FruitStore {
-    var fruitInventory: [Fruit: Int]
+    private var fruitInventory: [Fruit: Int]
     
     init(fruitInventory: [Fruit : Int]) {
         self.fruitInventory = fruitInventory
     }
     
-    func readCurrentStock(for fruit: Fruit) -> Int {
-        guard let currentAmount = fruitInventory[fruit] else {
-            return 0
-        }
-        return currentAmount
+    func readFruitInventory() -> [Fruit: Int] {
+        return fruitInventory
     }
     
     func isStockSufficient(_ fruit: Fruit, with requiredAmount: Int) throws {
-        let currentStock = readCurrentStock(for: fruit)
-        guard currentStock >= requiredAmount else {
+        guard let currentStock = fruitInventory[fruit],
+              currentStock >= requiredAmount else {
             throw FruitStoreError.insufficientFruit
         }
     }
     
     func decreaseFruitStock(_ fruit: Fruit, by amount: Int) {
-        let currentAmount = readCurrentStock(for: fruit)
-        fruitInventory[fruit] = currentAmount - amount
-    }
-    
-    func increaseFruitStock(_ fruit: Fruit, by amount: Int) {
-        let currentAmount = readCurrentStock(for: fruit)
-        fruitInventory[fruit] = currentAmount + amount
+        guard let currentStock = fruitInventory[fruit] else {
+            return
+        }
+        fruitInventory[fruit] = currentStock - amount
     }
 }
