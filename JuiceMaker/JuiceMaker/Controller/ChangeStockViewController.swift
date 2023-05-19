77790941e9c2b096da52e12d5c9be5c5918f9e6d
JusBug <pksgwo123@gmail.com>
2023-05-19 11:30:39 +0900

    feat: ChangeStockViewController와 view객체 연결 및 stepper에 tag를 각각 설정하여 stockStepper() 하나로 분기처리

--- JuiceMaker/JuiceMaker/Controller/ChangeStockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ChangeStockViewController.swift
@@ -9,13 +9,35 @@ import UIKit
 
 class ChangeStockViewController: UIViewController {
 
+    @IBOutlet private weak var strawberryStockLabel: UILabel!
+    @IBOutlet private weak var bananaStockLabel: UILabel!
+    @IBOutlet private weak var pineappleStockLabel: UILabel!
+    @IBOutlet private weak var kiwiStockLabel: UILabel!
+    @IBOutlet private weak var mangoStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
     
-    @IBAction func backButtonTap(_ sender: UIBarButtonItem) {
+    @IBAction func closeButtonTap(_ sender: UIBarButtonItem) {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func stockStepper(_ sender: UIStepper) {
+        switch sender.tag {
+        case 0:
+            break
+        case 1:
+            break
+        case 2:
+            break
+        case 3:
+            break
+        case 4:
+            break
+        default:
+            break
+        }
     }
 }
