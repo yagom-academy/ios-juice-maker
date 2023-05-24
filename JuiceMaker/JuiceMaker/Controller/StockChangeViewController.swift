7ba8783a1857b9a8a659b5f25f9f4d5daed2f1e9
Whales <Whales.w.Jin@gmail.com>
2023-05-25 01:30:15 +0900

    feat: 재고 수량 조정하는 Stepper 구현

--- JuiceMaker/JuiceMaker/Controller/StockChangeViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockChangeViewController.swift
@@ -16,6 +16,12 @@ class StockChangeViewController: UIViewController {
 
     @IBOutlet weak var closeButton: UIBarButtonItem!
     
+    @IBOutlet weak var strawberryStockStepper: UIStepper!
+    @IBOutlet weak var bananaStockStepper: UIStepper!
+    @IBOutlet weak var pineappleStockStepper: UIStepper!
+    @IBOutlet weak var kiwiStockStepper: UIStepper!
+    @IBOutlet weak var mangoStockStepper: UIStepper!
+    
     var strawberryStock: String = ""
     var bananaStock: String = ""
     var pineappleStock: String = ""
@@ -36,4 +42,38 @@ class StockChangeViewController: UIViewController {
         self.dismiss(animated: true)
     }
 
+    @IBAction func strawberryStockChangeStepper(_ sender: UIStepper) {
+        guard let strawberryStock = Int(strawberryStock) else {
+            return
+        }
+        strawberryStockLabel.text = String(strawberryStock + Int(sender.value))
+    }
+    
+    @IBAction func bananaStockChangeStepper(_ sender: UIStepper) {
+        guard let bananaStock = Int(bananaStock) else {
+            return
+        }
+        bananaStockLabel.text = String(bananaStock + Int(sender.value))
+    }
+    @IBAction func pineappleStockChangeStepper(_ sender: UIStepper) {
+        guard let pineappleStock = Int(pineappleStock) else {
+            return
+        }
+        pineappleStockLabel.text = String(pineappleStock + Int(sender.value))
+    }
+    
+    @IBAction func kiwiStockChangeStepper(_ sender: UIStepper) {
+        guard let kiwiStock = Int(kiwiStock) else {
+            return
+        }
+        kiwiStockLabel.text = String(kiwiStock + Int(sender.value))
+    }
+    
+    
+    @IBAction func mangoStockChangeStepper(_ sender: UIStepper) {
+        guard let mangoStock = Int(mangoStock) else {
+            return
+        }
+        mangoStockLabel.text = String(mangoStock + Int(sender.value))
+    }
 }
