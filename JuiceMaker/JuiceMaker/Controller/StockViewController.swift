df525f2bc2a7cfb4ddf53c6ed4a421daa6d58e62
Serena <sha0720@naver.com>
2023-05-26 11:31:17 +0900

    refactor: fatalError 런타임 에러가 아닌 컴파일 에러로 확인 가능하게 변경

--- JuiceMaker/JuiceMaker/Controller/StockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockViewController.swift
@@ -30,6 +30,7 @@ class StockViewController: UIViewController {
         super.init(coder: coder)
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
