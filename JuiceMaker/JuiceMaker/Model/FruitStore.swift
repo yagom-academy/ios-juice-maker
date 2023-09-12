0db8be2fa66f7fac91d4d51d1e3e66821116ea3e
mireu79 <https://github.com/mireu79>
2023-09-12 14:27:08 +0900

    feat: 과일개 변경하는 함수구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,11 +6,23 @@
 
 import Foundation
 
-// 과일 저장소 타입
 class FruitStore {
     private var fruitStock: [Fruit: Int]
     
     init(fruitStock: [Fruit : Int]) {
         self.fruitStock = fruitStock
+    }
+    
+    func addFruitStock(fruitName: Fruit, count: Int) {
+        if let currentStock = fruitStock[fruitName] {
+            fruitStock.updateValue(currentStock + count, forKey: fruitName)
+        }
+    }
+    
+    func subtractFruitStock(fruitName: Fruit, count: Int) {
+        guard let currentStock = fruitStock[fruitName], currentStock - count >= 0 else {
+            return
+        }
+        fruitStock.updateValue(currentStock - count, forKey: fruitName)
     }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,6 @@
 
 import Foundation
 
-// 쥬스 메이커 타입
 struct JuiceMaker {
     
 }
