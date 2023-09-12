cc7e16447907cd7a0991b4e94006699422045fc0
mireu79 <https://github.com/mireu79>
2023-09-12 14:19:15 +0900

    feat: Fruit와Juice enum타입 생성

---
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -0,0 +1,14 @@
+//
+//  Fruit.swift
+//  JuiceMaker
+//
+//  Created by mireu,charles on 2023/09/12.
+//
+
+enum Fruit {
+    case strawbery
+    case banana
+    case pineapple
+    case kiwi
+    case mango
+}
---
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -0,0 +1,16 @@
+//
+//  Juice.swift
+//  JuiceMaker
+//
+//  Created by mireu,charles on 2023/09/12.
+//
+
+enum Juice {
+    case strawberyJuice
+    case bananaJuice
+    case kiwiJuice
+    case pineappleJuice
+    case strawberyBananaJuice
+    case mangoJuice
+    case mangoKiwiJuice
+}
