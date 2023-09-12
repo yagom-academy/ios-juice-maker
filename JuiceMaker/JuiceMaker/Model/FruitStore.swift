9564c57da8f0b50e6c8a443b4096127ebb4542a4
charles <kch1759@naver.com>
2023-09-12 14:24:03 +0900

    feat: 과일재고를 담는 딕셔너리 타입의 fruitStock 변수 선언, 생성자 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,9 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
+    private var fruitStock: [Fruit: Int]
     
+    init(fruitStock: [Fruit : Int]) {
+        self.fruitStock = fruitStock
+    }
 }
