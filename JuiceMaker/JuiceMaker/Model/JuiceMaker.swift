deca128d719bc38fb7ec179b19b2049556c5f038
woohyeon3382 <woohyeon3382@naver.com>
2023-09-12 15:55:44 +0900

    feat: JuiceMaker구조체에서 쥬스생성하는 함수 구현

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -2,11 +2,35 @@
 //  JuiceMaker - JuiceMaker.swift
 //  Created by uemu, Toy.
 //  Copyright © yagom academy. All rights reserved.
-// 
-
-import Foundation
+//
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    private let fruitStore = FruitStore.singleTon
     
+    private func manufacturefruitJuice(in fruitJuice: FruitJuice) {
+        switch fruitJuice {
+        case .strawberryJuice:
+            fruitStore.branchHandler(fruit: .strawberry, number: 16)
+        case .bananaJuice:
+            fruitStore.branchHandler(fruit: .banana, number: 2)
+        case .kiwiJuice:
+            fruitStore.branchHandler(fruit: .kiwi, number: 3)
+        case .pineappleJuice:
+            fruitStore.branchHandler(fruit: .pineapple, number: 2)
+        case .mangoJuice:
+            fruitStore.branchHandler(fruit: .mango, number: 3)
+        }
+    }
+    
+    private func manufactureMixFruitJuice(in mixFruitJuice: MixFruitJuice) {
+        switch mixFruitJuice {
+        case .strawberryBananaJuice:
+            fruitStore.branchHandler(fruit: .strawberry, number: 10)
+            fruitStore.branchHandler(fruit: .banana, number: 1)
+        case .mangoKiwiJuice:
+            fruitStore.branchHandler(fruit: .mango, number: 2)
+            fruitStore.branchHandler(fruit: .kiwi, number: 1)
+        }
+    }
 }
