cb5262c133c3d3a9865f73b8c5d3dd2825013360
charles <kch1759@naver.com>
2023-09-12 14:31:20 +0900

    Juice 타입에 recipe 계산속성 추가

--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -13,4 +13,23 @@ enum Juice {
     case strawberyBananaJuice
     case mangoJuice
     case mangoKiwiJuice
+    
+    var recipe: [Fruit: Int] {
+        switch self {
+        case .strawberyJuice:
+            return [.strawbery: 16]
+        case .bananaJuice:
+            return [.banana: 2]
+        case .kiwiJuice:
+            return [.kiwi: 3]
+        case .pineappleJuice:
+            return [.pineapple: 2]
+        case .strawberyBananaJuice:
+            return [.strawbery: 10, .banana: 1]
+        case .mangoJuice:
+            return [.mango: 3]
+        case .mangoKiwiJuice:
+            return [.mango: 2, .kiwi: 1]
+        }
+    }
 }
