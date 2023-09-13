9c5d5552eafa7d22859d44639f136450ea0b2891
ue-mu <ym14170@gmail.com>
2023-09-13 17:07:53 +0900

    feat: reduceFruitQuantity 함수 수정 및 brancHandler 함수 삭제

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -11,23 +11,14 @@ class FruitStore {
     
     private init() {}
     
-    private func reduceFruitQuantity(in fruit: FruitStorage, with number: Int) {
-        switch fruit {
-        case .strawberry:
-            strawberry -= number
-            print("딸기 재고: \(strawberry)개")
-        case .banana:
-            banana -= number
-            print("바나나 재고: \(banana)개")
-        case .pineapple:
-            pineapple -= number
-            print("파인애플 재고: \(pineapple)개")
-        case .kiwi:
-            kiwi -= number
-            print("키위 재고: \(kiwi)개")
-        case .mango:
-            mango -= number
-            print("망고 재고: \(mango)개")
+    func reduceFruitQuantity(in fruiJuice: FruitJuice) throws {
+        try fruiJuice.juiceRecipe.forEach {
+            guard let number = fruitQuantity[$0] else { return }
+            do {
+                fruitQuantity[$0] = try checkQuantity(number: number, quantity: $1)
+            } catch {
+                throw JuiceMakerError.outOfStock
+            }
         }
     }
     
@@ -36,21 +27,6 @@ class FruitStore {
             return number - quantity
         } else {
             throw JuiceMakerError.outOfStock
-        }
-    }
-    
-    func branchHandler(fruit: FruitStorage, number: Int) {
-        switch fruit {
-        case .strawberry:
-            checkQuantity(fruit: strawberry, check: fruit, number: number)
-        case .banana:
-            checkQuantity(fruit: banana, check: fruit, number: number)
-        case .pineapple:
-            checkQuantity(fruit: pineapple, check: fruit, number: number)
-        case .kiwi:
-            checkQuantity(fruit: kiwi, check: fruit, number: number)
-        case .mango:
-            checkQuantity(fruit: mango, check: fruit, number: number)
         }
     }
     
