27ed1bdddcd1cbff30a0cfe5f382dc2b2e59b3a8
ue-mu <ym14170@gmail.com>
2023-09-13 17:53:47 +0900

    feat: FruitStore 클래스에 final 키워드 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -5,7 +5,7 @@
 //
 
 // 과일 저장소 타입
-class FruitStore {
+final class FruitStore {
     static let shared = FruitStore()
     private var fruitQuantity: [Fruit: Int] = [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10]
     
