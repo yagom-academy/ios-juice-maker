824e1e6f2f9b7ca5190a3d964f01f36af1ea39b4
woohyeon3382 <woohyeon3382@naver.com>
2023-09-19 11:59:40 +0900

    feat: 쥬스를 주문버튼 동작 함수 인 manufactureJuice함수 구현

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -14,7 +14,9 @@ final class ViewController: UIViewController {
     @IBOutlet weak var kiwiLabel: UILabel!
     @IBOutlet weak var mangoLabel: UILabel!
     
+    var juiceMaker = JuiceMaker()
     var fruitStore = FruitStore.shared
+    var fruitJuice: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +37,18 @@ final class ViewController: UIViewController {
             case .pineapple:
                 pineappleLabel.text = String(value)
             }
+        }
+    }
+    
+    @IBAction func manufactureJuice(_ sender: UIButton) {
+        guard let order = sender.currentTitle else { return }
+        guard let juice = FruitJuice.Order(rawValue: order)?.juiceOrder else { return }
+        do {
+            try juiceMaker.manufactureFruitJuice(in: juice)
+            fruitJuice = juice.rawValue
+            showStock()
+        } catch {
+            
         }
     }
 
