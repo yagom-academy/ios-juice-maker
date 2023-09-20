e117a98663907d9851451f53c6076116ba7f76e7
charles <kch1759@naver.com>
2023-09-20 12:27:10 +0900

    chore: 주석 삭제

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -7,12 +7,6 @@
 struct JuiceMaker {
     private(set) var fruitStore = FruitStore(fruitStock: [.strawberry: 10, .banana: 10, .pineapple: 10, .kiwi: 10, .mango: 10])
     
-//    private func checkFruitStock(juice: Juice) throws {
-//        for (name, count) in juice.recipe {
-//            try fruitStore.checkFruitStock(name: name, count: count)
-//        }
-//    }
-    
     private mutating func subtractFruitStock(juice: Juice) throws {
         for (name, count) in juice.recipe {
             try fruitStore.subtractFruitStock(name: name, count: count, juice: juice)
