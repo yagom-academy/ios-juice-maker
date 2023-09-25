3d6c524810b83dd06e542bcfcaec3d742051c755
ue-mu <ym14170@gmail.com>
2023-09-25 18:16:29 +0900

    feat: 화면 이동방식 수정

--- JuiceMaker/JuiceMaker/Controller/FruitQuantityViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitQuantityViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 final class FruitQuantityViewController: UIViewController {
     static let id = String(describing: FruitQuantityViewController.self)
     
+    func hideBackButton() {
+        navigationItem.hidesBackButton = true
+    }
+    
     @IBAction func closeButtonTapped(_ sender: UIBarButtonItem) {
-        dismiss(animated: true)
+        navigationController?.popViewController(animated: true)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideBackButton()
     }
 }
--- JuiceMaker/JuiceMaker/Controller/JuiceMarketViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/JuiceMarketViewController.swift
@@ -55,8 +55,8 @@ final class JuiceMarketViewController: UIViewController {
         let alert = UIAlertController(title: errorDescription, message: nil, preferredStyle: .alert)
         let success = UIAlertAction(title: "예", style: .default) { _ in
             guard let vc = self.storyboard?.instantiateViewController(identifier: FruitQuantityViewController.id) else { return }
-            vc.modalPresentationStyle = .fullScreen
-            self.present(vc, animated: true)
+            //vc.modalPresentationStyle = .fullScreen
+            self.navigationController?.pushViewController(vc, animated: true)
         }
         let cancel = UIAlertAction(title: "아니오", style: .cancel)
         
