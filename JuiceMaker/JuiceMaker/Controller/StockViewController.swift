84408669e4ddf1b695f848d0f9e7ce685f27ee7c
mireu79 <https://github.com/mireu79>
2023-09-26 13:43:32 +0900

    feat: 제고수정화면 +,-를 통한 재고수정 구현

--- JuiceMaker/JuiceMaker/Controller/StockViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/StockViewController.swift
@@ -11,10 +11,13 @@ class StockViewController: UIViewController {
     var fruitInventory: [Fruit: Int] = [:]
     
     @IBOutlet var fruitStockLabel: [UILabel]!
+    @IBOutlet var fruitStockStteper: [UIStepper]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureStock()
+        configureStepValue()
     }
 
     @IBAction func clickCloseButton(_ sender: UIBarButtonItem) {
@@ -29,11 +32,31 @@ class StockViewController: UIViewController {
         }
     }
     
+    func configureStepValue() {
+        for (index, stockStepper) in fruitStockStteper.enumerated() {
+            guard let fruit = Fruit(rawValue: index) else { return }
+            
+            stockStepper.value = Double(findStock(with: fruit))
+        }
+    }
+    
+    func updateStock(fruit: Fruit, stock: Int) {
+        fruitInventory[fruit] = stock
+    }
+    
     func findStock(with fruit: Fruit) -> Int {
         guard let stock = fruitInventory[fruit] else {
             return 0
         }
         
         return stock
+    }
+    
+    @IBAction func clickStepper(_ sender: UIStepper) {
+        guard let fruit = Fruit(rawValue: sender.tag) else { return }
+
+        fruitStockLabel[sender.tag].text = Int(sender.value).description
+        
+        updateStock(fruit: fruit, stock: Int(sender.value))
     }
 }
