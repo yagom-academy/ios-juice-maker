3219ef85a39dcd6a76a02f869d466018b0225e80
yawoong2 <adamzoong@naver.com>
2024-01-15 21:51:47 +0900

    feat: 각 과일의 재고를 10개로 초기화

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,7 +6,7 @@
 
 import Foundation
 
-enum Fruit {
+enum Fruit: CaseIterable {
     case strawberry
     case banana
     case pineapple
@@ -16,4 +16,18 @@ enum Fruit {
 // 과일 저장소 타입
 class FruitStore {
     var fruitsStock: Dictionary<Fruit, Int> = [:]
+
+    init() {
+        for fruit in Fruit.allCases {
+            fruitsStock[fruit] = 10
+        }
+        
+    }
+    
 }
+
+var test = FruitStore()
+
+
+
+
