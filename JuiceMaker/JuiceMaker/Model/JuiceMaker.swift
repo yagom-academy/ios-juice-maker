1d42bf92a7d043901a88d0377654d20ef9256598
yawoong2 <adamzoong@naver.com>
2024-01-15 23:43:10 +0900

    feat: 주스를 만드는데 필요한 재료를 파악하는 연산프로퍼티 생성

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,6 +6,35 @@
 
 import Foundation
 
+enum Juice {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case strawberryBanana
+    case mango
+    case mangoKiwi
+    
+    var requiredIngredient: [Fruit: Int] {
+        switch self {
+        case .strawberry:
+            return [.strawberry: 16]
+        case .banana:
+            return [.banana: 2]
+        case .kiwi:
+            return [.kiwi: 3]
+        case .pineapple:
+            return [.pineapple: 2]
+        case .strawberryBanana:
+            return [.strawberry: 10, .banana: 1]
+        case .mango:
+            return [.mango: 3]
+        case .mangoKiwi:
+            return [.mango: 2, .kiwi: 1]
+        }
+    }
+}
+
 // 쥬스 메이커 타입
 struct JuiceMaker {
     
