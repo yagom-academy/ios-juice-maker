8167406791e14156da8297a4f24ed60ae815300c
forseaest <u3z@naver.com>
2024-01-16 14:11:33 +0900

    feat: 과일 소모하는 함수 구현

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -18,14 +18,50 @@ class FruitStore {
     var kiwis = Fruit(name: "키위", count: 10)
     var mangos = Fruit(name: "망고", count: 10)
     
-    func changeCount(fruit: Fruit, usedCount: Int) -> Int {
-        var beforeCount = fruit.count
-        var afterCount = beforeCount - usedCount
-        
-        if afterCount >= 0 {
-            return afterCount
-        } else {
-            return Error.exceedCount
+    func changeCount(fruitName: String, usedCount: Int) {
+        switch fruitName {
+        case "딸기":
+            let afterCount = self.strawberries.count - usedCount
+            if afterCount >= 0 {
+                self.strawberries.count = self.strawberries.count - usedCount
+                print(self.strawberries.count)
+            } else {
+                print("\(fruitName)의 재고가 부족합니다.")
+            }
+        case "바나나":
+            let afterCount = self.bananas.count - usedCount
+            if afterCount >= 0 {
+                self.bananas.count = self.bananas.count - usedCount
+                print(self.bananas.count)
+            } else {
+                print("\(fruitName)의 재고가 부족합니다.")
+            }
+        case "파인애플":
+            let afterCount = self.pineapples.count - usedCount
+            if afterCount >= 0 {
+                self.pineapples.count = self.pineapples.count - usedCount
+                print(self.pineapples.count)
+            } else {
+                print("\(fruitName)의 재고가 부족합니다.")
+            }
+        case "키위":
+            let afterCount = self.kiwis.count - usedCount
+            if afterCount >= 0 {
+                self.kiwis.count = self.kiwis.count - usedCount         
+                print(self.kiwis.count)
+            } else {
+                print("\(fruitName)의 재고가 부족합니다.")
+            }
+        case "망고":
+            let afterCount = self.mangos.count - usedCount
+            if afterCount >= 0 {
+                self.mangos.count = self.mangos.count - usedCount               
+                print(self.mangos.count)
+            } else {
+                print("\(fruitName)의 재고가 부족합니다.")
+            }
+        default:
+            print("관리하는 과일의 종류가 아닙니다.")
         }
     }
 }
