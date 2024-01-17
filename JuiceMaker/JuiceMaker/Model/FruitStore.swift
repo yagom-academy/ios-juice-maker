0f211b1961029a8397b988b1be55e8232f9776ba
dannykim1215 <minzami1215@gmail.com>
2024-01-17 15:19:47 +0900

    style: 필요없는 주석 제거 및 코드 정리

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,11 +6,9 @@
 
 import Foundation
 
-// 과일 저장소 타입
 class FruitStore {
     private var fruitStorage = ["딸기": 10, "바나나": 10, "파인애플": 10, "키위": 10, "망고": 10]
     
-    // 과일의 개수를 n개로 변경 가능
     func changeFruitQuantity(fruitName: String, quantity: Int) {
         fruitStorage.updateValue(quantity, forKey: fruitName)
         print(fruitStorage)
@@ -20,7 +18,6 @@ class FruitStore {
         return fruitStorage[fruitName, default: 10]
     }
     
-    // 과일의 개수를 차감
     func deductFruit(fruitName: String, quantity: Int) {
         fruitStorage[fruitName, default: 10] -= quantity
         print(fruitStorage)
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -6,7 +6,6 @@
 
 import Foundation
 
-// 쥬스 메이커 타입
 struct JuiceMaker {
     private var fruitStore: FruitStore
     
@@ -14,45 +13,26 @@ struct JuiceMaker {
         self.fruitStore = FruitStore()
     }
     
-//    func makeStrawberryJuice(amount: Int){
-//        var strawberryAmount = fruitStore.showFruitAmount(fruitName: "딸기")
-//
-//        if(strawberryAmount < 16){
-//            print("과일의 재고가 부족하여 쥬스를 제조할 수 없습니다.")
-//        } else{
-//            strawberryAmount -= amount * 16
-//            print("\(amount) 개의 딸기쥬스를 만들었습니다.")
-//        }
-//    }
-    
     func makeJuice(juiceName: String) -> (String, [String: Int]) {
-        //var juiceName = ""
         var ingredients = [String: Int]()
         
         switch juiceName {
         case "딸기쥬스":
             ingredients["딸기"] = 16
-            //juiceName = "딸기쥬스"
         case "딸바쥬스":
             ingredients["딸기"] = 10
             ingredients["바나나"] = 1
-            //juiceName = "딸바쥬스"
         case "바나나쥬스":
             ingredients["바나나"] = 2
-            //juiceName = "바나나"
         case "키위쥬스":
             ingredients["키위"] = 3
-            //juiceName = "키위"
         case "파인애플쥬스":
             ingredients["파인애플"] = 2
-            //juiceName = "파인애플"
         case "망고쥬스":
             ingredients["망고"] = 3
-            //juiceName = "망고"
         case "망고키위쥬스":
             ingredients["망고"] = 2
             ingredients["키위"] = 1
-            //juiceName = "망고키위쥬스"
         default:
             return ("지원하지않는 쥬스입니다.", ingredients)
         }
@@ -62,10 +42,10 @@ struct JuiceMaker {
         return (juiceName, ingredients)
     }
     
-    // 과일의 재고를 체크한다.
     func checkStorage(juiceName: String, ingredients: [String: Int]) {
         for(fruits, quantity) in ingredients {
-            if (fruitStore.showFruitAmount(fruitName: fruits) >= quantity) {
+            let selectedFruits = fruitStore.showFruitAmount(fruitName: fruits)
+            if (selectedFruits >= quantity) {
                 fruitStore.deductFruit(fruitName: fruits, quantity: quantity)
             } else {
                 return print("재고가 없습니다")
@@ -74,7 +54,6 @@ struct JuiceMaker {
         print("\(juiceName) 를 만들었습니다")
     }
     
-    // 과일의 재고를 차감한다.
     func useFruits(juiceName: String, ingredients: [String: Int]) {
         for (fruits, quantity) in ingredients {
             fruitStore.deductFruit(fruitName: fruits, quantity: quantity)
