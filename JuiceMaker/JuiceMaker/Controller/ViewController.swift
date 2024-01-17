ad9b60cee2af9306be3459d328ed57e8e4b0add9
forseaest <u3z@naver.com>
2024-01-17 21:46:42 +0900

    refactor: JuiceMaker의 makeJuice 코드 축약

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -8,7 +8,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let juiceMaker = JuiceMaker(fruitStore: FruitStore())
+    var juiceMaker = JuiceMaker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
