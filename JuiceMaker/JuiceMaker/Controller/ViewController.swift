a8db7e677e04c144dbd37123e29813b6bcd4e2ee
dannykim1215 <minzami1215@gmail.com>
2024-01-22 21:26:06 +0900

    refactor: viewController 이름 변경, 함수명 기능 분리

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MakeJuiceViewController: UIViewController {
 
     private var juiceMaker: JuiceMaker?
         
@@ -14,14 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        makeJuiceAndHandleResult()
+        JuiceHandleResult()
     }
     
-    func makeJuiceAndHandleResult() {
-        juiceMaker = JuiceMaker()
+    func makeJuice() -> String? {
         let juiceResult = juiceMaker?.makeJuice(juiceName: "딸바쥬스", amount: 1)
-        
-        if let result = juiceResult {
+        return juiceResult
+    }
+    
+    func JuiceHandleResult() {
+        if let result = makeJuice() {
             print(result)
         } else {
             print("error: not working makeJuiceAndHandleResult()")
