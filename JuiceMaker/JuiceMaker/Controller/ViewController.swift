e8706af044f2daf393eaee8ef06524131c00193c
yawoong2 <adamzoong@naver.com>
2024-01-23 16:29:24 +0900

    refactor: juiceMaker인스턴스를 viewDidLoad 밖으로 이동

--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,20 +7,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var juiceMaker = JuiceMaker(fruitStore: FruitStore(fruits: [
+        .strawberry: 10,
+        .banana: 10,
+        .pineapple: 10,
+        .kiwi: 10,
+        .mango: 10
+    ]))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let fruits: [Fruit: Int] = [
-            .strawberry: 10,
-            .banana: 10,
-            .pineapple: 10,
-            .kiwi: 10,
-            .mango: 10
-        ]
-        
-        var fruitStore = FruitStore(fruits: fruits)
-        var juiceMaker = JuiceMaker(fruitStore: fruitStore)
     }
 }
 
