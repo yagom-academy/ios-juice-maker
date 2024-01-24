704124ca0e59612a1de2ce473e32faaa914a504c
dannykim1215 <minzami1215@gmail.com>
2024-01-25 00:06:35 +0900

    add: Fruit Enum 객체 분리 차 파일 생성, Assets, AppDelegate, SceneDelegate 폴더 생성 및 해당 파일 분리

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,14 +6,6 @@
 
 import Foundation
 
-enum Fruit: String {
-    case strawberry = "딸기"
-    case banana = "바나나"
-    case pineapple = "파인애플"
-    case kiwi = "키위"
-    case mango = "망고"
-}
-
 class FruitStore {
     var fruitStorage: [Fruit: Int]
 
