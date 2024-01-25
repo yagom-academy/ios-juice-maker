1954e28709da83de53513bd353791e374cdd81c6
danny <minzami1215@gmail.com>
2024-01-25 11:05:02 +0900

    add: Second ViewController인 FruitStockViewController 파일 추가

---
+++ JuiceMaker/JuiceMaker/Source/Controller/FruitStockViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FruitStockViewController.swift
+//  JuiceMaker
+//
+//  Created by Danny on 1/25/24.
+//
+
+import UIKit
+
+class FruitStockViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
