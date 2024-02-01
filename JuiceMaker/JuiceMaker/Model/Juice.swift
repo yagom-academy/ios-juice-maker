f7a0c40c52262578512f1cfa125972010617ef01
PrismSpirit <yeolkeut@gmail.com>
2024-02-01 21:16:28 +0900

    chore: Juice 순서 조정

--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -6,49 +6,49 @@
 //
 
 enum Juice {
+    case strawberryBanana
+    case mangoKiwi
     case strawberry
     case banana
-    case kiwi
     case pineapple
-    case strawberryBanana
+    case kiwi
     case mango
-    case mangoKiwi
     
     var recipe: [Fruit: Int] {
         switch self {
+        case .strawberryBanana:
+            return [.strawberry: 10, .banana: 1]
+        case .mangoKiwi:
+            return [.mango: 2, .kiwi: 1]
         case .strawberry:
             return [.strawberry: 16]
         case .banana:
             return [.banana: 2]
-        case .kiwi:
-            return [.kiwi: 3]
         case .pineapple:
             return [.pineapple: 2]
-        case .strawberryBanana:
-            return [.strawberry: 10, .banana: 1]
+        case .kiwi:
+            return [.kiwi: 3]
         case .mango:
             return [.mango: 3]
-        case .mangoKiwi:
-            return [.mango: 2, .kiwi: 1]
         }
     }
     
     var name: String {
         switch self {
+        case .strawberryBanana:
+            return "딸바"
+        case .mangoKiwi:
+            return "망키"
         case .strawberry:
             return "딸기"
         case .banana:
             return "바나나"
-        case .kiwi:
-            return "키위"
         case .pineapple:
             return "파인애플"
-        case .strawberryBanana:
-            return "딸바"
+        case .kiwi:
+            return "키위"
         case .mango:
             return "망고"
-        case .mangoKiwi:
-            return "망키"
         }
     }
 }
